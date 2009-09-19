02b72ffbc478761738d63b1a21fa443dc3a1ec2c
joshdreamland <joshdreamland@726d713c-bd1a-46cb-9ef0-1b6e075168bb>
2009-09-19 14:45:01 +0000

    This is a test file for the C parser. Any changes are irrelevant.
    git-svn-id: https://enigma-dev.svn.sourceforge.net/svnroot/enigma-dev@34 726d713c-bd1a-46cb-9ef0-1b6e075168bb

---
+++ CompilerSource/cfile_parse/parsein.h
@@ -0,0 +1,23 @@
+int main(int argc,char *argv[])
+{
+  
+}
+
+int some_other_function();
+
+struct here;
+struct of_stuff {};
+namespace enigma
+{
+  of_stuff lots;
+  here will_be;
+  namespace witless
+  {
+    int a;
+    typedef of_stuff c;
+    c b;
+    typedef unsigned float d;
+    d e;
+    typedef struct arpagio { int aomethingmusicy; } tengelo;
+  }
+}
