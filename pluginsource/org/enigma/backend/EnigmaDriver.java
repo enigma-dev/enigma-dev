1e0f9ad5598082371fca1aab125102ff253c5fd2
ismavatar <ismavatar@726d713c-bd1a-46cb-9ef0-1b6e075168bb>
2010-04-17 01:43:37 +0000

    Added new functions, and added new Driver class's source, which I omitted from last revision, although it was still in the Jar.
    git-svn-id: https://enigma-dev.svn.sourceforge.net/svnroot/enigma-dev/trunk@160 726d713c-bd1a-46cb-9ef0-1b6e075168bb

---
+++ pluginsource/org/enigma/backend/EnigmaDriver.java
@@ -0,0 +1,58 @@
+package org.enigma.backend;
+
+import com.sun.jna.Native;
+import com.sun.jna.NativeLibrary;
+import com.sun.jna.Pointer;
+import com.sun.jna.Structure;
+
+public class EnigmaDriver
+	{
+	static
+		{
+		String lib = "compileEGMf";
+		NativeLibrary.addSearchPath(lib,".");
+		Native.register(lib);
+		}
+
+	public static class SyntaxError extends Structure
+		{
+		public String errorString;
+		public int line, position;
+		public int absoluteIndex;
+		}
+
+	public static native int libInit();
+
+	public static native int gccDefinePath(String path);
+
+	public static native SyntaxError whitespaceModified(String wscode);
+
+	/** @param scriptNames (StringArray) */
+	public static native SyntaxError syntaxCheck(int scriptCount, Pointer scriptNames, String code);
+
+	public static native int compileEGMf(EnigmaStruct es, String outname, int mode);
+
+	/** Returns the name of the first resource on the list, or "" otherwise. */
+	public static native String first_available_resource();
+
+	/** Returns the name of the next resource on the list, or "" otherwise. */
+	public static native String next_available_resource();
+
+	/** Returns whether we're really done iterating the list */
+	public static native boolean resources_atEnd();
+
+	/** Returns the minimum number of arguments to the function; the names cannot be fetched. */
+	public static native int resource_argCountMin();
+
+	/** Returns the maximum number of arguments to the function; the names cannot be fetched. */
+	public static native int resource_argCountMax();
+
+	/** Returns whether the resource can be called as a function */
+	public static native boolean resource_isFunction();
+
+	/** Returns whether the resource is nothing but a global variable. */
+	public static native boolean resource_isGlobal();
+
+	/** Returns whether the resource can be used as a typename. */
+	public static native boolean resource_isTypeName();
+	}
