3db71f30c330a9ba3c71c4578dccff3bbf83d0a2
JoshDreamland <JoshDreamland@726d713c-bd1a-46cb-9ef0-1b6e075168bb>
2010-06-28 16:02:23 +0000

    Forgot a file. As usual. Re-added desktop file.
    git-svn-id: https://enigma-dev.svn.sourceforge.net/svnroot/enigma-dev/trunk@276 726d713c-bd1a-46cb-9ef0-1b6e075168bb

---
+++ ENIGMAsystem/SHELL/Platforms/platforms_mandatory.h
@@ -0,0 +1,33 @@
+/********************************************************************************\
+**                                                                              **
+**  Copyright (C) 2008 Josh Ventura                                             **
+**                                                                              **
+**  This file is a part of the ENIGMA Development Environment.                  **
+**                                                                              **
+**                                                                              **
+**  ENIGMA is free software: you can redistribute it and/or modify it under the **
+**  terms of the GNU General Public License as published by the Free Software   **
+**  Foundation, version 3 of the license or any later version.                  **
+**                                                                              **
+**  This application and its source code is distributed AS-IS, WITHOUT ANY      **
+**  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS   **
+**  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more       **
+**  details.                                                                    **
+**                                                                              **
+**  You should have recieved a copy of the GNU General Public License along     **
+**  with this code. If not, see <http://www.gnu.org/licenses/>                  **
+**                                                                              **
+**  ENIGMA is an environment designed to create games and other programs with a **
+**  high-level, fully compilable language. Developers of ENIGMA or anything     **
+**  associated with ENIGMA are in no way responsible for its users or           **
+**  applications created by its users, or damages caused by the environment     **
+**  or programs made in the environment.                                        **
+**                                                                              **
+\********************************************************************************/
+
+// This file contains functions that are required to be implemented by any API
+// under this directory, /Platforms/. They are not required to do anything at all
+// if the platform in question lacks support for them entirely.
+
+// This method should write the name of the running module to exenamehere.
+void windowsystem_write_exename(char* exenamehere);
---
+++ ENIGMAsystem/SHELL/auxilary.h
@@ -0,0 +1,13 @@
+/**  Copyright (C) 2008 Josh Ventura
+**  This file is a part of the ENIGMA Development Environment.
+**  It is released under the GNU General Public License, version 3.
+**  See the license of local files for more details.
+**/
+
+#define types_extrapolate_real_p(prefix,dd,ddd,dddd) ass
+
+#define types_extrapolate_real_p(prefix,dd,ddd,dddd,excess,excess2)\
+ prefix (double x);
+
+types_extrapolate_real_p  (double& poop,1,2,3)
+double asswipeorsomething(double& a);
