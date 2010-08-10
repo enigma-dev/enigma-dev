c227a7374ee49c371fbd337908d0a773b110437b
joshdreamland <joshdreamland@726d713c-bd1a-46cb-9ef0-1b6e075168bb>
2010-08-10 02:57:09 +0000

    Forgot this. Forgot that everything is a piece of shit on Windows, including RapidSVN.
    git-svn-id: https://enigma-dev.svn.sourceforge.net/svnroot/enigma-dev/trunk@324 726d713c-bd1a-46cb-9ef0-1b6e075168bb

---
+++ CompilerSource/compiler/components/parse_secondary.cpp
@@ -0,0 +1,53 @@
+/********************************************************************************\
+**                                                                              **
+**  Copyright (C) 2008 Josh Ventura                                             **
+**                                                                              **
+**  This file is a part of the ENIGMA Development Environment.                  **
+**                                                                              **
+**                                                                              **
+**  ENIGMA is free software: you can redistribute it and/or modify it under the **
+**  terms of the GNU General Public License as published by the Free Software   **
+**  Foundation, version 3 of the license or any later version.                  **
+**                                                                              **
+**  This application and its source code is distributed AS-IS, WITHOUT ANY      **
+**  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS   **
+**  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more       **
+**  details.                                                                    **
+**                                                                              **
+**  You should have recieved a copy of the GNU General Public License along     **
+**  with this code. If not, see <http://www.gnu.org/licenses/>                  **
+**                                                                              **
+**  ENIGMA is an environment designed to create games and other programs with a **
+**  high-level, fully compilable language. Developers of ENIGMA or anything     **
+**  associated with ENIGMA are in no way responsible for its users or           **
+**  applications created by its users, or damages caused by the environment     **
+**  or programs made in the environment.                                        **
+**                                                                              **
+\********************************************************************************/
+
+#include <stdio.h>
+#include <iostream>
+#include "../../backend/ideprint.h"
+
+using namespace std;
+
+#include "../../externs/externs.h"
+#include "../../syntax/syncheck.h"
+#include "../../parser/parser.h"
+
+#include "../../backend/EnigmaStruct.h" //LateralGM interface structures
+#include "../../parser/object_storage.h"
+#include "../compile_common.h"
+#include "../event_reader/event_parser.h"
+
+int compile_parseSecondary(map<int,parsed_object*> &parsed_objects,parsed_script* scripts[],parsed_object* EGMglobal)
+{
+  for (po_i it = parsed_objects.begin(); it != parsed_objects.end(); it++)
+  {
+    parsed_object *ito = it->second;
+    for (unsigned iit = 0; iit < ito->events.size; iit++)
+      parser_secondary(ito->events[iit].code,ito->events[iit].synt);
+  }
+  return 0;
+}
+
