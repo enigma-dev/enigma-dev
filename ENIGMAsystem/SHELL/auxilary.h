333e403696eb5a2a8d75653dad387867e727b34e
tgmg <tgmg@726d713c-bd1a-46cb-9ef0-1b6e075168bb>
2010-09-06 04:04:06 +0000

    Isolated error on Apple for correction on competent system that TGMG may sleep
    git-svn-id: https://enigma-dev.svn.sourceforge.net/svnroot/enigma-dev/trunk@408 726d713c-bd1a-46cb-9ef0-1b6e075168bb

--- ENIGMAsystem/SHELL/auxilary.h
+++ ENIGMAsystem/SHELL/auxilary.h
@@ -3,4 +3,47 @@
 **  It is released under the GNU General Public License, version 3.
 **  See the license of local files for more details.
 **/
+#define	const		__const
+#define	inline		__inline
+#define	signed		__signed
+#define	volatile	__volatile
+  
+  template<typename _Category, typename _Tp, typename _Distance = ptrdiff_t,
+           typename _Pointer = _Tp*, typename _Reference = _Tp&>
+    struct iterator
+    {
+      /// One of the @link iterator_tags tag types@endlink.
+      typedef _Category  iterator_category;
+      /// The type "pointed to" by the iterator.
+      typedef _Tp        value_type;
+      /// Distance between iterators is represented as this type.
+    };
 
+  template<typename _Iterator>
+    struct iterator_traits
+    {
+      typedef typename _Iterator::iterator_category iterator_category;
+      typedef typename _Iterator::value_type        value_type;
+      typedef typename _Iterator::difference_type   difference_type;
+      typedef typename _Iterator::pointer           pointer;
+      typedef typename _Iterator::reference         reference;
+    };
+
+  template<typename _Tp>
+    struct iterator_traits<_Tp*>
+    {
+      typedef _Tp                         value_type;
+      typedef _Tp*                        pointer;
+      typedef _Tp&                        reference;
+    };
+
+  template<typename _Tp>
+    struct iterator_traits<const _Tp*>
+    {
+      typedef _Tp                         value_type;
+      typedef ptrdiff_t                   difference_type;
+      typedef const _Tp*                  pointer;
+      typedef const _Tp&                  reference;
+    };
+    
+#error Ass caekz
