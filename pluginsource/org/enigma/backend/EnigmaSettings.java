ec3b87fe8f244732a6fe2eab3dd29008c4ceab7a
IsmAvatar <IsmAvatar@726d713c-bd1a-46cb-9ef0-1b6e075168bb>
2010-09-08 03:19:11 +0000

    Forgot this somehow. Not important unless you're trying to look at/build from the source code.
    git-svn-id: https://enigma-dev.svn.sourceforge.net/svnroot/enigma-dev/trunk@423 726d713c-bd1a-46cb-9ef0-1b6e075168bb

---
+++ pluginsource/org/enigma/backend/EnigmaSettings.java
@@ -0,0 +1,95 @@
+package org.enigma.backend;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import org.lateralgm.main.LGM;
+
+public class EnigmaSettings
+	{
+	//Compatibility / Progess options
+	public int cppStrings = 0; // Defines what language strings are inherited from.               0 = GML,               1 = C
+	public int cppOperators = 0; // Defines what language operators ++ and -- are inherited from. 0 = GML,               1 = C
+	public int cppEquals = 0; // Defines whether = should be exclusively treated as a setter.     0 = GML (= or ==)      1 = C (= only)
+	public int literalHandling = 0; // Determines how literals are treated.                       0 = enigma::variant,   1 = C-scalar
+	public int structHandling = 0; // Defines behavior of the closing brace of struct {}.         0 = Implied semicolon, 1 = ISO C
+
+	//Advanced options
+	public int instanceTypes = 0; // Defines how to represent instances.           0 = Integer, 1 = Pointer
+	public int storageClass = 0; // Determines how instances are stored in memory. 0 = Map,     1 = List,    2 = Array
+
+	public String definitions = "", globalLocals = "";
+	public String initialization = "", cleanup = "";
+
+	public EnigmaSettings()
+		{
+		this(true);
+		}
+
+	private EnigmaSettings(boolean load)
+		{
+		if (load) loadDefinitions();
+		}
+
+	void loadDefinitions()
+		{
+		definitions = fileToString(new File(LGM.workDir.getParentFile(),"definitions.h"));
+		}
+
+	public void saveDefinitions()
+		{
+		writeString(new File(LGM.workDir.getParentFile(),"definitions.h"),definitions);
+		}
+
+	String fileToString(File f)
+		{
+		StringBuffer sb = new StringBuffer(1024);
+		try
+			{
+			FileReader in = new FileReader(f);
+			char[] cbuf = new char[1024];
+			int size = 0;
+			while ((size = in.read(cbuf)) > -1)
+				sb.append(cbuf,0,size);
+			in.close();
+			}
+		catch (IOException e)
+			{
+			}
+		return sb.toString();
+		}
+
+	void writeString(File f, String s)
+		{
+		try
+			{
+			PrintStream ps = new PrintStream(f);
+			ps.print(s);
+			ps.close();
+			}
+		catch (FileNotFoundException e)
+			{
+			}
+		}
+
+	public EnigmaSettings copy()
+		{
+		EnigmaSettings es = new EnigmaSettings(false);
+		es.cppStrings = cppStrings;
+		es.cppOperators = cppOperators;
+		es.literalHandling = literalHandling;
+		es.structHandling = structHandling;
+
+		es.instanceTypes = instanceTypes;
+		es.storageClass = storageClass;
+
+		es.definitions = definitions;
+		es.globalLocals = globalLocals;
+		es.initialization = initialization;
+		es.cleanup = cleanup;
+		return es;
+		}
+	}
