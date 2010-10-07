37cbb8f7b2119d7338351aba4c4437ae9f5e628d
r9k <r9k@726d713c-bd1a-46cb-9ef0-1b6e075168bb>
2010-10-07 17:51:07 +0000

    Fixed Compilation error. printf doesn't exists without stdio.h on windows.
    git-svn-id: https://enigma-dev.svn.sourceforge.net/svnroot/enigma-dev/trunk@489 726d713c-bd1a-46cb-9ef0-1b6e075168bb

--- CompilerSource/general/bettersystem.cpp
+++ CompilerSource/general/bettersystem.cpp
@@ -34,6 +34,7 @@ using namespace std;
 
 #if CURRENT_PLATFORM_ID == OS_WINDOWS
   #include <windows.h>
+  #include <stdio.h>
 #endif
 
   #if CURRENT_PLATFORM_ID == OS_WINDOWS
@@ -45,24 +46,24 @@ using namespace std;
 int better_system(string program,string arguments, string redirchar = "", const char* redirf = NULL)
 {
   sys_result_type exit_status = sys_result_type(-1);
-  
+
   #if CURRENT_PLATFORM_ID == OS_WINDOWS
     STARTUPINFO StartupInfo;
     PROCESS_INFORMATION ProcessInformation;
-    
+
     ZeroMemory(&StartupInfo, sizeof(StartupInfo));
     StartupInfo.cb = sizeof(StartupInfo);
-    
+
     ZeroMemory(&ProcessInformation, sizeof(ProcessInformation));
-    
+
     SECURITY_ATTRIBUTES inheritibility;
         inheritibility.nLength = sizeof(inheritibility);
         inheritibility.lpSecurityDescriptor = NULL;
         inheritibility.bInheritHandle = TRUE;
-    
+
     // Output redirection
     HANDLE of = NULL;
-    if (redirf) 
+    if (redirf)
     {
       of = CreateFile(redirf, FILE_WRITE_DATA, FILE_SHARE_READ|FILE_SHARE_WRITE, &inheritibility, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
       if (of != NULL)
@@ -71,28 +72,28 @@ int better_system(string program,string arguments, string redirchar = "", const 
         StartupInfo.hStdInput = GetStdHandle((DWORD)-10);
         StartupInfo.hStdOutput = GetStdHandle((DWORD)-11);
         StartupInfo.hStdError = GetStdHandle((DWORD)-12);
-        
+
         if (!StartupInfo.hStdInput or StartupInfo.hStdInput == INVALID_HANDLE_VALUE)
         {
           HANDLE conin = CreateFile("CONIN$", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, FALSE, OPEN_EXISTING, 0, NULL);
             if (!conin || conin == INVALID_HANDLE_VALUE) { printf("CreateFile(CONIN$) failed (Error%d)\n", (int)GetLastError()); }
           StartupInfo.hStdInput = conin;
         }
-        
+
         if (!StartupInfo.hStdOutput or StartupInfo.hStdOutput == INVALID_HANDLE_VALUE or !StartupInfo.hStdError or StartupInfo.hStdError == INVALID_HANDLE_VALUE)
         {
           HANDLE conout = CreateFile("CONOUT$", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, FALSE, OPEN_EXISTING, 0, NULL);
             if (!conout || conout == INVALID_HANDLE_VALUE) { printf("CreateFile(CONOUT$) failed (Error %d)\n", (int)GetLastError()); }
-            
+
           if (!StartupInfo.hStdOutput or StartupInfo.hStdOutput == INVALID_HANDLE_VALUE)  StartupInfo.hStdInput = conout;
           if (!StartupInfo.hStdError  or StartupInfo.hStdError  == INVALID_HANDLE_VALUE)  StartupInfo.hStdError = conout;
         }
-        
+
         if (redirchar == ">" or redirchar == "1>" or redirchar == "&>") StartupInfo.hStdOutput = of;
         if (redirchar == "2>" or redirchar == "&>") StartupInfo.hStdError = of;
       }
     }
-    
+
     if (CreateProcess(program.c_str(),(CHAR*)("\"" + program + "\" " + arguments).c_str(),NULL,&inheritibility,TRUE,CREATE_DEFAULT_ERROR_MODE,NULL,NULL,&StartupInfo,&ProcessInformation ))
     {
       WaitForSingleObject(ProcessInformation.hProcess, INFINITE);
@@ -105,7 +106,7 @@ int better_system(string program,string arguments, string redirchar = "", const 
       cout << "ENIGMA: Failed to create process `" << program << "'.\n";
       exit_status = DWORD(-1);
     }
-    
+
     if (of) CloseHandle(of);
   #else
     string rd = redirf ? ((redirchar == "&>") ? " >" + string(redirf) + " 2>&1" : " " + redirchar + " " + string(redirf)) : "";
@@ -114,6 +115,6 @@ int better_system(string program,string arguments, string redirchar = "", const 
     exit_status = system(cmd.c_str());
     cout << flush;
   #endif
-  
+
   return exit_status;
 }
