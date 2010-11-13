696b018564ccaddee844b5f5b6535baa4ced4bed
ismavatar <ismavatar@726d713c-bd1a-46cb-9ef0-1b6e075168bb>
2010-11-13 03:02:15 +0000

    Made a macro.
    git-svn-id: https://enigma-dev.svn.sourceforge.net/svnroot/enigma-dev/trunk@536 726d713c-bd1a-46cb-9ef0-1b6e075168bb

--- ENIGMAsystem/SHELL/Universal_System/pseudo_components.h
+++ ENIGMAsystem/SHELL/Universal_System/pseudo_components.h
@@ -1,1 +1,2 @@
-ptrdiff_t alarmStructOffset = (char*)&((enigma::object_locals*)&enigma::ENIGMA_global_instance)->alarm - (char*)&enigma::ENIGMA_global_instance;
+#define e_offsetof(local_member)(char*)&((enigma::object_locals*)&enigma::ENIGMA_global_instance)->local_member - (char*)&enigma::ENIGMA_global_instance
+ptrdiff_t alarmStructOffset = e_offsetof(alarm);
