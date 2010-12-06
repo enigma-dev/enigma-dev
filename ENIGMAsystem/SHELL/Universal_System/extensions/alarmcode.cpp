62dc32ffb35b2f46c4b395652c9b89aec3af319e
ismavatar <ismavatar@726d713c-bd1a-46cb-9ef0-1b6e075168bb>
2010-12-06 05:53:33 +0000

    Alarm stuff compiles again. Now with moar macros. Note, users will have to use the recast macro instead of recasting manually, so as to access the proper memory, since masking failed. Also, definitions still can't access it, but we're getting closer.
    git-svn-id: https://enigma-dev.svn.sourceforge.net/svnroot/enigma-dev/trunk@541 726d713c-bd1a-46cb-9ef0-1b6e075168bb

--- ENIGMAsystem/SHELL/Universal_System/extensions/alarmcode.cpp
+++ ENIGMAsystem/SHELL/Universal_System/extensions/alarmcode.cpp
@@ -1,5 +1,9 @@
 #include "../object.h"
 
+#define recast(inst,struct) (*(struct*)((char*)inst+struct##Offset))
+//Note: We could alternatively provide alarmStruct with a fluff[0] for explicit memory mapping, and define recast thusly:
+//#define recast(inst,struct) (*(struct*)&((struct*)inst)->fluff[struct##Offset])
+
 extern ptrdiff_t alarmStructOffset;
-struct alarmStruct { char fluff[alarmStructOffset]; var alarm; }
+struct alarmStruct { var alarm; };
 
