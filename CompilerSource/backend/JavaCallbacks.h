0af919b44220b69c4597e9482aac1987956ba578
retroxyz <retroxyz@726d713c-bd1a-46cb-9ef0-1b6e075168bb>
2011-01-29 18:47:27 +0000

    Built windows release and added LinuxExtra folder to make root a bit cleaner.  Added blank files to indicate files that LGM modifies; this is for the package, so that on uninstall, they are removed.  Might have changed other stuff, but probably not.
    git-svn-id: https://enigma-dev.svn.sourceforge.net/svnroot/enigma-dev/trunk@627 726d713c-bd1a-46cb-9ef0-1b6e075168bb

--- CompilerSource/backend/JavaCallbacks.h
+++ CompilerSource/backend/JavaCallbacks.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2010 IsmAvatar <IsmAvatar@gmail.com>, Josh Ventura
- * 
+ *
  * This file is part of Enigma Plugin.
  * Enigma Plugin is free software and comes with ABSOLUTELY NO WARRANTY.
  * See LICENSE for details. If you can find it. But I mean, this is just
@@ -27,11 +27,11 @@ struct EnigmaCallbacks
 
  //Applies a given text to the progress bar
  void (*dia_progress_text) (const char *);
- 
- 
+
+
  //Sets the file from which data is redirected to frame log
  void (*output_redirect_file) (const char *);
- 
+
  //Indicates file completion, dumps remainder to frame log
  void (*output_redirect_reset) ();
 
@@ -53,7 +53,7 @@ extern void (*ide_dia_clear) ();
 extern void (*ide_dia_progress) (int);
 //Applies a given text to the progress bar
 extern void (*ide_dia_progress_text) (const char *);
- 
+
 //Sets the file from which data is redirected to frame log
 extern void (*ide_output_redirect_file) (const char *);
 //Indicates file completion, dumps remainder to frame log
--- CompilerSource/backend/JavaStruct.h
+++ CompilerSource/backend/JavaStruct.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2010 IsmAvatar <IsmAvatar@gmail.com>, Josh Ventura
- * 
+ *
  * This file is part of Enigma Plugin.
  * Enigma Plugin is free software and comes with ABSOLUTELY NO WARRANTY.
  * See LICENSE for details.
