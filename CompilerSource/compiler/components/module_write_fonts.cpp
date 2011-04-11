79e155d0569dd5bc13ac97e3470e7b1e3bb09cec
joshdreamland <joshdreamland@726d713c-bd1a-46cb-9ef0-1b6e075168bb>
2011-04-11 17:44:15 +0000

    Some missing fucking files because RapidSVN is a temperamental cocksucker when it comes to highlighting directories with new files.
    git-svn-id: https://enigma-dev.svn.sourceforge.net/svnroot/enigma-dev/trunk@686 726d713c-bd1a-46cb-9ef0-1b6e075168bb

---
+++ CompilerSource/compiler/components/module_write_fonts.cpp
@@ -0,0 +1,171 @@
+/********************************************************************************\
+**                                                                              **
+**  Copyright (C) 2008 Josh Ventura                                             **
+**                                                                              **
+**  This file is a part of the ENIGMA Development Environment.                  **
+**                                                                              **
+**                                                                              **
+**  ENIGMA is free software: you can redistribute it and/or modify it under the **
+**  terms of the GNU General Public License as published by the Free Software   **
+**  Foundation, version 3 of the license or any later version.                  **
+**                                                                              **
+**  This application and its source code is distributed AS-IS, WITHOUT ANY      **
+**  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS   **
+**  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more       **
+**  details.                                                                    **
+**                                                                              **
+**  You should have recieved a copy of the GNU General Public License along     **
+**  with this code. If not, see <http://www.gnu.org/licenses/>                  **
+**                                                                              **
+**  ENIGMA is an environment designed to create games and other programs with a **
+**  high-level, fully compilable language. Developers of ENIGMA or anything     **
+**  associated with ENIGMA are in no way responsible for its users or           **
+**  applications created by its users, or damages caused by the environment     **
+**  or programs made in the environment.                                        **
+**                                                                              **
+\********************************************************************************/
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <list>
+
+using namespace std;
+
+#include "../../externs/externs.h"
+#include "../../syntax/syncheck.h"
+#include "../../parser/parser.h"
+
+#include "../../backend/EnigmaStruct.h" //LateralGM interface structures
+#include "../../parser/object_storage.h"
+#include "../compile_common.h"
+
+#include "../../backend/ideprint.h"
+
+#include "../reshandlers/rectpack.h"
+
+inline void writei(int x, FILE *f) {
+  fwrite(&x,4,1,f);
+}
+inline void writef(float x, FILE *f) {
+  fwrite(&x,4,1,f);
+}
+
+using namespace rect_packer;
+int module_write_fonts(EnigmaStruct *es, FILE *gameModule)
+{
+  // Now we're going to add backgrounds
+  edbg << es->fontCount << " Adding Fonts to Game Module: " << flushl;
+
+  //Magic Number
+  fwrite("rfnt",4,1,gameModule);
+
+  //Indicate how many
+  int font_count = es->fontCount;
+  writei(font_count,gameModule);
+  
+  // For each included font
+  for (int i = 0; i < font_count; i++)
+  {
+    cout << "Iterating included fonts..." << endl;
+    // Simple allocations and initializations
+    const int gc = es->fonts[i].rangeMax - es->fonts[i].rangeMin + 1;
+    pvrect boxes[gc]; list<unsigned int> box_order;
+    cout << "Allocated some shit" << endl;
+    
+    /*cout << "Font name `" << es->fonts[i].name << "` uses " << es->fonts[i].fontName << endl;
+    cout << "Basic metric info: " << (es->fonts[i].rangeMax - es->fonts[i].rangeMin + 1)
+         << " glyphs from " << (int)es->fonts[i].rangeMin << " to " << (int)es->fonts[i].rangeMax
+         << ", glyph pointer is at " << es->fonts[i].glyphs << "." << endl;
+    cout << "Dump of concerned memory: " << endl;*/
+    
+    for (size_t ii = 0; ii < sizeof(Font); ii++)
+      printf("%02X%c",((const unsigned char*)(es->fonts+i))[ii],((ii+1)%16)?' ':'\n');
+    cout << endl;
+    for (int ii = 0; ii < gc; ii++)
+    cout << "The dimensions of glyph " << ii << "are " << flush << es->fonts[i].glyphs[ii].width << "x" << es->fonts[i].glyphs[ii].height << "." << endl;
+    cout << "End of list." << endl;
+    
+    // Copy our glyph metrics into it
+    for (int ii = 0; ii < gc; ii++)
+    cout << "The dimensions of glyph " << ii << "are " << flush, cout << es->fonts[i].glyphs[ii].width << "x" << es->fonts[i].glyphs[ii].height << "." << endl,
+      boxes[ii].w = es->fonts[i].glyphs[ii].width,
+      boxes[ii].h = es->fonts[i].glyphs[ii].height;
+    cout << "Copied metrics" << endl;
+    
+    // Sort our boxes from largest to smallest in area.
+    for (int ii = 0; ii < gc; ii++)
+      box_order.push_back((es->fonts[i].glyphs[ii].width * es->fonts[i].glyphs[ii].height << 8) + ii); // This reserves only eight bits for the glyph id; unicode will break a little.
+    box_order.sort(); // In actuality, unicode will only cause the area sort to be inaccurate, leading to an inefficient pack.
+    cout << "Sorted out some shit" << endl;
+    
+    // Now we actually pack the mothers. We'll iterate our area-sorted list backwards (largest to smallest)
+    int w = 64, h = 64;
+    rectpnode *rectplane = new rectpnode(0,0,w,h);
+    for (list<unsigned int>::reverse_iterator ii = box_order.rbegin(); ii != box_order.rend(); )
+    {
+      printf("Add rectangle %d, which is of size %d x %d, to main cell of size %d x %d (%d, %d)\n", *ii & 255, boxes[*ii & 255].w, boxes[*ii & 255].h, rectplane->wid, rectplane->hgt, w, h);
+      rectpnode *nn = rninsert(rectplane, *ii & 0xFF, boxes);
+      if (nn)
+        rncopy(nn, boxes, *ii & 0xFF),
+        ii++;
+      else
+      {
+        w > h ? h <<= 1 : w <<= 1,
+        rectplane = expand(rectplane, w, h);
+        printf("Expanded to %d by %d\n", w, h);
+        if (!w or !h) return -1;
+      }
+    }
+    
+    cout << "Finished packing shit." << endl;
+    
+    unsigned char bigtex[w*h];
+    for (int ii = 0; ii < w*h; ii++)
+      bigtex[ii] = 0;
+    struct { float x,y,x2,y2; } glyphtexc[gc];
+    
+    cout << "Allocated a big texture. Moving shit into it..." << endl;
+    for (int ii = 0; ii < gc; ii++)
+    {
+      for (int yy = 0; yy < boxes[ii].h; yy++)
+        for (int xx = 0; xx < boxes[ii].w; xx++)
+          bigtex[w*(boxes[ii].y + yy) + boxes[ii].x + xx] = es->fonts[i].glyphs[ii].data[yy*boxes[ii].w + xx];
+      
+      glyphtexc[ii].x  = boxes[ii].x / double(w);
+      glyphtexc[ii].y  = boxes[ii].y / double(h);
+      glyphtexc[ii].x2 = (boxes[ii].x + boxes[ii].w) / double(w);
+      glyphtexc[ii].y2 = (boxes[ii].y + boxes[ii].h) / double(h);
+    }
+    
+    /*cout << "Populated. Debugging..." << endl;
+    FILE* sex = fopen("/home/josh/Desktop/lol.txt","wb");
+    char grades[] = " =COBM";
+    for (int yy = 0; yy < h; yy++)
+    {
+      for (int xx = 0; xx < w; xx++)
+        fputc(grades[bigtex[(yy*w+xx)]*5/255],sex);
+      fputc('\n',sex);
+    }
+    fclose(sex);*/
+    
+    writei(es->fonts[i].id,gameModule);
+    writei(w,gameModule), writei(h,gameModule);
+    fwrite(bigtex,1,w*h,gameModule);
+    fwrite("done",1,4,gameModule);
+    
+    for (int ii = 0; ii < gc; ii++)
+      writef(es->fonts[i].glyphs[ii].advance, gameModule),
+      writef(es->fonts[i].glyphs[ii].baseline,gameModule),
+      writef(es->fonts[i].glyphs[ii].origin, gameModule),
+      writei(es->fonts[i].glyphs[ii].width,  gameModule),
+      writei(es->fonts[i].glyphs[ii].height, gameModule),
+      writef(glyphtexc[ii].x,  gameModule),
+      writef(glyphtexc[ii].y,  gameModule),
+      writef(glyphtexc[ii].x2, gameModule),
+      writef(glyphtexc[ii].y2, gameModule);
+  }
+
+  edbg << "Done writing fonts." << flushl;
+  return 0;
+}
---
+++ ENIGMAsystem/SHELL/Universal_System/fontinit.cpp
@@ -0,0 +1,162 @@
+/********************************************************************************\
+**                                                                              **
+**  Copyright (C) 2008 Josh Ventura                                             **
+**  Copyright (C) 2010 Alasdair Morrison <tgmg@g-java.com>                      **
+**                                                                              **
+**  This file is a part of the ENIGMA Development Environment.                  **
+**                                                                              **
+**                                                                              **
+**  ENIGMA is free software: you can redistribute it and/or modify it under the **
+**  terms of the GNU General Public License as published by the Free Software   **
+**  Foundation, version 3 of the license or any later version.                  **
+**                                                                              **
+**  This application and its source code is distributed AS-IS, WITHOUT ANY      **
+**  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS   **
+**  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more       **
+**  details.                                                                    **
+**                                                                              **
+**  You should have recieved a copy of the GNU General Public License along     **
+**  with this code. If not, see <http://www.gnu.org/licenses/>                  **
+**                                                                              **
+**  ENIGMA is an environment designed to create games and other programs with a **
+**  high-level, fully compilable language. Developers of ENIGMA or anything     **
+**  associated with ENIGMA are in no way responsible for its users or           **
+**  applications created by its users, or damages caused by the environment     **
+**  or programs made in the environment.                                        **
+**                                                                              **
+\********************************************************************************/
+
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+#include "backgroundstruct.h"
+#include "../Graphics_Systems/graphics_mandatory.h"
+#include "fontstruct.h"
+#include "../libEGMstd.h"
+#include "compression.h"
+
+#include <iostream>
+
+namespace enigma
+{
+  void exe_loadfonts(FILE *exe)
+  {
+    int nullhere;
+	  unsigned fontcount, fntid, twid, thgt, gwid, ghgt;
+	  float advance, baseline, origin, gtx, gty, gtx2, gty2;
+	  
+    fread(&nullhere,4,1,exe);
+    if (nullhere != *(int*)"rfnt")
+      return;
+    
+    fread(&fontcount,4,1,exe);
+    if ((int)fontcount != rawfontcount) {
+      show_error("Resource data does not match up with game metrics. Unable to improvise.",0);
+      return;
+    }
+    printf("Reading %d fonts\n", fontcount);
+	  
+	  fontstructarray = (new font*[rawfontmaxid + 2]) + 1;
+	  
+	  for (int rf = 0; rf < rawfontcount; rf++)
+	  {
+		  // int unpacked;
+		  fread(&fntid, 4,1,exe);
+		  printf("id: %d\n", fntid);
+		  fread(&twid, 4,1,exe);
+		  printf("width: %d\n", twid);
+		  fread(&thgt,4,1,exe);
+		  printf("height: %d\n", thgt);
+		  const int i = fntid;
+		  
+		  cout << "Assign some shit for font " << i << endl;
+		  fontstructarray[i] = new font;
+		  
+		  fontstructarray[i]->name = rawfontdata[rf].name;
+		  fontstructarray[i]->fontname = rawfontdata[rf].fontname;
+		  fontstructarray[i]->fontsize = rawfontdata[rf].fontsize;
+		  fontstructarray[i]->bold = rawfontdata[rf].bold;
+		  fontstructarray[i]->italic = rawfontdata[rf].italic;
+		  
+		  fontstructarray[i]->glyphstart = rawfontdata[rf].glyphstart;
+		  fontstructarray[i]->glyphcount = rawfontdata[rf].glyphcount;
+		  
+		  fontstructarray[i]->height = 0;
+		  
+		  cout << "Assigned some shit for font. Doing allocations." << endl;
+		  
+		  fontstructarray[i]->glyphs = new fontglyph[fontstructarray[i]->glyphcount];
+		  
+		  const unsigned int size = twid*thgt;
+		  
+		  int* pixels=new int[size+1]; //FYI: This variable was once called "cpixels." When you do compress them, change it back.
+		  
+		  unsigned int sz2;
+		  for (sz2 = 0; !feof(exe) and sz2 < size; sz2++)
+		    pixels[sz2] = 0x00FFFFFF | ((unsigned char)fgetc(exe) << 24);
+		  
+		  if (size!=sz2) {
+			  show_error("Failed to load font: Data is truncated before exe end. Read "+toString(sz2)+" out of expected "+toString(size),0);
+			  return;
+		  }
+      fread(&nullhere,4,1,exe);
+      if (nullhere != *(int*)"done")
+        return;
+		  //unpacked = width*height*4;
+		  /*unsigned char* pixels=new unsigned char[unpacked+1];
+		  if (zlib_decompress(cpixels,size,unpacked,pixels) != unpacked)
+		  {
+			  show_error("Background load error: Background does not match expected size",0);
+			  continue;
+		  }
+		  delete[] cpixels;*/
+		  
+		  
+		  cout << "Allocations done. Doing some other shit." << endl;
+		  for (int gi = 0; gi < enigma::fontstructarray[i]->glyphcount; gi++)
+		  {
+		    fread(&advance,4,1,exe);
+        fread(&baseline,4,1,exe);
+        fread(&origin,4,1,exe);
+        fread(&gwid,4,1,exe);
+        fread(&ghgt,4,1,exe);
+        fread(&gtx,4,1,exe);
+        fread(&gty,4,1,exe);
+        fread(&gtx2,4,1,exe);
+        fread(&gty2,4,1,exe);
+        
+        fontstructarray[i]->glyphs[gi].x = origin;
+        fontstructarray[i]->glyphs[gi].y = baseline;
+        fontstructarray[i]->glyphs[gi].x2 = origin + gwid;
+        fontstructarray[i]->glyphs[gi].y2 = baseline + ghgt;
+        fontstructarray[i]->glyphs[gi].tx = gtx;
+        fontstructarray[i]->glyphs[gi].ty = gty;
+        fontstructarray[i]->glyphs[gi].tx2 = gtx2;
+        fontstructarray[i]->glyphs[gi].ty2 = gty2;
+        fontstructarray[i]->glyphs[gi].xs = advance;
+        
+        if (fontstructarray[i]->glyphs[gi].y2 > fontstructarray[i]->height)
+          fontstructarray[i]->height = fontstructarray[i]->glyphs[gi].y2;
+        
+        printf("fntid%d, twid%d, thgt%d, advance%f, baseline%f, origin%f, gwid%d, ghgt%d, gtx%f, gty%f, gtx2%f, gty2%f\n",fntid, twid, thgt, advance, baseline, origin, gwid, ghgt, gtx, gty, gtx2, gty2);
+		  }
+		  
+      cout << "Did other shit." << endl;
+		  fontstructarray[i]->texture = graphics_create_texture(twid,thgt,pixels);
+      cout << "Applied for texture. Received id " << fontstructarray[i]->texture << endl;
+      
+      /*int sss = 'A' - fontstructarray[i]->glyphstart;
+      fontstructarray[i]->glyphs[sss].x = 0;
+      fontstructarray[i]->glyphs[sss].y = 0;
+      fontstructarray[i]->glyphs[sss].x2 = twid;
+      fontstructarray[i]->glyphs[sss].y2 = thgt;
+      fontstructarray[i]->glyphs[sss].tx = 0;
+      fontstructarray[i]->glyphs[sss].ty = 0;
+      fontstructarray[i]->glyphs[sss].tx2 = 1;
+      fontstructarray[i]->glyphs[sss].ty2 = 1;*/
+		  
+		  delete[] pixels;
+	  }
+  }
+}
