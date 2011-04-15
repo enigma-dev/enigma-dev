457fbcd1120384b67ce6a3bbcd670da52ed0d8d1
retroxyz <retroxyz@726d713c-bd1a-46cb-9ef0-1b6e075168bb>
2011-04-15 02:00:45 +0000

    stdlib.h != cstdlib
    git-svn-id: https://enigma-dev.svn.sourceforge.net/svnroot/enigma-dev/trunk@694 726d713c-bd1a-46cb-9ef0-1b6e075168bb

--- ENIGMAsystem/SHELL/SHELLmain.cpp
+++ ENIGMAsystem/SHELL/SHELLmain.cpp
@@ -25,7 +25,7 @@
 **                                                                              **
 \********************************************************************************/
 
-#include <stdlib.h>
+#include <cstdlib>
 #include <string>
 
 // Simple Universal libraries
