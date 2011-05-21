bb10cb1dde9d91377949cf5edff4e7ee723f09c6
joshdreamland <joshdreamland@726d713c-bd1a-46cb-9ef0-1b6e075168bb>
2011-05-21 22:39:16 +0000

    For IsmAvatar and TGMG: Implements the remaining headers and About files in */Info/.
    git-svn-id: https://enigma-dev.svn.sourceforge.net/svnroot/enigma-dev/trunk@754 726d713c-bd1a-46cb-9ef0-1b6e075168bb

---
+++ ENIGMAsystem/SHELL/Collision_Systems/BBox/Info/collision_info.h
@@ -0,0 +1,5 @@
+// Informative header designed to grant superior control over platform-
+// or API-dependent behavior. This file can define any number of macros
+// describing various compatibility and feature points.
+
+#define ENIGMA_CS_BBOX 1
