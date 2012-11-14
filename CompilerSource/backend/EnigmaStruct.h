d990d1124714cb28e36ec3acb9064f5f4844afeb
polygoned <polygoned1@gmail.com>
2012-11-14 21:19:48 +0000

    Update rand error and Win32 segfault.

--- CompilerSource/backend/EnigmaStruct.h
+++ CompilerSource/backend/EnigmaStruct.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2010 IsmAvatar <IsmAvatar@gmail.com>, Josh Ventura
- * 
+ *
  * This file is part of Enigma Plugin.
  * Enigma Plugin is free software and comes with ABSOLUTELY NO WARRANTY.
  * See LICENSE for details.
@@ -56,11 +56,9 @@ struct EnigmaStruct
 	int constantCount;
 	Include *includes;
 	int includeCount;
-#ifdef WIN32
-    char *packages[];
-#else
+
 	char **packages;
-#endif
+
 	int packageCount;
 
 	Extension *extensions;
