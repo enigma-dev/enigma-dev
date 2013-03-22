52f6dd93c806cb8950162346d7db55422f586620
Owner <robertbcolton@hotmail.com>
2013-03-22 14:07:59 -0400

    Attempted to fix merge conflicts.

--- CompilerSource/compiler/compile.cpp
+++ CompilerSource/compiler/compile.cpp
@@ -254,12 +254,8 @@ double lang_CPP::compile(EnigmaStruct *es, const char* exe_filename, int mode)
   idpr("Copying resources",1);
 
   //Next, add the resource names to that list
-<<<<<<< HEAD
-  edbg << "\nCOPYING THE FOLLOWING RESOURCES:" << flushl;
-=======
   edbg << "Copying resources:" << flushl;
->>>>>>> upstream/master
-
+  
   edbg << "Copying sprite names [" << es->spriteCount << "]" << flushl;
   for (int i = 0; i < es->spriteCount; i++) {
     cout << "Name on this side: " << globals_scope.name << endl;
