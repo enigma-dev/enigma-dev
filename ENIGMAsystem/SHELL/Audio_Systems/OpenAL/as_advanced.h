5d7fb5c903a4e1f9352c958e1866fa378e98e199
Owner <robertbcolton@hotmail.com>
2013-04-14 02:49:08 -0400

    OpenAL Doxy Added doxygen comments to the OpenAL audio system.

--- ENIGMAsystem/SHELL/Audio_Systems/OpenAL/as_advanced.h
+++ ENIGMAsystem/SHELL/Audio_Systems/OpenAL/as_advanced.h
@@ -29,53 +29,216 @@ enum {
   audio_new_system
 };
 
-
+/** des
+@param index The index of the audio asset.
+@return 
+**/
 bool audio_exists(int index);
+/** des
+@param index The index of the audio asset.
+@return 
+**/
 bool audio_is_playing(int index);
 
+/** des
+@param index The index of the audio asset.
+@param loop Whether or not to loop the sound at the end.
+**/
 void audio_play_music(int index, bool loop);
+/** des
+**/
 void audio_pause_music();
+/** des
+**/
 void audio_resume_music();
+/** des
+**/
 void audio_stop_music(); 
 
+/** des
+@param index The index of the audio asset.
+@param priority The priority of the sound in queue.
+@param loop Whether or not to loop the sound at the end.
+@return
+**/
 int audio_play_sound(int index, double priority, bool loop);
+/** des
+@param sound The index of the audio asset.
+@param x
+@param y
+@param z
+@param falloff_ref
+@param falloff_max
+@param falloff_factor
+@param loop Whether or not to loop the sound at the end.
+@param priority The priority of the sound in queue.
+@return
+**/
 int audio_play_sound_at(int sound, double x, double y, double z, int falloff_ref, double falloff_max, double falloff_factor, bool loop, double priority);
+/** des
+@param index The index of the audio asset.
+**/
 void audio_pause_sound(int index);
+/** des
+@param index The index of the audio asset.
+**/
 void audio_resume_sound(int index);
+/** des
+@param index The index of the audio asset.
+**/
 void audio_stop_sound(int index);
+/** des
+**/
 void audio_pause_all();
+/** des
+**/
 void audio_resume_all();
+/** des
+**/
 void audio_stop_all();
 
+/** des
+@param offset
+**/
 void audio_music_seek(double offset);
+/** des
+@param index The index of the audio asset.
+@param offset 
+**/
 void audio_sound_seek(int index, double offset);
+/** des
+@return
+**/
 double audio_music_offset();
+/** des
+@param index The index of the audio asset.
+@return
+**/
 double audio_sound_offset(int index);
 
+/** des
+@param lookat_x
+@param lookat_y
+@param lookat_z
+@param up_x
+@param up_y
+@param up_z
+**/
 void audio_listener_orientation(double lookat_x, double lookat_y, double lookat_z, double up_x, double up_y, double up_z);
+/** des
+@param x
+@param y
+@param z
+**/
 void audio_listener_position(double x, double y, double z);
+/** des
+@param vx
+@param vy
+@param vz
+**/
 void audio_listener_velocity(double vx, double vy, double vz);
 
+/** des
+@param index The index of the audio asset.
+@return
+**/
 double audio_sound_length(int index);
+/** des
+@param volume
+@param time
+**/
 void audio_master_gain(float volume, double time);
+/** des
+@param index The index of the audio asset.
+@param volume
+@param time
+**/
 void audio_music_gain(int index, float volume, double time);
+/** des
+@param index The index of the audio asset.
+@param volume
+@param time
+**/
 void audio_sound_gain(int index, float volume, double time);
 
+/** des
+@param index The index of the audio asset.
+@return
+**/
 int audio_get_type(int index);
+/** des
+@param num
+**/
 void audio_channel_num(int num);
 
+/** des
+@return
+**/
 int audio_system();
+/** des
+@param fname
+@param type
+@return
+**/
 int audio_add(string fname, int type);
+/** des
+@param sound The index of the audio asset.
+**/
 void audio_delete(int sound);
+/** des
+@param model
+**/
 void audio_falloff_set_model(int model);
 
+/** des
+@return
+**/
 int audio_emitter_create();
+/** des
+@param index The index of the audio emitter.
+@return
+**/
 bool audio_emitter_exists(int index);
+/** des
+@param emitter The index of the audio emitter.
+@param falloff_ref
+@param falloff_max
+@param falloff_factor
+**/
 void audio_emitter_falloff(int emitter, double falloff_ref, double falloff_max, double falloff_factor);
+/** des
+@param emitter The index of the audio emitter.
+**/
 void audio_emitter_free(int emitter);
+/** des
+@param emitter The index of the audio emitter.
+@param gain
+**/
 void audio_emitter_gain(int emitter, double gain);
+/** des
+@param emitter The index of the audio emitter.
+@param pitch
+**/
 void audio_emitter_pitch(int emitter, double pitch);
+/** des
+@param emitter The index of the audio emitter.
+@param x
+@param y
+@param z
+**/
 void audio_emitter_position(int emitter, double x, double y, double z);
+/** des
+@param emitter The index of the audio emitter.
+@param vx
+@param vy
+@param vz
+**/
 void audio_emitter_velocity(int emitter, double vx, double vy, double vz);
+/** des
+@param emitter The index of the audio emitter.
+@param sound
+@param loop
+@param priority
+**/
 void audio_play_sound_on(int emitter, int sound, bool loop, double priority);
 
