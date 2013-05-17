33cad6d88179f3496c5344efa8e6cd3f8876b74d
Owner <robertbcolton@hotmail.com>
2013-05-16 22:27:49 -0400

    Fixed Definition

--- ENIGMAsystem/SHELL/Audio_Systems/SFML/SFML_basic.cpp
+++ ENIGMAsystem/SHELL/Audio_Systems/SFML/SFML_basic.cpp
@@ -15,7 +15,8 @@
 *** with this code. If not, see <http://www.gnu.org/licenses/>
 **/
 
-#ifdef _WIN32 #define SFML_STATIC 
+#ifdef _WIN32 
+#define SFML_STATIC 
 
 #include <SFML/Audio.hpp>
 #include <vector>
