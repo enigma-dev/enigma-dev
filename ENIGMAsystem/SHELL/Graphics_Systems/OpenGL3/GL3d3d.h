e713f3a86c10dc18facb246a669b4a616164d429
Owner <robertbcolton@hotmail.com>
2013-05-25 06:02:46 -0400

    Fixed Regression I had accidentally reverted some of forthevins changes to my graphics system.

--- ENIGMAsystem/SHELL/Graphics_Systems/OpenGL3/GL3d3d.h
+++ ENIGMAsystem/SHELL/Graphics_Systems/OpenGL3/GL3d3d.h
@@ -15,6 +15,9 @@
 *** with this code. If not, see <http://www.gnu.org/licenses/>
 **/
 
+#ifndef ENIGMA_GL3D3D_H
+#define ENIGMA_GL3D3D_H 
+
 #include <string>
 
 namespace enigma {
@@ -151,3 +154,4 @@ bool d3d_light_enable(int id, bool enable);
 
 }
 
+#endif // ENIGMA_GL3D3D_H 
