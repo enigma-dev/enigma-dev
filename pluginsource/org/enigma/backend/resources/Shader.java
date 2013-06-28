2f3f95175518a8d8c9ffcaf57dd1a7a5094842b9
RobertBColton <robertbcolton@hotmail.com>
2013-06-28 13:37:30 -0400

    Updated plugin fonts to Quantum theme.

---
+++ pluginsource/org/enigma/backend/resources/Shader.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2013 Robert B. Colton
+ * 
+ * This file is part of Enigma Plugin.
+ * Enigma Plugin is free software and comes with ABSOLUTELY NO WARRANTY.
+ * See LICENSE for details.
+ */
+
+package org.enigma.backend.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.sun.jna.Structure;
+
+public class Shader extends Structure
+	{
+	public String name;
+	public int id;
+
+	public String code;
+
+	@Override
+	protected List<String> getFieldOrder()
+		{
+		return Arrays.asList("name","id","code");
+		}
+
+	public static class ByReference extends Shader implements Structure.ByReference
+		{
+		}
+	}
