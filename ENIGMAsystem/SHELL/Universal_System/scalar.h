32da10389c8420ec548dcce5cf35e13d7c693c67
RobertBColton <robertbcolton@hotmail.com>
2013-07-26 20:32:18 -0400

    Added scalar definition header to universal systems and implemented them to audio function coordinate space.

---
+++ ENIGMAsystem/SHELL/Universal_System/scalar.h
@@ -0,0 +1,37 @@
+/** Copyright (C) 2013 Robert B. Colton
+***
+*** This file is a part of the ENIGMA Development Environment.
+***
+*** ENIGMA is free software: you can redistribute it and/or modify it under the
+*** terms of the GNU General Public License as published by the Free Software
+*** Foundation, version 3 of the license or any later version.
+***
+*** This application and its source code is distributed AS-IS, WITHOUT ANY
+*** WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+*** FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+*** details.
+***
+*** You should have received a copy of the GNU General Public License along
+*** with this code. If not, see <http://www.gnu.org/licenses/>
+**/
+
+// This header implements scalar data types for double and floating point precision
+// for major systems and coordinate space.
+
+#ifdef GS_SCALAR_64
+typedef double gs_scalar;
+#else
+typedef float gs_scalar;
+#endif
+
+#ifdef AS_SCALAR_64
+typedef double as_scalar;
+#else
+typedef float as_scalar;
+#endif
+
+#ifdef CS_SCALAR_64
+typedef double cs_scalar;
+#else
+typedef float cs_scalar;
+#endif
