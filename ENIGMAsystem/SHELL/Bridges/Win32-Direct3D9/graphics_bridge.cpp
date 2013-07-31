00c7e7160ce05eb2b758b9f87ee0dfd5ff58f69b
RobertBColton <robertbcolton@hotmail.com>
2013-07-31 12:58:41 -0700

    D3D9 Hardware Vertex Processing Enabled hardware as default instead of software.

--- ENIGMAsystem/SHELL/Bridges/Win32-Direct3D9/graphics_bridge.cpp
+++ ENIGMAsystem/SHELL/Bridges/Win32-Direct3D9/graphics_bridge.cpp
@@ -52,31 +52,30 @@ namespace enigma
 		
 		ZeroMemory(&d3dpp, sizeof(d3dpp));    // clear out the struct for use
 		d3dpp.Windowed = TRUE;    // program windowed, not fullscreen
-		//d3dpp.BackBufferCount = 1;  //We only need a single back buffer
-		//d3dpp.MultiSampleType = D3DMULTISAMPLE_NONE; //No multi-sampling
+		d3dpp.BackBufferCount = 1;  //We only need a single back buffer
+		//d3dpp.MultiSampleType = D3DMULTISAMPLE_4_SAMPLES; //No multi-sampling
 		//d3dpp.MultiSampleQuality = 0;                //No multi-sampling
 		d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;  // Throw away previous frames, we don't need them
-		//d3dpp.hDeviceWindow = hWnd;  //This is our main (and only) window
-		//d3dpp.Flags = 0;            //No flags to set
+		d3dpp.hDeviceWindow = hWnd;  //This is our main (and only) window
+		d3dpp.Flags = 0;            //No flags to set
 		//d3dpp.FullScreen_RefreshRateInHz = D3DPRESENT_RATE_DEFAULT; //Default Refresh Rate
 		//d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_DEFAULT;   //Default Presentation rate
-		//d3dpp.BackBufferFormat = format;      //Display format
-		//d3dpp.EnableAutoDepthStencil = FALSE; //No depth/stencil buffer
+		d3dpp.BackBufferFormat = format;      //Display format
+		d3dpp.EnableAutoDepthStencil = FALSE; //No depth/stencil buffer
 		
 		// create a device class using this information and information from the d3dpp stuct
 		hr = d3dr->CreateDevice(D3DADAPTER_DEFAULT,
                       D3DDEVTYPE_HAL,
                       hWnd,
-                      D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+                      D3DCREATE_HARDWARE_VERTEXPROCESSING,
                       &d3dpp,
                       &d3ddev);
 		if(FAILED(hr)){
-			    // create a "Hello World" message box using MessageBox()
-    MessageBox(NULL,
+			MessageBox(NULL,
                DXGetErrorDescription9(hr),
                DXGetErrorString9(hr),
                MB_ICONERROR | MB_OK);
-			}
+		}
     }
 
     void DisableDrawing (HWND hWnd, HDC hDC, HGLRC hRC)
