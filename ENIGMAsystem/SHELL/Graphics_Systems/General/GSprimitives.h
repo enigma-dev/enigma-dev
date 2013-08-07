c717b69a0873d95136e78345eb8ed768e3b72932
RobertBColton <robertbcolton@hotmail.com>
2013-08-07 00:37:03 -0700

    Graphics Systems Deprecation Deprecating support for old primitive types such as quad, primitive quality hints are now rs_nicest, rs_fastest, or in other words part of the render state constants. Please do not merge yet I want to deprecate some other things as well.

--- ENIGMAsystem/SHELL/Graphics_Systems/General/GSprimitives.h
+++ ENIGMAsystem/SHELL/Graphics_Systems/General/GSprimitives.h
@@ -20,21 +20,12 @@
 namespace enigma_user
 {
 
-const int pr_pointlist      = 1;     //GL_POINTS
-const int pr_linelist       = 2;     //GL_LINES
-const int pr_linestrip      = 3;     //GL_LINE_STRIP
-const int pr_trianglelist   = 4;     //GL_TRIANGLES
-const int pr_trianglestrip  = 5;     //GL_TRIANGLE_STRIP
-const int pr_trianglefan    = 6;     //GL_TRIANGLE_FAN
-
-const int pr_lineloop       = 7;     //GL_LINE_LOOP
-const int pr_quadlist       = 8;     //GL_QUADS
-const int pr_quadstrip      = 9;     //GL_QUAD_STRIP
-const int pr_polygon        = 10;    //GL_POLYGON
-
-const int pr_aa_nicest      = 4354;  //GL_NICEST
-const int pr_aa_fastest     = 4353;  //GL_FASTEST
-const int pr_aa_dontcare    = 4352;  //GL_DONT_CARE
+const int pr_pointlist      = 1;     // GL_POINTS            D3DPT_POINTLIST
+const int pr_linelist       = 2;     // GL_LINES             D3DPT_LINELIST
+const int pr_linestrip      = 3;     // GL_LINE_STRIP        D3DPT_LINESTRIP
+const int pr_trianglelist   = 4;     // GL_TRIANGLES         D3DPT_TRIANGLELIST
+const int pr_trianglestrip  = 5;     // GL_TRIANGLE_STRIP    D3DPT_TRIANGLESTRIP
+const int pr_trianglefan    = 6;     // GL_TRIANGLE_FAN      D3DPT_TRIANGLEFAN
 
 int draw_primitive_begin(int kind);
 int draw_primitive_begin_texture(int kind,unsigned tex);
