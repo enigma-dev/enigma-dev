c37bbb7601cd7d9488652004091907c940b82c06
RobertBColton <robertbcolton@hotmail.com>
2013-08-23 22:12:37 -0700

    Fixed MinGW You can now use it with MinGW already installed, just not with TDM installer cause its crap.

--- pluginsource/org/enigma/file/EFileReader.java
+++ pluginsource/org/enigma/file/EFileReader.java
@@ -80,6 +80,8 @@ import org.lateralgm.resources.Room;
 import org.lateralgm.resources.Room.PRoom;
 import org.lateralgm.resources.Script;
 import org.lateralgm.resources.Script.PScript;
+import org.lateralgm.resources.Shader;
+import org.lateralgm.resources.Shader.PShader;
 import org.lateralgm.resources.Sound;
 import org.lateralgm.resources.Sound.PSound;
 import org.lateralgm.resources.Sprite;
@@ -147,6 +149,7 @@ public class EFileReader
 		readers.put(Background.class,new BackgroundReader());
 		readers.put(Path.class,new PathTextReader());
 		readers.put(Script.class,new ScriptReader());
+		readers.put(Shader.class,new ShaderReader());
 		readers.put(Font.class,new FontRawReader());
 		//		readers.put(Timeline.class,new TimelineIO());
 		readers.put(GmObject.class,new ObjectEefReader());
@@ -705,6 +708,30 @@ public class EFileReader
 			}
 		}
 
+	static class ShaderReader extends DataPropReader<Shader,PShader>
+	{
+	@Override
+	protected boolean allowProperty(PShader key)
+		{
+		return false;
+		}
+
+	@Override
+	protected void readData(ProjectFile gf, Shader s, InputStream in)
+		{
+		try
+			{
+			String[] code = new String(Util.readFully(in).toByteArray()).split("############SPLITSHADERCODE############\n");
+			s.setVertexCode(code[0]);
+			s.setFragmentCode(code[1]);
+			}
+		catch (IOException e)
+			{
+			e.printStackTrace();
+			}
+		}
+	}
+	
 	static class FontRawReader extends DataPropReader<Font,PFont>
 		{
 		@Override
