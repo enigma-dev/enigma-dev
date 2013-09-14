c1966470a5dd9b0f8a64899665495c0f3d3778af
RobertBColton <robertbcolton@hotmail.com>
2013-09-13 20:29:42 -0700

    Finished outlining buffer functions.

--- ENIGMAsystem/SHELL/Universal_System/bufferstruct.cpp
+++ ENIGMAsystem/SHELL/Universal_System/bufferstruct.cpp
@@ -23,109 +23,195 @@ using namespace std;
 #include "libEGMstd.h"
 #include "bufferstruct.h"
 
-#ifdef DEBUG_MODE
-  #include "Widget_Systems/widgets_mandatory.h"
-  #define get_background(bck2d,back)\
-    if (back < 0 or size_t(back) >= enigma::background_idmax or !enigma::backgroundstructarray[back]) {\
-      show_error("Attempting to draw non-existing background " + toString(back), false);\
-      return;\
-    }\
-    enigma::background *bck2d = enigma::backgroundstructarray[back];
-  #define get_backgroundnv(bck2d,back,r)\
-    if (back < 0 or size_t(back) >= enigma::background_idmax or !enigma::backgroundstructarray[back]) {\
-      show_error("Attempting to draw non-existing background " + toString(back), false);\
-      return r;\
-    }\
-    enigma::background *bck2d = enigma::backgroundstructarray[back];
-#else
-  #define get_background(bck2d,back)\
-    enigma::background *bck2d = enigma::backgroundstructarray[back];
-  #define get_backgroundnv(bck2d,back,r)\
-    enigma::background *bck2d = enigma::backgroundstructarray[back];
-#endif
-
 namespace enigma
 {
-
+	vector<BinaryBuffer*> buffers(0);
+	
+	int get_free_buffer() {
+		for (int i = 0; i < buffers.size(); i++) {
+			if (!buffers[i]) {
+				return i;
+			}
+		}
+		return buffers.size();
+	}
 }
 
 namespace enigma_user
 {
 
 int buffer_create(unsigned size, int type, unsigned alignment) {
+	enigma::BinaryBuffer* buffer = new enigma::BinaryBuffer(size);
+	buffer->type = type;
+	buffer->alignment = alignment;
+	int id = enigma::get_free_buffer();
+	enigma::buffers.insert(enigma::buffers.begin() + id, buffer);
+	return id;
 }
 
 void buffer_delete(int buffer) {
+	get_buffer(binbuff, buffer);
+	delete binbuff;
 }
 
 void buffer_copy(int src_buffer, unsigned src_offset, unsigned size, int dest_buffer, unsigned dest_offset) {
+	get_buffer(srcbuff, src_buffer);
+	get_buffer(dstbuff, dest_buffer);
+	//TODO: Write this function
 }
 
 void buffer_save(int buffer, string filename) {
+	get_buffer(binbuff, buffer);
+	//TODO: Write this function
 }
 
 void buffer_save_ext(int buffer, string filename, unsigned offset, unsigned size) {
+	get_buffer(binbuff, buffer);
+	//TODO: Write this function
 }
 
-void buffer_load(string filename) {
+int buffer_load(string filename) {
+	enigma::BinaryBuffer* buffer = new enigma::BinaryBuffer(0);
+	buffer->type = buffer_grow;
+	buffer->alignment = 1;
+	int id = enigma::get_free_buffer();
+	enigma::buffers.insert(enigma::buffers.begin() + id, buffer);
+	//TODO: Write this function
+	return id;
 }
 
 void buffer_load_ext(int buffer, string filename, unsigned offset) {
+	get_buffer(binbuff, buffer);
+	//TODO: Write this function
 }
 
 void buffer_fill(int buffer, unsigned offset, int type, variant value, unsigned size) {
+	get_buffer(binbuff, buffer);
+	//TODO: Write this function
 }
 
-int buffer_get_size(int buffer) {
+unsigned buffer_get_size(int buffer) {
+	get_buffer(binbuff, buffer);
+	return binbuff->GetSize();
 }
 
 void buffer_get_surface(int buffer, int surface, int mode, unsigned offset, int modulo) {
+	get_buffer(binbuff, buffer);
+	//TODO: Write this function
 }
 
 string buffer_md5(int buffer, unsigned offset, unsigned size) {
+	get_buffer(binbuff, buffer);
+	//TODO: Write this function
 }
 
 string buffer_sha1(int buffer, unsigned offset, unsigned size) {
+	get_buffer(binbuff, buffer);
+	//TODO: Write this function
 }
 
 int buffer_base64_decode(string str) {
+	enigma::BinaryBuffer* buffer = new enigma::BinaryBuffer(0);
+	buffer->type = buffer_grow;
+	buffer->alignment = 1;
+	int id = enigma::get_free_buffer();
+	enigma::buffers.insert(enigma::buffers.begin() + id, buffer);
+	//TODO: Write this function
+	return id;
 }
 
 int buffer_base64_decode_ext(int buffer, string str, unsigned offset) {
+	get_buffer(binbuff, buffer);
+	//TODO: Write this function
 }
 
 string buffer_base64_encode(int buffer, unsigned offset, unsigned size) {
+	get_buffer(binbuff, buffer);
+	//TODO: Write this function
 }
 
 void buffer_poke(int buffer, unsigned offset, int type, variant value) {
+	get_buffer(binbuff, buffer);
+	//TODO: Write this function
 }
 
 void buffer_resize(int buffer, unsigned size) {
+	get_buffer(binbuff, buffer);
+	binbuff->Resize(size);
 }
 
-void buffer_seek(int buffer, unsigned base, unsigned offset) {
+void buffer_seek(int buffer, int base, unsigned offset) {
+	get_buffer(binbuff, buffer);
+	switch (base) {
+		case buffer_seek_start:
+		binbuff->Seek(offset);
+		break;
+		case buffer_seek_end:
+		binbuff->Seek(binbuff->GetSize() + offset);
+		break;
+		case buffer_seek_relative:
+		binbuff->Seek(binbuff->position + offset);
+		break;
+	}
 }
 
-void buffer_sizeof(int type) {
+unsigned buffer_sizeof(int type) {
+	switch (type) {
+		case buffer_u8:
+		return 1;
+		case buffer_s8:
+		return 1;
+		case buffer_u16:
+		return 2;
+		case buffer_s16:
+		return 2;
+		case buffer_u32:
+		return 4;
+		case buffer_s32:
+		return 4;
+		case buffer_f16:
+		return 2;
+		case buffer_f32:
+		return 4;
+		case buffer_f64:
+		return 8;
+		case buffer_bool:
+		return 1;
+		case buffer_string:
+		return 0;
+		default:
+		return 0;
+	}
 }
 
 int buffer_tell(int buffer) {
+	get_buffer(binbuff, buffer);
+	return binbuff->position;
 }
 
 variant buffer_peek(int buffer, unsigned offset, int type) {
+	get_buffer(binbuff, buffer);
+	//TODO: Write this function
 }
 
 variant buffer_read(int buffer, int type) {
+	get_buffer(binbuff, buffer);
+	//TODO: Write this function
 }
 
 void buffer_write(int buffer, int type, variant value) {
+	get_buffer(binbuff, buffer);
+	//TODO: Write this function
 }
 
 void game_save_buffer(int buffer) {
+	get_buffer(binbuff, buffer);
+	//TODO: Write this function
 }
 
 void game_load_buffer(int buffer) {
+	get_buffer(binbuff, buffer);
+	//TODO: Write this function
 }
 
 }
-
--- ENIGMAsystem/SHELL/Universal_System/bufferstruct.h
+++ ENIGMAsystem/SHELL/Universal_System/bufferstruct.h
@@ -26,17 +26,54 @@ namespace enigma
   {
 	vector<char> data;
 	unsigned position;
+	unsigned alignment;
+	int type;
 	
-    BinaryBuffer() {
-	
+    BinaryBuffer(unsigned size) {
+		data.resize(size);
+		std::fill(data.begin(), data.end(), 0);
 	}
 	
 	~BinaryBuffer() {
 	
 	}
+	
+	void Seek(unsigned offset) {
+		position = offset;
+	}
+	
+	unsigned GetSize() {
+		return data.size();
+	}
+	
+	void Resize(unsigned size) {
+		data.resize(size);
+	}
+
   };
   
-  extern vector<BinaryBuffer> buffers;
+  extern vector<BinaryBuffer*> buffers;
+  
+  #ifdef DEBUG_MODE
+  #include "Widget_Systems/widgets_mandatory.h"
+  #define get_buffer(binbuff,buff)\
+    if (buff < 0 or size_t(buff) >= enigma::buffers.size() or !enigma::backgroundstructarray[buff]) {\
+      show_error("Attempting to access non-existing buffer " + toString(buff), false);\
+      return;\
+    }\
+    enigma::BinaryBuffer *binbuff = enigma::buffers[buff];
+  #define get_bufferr(binbuff,buff,r)\
+    if (buff < 0 or size_t(buff) >= enigma::buffers.size() or !enigma::backgroundstructarray[buff]) {\
+      show_error("Attempting to access non-existing buffer " + toString(buff), false);\
+      return r;\
+    }\
+    enigma::BinaryBuffer *binbuff = enigma::buffers[buff];
+	#else
+	  #define get_buffer(binbuff,buff)\
+		enigma::BinaryBuffer *binbuff = enigma::buffers[buff];
+	  #define get_bufferr(binbuff,buff,r)\
+		enigma::BinaryBuffer *binbuff = enigma::buffers[buff];
+	#endif
 }
 
 namespace enigma_user
@@ -49,32 +86,43 @@ enum {
 	buffer_outofbounds,
 	buffer_outofspace,
 	buffer_vbuffer,
-	buffer_seek_start,
-	buffer_seek_end,
-	buffer_seek_relative
+};
+
+// Buffer surface constants
+enum {
+	buffer_surface_copy,
+	buffer_surface_grayscale,
+	buffer_surface_mask
+};
+
+// Buffer seek base
+enum {
+	buffer_seek_start = 0,
+	buffer_seek_end = 2,
+	buffer_seek_relative = 1
 };
 
 // Buffer types
 enum {
-	buffer_fast,
-	buffer_fixed,
-	buffer_grow,
-	buffer_wrap
+	buffer_fixed = 0,
+	buffer_grow = 1,
+	buffer_wrap = 2,
+	buffer_fast = 3
 };
 
 // Buffer data types
 enum {
-	buffer_u8,
-	buffer_s8,
-	buffer_u16,
-	buffer_s16,
-	buffer_u32,
-	buffer_s32,
-	buffer_f16,
-	buffer_f32,
-	buffer_f64,
-	buffer_bool,
-	buffer_string
+	buffer_u8 = 1,
+	buffer_s8 = 2,
+	buffer_u16 = 3,
+	buffer_s16 = 4,
+	buffer_u32 = 5,
+	buffer_s32 = 6,
+	buffer_f16 = 7,
+	buffer_f32 = 8,
+	buffer_f64 = 9,
+	buffer_bool = 10,
+	buffer_string = 11
 };
 
 int buffer_create(unsigned size, int type, unsigned alignment);
@@ -82,24 +130,26 @@ void buffer_delete(int buffer);
 void buffer_copy(int src_buffer, unsigned src_offset, unsigned size, int dest_buffer, unsigned dest_offset);
 void buffer_save(int buffer, string filename);
 void buffer_save_ext(int buffer, string filename, unsigned offset, unsigned size);
-void buffer_load(string filename);
+int buffer_load(string filename);
 void buffer_load_ext(int buffer, string filename, unsigned offset);
 
-void buffer_fill(int buffer, unsigned offset, int type, variant value, unsigned size);
-int buffer_get_size(int buffer);
-void buffer_get_surface(int buffer, int surface, int mode, unsigned offset, int modulo);
-string buffer_md5(int buffer, unsigned offset, unsigned size);
-string buffer_sha1(int buffer, unsigned offset, unsigned size);
 int buffer_base64_decode(string str);
 int buffer_base64_decode_ext(int buffer, string str, unsigned offset);
 string buffer_base64_encode(int buffer, unsigned offset, unsigned size);
-void buffer_poke(int buffer, unsigned offset, int type, variant value);
+string buffer_md5(int buffer, unsigned offset, unsigned size);
+string buffer_sha1(int buffer, unsigned offset, unsigned size);
+
+unsigned buffer_get_size(int buffer);
+void buffer_get_surface(int buffer, int surface, int mode, unsigned offset, int modulo);
 void buffer_resize(int buffer, unsigned size);
-void buffer_seek(int buffer, unsigned base, unsigned offset);
-void buffer_sizeof(int type);
+void buffer_seek(int buffer, int base, unsigned offset);
+unsigned buffer_sizeof(int type);
 int buffer_tell(int buffer);
+
 variant buffer_peek(int buffer, unsigned offset, int type);
 variant buffer_read(int buffer, int type);
+void buffer_fill(int buffer, unsigned offset, int type, variant value, unsigned size);
+void buffer_poke(int buffer, unsigned offset, int type, variant value);
 void buffer_write(int buffer, int type, variant value);
 
 void game_save_buffer(int buffer);
