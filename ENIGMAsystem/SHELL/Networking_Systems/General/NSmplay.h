f7ebf3ce469aea34e68cecc4b9e5375a4600f920
RobertBColton <robertbcolton@hotmail.com>
2013-09-21 02:59:40 -0400

    More DirectPlay shit, fuck this I am soooo not finishing it.

--- ENIGMAsystem/SHELL/Networking_Systems/General/NSmplay.h
+++ ENIGMAsystem/SHELL/Networking_Systems/General/NSmplay.h
@@ -20,44 +20,39 @@ using std::string;
 
 #include "Universal_System/var4.h"
 
-#ifndef ___NSMPlAY_H_
-#define ___NSMPLAY_H_
-
 namespace enigma_user {
 	int mplay_connect_status(); 
-	void mplay_data_mode(bool guar); 
-	variant mplay_data_read(unsigned ind); 
-	void mplay_data_write(unsigned ind, variant val); 
 	void mplay_end(); 
-	void mplay_init_ipx();  
-	void mplay_init_modem(string initstr, string phonenr); 
+	bool mplay_init_ipx();  
+	bool mplay_init_modem(string initstr, string phonenr); 
 	bool mplay_init_serial(unsigned portno, unsigned baudrate, unsigned stopbits, unsigned parity, unsigned flow); 
 	bool mplay_init_tcpip(string addr); 
 	string mplay_ipaddress(); 
-	void mplay_message_clear(); 
-	unsigned mplay_message_count(string player);
-	unsigned mplay_message_count(unsigned player);
-	unsigned mplay_message_id(string player);
-	unsigned mplay_message_id(unsigned player);
-	string mplay_message_name(); 
-	variant mplay_message_value(); 
-	unsigned mplay_message_player(); 
-	bool mplay_message_receive(string player); 
-	bool mplay_message_receive(unsigned player); 
-	void mplay_message_send(string player, unsigned id, variant val);
-	void mplay_message_send(unsigned player, unsigned id, variant val); 
-	void mplay_message_send_guaranteed(string player, unsigned id, variant val); 
-	void mplay_message_send_guaranteed(unsigned player, unsigned id, variant val); 
 	unsigned mplay_player_find(); 
 	int mplay_player_id(unsigned numb); 
+	int mplay_player_id(string name);
 	string mplay_player_name(unsigned numb); 
-	void mplay_session_create(string sesname, unsigned playnumb, string playername); 
+	bool mplay_session_create(string sesname, unsigned playnumb, string playername); 
 	void mplay_session_end(); 
 	unsigned mplay_session_find(); 
 	bool mplay_session_join(unsigned numb, string playername); 
 	void mplay_session_mode(bool move);
 	string mplay_session_name(unsigned numb); 
 	int mplay_session_status(); 
+	void mplay_data_mode(bool guar); 
+	variant mplay_data_read(unsigned ind); 
+	void mplay_data_write(unsigned ind, variant val); 
+	void mplay_message_clear(); 
+	unsigned mplay_message_count(string player);
+	unsigned mplay_message_count(unsigned player);
+	unsigned mplay_message_id();
+	string mplay_message_name(); 
+	variant mplay_message_value(); 
+	unsigned mplay_message_player(); 
+	bool mplay_message_receive(string player); 
+	bool mplay_message_receive(unsigned player); 
+	bool mplay_message_send(string player, unsigned id, variant val);
+	bool mplay_message_send(unsigned player, unsigned id, variant val); 
+	bool mplay_message_send_guaranteed(string player, unsigned id, variant val); 
+	bool mplay_message_send_guaranteed(unsigned player, unsigned id, variant val); 
 }
-
-#endif
