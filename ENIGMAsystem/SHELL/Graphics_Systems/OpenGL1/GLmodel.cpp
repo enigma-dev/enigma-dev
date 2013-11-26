5960db0c5955e93e2cf86c7b5576a8ee0d379fae
RobertBColton <robertbcolton@hotmail.com>
2013-11-26 17:00:03 -0500

    Added fixes to d3d_model_load to make it work on Linux and set OpenGL/OpenAL as default systems again until other shit is accomplished just good enough to make this pull request mergeable for now.

--- ENIGMAsystem/SHELL/Graphics_Systems/OpenGL1/GLmodel.cpp
+++ ENIGMAsystem/SHELL/Graphics_Systems/OpenGL1/GLmodel.cpp
@@ -51,7 +51,7 @@ unsigned int split(const std::string &txt, std::vector<std::string> &strs, char 
 	strs.clear();
 
 	// Decompose statement
-	while( pos != std::string::npos ) {
+	while( pos != txt.length() ) {
 		strs.push_back( txt.substr( initialPos, pos - initialPos + 1 ) );
 		initialPos = pos + 1;
 
@@ -59,7 +59,7 @@ unsigned int split(const std::string &txt, std::vector<std::string> &strs, char 
 	}
 
 	// Add the last one
-	strs.push_back( txt.substr( initialPos, std::min( pos, txt.size() ) - initialPos + 1 ) );
+	strs.push_back( txt.substr( initialPos, std::min( pos, (unsigned)txt.size() ) - initialPos + 1 ) );
 
 	return strs.size();
 }
