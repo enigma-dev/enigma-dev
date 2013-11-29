9b4cf96de2fe98dea4f95b3f6228a90274cdd04b
RobertBColton <robertbcolton@hotmail.com>
2013-11-29 16:07:08 -0500

    Refactored graphics code to be reusable between systems with the new batching system.

---
+++ ENIGMAsystem/SHELL/Graphics_Systems/General/GSsurface.cpp
@@ -0,0 +1,262 @@
+/** Copyright (C) 2013 Robert B. Colton
+***
+*** This file is a part of the ENIGMA Development Environment.
+***
+*** ENIGMA is free software: you can redistribute it and/or modify it under the
+*** terms of the GNU General Public License as published by the Free Software
+*** Foundation, version 3 of the license or any later version.
+***
+*** This application and its source code is distributed AS-IS, WITHOUT ANY
+*** WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+*** FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+*** details.
+***
+*** You should have received a copy of the GNU General Public License along
+*** with this code. If not, see <http://www.gnu.org/licenses/>
+**/
+
+using namespace std;
+#include <cstddef>
+#include <iostream>
+#include <math.h>
+
+
+#include <stdio.h> //for file writing (surface_save)
+#include "Universal_System/nlpo2.h"
+#include "Universal_System/spritestruct.h"
+#include "Universal_System/backgroundstruct.h"
+#include "Collision_Systems/collision_types.h"
+
+#define __GETR(x) ((x & 0x0000FF))
+#define __GETG(x) ((x & 0x00FF00) >> 8)
+#define __GETB(x) ((x & 0xFF0000) >> 16)
+
+namespace enigma_user {
+extern int room_width, room_height/*, sprite_idmax*/;
+}
+
+#include "../General/GSprimitives.h"
+#include "../General/GSsurface.h"
+
+
+namespace enigma_user
+{
+
+void draw_surface(int id, gs_scalar x, gs_scalar y)
+{
+	int w=surface_get_width(id);
+	int h=surface_get_height(id);
+  
+	draw_primitive_begin_texture(pr_trianglestrip, surface_get_texture(id));
+	draw_vertex_texture(x,y,0,0);
+	draw_vertex_texture(x+w,y,1,0);
+	draw_vertex_texture(x,y+h,0,1);
+	draw_vertex_texture(x+w,y+h,1,1);
+	draw_primitive_end();
+}
+
+void draw_surface_ext(int id,gs_scalar x, gs_scalar y,gs_scalar xscale, gs_scalar yscale,double rot,int color,double alpha)
+{
+    const gs_scalar w=surface_get_width(id)*xscale, h=surface_get_height(id)*yscale;
+    rot *= M_PI/180;
+
+    gs_scalar ulcx = x + xscale * cos(M_PI+rot) + yscale * cos(M_PI/2+rot),
+          ulcy = y - yscale * sin(M_PI+rot) - yscale * sin(M_PI/2+rot);
+
+	draw_primitive_begin_texture(pr_trianglestrip, surface_get_texture(id));
+	draw_vertex_texture_color(ulcx,ulcy,0,0,color,alpha);
+	draw_vertex_texture_color(ulcx + w*cos(rot), ulcy - w*sin(rot),1,0,color,alpha);
+	draw_vertex_texture_color(ulcx,ulcy,0,1,color,alpha);
+	draw_vertex_texture_color(ulcx + w*cos(rot), ulcy - w*sin(rot),1,1,color,alpha);
+	draw_primitive_end();
+}
+
+void draw_surface_stretched(int id, gs_scalar x, gs_scalar y, float w, float h)
+{
+	draw_primitive_begin_texture(pr_trianglestrip, surface_get_texture(id));
+	draw_vertex_texture(x,y,0,0);
+	draw_vertex_texture(x+w,y,1,0);
+	draw_vertex_texture(x,y+h,0,1);
+	draw_vertex_texture(x+w,y+h,1,1);
+	draw_primitive_end();
+}
+
+void draw_surface_stretched_ext(int id, gs_scalar x, gs_scalar y, gs_scalar w, gs_scalar h, int color, double alpha)
+{
+	draw_primitive_begin_texture(pr_trianglestrip, surface_get_texture(id));
+	draw_vertex_texture_color(x,y,0,0,color,alpha);
+	draw_vertex_texture_color(x+w,y,1,0,color,alpha);
+	draw_vertex_texture_color(x,y+h,0,1,color,alpha);
+	draw_vertex_texture_color(x+w,y+h,1,1,color,alpha);
+	draw_primitive_end();
+}
+
+void draw_surface_part(int id, gs_scalar left, gs_scalar top, gs_scalar w, gs_scalar h, gs_scalar x, gs_scalar y)
+{
+	const gs_scalar tbw=surface_get_width(id),tbh=surface_get_height(id);
+	
+	draw_primitive_begin_texture(pr_trianglestrip, surface_get_texture(id));
+	draw_vertex_texture(x,y,left/tbw,top/tbh);
+	draw_vertex_texture(x+w,y,(left+w)/tbw,top/tbh);
+	draw_vertex_texture(x,y+h,left/tbw,(top+h)/tbh);
+	draw_vertex_texture(x+w,y+h,(left+w)/tbw,(top+h)/tbh);
+	draw_primitive_end();
+}
+
+void draw_surface_part_ext(int id, gs_scalar left, gs_scalar top, gs_scalar w, gs_scalar h, gs_scalar x, gs_scalar y, gs_scalar xscale, gs_scalar yscale,int color, double alpha)
+{
+	const gs_scalar tbw = surface_get_width(id), tbh = surface_get_height(id);
+	draw_primitive_begin_texture(pr_trianglestrip, surface_get_texture(id));
+	draw_vertex_texture_color(x,y,left/tbw,top/tbh,color,alpha);
+	draw_vertex_texture_color(x+w*xscale,y,(left+w)/tbw,top/tbh,color,alpha);
+	draw_vertex_texture_color(x,y+h*yscale,left/tbw,(top+h)/tbh,color,alpha);
+	draw_vertex_texture_color(x+w*xscale,y+h*yscale,(left+w)/tbw,(top+h)/tbh,color,alpha);
+	draw_primitive_end();
+}
+
+void draw_surface_general(int id, gs_scalar left, gs_scalar top, gs_scalar width, gs_scalar height, gs_scalar x, gs_scalar y, gs_scalar xscale, gs_scalar yscale, double rot, int c1, int c2, int c3, int c4, double a1, double a2, double a3, double a4)
+{
+	const gs_scalar tbw = surface_get_width(id), tbh = surface_get_height(id),
+	  w = width*xscale, h = height*yscale;
+
+    rot *= M_PI/180;
+
+    gs_scalar ulcx = x + xscale * cos(M_PI+rot) + yscale * cos(M_PI/2+rot),
+          ulcy = y - yscale * sin(M_PI+rot) - yscale * sin(M_PI/2+rot);
+
+    draw_primitive_begin_texture(pr_trianglestrip, surface_get_texture(id));
+	draw_vertex_texture_color(ulcx,ulcy,left/tbw,top/tbh,c1,a1);
+	draw_vertex_texture_color((ulcx + w*cos(rot)), (ulcy - w*sin(rot)), (left+width)/tbw,top/tbh, c2, a2);
+		
+      ulcx += h * cos(3*M_PI/2 + rot);
+      ulcy -= h * sin(3*M_PI/2 + rot);
+	  
+	draw_vertex_texture_color((ulcx + w*cos(rot)), (ulcy - w*sin(rot)), (left+width)/tbw,(top+height)/tbh, c3, a3);
+	draw_vertex_texture_color(ulcx, ulcy, left/tbw, (top+height)/tbh, c4, a4);
+    draw_primitive_end();
+}
+
+void draw_surface_tiled(int id, gs_scalar x, gs_scalar y)
+{
+	const gs_scalar tbw = surface_get_width(id), tbh = surface_get_height(id);
+	x=surface_get_width(id)-fmod(x,surface_get_width(id));
+	y=surface_get_height(id)-fmod(y,surface_get_height(id));
+	const int hortil= int (ceil(room_width/(surface_get_width(id)))),
+			  vertil= int (ceil(room_height/(surface_get_height(id))));
+
+    
+	for (int i=0; i<hortil; i++)
+	{
+		for (int c=0; c<vertil; c++)
+		{
+			draw_primitive_begin_texture(pr_trianglestrip, surface_get_texture(id));
+			draw_vertex_texture(i*tbw-x,c*tbh-y,0,0);
+			draw_vertex_texture((i+1)*tbw-x,c*tbh-y,1,0);
+			draw_vertex_texture(i*tbw-x,(c+1)*tbh-y,0,1);
+			draw_vertex_texture((i+1)*tbw-x,(c+1)*tbh-y,1,1);
+			draw_primitive_end();
+		}
+	}
+}
+
+void draw_surface_tiled_ext(int id, gs_scalar x, gs_scalar y, gs_scalar xscale, gs_scalar yscale, int color, double alpha)
+{
+    const gs_scalar w=surface_get_width(id)*xscale, h=surface_get_height(id)*yscale;
+    const int hortil= int (ceil(room_width/(surface_get_width(id)))),
+        vertil= int (ceil(room_height/(surface_get_height(id))));
+    x=w-fmod(x,w);
+    y=h-fmod(y,h);
+ 
+    for (int i=0; i<hortil; i++)
+    {
+      for (int c=0; c<vertil; c++)
+      {		  
+		draw_primitive_begin_texture(pr_trianglestrip, surface_get_texture(id));
+		draw_vertex_texture_color(i*w-x,c*h-y,0,0,color,alpha);
+		draw_vertex_texture_color((i+1)*w-x,c*h-y,1,0,color,alpha);
+		draw_vertex_texture_color(i*w-x,(c+1)*h-y,0,1,color,alpha);
+		draw_vertex_texture_color((i+1)*w-x,(c+1)*h-y,1,1,color,alpha);
+		draw_primitive_end();
+      }
+    }
+}
+
+void draw_surface_tiled_area(int id, gs_scalar x, gs_scalar y, float x1, float y1, float x2, float y2)
+{
+    gs_scalar sw,sh,i,j,jj,left,top,width,height,X,Y;
+    sw = surface_get_width(id);
+    sh = surface_get_height(id);
+
+    i = x1-(fmod(x1,sw) - fmod(x,sw)) - sw*(fmod(x1,sw)<fmod(x,sw));
+    j = y1-(fmod(y1,sh) - fmod(y,sh)) - sh*(fmod(y1,sh)<fmod(y,sh));
+    jj = j;
+
+    for(; i<=x2; i+=sw)
+    {
+      for(; j<=y2; j+=sh)
+      {
+        if(i <= x1) left = x1-i;
+        else left = 0;
+        X = i+left;
+
+        if(j <= y1) top = y1-j;
+        else top = 0;
+        Y = j+top;
+
+        if(x2 <= i+sw) width = ((sw)-(i+sw-x2)+1)-left;
+        else width = sw-left;
+
+        if(y2 <= j+sh) height = ((sh)-(j+sh-y2)+1)-top;
+        else height = sh-top;
+		  
+		draw_primitive_begin_texture(pr_trianglestrip, surface_get_texture(id));
+		draw_vertex_texture(X,Y,left/sw,top/sh);
+		draw_vertex_texture(X+width,Y,(left+width)/sw,top/sh);
+		draw_vertex_texture(X,Y+height,left/sw,(top+height)/sh);
+		draw_vertex_texture(X+width,Y+height,(left+width)/sw,(top+height)/sh);
+		draw_primitive_end();
+      }
+      j = jj;
+    }
+}
+
+void draw_surface_tiled_area_ext(int id, gs_scalar x, gs_scalar y, float x1, float y1, float x2, float y2, gs_scalar xscale, gs_scalar yscale, int color, double alpha)
+{
+    gs_scalar sw,sh,i,j,jj,left,top,width,height,X,Y;
+    sw = surface_get_width(id)*xscale;
+    sh = surface_get_height(id)*yscale;
+
+    i = x1-(fmod(x1,sw) - fmod(x,sw)) - sw*(fmod(x1,sw)<fmod(x,sw));
+    j = y1-(fmod(y1,sh) - fmod(y,sh)) - sh*(fmod(y1,sh)<fmod(y,sh));
+    jj = j;
+
+    for(; i<=x2; i+=sw)
+    {
+      for(; j<=y2; j+=sh)
+      {
+        if(i <= x1) left = x1-i;
+        else left = 0;
+        X = i+left;
+
+        if(j <= y1) top = y1-j;
+        else top = 0;
+        Y = j+top;
+
+        if(x2 <= i+sw) width = ((sw)-(i+sw-x2)+1)-left;
+        else width = sw-left;
+
+        if(y2 <= j+sh) height = ((sh)-(j+sh-y2)+1)-top;
+        else height = sh-top;
+		  
+		draw_primitive_begin_texture(pr_trianglestrip, surface_get_texture(id));
+		draw_vertex_texture_color(X,Y,left/sw,top/sh,color,alpha);
+		draw_vertex_texture_color(X+width,Y,(left+width)/sw,top/sh,color,alpha);
+		draw_vertex_texture_color(X,Y+height,left/sw,(top+height)/sh,color,alpha);
+		draw_vertex_texture_color(X+width,Y+height,(left+width)/sw,(top+height)/sh,color,alpha);
+		draw_primitive_end();
+      }
+      j = jj;
+    }
+}
+
+}
