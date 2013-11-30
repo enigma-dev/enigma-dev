bc34ae2a718171f82ab635900955e7ab8de04609
RobertBColton <robertbcolton@hotmail.com>
2013-11-29 21:13:44 -0500

    Made single general ini header.

--- ENIGMAsystem/SHELL/Platforms/Win32/file_manip.h
+++ ENIGMAsystem/SHELL/Platforms/Win32/file_manip.h
@@ -25,19 +25,10 @@
 **                                                                              **
 \********************************************************************************/
 
+#include "../General/PFini.h"
+
 namespace enigma_user
 {
-
-void ini_open(std::string fname);
-void ini_close();
-std::string ini_read_string(std::string section, std::string key, string defaultValue);
-int ini_read_real(std::string section, std::string key, int defaultValue);
-void ini_write_string(std::string section, std::string key, string value);
-void ini_write_real(std::string section, std::string key, int value);
-bool ini_key_exists(std::string section, std::string key);
-bool ini_section_exists(std::string section);
-void ini_key_delete(std::string section, std::string key);
-void ini_section_delete(std::string section);
 
 long long file_size(std::string fname);
 time_t file_access_time(std::string fname);
