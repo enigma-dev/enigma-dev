df5b874d504411ee5b0b01713d7d2d4dfe2fddd0
Kyle J. Van Berendonck <kvanberendonck@gmail.com>
2013-12-08 21:35:48 +1100

    Added lots of boilerplate to object.cpp, breaks build

--- newtree/packages/enigma-rt/src/object.cpp
+++ newtree/packages/enigma-rt/src/object.cpp
@@ -24,4 +24,318 @@ namespace ert {
   object::object(unsigned long id, real_t x, real_t y)
     : id(id), xstart(x), ystart(y), x(x), y(y), xprevious(x), yprevious(y) {
   }
+  
+  property_ro<object, unsigned long, &object::get_object_index> object::object_index() {
+    return {this};
+  }
+  
+  bool object::get_solid() {
+    //
+  }
+  
+  void object::set_solid(bool) {
+    //
+  }
+  
+  property<object, bool, &object::get_solid, &object::set_solid> object::solid() {
+    return {this};
+  }
+  
+  bool object::get_visible() {
+    //
+  }
+  
+  void object::set_visible(bool) {
+    //
+  }
+  
+  property<object, bool, &object::get_visible, &object::set_visible> object::visible() {
+    return {this};
+  }
+  
+  bool object::get_persistent() {
+    //
+  }
+  
+  void object::set_persistent(bool) {
+    //
+  }
+  property<object, bool, &object::get_persistent, &object::set_persistent> object::persistent() {
+    return {this};
+  }
+  
+  real_t object::get_depth() {
+    //
+  }
+  
+  void object::set_depth(real_t) {
+    //
+  }
+  
+  property<object, real_t, &object::get_depth, &object::set_depth> object::depth() {
+    return {this};
+  }
+  
+  real_t object::get_sprite_index() {
+    //
+  }
+  
+  void object::set_sprite_index(real_t) {
+    //
+  }
+  
+  property<object, real_t, &object::get_sprite_index, &object::set_sprite_index> object::sprite_index() {
+    return {this};
+  }
+  
+  real_t object::get_sprite_width() {
+    //
+  }
+  
+  property_ro<object, real_t, &object::get_sprite_width> object::sprite_width() {
+    return {this};
+  }
+  
+  real_t object::get_sprite_height() {
+    //
+  }
+  
+  property_ro<object, real_t, &object::get_sprite_height> object::sprite_height() {
+    return {this};
+  }
+  
+  real_t object::get_sprite_xoffset() {
+    //
+  }
+  
+  property_ro<object, real_t, &object::get_sprite_xoffset> object::sprite_xoffset() {
+    return {this};
+  }
+  
+  real_t object::get_sprite_yoffset() {
+    //
+  }
+  
+  property_ro<object, real_t, &object::get_sprite_yoffset> object::sprite_yoffset() {
+    return {this};
+  }
+  
+  real_t object::get_image_alpha() {
+    //
+  }
+  
+  void object::set_image_alpha(real_t) {
+    //
+  }
+  
+  property<object, real_t, &object::get_image_alpha, &object::set_image_alpha> object::image_alpha() {
+    return {this};
+  }
+  
+  real_t object::get_image_angle() {
+    //
+  }
+  
+  void object::set_image_angle(real_t) {
+    //
+  }
+  
+  property<object, real_t, &object::get_image_angle, &object::set_image_angle> object::image_angle() {
+    return {this};
+  }
+  
+  real_t object::get_image_blend() {
+    //
+  }
+  
+  void object::set_image_blend(real_t) {
+    //
+  }
+  
+  property<object, real_t, &object::get_image_blend, &object::set_image_blend> object::image_blend() {
+    return {this};
+  }
+  
+  real_t object::get_image_index() {
+    //
+  }
+  
+  void object::set_image_index(real_t) {
+    //
+  }
+  
+  property<object, real_t, &object::get_image_index, &object::set_image_index> object::image_index() {
+    return {this};
+  }
+  
+  real_t object::get_image_number() {
+    //
+  }
+  property_ro<object, real_t, &object::get_image_number> object::image_number() {
+    return {this};
+  }
+  
+  real_t object::get_image_speed() {
+    //
+  }
+  
+  void object::set_image_speed(real_t) {
+    //
+  }
+  
+  property<object, real_t, &object::get_image_speed, &object::set_image_speed> object::image_speed() {
+    return {this};
+  }
+  
+  real_t object::get_image_xscale() {
+    //
+  }
+  
+  void object::set_image_xscale(real_t) {
+    //
+  }
+  
+  property<object, real_t, &object::get_image_xscale, &object::set_image_xscale> object::image_xscale() {
+    return {this};
+  }
+  
+  real_t object::get_image_yscale() {
+    //
+  }
+  
+  void object::set_image_yscale(real_t) {
+    //
+  }
+  
+  property<object, real_t, &object::get_image_yscale, &object::set_image_yscale> object::image_yscale() {
+    return {this};
+  }
+  
+  real_t object::get_mask_index() {
+    //
+  }
+  
+  void object::set_mask_index(real_t) {
+    //
+  }
+  
+  property<object, real_t, &object::get_mask_index, &object::set_mask_index> object::mask_index() {
+    return {this};
+  }
+  
+  real_t object::get_bbox_bottom() {
+    //
+  }
+  
+  property_ro<object, real_t, &object::get_bbox_bottom> object::bbox_bottom() {
+    return {this};
+  }
+  
+  real_t object::get_bbox_left() {
+    //
+  }
+  
+  property_ro<object, real_t, &object::get_bbox_left> object::bbox_left() {
+    return {this};
+  }
+  
+  real_t object::get_bbox_right() {
+    //
+  }
+  
+  property_ro<object, real_t, &object::get_bbox_right> object::bbox_right() {
+    return {this};
+  }
+  
+  real_t object::get_bbox_top() {
+    //
+  }
+  
+  property_ro<object, real_t, &object::get_bbox_top> object::bbox_top() {
+    return {this};
+  }
+  
+  real_t object::get_direction() {
+    //
+  }
+  
+  void object::set_direction(real_t) {
+    //
+  }
+  
+  property<object, real_t, &object::get_direction, &object::set_direction> object::direction() {
+    return {this};
+  }
+  
+  real_t object::get_friction() {
+    //
+  }
+  
+  void object::set_friction(real_t) {
+    //
+  }
+  
+  property<object, real_t, &object::get_friction, &object::set_friction> object::friction() {
+    return {this};
+  }
+  
+  real_t object::get_gravity() {
+    //
+  }
+  
+  void object::set_gravity(real_t) {
+    //
+  }
+  
+  property<object, real_t, &object::get_gravity, &object::set_gravity> object::gravity() {
+    return {this};
+  }
+  
+  real_t object::get_gravity_direction() {
+    //
+  }
+  
+  void object::set_gravity_direction(real_t) {
+    //
+  }
+  
+  property<object, real_t, &object::get_gravity_direction, &object::set_gravity_direction> object::gravity_direction() {
+    return {this};
+  }
+  
+  real_t object::get_hspeed() {
+    //
+  }
+  
+  void object::set_hspeed(real_t) {
+    //
+  }
+  
+  property<object, real_t, &object::get_hspeed, &object::set_hspeed> object::hspeed() {
+    return {this};
+  }
+  
+  real_t object::get_vspeed() {
+    //
+  }
+  
+  void object::set_vspeed(real_t) {
+    //
+  }
+  
+  property<object, real_t, &object::get_vspeed, &object::set_vspeed> object::vspeed() {
+    return {this};
+  }
+  
+  real_t object::get_speed() {
+    //
+  }
+  
+  void object::set_speed(real_t) {
+    //
+  }
+  
+  property<object, real_t, &object::get_speed, &object::set_speed> object::speed() {
+    return {this};
+  }
 }
