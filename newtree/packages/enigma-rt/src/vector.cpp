4fa52590537c956120533beedd15e2fa09700367
Kyle J. Van Berendonck <kvanberendonck@gmail.com>
2013-12-09 10:59:19 +1100

    Generalize a lot of vector code

--- newtree/packages/enigma-rt/src/vector.cpp
+++ newtree/packages/enigma-rt/src/vector.cpp
@@ -33,6 +33,14 @@ namespace ert {
     real_t vector_direction_rad(real_t x, real_t y) {
       return std::atan2(-y, x) + 2 * pi * (y > 0);
     }
+    
+    real_t vector_length(real_t x, real_t y) {
+      return std::sqrt(x * x + y * y);
+    }
+    
+    real_t vector_length_3d(real_t x, real_t y, real_t z) {
+      return std::sqrt(x * x + y * y + z * z);
+    }
   }
   
   real_t point_direction(real_t x1, real_t y1, real_t x2, real_t y2) {
@@ -40,16 +48,11 @@ namespace ert {
   }
 
   real_t point_distance(real_t x1, real_t y1, real_t x2, real_t y2) {
-    real_t dx = x2 - x1;
-    real_t dy = y2 - y1;
-    return std::sqrt(dx * dx + dy * dy);
+    return internal::vector_length(x2 - x1, y2 - y1);
   }
 
   real_t point_distance_3d(real_t x1, real_t y1, real_t z1, real_t x2, real_t y2, real_t z2) {
-    real_t dx = x2 - x1;
-    real_t dy = y2 - y1;
-    real_t dz = z2 - z1;
-    return std::sqrt(dx * dx + dy * dy + dz * dz);
+    return internal::vector_length_3d(x2 - x1, y2 - y1, z2 - z1);
   }
 
   real_t dot_product(real_t x1, real_t y1, real_t x2, real_t y2) {
@@ -61,15 +64,10 @@ namespace ert {
   }
 
   real_t dot_product_normalised(real_t x1, real_t y1, real_t x2, real_t y2) {
-    real_t dx = x2 - x1;
-    real_t dy = y2 - y1;
-    return (x1 * x2 + y1 * y2) / std::sqrt(dx * dx + dy * dy);
+    return (x1 * x2 + y1 * y2) / internal::vector_length(x2 - x1, y2 - y1);
   }
 
   real_t dot_product_normalised_3d(real_t x1, real_t y1, real_t z1, real_t x2, real_t y2, real_t z2) {
-    real_t dx = x2 - x1;
-    real_t dy = y2 - y1;
-    real_t dz = z2 - z1;
-    return (x1 * x2 + y1 * y2 + z1 * z2) / std::sqrt(dx * dx + dy * dy + dz * dz);
+    return (x1 * x2 + y1 * y2 + z1 * z2) / internal::vector_length_3d(x2 - x1, y2 - y1, z2 - z1);
   }
 }
