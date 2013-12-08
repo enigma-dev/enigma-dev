7bcd225911f4ae3ec41f7ae63ed1387d1128b393
Kyle J. Van Berendonck <kvanberendonck@gmail.com>
2013-12-09 10:59:19 +1100

    Generalize a lot of vector code

--- newtree/packages/enigma-rt/include/ert/vector.hpp
+++ newtree/packages/enigma-rt/include/ert/vector.hpp
@@ -27,6 +27,8 @@ namespace ert {
   namespace internal {
     real_t point_direction_rad(real_t x1, real_t y1, real_t x2, real_t y2);
     real_t vector_direction_rad(real_t x, real_t y);
+    real_t vector_length(real_t x, real_t y);
+    real_t vector_length_3d(real_t x, real_t y, real_t z);
   }
   
   // Vector Functions
