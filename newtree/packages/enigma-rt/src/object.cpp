8ce2a8b9531065a5d00e6f4de25f9943beb554a0
Kyle J. Van Berendonck <kvanberendonck@gmail.com>
2013-12-08 20:59:13 +1100

    Remove properties construction: to be safe it has to be done in the create event
    You can’t use virtual methods in constructors.

--- newtree/packages/enigma-rt/src/object.cpp
+++ newtree/packages/enigma-rt/src/object.cpp
@@ -22,20 +22,6 @@
 
 namespace ert {
   object::object(unsigned long id, real_t x, real_t y)
-    : id(id), xstart(x), ystart(y), x(x), y(y), xprevious(x), yprevious(y), properties{} {
-    this->initialize_properties(this->properties);
-  }
-  
-  void object::initialize_properties(object::object_properties& prop) {
-    prop.solid = this->object_is_solid();
-    prop.visible = this->object_is_visible();
-    prop.persistent = this->object_is_persistent();
-    prop.depth = this->object_depth();
-    prop.sprite_index = this->object_sprite_index();
-    prop.mask_index = this->object_mask_index();
-    prop.image_alpha = 1;
-    prop.image_speed = 1;
-    prop.image_xscale = 1;
-    prop.image_yscale = 1;
+    : id(id), xstart(x), ystart(y), x(x), y(y), xprevious(x), yprevious(y) {
   }
 }
