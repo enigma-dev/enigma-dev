b19f7260de0f9cd2f230d701fcd2352751df0900
RobertBColton <robertbcolton@hotmail.com>
2013-12-12 06:14:36 -0500

    Outlined Direct3D 11.0, able to get a basic device coded, but undefined reference to CreateDevice/CreateDevice and swap chain, probably need to swap the includes and libs for the SDK ones. I am going to file a bug report on the MinGW tracker.

---
+++ ENIGMAsystem/SHELL/Graphics_Systems/Direct3D11/DX11SurfaceStruct.h
@@ -0,0 +1,68 @@
+/** Copyright (C) 2008-2013 Josh Ventura, Robert B. Colton, Dave "biggoron", Harijs Grinbergs
+***
+*** This file is a part of the ENIGMA Development Environment.
+***
+*** ENIGMA is free software: you can redistribute it and/or modify it under the
+*** terms of the GNU General Public License as published by the Free Software
+*** Foundation, version 3 of the license or any later version.
+***
+*** This application and its source code is distributed AS-IS, WITHOUT ANY
+*** WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+*** FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+*** details.
+***
+*** You should have received a copy of the GNU General Public License along
+*** with this code. If not, see <http://www.gnu.org/licenses/>
+**/
+
+#ifndef _SURFSTRUCT__H
+#define _SURFSTRUCT__H
+
+#include <string>
+#include "Direct3D11Headers.h"
+using std::string;
+
+#include <vector>
+using std::vector;
+
+namespace enigma
+{
+  struct Surface
+  {
+
+	Surface() {
+
+	};
+	
+	~Surface() {
+
+	};
+  };
+  
+  extern vector<Surface*> Surfaces;
+}
+
+#ifdef DEBUG_MODE
+  #include <string>
+  #include "libEGMstd.h"
+  #include "Widget_Systems/widgets_mandatory.h"
+  #define get_surface(surf,id)\
+    if (id < 0 or id >= enigma::Surfaces.size() or !enigma::Surfaces[id]) {\
+      show_error("Attempting to use non-existing surface " + toString(id), false);\
+      return;\
+    }\
+    enigma::Surface* surf = enigma::Surfaces[id];
+  #define get_surfacev(surf,id,r)\
+    if (id < 0 or size_t(id) >= enigma::Surfaces.size() or !enigma::Surfaces[id]) {\
+      show_error("Attempting to use non-existing surface " + toString(id), false);\
+      return r;\
+    }\
+    enigma::Surface* surf = enigma::Surfaces[id];
+#else
+  #define get_surface(surf,id)\
+    enigma::Surface* surf = enigma::Surfaces[id];
+  #define get_surfacev(surf,id,r)\
+    enigma::Surface* surf = enigma::Surfaces[id];
+#endif
+
+#endif
---
+++ ENIGMAsystem/SHELL/Graphics_Systems/Direct3D11/DX11screen.cpp
@@ -0,0 +1,141 @@
+/** Copyright (C) 2008-2013 Josh Ventura, Robert B. Colton
+***
+*** This file is a part of the ENIGMA Development Environment.
+***
+*** ENIGMA is free software: you can redistribute it and/or modify it under the
+*** terms of the GNU General Public License as published by the Free Software
+*** Foundation, version 3 of the license or any later version.
+***
+*** This application and its source code is distributed AS-IS, WITHOUT ANY
+*** WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+*** FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+*** details.
+***
+*** You should have received a copy of the GNU General Public License along
+*** with this code. If not, see <http://www.gnu.org/licenses/>
+**/
+
+#include <string>
+#include <cstdio>
+#include "Bridges/General/DX11Context.h"
+#include "Direct3D11Headers.h"
+#include "../General/GSbackground.h"
+#include "../General/GSscreen.h"
+#include "../General/GSd3d.h"
+#include "../General/GStextures.h"
+#include "../General/GScolors.h"
+
+using namespace std;
+
+#include "Universal_System/var4.h"
+#include "Universal_System/estring.h"
+
+#define __GETR(x) (((unsigned int)x & 0x0000FF))
+#define __GETG(x) (((unsigned int)x & 0x00FF00) >> 8)
+#define __GETB(x) (((unsigned int)x & 0xFF0000) >> 16)
+
+#include "Universal_System/roomsystem.h"
+#include "Universal_System/instance_system.h"
+#include "Universal_System/graphics_object.h"
+#include "Universal_System/depth_draw.h"
+#include "Platforms/platforms_mandatory.h"
+#include "Platforms/General/PFwindow.h"
+#include "Universal_System/CallbackArrays.h"
+#include "Graphics_Systems/graphics_mandatory.h"
+#include <limits>
+
+//Fuck whoever did this to the spec
+#ifndef GL_BGR
+  #define GL_BGR 0x80E0
+#endif
+
+using namespace enigma;
+
+#include "../General/GSmodel.h"
+
+namespace enigma_user {
+  extern int window_get_width();
+  extern int window_get_height();
+}
+
+static inline void draw_back()
+{
+    using enigma_user::background_x;
+    using enigma_user::background_y;
+    using enigma_user::background_visible;
+    using enigma_user::background_alpha;
+    using enigma_user::background_xscale;
+    using enigma_user::background_yscale;
+    using enigma_user::background_htiled;
+    using enigma_user::background_vtiled;
+	using enigma_user::background_hspeed;
+	using enigma_user::background_vspeed;
+    using enigma_user::background_index;
+    using enigma_user::background_coloring;
+    using enigma_user::draw_background_tiled_ext;
+    using enigma_user::draw_background_ext;
+    // Draw the rooms backgrounds
+    for (int back_current = 0; back_current < 8; back_current++) {
+        if (background_visible[back_current] == 1) {
+			//NOTE: This has been double checked with Game Maker 8.1 to work exactly the same, the background_x/y is modified just as object locals are
+			//and also just as one would assume the system to work.
+			//TODO: This should probably be moved to room system.
+			background_x[back_current] += background_hspeed[back_current];
+			background_y[back_current] += background_vspeed[back_current];
+            if (background_htiled[back_current] || background_vtiled[back_current]) {
+                draw_background_tiled_ext(background_index[back_current], background_x[back_current], background_y[back_current], background_xscale[back_current], 
+					background_xscale[back_current], background_coloring[back_current], background_alpha[back_current], background_htiled[back_current], background_vtiled[back_current]);
+            } else {
+                draw_background_ext(background_index[back_current], background_x[back_current], background_y[back_current], background_xscale[back_current], background_xscale[back_current], 0, background_coloring[back_current], background_alpha[back_current]);
+			}
+		}
+    }
+}
+
+namespace enigma
+{
+	extern bool d3dMode;
+    extern bool d3dHidden;
+	extern int d3dCulling;
+    extern std::map<int,roomstruct*> roomdata;
+    particles_implementation* particles_impl;
+    void set_particles_implementation(particles_implementation* part_impl)
+    {
+        particles_impl = part_impl;
+    }
+
+	unsigned gui_width;
+	unsigned gui_height;
+}
+
+namespace enigma_user
+{
+
+void screen_redraw()
+{
+	
+	screen_refresh();
+}
+
+void screen_init()
+{
+
+}
+
+int screen_save(string filename) //Assumes native integers are little endian
+{
+
+}
+
+int screen_save_part(string filename,unsigned x,unsigned y,unsigned w,unsigned h) //Assumes native integers are little endian
+{
+
+}
+
+void display_set_gui_size(unsigned width, unsigned height) {
+	enigma::gui_width = width;
+	enigma::gui_height = height;
+}
+
+}
+
