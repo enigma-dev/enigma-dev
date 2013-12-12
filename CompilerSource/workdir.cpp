ad46f4ec29105f3bdf1fa9b12d73e480ed865e7c
RobertBColton <robertbcolton@hotmail.com>
2013-12-11 23:33:54 -0500

    Forgot to commit quotes

--- CompilerSource/workdir.cpp
+++ CompilerSource/workdir.cpp
@@ -15,7 +15,7 @@ std::string myReplace(std::string str, const std::string& oldStr, const std::str
   return nstr;
 }
 
-std::string workdir = myReplace(getenv("APPDATA"), "\\","/") + std::string("/ENIGMA/");
+std::string workdir = myReplace(getenv("APPDATA"), "\\","/") + std::string("/ENIGMA Ass/");
 #else
 std::string workdir = getenv("HOME") + std::string("/.enigma/");
 #endif
