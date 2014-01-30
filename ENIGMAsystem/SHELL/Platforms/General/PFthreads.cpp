78aadeaae258f1a0b4aaf6a5fb237a3d7f934faa
RobertBColton <robertbcolton@hotmail.com>
2014-01-30 04:43:09 -0500

    More to do comments and cleanup.

--- ENIGMAsystem/SHELL/Platforms/General/PFthreads.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/PFthreads.cpp
@@ -44,6 +44,10 @@ int script_thread(int scr,variant arg0, variant arg1, variant arg2, variant arg3
   scrtdata *sd = new scrtdata(scr, args, newthread);
 #if CURRENT_PLATFORM_ID == OS_WINDOWS
   uintptr_t ret = _beginthread((void (*)(void*))thread_script_func, 0, sd);
+  //TODO: May need to check if ret is -1L, and yes it is quite obvious the return value is
+  //an unsigned integer, but Microsoft says to for some reason. See their documentation here.
+  //http://msdn.microsoft.com/en-us/library/kdzttdcb.aspx
+  //NOTE: Same issue is in Universal_Systems/Extensions/Asynchronous/ASYNCdialog.cpp
   if (ret == -1L || ret == NULL) {
 #else
   if (pthread_create(&newthread->me, NULL, thread_script_func, sd)) {
