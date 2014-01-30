c2977b24d82dd909c1e8d5f6649c5df698d130dd
RobertBColton <robertbcolton@hotmail.com>
2014-01-29 20:10:14 -0500

    Implemented Win32 Threads Also cleaned up POSIX threads and moved them to Platforms/General no need for 3 extra copies of the same code.

--- ENIGMAsystem/SHELL/Platforms/General/POSIXthreads.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIXthreads.cpp
@@ -1,4 +1,4 @@
-/** Copyright (C) 2011 Josh Ventura
+/** Copyright (C) 2008-2011 Josh Ventura
 ***
 *** This file is a part of the ENIGMA Development Environment.
 ***
@@ -20,7 +20,7 @@
 #include <pthread.h>
 #include "Universal_System/var4.h"
 #include "Universal_System/resource_data.h"
-#include "XLIBthreads.h"
+#include "PFthreads.h"
 
 using namespace std;
 
@@ -41,7 +41,6 @@ struct scrtdata {
   scrtdata(int s, variant nargs[8], ethread* mythread): scr(s), mt(mythread) { for (int i = 0; i < 8; i++) args[i] = nargs[i]; }
 };
 
-
 static void* thread_script_func(void* data) {
   const scrtdata* const md = (scrtdata*)data;
   md->mt->ret = enigma_user::script_execute(md->scr,md->args[0],md->args[1],md->args[2],md->args[3],md->args[4],md->args[5],md->args[6],md->args[7]);
@@ -49,8 +48,7 @@ static void* thread_script_func(void* data) {
   return NULL;
 }
 
-namespace enigma_user
-{
+namespace enigma_user {
 
 int script_thread(int scr,variant arg0, variant arg1, variant arg2, variant arg3, variant arg4, variant arg5, variant arg6, variant arg7)
 {
@@ -65,12 +63,13 @@ int script_thread(int scr,variant arg0, variant arg1, variant arg2, variant arg3
   threads.push_back(newthread);
   return ret;
 }
+
 bool thread_finished(int thread) {
   return !threads[thread]->active;
 }
+
 variant thread_get_return(int thread) {
   return threads[thread]->ret;
 }
 
 }
-
