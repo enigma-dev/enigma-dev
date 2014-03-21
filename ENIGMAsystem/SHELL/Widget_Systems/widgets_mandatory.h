278649d4ee4875f1d7e363403c066c24e972f7f5
Robert B Colton <robertbcolton@hotmail.com>
2014-03-21 16:21:41 -0500

    Fixes Information Dialog Properly moves it to widgets mandatory where it belongs as Josh wanted, I understand now how that is set up. show_message should also be moved out of the XLIB code as well. Also fixes the "None" widget system and tested on several games and confirmed working.

--- ENIGMAsystem/SHELL/Widget_Systems/widgets_mandatory.h
+++ ENIGMAsystem/SHELL/Widget_Systems/widgets_mandatory.h
@@ -1,34 +1,26 @@
-/********************************************************************************\
-**                                                                              **
-**  Copyright (C) 2008 Josh Ventura                                             **
-**                                                                              **
-**  This file is a part of the ENIGMA Development Environment.                  **
-**                                                                              **
-**                                                                              **
-**  ENIGMA is free software: you can redistribute it and/or modify it under the **
-**  terms of the GNU General Public License as published by the Free Software   **
-**  Foundation, version 3 of the license or any later version.                  **
-**                                                                              **
-**  This application and its source code is distributed AS-IS, WITHOUT ANY      **
-**  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS   **
-**  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more       **
-**  details.                                                                    **
-**                                                                              **
-**  You should have recieved a copy of the GNU General Public License along     **
-**  with this code. If not, see <http://www.gnu.org/licenses/>                  **
-**                                                                              **
-**  ENIGMA is an environment designed to create games and other programs with a **
-**  high-level, fully compilable language. Developers of ENIGMA or anything     **
-**  associated with ENIGMA are in no way responsible for its users or           **
-**  applications created by its users, or damages caused by the environment     **
-**  or programs made in the environment.                                        **
-**                                                                              **
-\********************************************************************************/
+/** Copyright (C) 2008 Josh Ventura
+*** Copyright (C) 2014 Robert B. Colton
+***
+*** This file is a part of the ENIGMA Development Environment.
+***
+*** ENIGMA is free software: you can redistribute it and/or modify it under the
+*** terms of the GNU General Public License as published by the Free Software
+*** Foundation, version 3 of the license or any later version.
+***
+*** This application and its source code is distributed AS-IS, WITHOUT ANY
+*** WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+*** FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+*** details.
+***
+*** You should have received a copy of the GNU General Public License along
+*** with this code. If not, see <http://www.gnu.org/licenses/>
+**/
 
 #ifndef _WIDGETS_MANDATORY__H
 #define _WIDGETS_MANDATORY__H
 
 #include <string>
+using std::string;
 
 namespace enigma
 {
@@ -38,6 +30,14 @@ namespace enigma
 
 // This obviously displays an error message.
 // It should offer a button to end the game, and if not fatal, a button to ignore the error.
-void show_error(std::string msg, const bool fatal); 
+void show_error(std::string msg, const bool fatal);
+
+namespace enigma_user {
+
+void show_info(string caption="Game Information");
+void show_info(string info,string caption="Information");
+static inline void action_show_info() { show_info(); }
+
+}
 
 #endif
