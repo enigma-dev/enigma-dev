41831e45cfcb1d90d68cb5c909ebb561dfaf948f
Robert B Colton <robertbcolton@hotmail.com>
2014-03-21 16:21:41 -0500

    Fixes Information Dialog Properly moves it to widgets mandatory where it belongs as Josh wanted, I understand now how that is set up. show_message should also be moved out of the XLIB code as well. Also fixes the "None" widget system and tested on several games and confirmed working.

--- ENIGMAsystem/SHELL/Widget_Systems/None/nowidget_impl.cpp
+++ ENIGMAsystem/SHELL/Widget_Systems/None/nowidget_impl.cpp
@@ -1,32 +1,23 @@
-/********************************************************************************\
-**                                                                              **
-**  Copyright (C) 2008 Josh Ventura                                             **
-**                                                                              **
-**  This file is a part of the ENIGMA Development Environment.                  **
-**                                                                              **
-**                                                                              **
-**  ENIGMA is free software: you can redistribute it and/or modify it under the **
-**  terms of the GNU General Public License as published by the Free Software   **
-**  Foundation, version 3 of the license or any later version.                  **
-**                                                                              **
-**  This application and its source code is distributed AS-IS, WITHOUT ANY      **
-**  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS   **
-**  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more       **
-**  details.                                                                    **
-**                                                                              **
-**  You should have recieved a copy of the GNU General Public License along     **
-**  with this code. If not, see <http://www.gnu.org/licenses/>                  **
-**                                                                              **
-**  ENIGMA is an environment designed to create games and other programs with a **
-**  high-level, fully compilable language. Developers of ENIGMA or anything     **
-**  associated with ENIGMA are in no way responsible for its users or           **
-**  applications created by its users, or damages caused by the environment     **
-**  or programs made in the environment.                                        **
-**                                                                              **
-\********************************************************************************/
+/** Copyright (C) 2008 Josh Ventura
+*** Copyright (C) 2014 Robert B. Colton
+***
+*** This file is a part of the ENIGMA Development Environment.
+***
+*** ENIGMA is free software: you can redistribute it and/or modify it under the
+*** terms of the GNU General Public License as published by the Free Software
+*** Foundation, version 3 of the license or any later version.
+***
+*** This application and its source code is distributed AS-IS, WITHOUT ANY
+*** WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+*** FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+*** details.
+***
+*** You should have received a copy of the GNU General Public License along
+*** with this code. If not, see <http://www.gnu.org/licenses/>
+**/
 
 #include <string>
-using namespace std;
+using std::string;
 #include "Widget_Systems/widgets_mandatory.h"
 #include "Universal_System/instance_system.h"
 #include <cstdlib>
@@ -40,6 +31,7 @@ namespace enigma
   {
     return 0;
   }
+  extern string gameInformation;
 }
 
 void show_error(string err, const bool fatal)
@@ -57,3 +49,15 @@ void show_error(string err, const bool fatal)
   ABORT_ON_ALL_ERRORS();
 }
 
+
+namespace enigma_user {
+
+void show_info(string info, string caption) {
+
+}
+
+void show_info(string caption) {
+  show_info(enigma::gameInformation, caption);
+}
+
+}
