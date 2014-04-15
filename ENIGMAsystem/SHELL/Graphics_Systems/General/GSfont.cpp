864da89374bbaab0668cb6f8009ad032666f7c5c
Robert B Colton <robertbcolton@hotmail.com>
2014-04-14 22:05:53 -0400

    Fix for unicode glyphs larger than U+FFFF by JoshDreamland.

--- ENIGMAsystem/SHELL/Graphics_Systems/General/GSfont.cpp
+++ ENIGMAsystem/SHELL/Graphics_Systems/General/GSfont.cpp
@@ -49,19 +49,21 @@ const int fa_bottom = 2;*/
 unsigned halign = enigma_user::fa_left; //default alignment
 unsigned valign = enigma_user::fa_top; //default alignment
 
+const string unicodeAnds = "\x1F\x1F\x1F\x1F\x1F\x1F\x1F\x1F\x1F\x1F\x1F\x1F\x1F\x1F\x1F\x1F\x0F\x0F\x0F\x0F\x0F\x0F\x0F\x0F\x07\x07\x07\x07\x03\x03\x01";
+
 static uint32_t getUnicodeCharacter(const string str, size_t& pos) {
-	uint32_t character = 0;
-	if (str[pos] & 0x80) {
-		character = (((uint32_t)str[pos] & 0x1F));
-		for (size_t ii = 1; ii <= 6; ii++) {
+  uint32_t character = 0;
+  if (str[pos] & 0x80) {
+    character = (str[pos] & unicodeAnds[(str[pos] >> 1) & 0x1F]);
+    for (size_t ii = 1; ii <= 6; ii++) {
       if ((str[pos + ii] & 0xC0) != 0x80) { pos += ii - 1; break; }
       character <<= 6;
       character |= (str[pos + ii] & 0x3F);
-		}
-	} else {
-		character = (uint32_t)str[pos];
-	}
-	return character;
+    }
+  } else {
+    character = (uint32_t)str[pos];
+  }
+  return character;
 }
 
 static fontglyph* findGlyph(const font *const fnt, uint32_t character) {
