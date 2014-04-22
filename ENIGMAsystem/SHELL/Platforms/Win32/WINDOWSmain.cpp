8a62f40efd26696a7ad1184896d131aaf033119b
Robert B Colton <robertbcolton@hotmail.com>
2014-04-21 23:03:11 -0400

    Fixes temp files working directory.

--- ENIGMAsystem/SHELL/Platforms/Win32/WINDOWSmain.cpp
+++ ENIGMAsystem/SHELL/Platforms/Win32/WINDOWSmain.cpp
@@ -193,9 +193,8 @@ int WINAPI WinMain (HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,
     
     if (enigma_user::working_directory.length() == 0) {
       char buffer[MAX_PATH];
-      GetModuleFileName( NULL, buffer, MAX_PATH );
-      string::size_type pos = string( buffer ).find_last_of( "\\/" );
-      enigma_user::working_directory = string( buffer ).substr( 0, pos);
+      GetCurrentDirectory( MAX_PATH, buffer );
+      enigma_user::working_directory = string( buffer );
     }
 
     LPWSTR *argv;
