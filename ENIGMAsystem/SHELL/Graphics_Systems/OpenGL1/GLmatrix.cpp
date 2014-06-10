d3bdf84e44f58efaabb53f3e22e19eed040bdaa0
Darkstar2 <jpsaba@yahoo.com>
2014-06-10 14:24:41 -0400

    Update to GLMatrix.cpp - Round first then offset
    Adds rounding to the projection ortho X and Y before adding the
    +0.01 offsets.  This prevents the fix from being negated by the
    use of moving projections and fractional coordinates.

--- ENIGMAsystem/SHELL/Graphics_Systems/OpenGL1/GLmatrix.cpp
+++ ENIGMAsystem/SHELL/Graphics_Systems/OpenGL1/GLmatrix.cpp
@@ -109,7 +109,8 @@ void d3d_set_projection_ortho(gs_scalar x, gs_scalar y, gs_scalar width, gs_scal
 {
     // This fixes font glyph edge artifacting and vertical scroll gaps
     // seen by mostly NVIDIA GPU users.  Rounds x and y and adds +0.01 offset.
-    // This will prevent the fix from being negated. 
+    // This will prevent the fix from being negated through moving projections
+    // and fractional coordinates.
     x = round(x) + 0.01f; y = round(y) + 0.01f;
     enigma::projection_matrix.InitScaleTransform(1, -1, 1);
     enigma::projection_matrix.rotateZ(angle);
