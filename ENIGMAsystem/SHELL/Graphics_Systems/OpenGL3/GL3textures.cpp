24df444edbe0ed62ebb1bf970f9c3b216dcbb58f
Robert B Colton <robertbcolton@hotmail.com>
2014-07-05 21:07:28 -0400

    Cleanup texture code.

--- ENIGMAsystem/SHELL/Graphics_Systems/OpenGL3/GL3textures.cpp
+++ ENIGMAsystem/SHELL/Graphics_Systems/OpenGL3/GL3textures.cpp
@@ -79,17 +79,17 @@ namespace enigma
     glGenTextures(1, &texture);
     oglmgr->BindTexture(GL_TEXTURE_2D, texture);
     glTexImage2D(GL_TEXTURE_2D, 0, 4, fullwidth, fullheight, 0, GL_BGRA, GL_UNSIGNED_BYTE, pxdata);
-	bool interpolate = (interpolate_textures && !isfont);
+    bool interpolate = (interpolate_textures && !isfont);
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,interpolate?GL_LINEAR:GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,interpolate?GL_LINEAR:GL_NEAREST);
     oglmgr->ResetTextureStates();
 
-	TextureStruct* textureStruct = new TextureStruct(texture);
-	textureStruct->isFont = isfont;
-	textureStruct->width = width;
-	textureStruct->height = height;
-	textureStruct->fullwidth = fullwidth;
-	textureStruct->fullheight = fullheight;
+    TextureStruct* textureStruct = new TextureStruct(texture);
+    textureStruct->isFont = isfont;
+    textureStruct->width = width;
+    textureStruct->height = height;
+    textureStruct->fullwidth = fullwidth;
+    textureStruct->fullheight = fullheight;
     textureStructs.push_back(textureStruct);
     return textureStructs.size()-1;
   }
@@ -98,14 +98,14 @@ namespace enigma
   {
     GLuint texture = textureStructs[tex]->gltex;
     oglmgr->BindTexture(GL_TEXTURE_2D, texture);
-	unsigned w, h, fw, fh;
-	bool interpolate = (interpolate_textures && !textureStructs[tex]->isFont);
+    unsigned w, h, fw, fh;
+    bool interpolate = (interpolate_textures && !textureStructs[tex]->isFont);
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,interpolate?GL_LINEAR:GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,interpolate?GL_LINEAR:GL_NEAREST);
-	w = textureStructs[tex]->width;
-	h = textureStructs[tex]->height;
-	fw = textureStructs[tex]->fullwidth;
-	fh = textureStructs[tex]->fullheight;
+    w = textureStructs[tex]->width;
+    h = textureStructs[tex]->height;
+    fw = textureStructs[tex]->fullwidth;
+    fh = textureStructs[tex]->fullheight;
     char* bitmap = new char[(fh<<(lgpp2(fw)+2))|2];
     glGetTexImage(GL_TEXTURE_2D, 0, GL_BGRA, GL_UNSIGNED_BYTE, bitmap);
     unsigned dup_tex = graphics_create_texture(w, h, fw, fh, bitmap, textureStructs[tex]->isFont);
@@ -122,10 +122,10 @@ namespace enigma
 
     unsigned w, h, fw, fh, size;
     oglmgr->BindTexture(GL_TEXTURE_2D, texture);
-	w = textureStructs[tex]->width;
-	h = textureStructs[tex]->height;
-	fw = textureStructs[tex]->fullwidth;
-	fh = textureStructs[tex]->fullheight;
+    w = textureStructs[tex]->width;
+    h = textureStructs[tex]->height;
+    fw = textureStructs[tex]->fullwidth;
+    fh = textureStructs[tex]->fullheight;
     size = (fh<<(lgpp2(fw)+2))|2;
     char* bitmap = new char[size];
     char* bitmap2 = new char[size];
@@ -138,7 +138,7 @@ namespace enigma
 
     oglmgr->BindTexture(GL_TEXTURE_2D, texture);
     glTexImage2D(GL_TEXTURE_2D, 0, 4, fw, fh, 0, GL_BGRA, GL_UNSIGNED_BYTE, bitmap);
-	bool interpolate = (interpolate_textures && !textureStructs[tex]->isFont);
+    bool interpolate = (interpolate_textures && !textureStructs[tex]->isFont);
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,interpolate?GL_LINEAR:GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,interpolate?GL_LINEAR:GL_NEAREST);
     oglmgr->ResetTextureStates();
@@ -157,13 +157,13 @@ namespace enigma
   unsigned char* graphics_get_texture_pixeldata(unsigned texture, unsigned* fullwidth, unsigned* fullheight)
   {
     //enigma_user::texture_set(texture); Wtf? This don't work anymore? - Robert
-	oglmgr->BindTexture(GL_TEXTURE_2D, textureStructs[texture]->gltex);
+    oglmgr->BindTexture(GL_TEXTURE_2D, textureStructs[texture]->gltex);
 
     *fullwidth = textureStructs[texture]->fullwidth;
-	*fullheight = textureStructs[texture]->fullheight;
+    *fullheight = textureStructs[texture]->fullheight;
 
     unsigned char* ret = new unsigned char[((*fullwidth)*(*fullheight)*4)];
-	glPixelStorei(GL_PACK_ALIGNMENT, 1);
+    glPixelStorei(GL_PACK_ALIGNMENT, 1);
     glGetTexImage(GL_TEXTURE_2D, 0, GL_BGRA, GL_UNSIGNED_BYTE, ret);
 
     return ret;
