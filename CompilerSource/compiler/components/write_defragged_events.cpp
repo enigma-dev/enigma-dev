db96797ddf0682089f6b648621da39ed5213f428
Seth N. Hetu <seth.hetu@gmail.com>
2014-07-15 00:32:45 -0400

    Timelines now generate object-local copies for relevant scripts. A virtual lookup is created per-object (but only for the timelines it uses, if any).

--- CompilerSource/compiler/components/write_defragged_events.cpp
+++ CompilerSource/compiler/components/write_defragged_events.cpp
@@ -87,11 +87,6 @@ int lang_CPP::compile_writeDefraggedEvents(EnigmaStruct* es)
   *****************************************************************/
   wto << "namespace enigma" << endl << "{" << endl;
 
-  //Forward declarations
-  wto <<"void advance_curr_timeline(gs_scalar& timeline_position, gs_scalar timeline_speed, int timeline_index, bool timeline_loop);\n";
-  wto <<"void loop_curr_timeline(gs_scalar& timeline_position, gs_scalar timeline_speed, int timeline_index);\n\n";
-  wto <<"\n";
-
   wto << "  struct event_parent: " << system_get_uppermost_tier() << endl;
   wto << "  {" << endl;
             for (evfit it = used_events.begin(); it != used_events.end(); it++)
