8fabaf5a6ccbd30adb455e0e7af17a1ad7607073
Robert B Colton <robertbcolton@hotmail.com>
2014-08-11 00:08:24 -0400

    Cleanup

--- ENIGMAsystem/SHELL/Platforms/Win32/WINDOWSthreads.cpp
+++ ENIGMAsystem/SHELL/Platforms/Win32/WINDOWSthreads.cpp
@@ -27,13 +27,23 @@ static void* thread_script_func(void* data) {
   const scrtdata* const md = (scrtdata*)data;
   md->mt->ret = enigma_user::script_execute(md->scr,md->args[0],md->args[1],md->args[2],md->args[3],md->args[4],md->args[5],md->args[6],md->args[7]);
   md->mt->active = false;
+  CloseHandle(md->mt->handle);
   return NULL;
 }
 
 namespace enigma_user
 {
 
-int script_thread(int scr,variant arg0, variant arg1, variant arg2, variant arg3, variant arg4, variant arg5, variant arg6, variant arg7)
+int script_thread(int scr,variant arg0, variant arg1, variant arg2, variant arg3, variant arg4, variant arg5, variant arg6, variant arg7) {
+  int thread = thread_create_script(scr,arg0,arg1,arg2,arg3,arg4,arg5,arg6,arg7);
+  int res = thread_start(thread);
+  if (res != 0) {
+    return res;
+  }
+  return thread;
+}
+
+int thread_create_script(int scr,variant arg0, variant arg1, variant arg2, variant arg3, variant arg4, variant arg5, variant arg6, variant arg7)
 {
   ethread* newthread = new ethread();
   variant args[] = {arg0,arg1,arg2,arg3,arg4,arg5,arg6,arg7};
@@ -43,6 +53,8 @@ int script_thread(int scr,variant arg0, variant arg1, variant arg2, variant arg3
 }
 
 int thread_start(int thread) {
+  if (threads[thread]->active) { return -1; }
+  
   DWORD dwThreadId;
   threads[thread]->handle = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)&thread_script_func, (LPVOID)threads[thread]->sd, 0, &dwThreadId);
   //TODO: May need to check if ret is -1L, and yes it is quite obvious the return value is
@@ -50,8 +62,9 @@ int thread_start(int thread) {
   //http://msdn.microsoft.com/en-us/library/kdzttdcb.aspx
   //NOTE: Same issue is in Universal_Systems/Extensions/Asynchronous/ASYNCdialog.cpp
   if (threads[thread]->handle == NULL) {
-    return -1;
+    return -2;
   }
+  threads[thread]->active = true;
   return 0;
 }
 
@@ -70,7 +83,12 @@ void thread_join(int thread) {
 }
 
 void thread_delete(int thread) {
+  if (threads[thread]->active) { return; }
   delete threads[thread];
+}
+
+bool thread_exists(int thread) {
+  return threads[thread] != NULL;
 }
 
 bool thread_get_finished(int thread) {
