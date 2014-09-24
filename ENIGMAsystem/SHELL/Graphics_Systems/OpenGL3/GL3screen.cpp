351a815690829c9dbcefadb6afdcf94b0d8f0ead
Robert B Colton <robertbcolton@hotmail.com>
2014-09-24 00:15:57 -0400

    Patch OpenGL Views They are bottom left not top right like Direct3D, I forgot.

--- ENIGMAsystem/SHELL/Graphics_Systems/OpenGL3/GL3screen.cpp
+++ ENIGMAsystem/SHELL/Graphics_Systems/OpenGL3/GL3screen.cpp
@@ -473,8 +473,9 @@ void screen_set_viewport(gs_scalar x, gs_scalar y, gs_scalar width, gs_scalar he
   gs_scalar sx, sy;
   sx = (window_get_width() - window_get_region_width_scaled()) / 2;
   sy = (window_get_height() - window_get_region_height_scaled()) / 2;
-	glViewport(sx + x, sy + y, width, height);
-  glScissor(sx + x, sy + y, width, height);
+  //NOTE: OpenGL viewports are bottom left unlike Direct3D viewports which are top left
+	glViewport(sx + x, window_get_height() - (sy + y) - height, width, height);
+  glScissor(sx + x, window_get_height() - (sy + y) - height, width, height);
 }
 
 void display_set_gui_size(unsigned width, unsigned height) {
