02942aa72d585cf935fa2e6f313564518ebb6bd6
Seth N. Hetu <seth.hetu@gmail.com>
2014-11-29 01:41:25 -0500

    Fix for myevent_pathend() not firing.

--- ENIGMAsystem/SHELL/Universal_System/Extensions/Paths/implement.h
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/Paths/implement.h
@@ -17,5 +17,7 @@ namespace enigma {
     cs_scalar path_ystart;
     
     extension_path(): path_index(-1), path_endaction(0), path_orientation(0), path_position(0), path_positionprevious(0), path_scale(1), path_speed(0) {}
+
+    virtual variant myevent_pathend() { return 1; }
   };
 }
