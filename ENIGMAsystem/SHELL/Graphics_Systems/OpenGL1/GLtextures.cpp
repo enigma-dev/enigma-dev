78678b8b44e2bd4c898f9093ba78eeb5050cc59b
Seth N. Hetu <seth.hetu@gmail.com>
2014-12-14 01:06:24 -0500

    First try at a GIF loader.

--- ENIGMAsystem/SHELL/Graphics_Systems/OpenGL1/GLtextures.cpp
+++ ENIGMAsystem/SHELL/Graphics_Systems/OpenGL1/GLtextures.cpp
@@ -172,8 +172,9 @@ namespace enigma_user
 
 int texture_add(string filename, bool mipmap) {
   unsigned int w, h, fullwidth, fullheight;
+  int img_num;
 
-  unsigned char *pxdata = enigma::image_load(filename,&w,&h,&fullwidth,&fullheight,false);
+  unsigned char *pxdata = enigma::image_load(filename,&w,&h,&fullwidth,&fullheight,&img_num,false);
   if (pxdata == NULL) { printf("ERROR - Failed to append sprite to index!\n"); return -1; }
   unsigned texture = enigma::graphics_create_texture(w, h, fullwidth, fullheight, pxdata, mipmap);
   delete[] pxdata;
