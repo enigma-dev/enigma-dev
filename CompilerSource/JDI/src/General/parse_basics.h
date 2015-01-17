9cf80175821476e26d679d95a861906fecac3675
Robert B Colton <robertbcolton@gmail.com>
2015-01-17 11:43:52 -0500

    More fixes for Mingw64

--- CompilerSource/JDI/src/General/parse_basics.h
+++ CompilerSource/JDI/src/General/parse_basics.h
@@ -89,6 +89,7 @@ namespace parse_bacics {
 
     using std::string;
     string toString(int);
+    string toString(size_t);
     string toString(pt);
     string toString(long);
     string toString(short);
