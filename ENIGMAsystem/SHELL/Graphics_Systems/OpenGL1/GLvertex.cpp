5ed176eafe0b7446af35a48e1cbdc9dcddd3a0a9
TheExDeus <grinbergs.harijs@gmail.com>
2015-10-03 00:20:47 +0300

    Added custom attributes and new functions
    -Basically implemented vertex_buffer functions, but inside d3d_model,
    because I don't want code duplication and it didn't make sense to make
    them.
    -Added d3d_model_vertex_float, d3d_model_vertex_float2,
    d3d_model_vertex_float3, d3d_model_vertex_float4 and
    d3d_model_vertex_ubyte4 for custom attributes.
    -Added vertex_format_ functions, but they are totally different from
    GM:S. Added vertex_format_create, vertex_format_destroy,
    vertex_format_exists, vertex_format_add. Removed the weird "_end returns
    the ID" thing because it is totally different from how the other systems
    work. So now we use ID's instead (with creation and destruction which
    GM:S doesn't allow).
    -I removed vertex_format_add_color, vertex_format_add_position and so on
    because they don't make sense in a custom shader.
    -vertex_format_add is different than GM:S because the last argument is
    attribute to add. This can be found using glsl_get_attribute_location
    allowing you to use custom attributes (GM:S didn't actually allow that
    as far as I know).
    -Fixed tab formatting in GL3model.cpp.
    -Added glsl_program_get() which returns the currently bound shader. Most
    useful to actually get the default shader.
    -Added glsl_attribute_enable_all() which is used in the modelstruct and
    not much useful anywhere else.

--- ENIGMAsystem/SHELL/Graphics_Systems/OpenGL1/GLvertex.cpp
+++ ENIGMAsystem/SHELL/Graphics_Systems/OpenGL1/GLvertex.cpp
@@ -16,164 +16,23 @@
 **/
 
 #include "../General/OpenGLHeaders.h"
-#include <vector>
-#include <map>
-using std::vector;
-using std::map;
-
 #include "../General/GSvertex.h"
 
-namespace enigma {
-
-struct VertexFormat {
-  map<int,int> flags;
-  
-  VertexFormat() {
-  
-  }
-  
-  ~VertexFormat() {
-  
-  }
-  
-  void AddAttribute(int type, int usage) {
-    flags.insert(map<int,int>::value_type(type, usage));
-  }
-};
-
-struct VertexBuffer {
-  vector<gs_scalar> vertices;
-  vector<gs_scalar> indices;
-  
-  GLuint vertexBuffer;
-  GLuint indexBuffer;
-};
-
-vector<VertexFormat*> vertexFormats;
-vector<VertexBuffer*> vertexBuffers;
-
-VertexFormat* vertexFormat = 0;
-
-}
-
+//GL1 cannot have this stuff!!! Use GL3! Maybe an error here?
 namespace enigma_user {
 
-int vertex_create_buffer() {
-  return -1;
-}
+  unsigned vertex_format_create(){
+    return 0;
+  }
 
-int vertex_create_buffer_ext(unsigned size) {
-  (void) size;
-  return -1;
-}
+  void vertex_format_destroy(int id){
+  }
 
-void vertex_delete_buffer(int buffer) {
+  bool vertex_format_exists(int id){
+	  return false;
+  }
 
-}
-
-void vertex_begin(int buffer, int format) {
-
-}
-
-void vertex_end(int buffer) {
-
-}
-
-void vertex_freeze(int buffer) {
-
-}
-
-void vertex_submit(int buffer, int primitive) {
-
-}
-
-void vertex_submit(int buffer, int primitive, int texture) {
-
-}
-
-void vertex_delete(int buffer) {
-
-}
-
-void vertex_index(int buffer, unsigned id) {
-
-}
-
-void vertex_position(int buffer, gs_scalar x, gs_scalar y) {
-
-}
-
-void vertex_position_3d(int buffer, gs_scalar x, gs_scalar y, gs_scalar z) {
-
-}
-
-void vertex_normal(int buffer, gs_scalar nx, gs_scalar ny, gs_scalar nz) {
-
-}
-
-void vertex_texcoord(int buffer, gs_scalar u, gs_scalar v) {
-
-}
-
-void vertex_argb(int buffer, double alpha, unsigned char r, unsigned char g, unsigned char b) {
-
-}
-
-void vertex_colour(int buffer, int color, double alpha) {
-
-}
-
-void vertex_float1(int buffer, float f1) {
-
-}
-
-void vertex_float2(int buffer, float f1, float f2) {
-
-}
-
-void vertex_float3(int buffer, float f1, float f2, float f3) {
-
-}
-
-void vertex_float4(int buffer, float f1, float f2, float f3, float f4) {
-
-}
-
-void vertex_ubyte4(int buffer, unsigned char u1, unsigned char u2, unsigned char u3, unsigned char u4) {
-
-}
-
-void vertex_format_begin() {
-  enigma::vertexFormat = new enigma::VertexFormat();
-}
-
-void vertex_format_add_colour() {
-  enigma::vertexFormat->AddAttribute(vertex_type_colour, vertex_usage_colour);
-}
-
-void vertex_format_add_position() {
-  enigma::vertexFormat->AddAttribute(vertex_type_float2, vertex_usage_position);
-}
-
-void vertex_format_add_position_3d() {
-  enigma::vertexFormat->AddAttribute(vertex_type_float3, vertex_usage_position);
-} 
-
-void vertex_format_add_textcoord() {
-  enigma::vertexFormat->AddAttribute(vertex_type_float2, vertex_usage_textcoord);
-}
-
-void vertex_format_add_normal() {
-  enigma::vertexFormat->AddAttribute(vertex_type_float3, vertex_usage_normal);
-}
-
-void vertex_format_add_custom(int type, int usage) {
-  enigma::vertexFormat->AddAttribute(type, usage);
-}
-
-int vertex_format_end() {
-  enigma::vertexFormats.push_back(enigma::vertexFormat);
-  return enigma::vertexFormats.size() - 1;
-}
+  void vertex_format_add(int id, int type, int attribute) {
+  }
 
 }
