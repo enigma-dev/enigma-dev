0ca0be9659c12976014e4396e09d741ff763313c
faissaloo <faissaloo@gmail.com>
2016-05-31 20:29:05 +0100

    Removed incorrect blending effect

--- ENIGMAsystem/SHELL/Graphics_Systems/General/GSbackground.cpp
+++ ENIGMAsystem/SHELL/Graphics_Systems/General/GSbackground.cpp
@@ -73,7 +73,7 @@ namespace enigma_user
 
 void draw_background(int back, gs_scalar x, gs_scalar y, int color, gs_scalar alpha)
 {
-  alpha=bind_alpha(alpha)*draw_get_alpha();
+  alpha=bind_alpha(alpha);
 	get_background(bck2d,back);
 
 	const gs_scalar tbx=bck2d->texbordx,tby=bck2d->texbordy;
@@ -87,7 +87,7 @@ void draw_background(int back, gs_scalar x, gs_scalar y, int color, gs_scalar al
 
 void draw_background_stretched(int back, gs_scalar x, gs_scalar y, gs_scalar width, gs_scalar height, int color, gs_scalar alpha)
 {
-  alpha=bind_alpha(alpha)*draw_get_alpha();
+  alpha=bind_alpha(alpha);
 	get_background(bck2d, back);
 
 	const gs_scalar tbx=bck2d->texbordx,tby=bck2d->texbordy;
@@ -101,7 +101,7 @@ void draw_background_stretched(int back, gs_scalar x, gs_scalar y, gs_scalar wid
 
 void draw_background_part(int back, gs_scalar left, gs_scalar top, gs_scalar width, gs_scalar height, gs_scalar x, gs_scalar y, int color, gs_scalar alpha)
 {
-  alpha=bind_alpha(alpha)*draw_get_alpha();
+  alpha=bind_alpha(alpha);
 	get_background(bck2d, back);
 
 	gs_scalar tbw = bck2d->width/(gs_scalar)bck2d->texbordx, tbh = bck2d->height/(gs_scalar)bck2d->texbordy,
@@ -118,7 +118,7 @@ void draw_background_part(int back, gs_scalar left, gs_scalar top, gs_scalar wid
 
 void draw_background_ext(int back, gs_scalar x, gs_scalar y, gs_scalar xscale, gs_scalar yscale, double rot, int color, gs_scalar alpha)
 {
-    alpha=bind_alpha(alpha)*draw_get_alpha();
+    alpha=bind_alpha(alpha);
     get_background(bck2d,back);
 
     rot *= M_PI/180;
@@ -149,7 +149,7 @@ void draw_background_ext(int back, gs_scalar x, gs_scalar y, gs_scalar xscale, g
 
 void draw_background_stretched_ext(int back, gs_scalar x, gs_scalar y, gs_scalar width, gs_scalar height, int color, gs_scalar alpha)
 {
-  alpha=bind_alpha(alpha)*draw_get_alpha();
+  alpha=bind_alpha(alpha);
 	get_background(bck2d,back);
 
 	const gs_scalar tbx=bck2d->texbordx, tby=bck2d->texbordy;
@@ -164,7 +164,7 @@ void draw_background_stretched_ext(int back, gs_scalar x, gs_scalar y, gs_scalar
 
 void draw_background_part_ext(int back, gs_scalar left, gs_scalar top, gs_scalar width, gs_scalar height, gs_scalar x, gs_scalar y, gs_scalar xscale, gs_scalar yscale, int color, gs_scalar alpha)
 {
-  alpha=bind_alpha(alpha)*draw_get_alpha();
+  alpha=bind_alpha(alpha);
 	get_background(bck2d, back);
 
 	gs_scalar tbw = bck2d->width/(gs_scalar)bck2d->texbordx, tbh = bck2d->height/(gs_scalar)bck2d->texbordy,
@@ -183,7 +183,7 @@ void draw_background_part_ext(int back, gs_scalar left, gs_scalar top, gs_scalar
 
 void draw_background_general(int back, gs_scalar left, gs_scalar top, gs_scalar width, gs_scalar height, gs_scalar x, gs_scalar y, gs_scalar xscale, gs_scalar yscale, double rot, int c1, int c2, int c3, int c4, gs_scalar alpha)
 {
-  alpha=bind_alpha(alpha)*draw_get_alpha();
+  alpha=bind_alpha(alpha);
 	get_background(bck2d, back);
     const gs_scalar
       tbx = bck2d->texbordx,  tby = bck2d->texbordy,
@@ -210,7 +210,7 @@ void draw_background_general(int back, gs_scalar left, gs_scalar top, gs_scalar 
 
 void draw_background_tiled(int back, gs_scalar x, gs_scalar y, int color, gs_scalar alpha)
 {
-    alpha=bind_alpha(alpha)*draw_get_alpha();
+    alpha=bind_alpha(alpha);
     get_background(bck2d,back);
     x = (x<0?0:bck2d->width)-fmod(x,bck2d->width);
     y = (y<0?0:bck2d->height)-fmod(y,bck2d->height);
@@ -243,7 +243,7 @@ void draw_background_tiled(int back, gs_scalar x, gs_scalar y, int color, gs_sca
 
 void draw_background_tiled_ext(int back, gs_scalar x, gs_scalar y, gs_scalar xscale, gs_scalar yscale, int color, gs_scalar alpha, bool htiled, bool vtiled)
 {
-  alpha=bind_alpha(alpha)*draw_get_alpha();
+  alpha=bind_alpha(alpha);
 	get_background(bck2d,back);
 
 	const gs_scalar
@@ -286,7 +286,7 @@ void draw_background_tiled_ext(int back, gs_scalar x, gs_scalar y, gs_scalar xsc
 
 void draw_background_tiled_area(int back, gs_scalar x, gs_scalar y, gs_scalar x1, gs_scalar y1, gs_scalar x2, gs_scalar y2, int color, gs_scalar alpha)
 {
-  alpha=bind_alpha(alpha)*draw_get_alpha();
+  alpha=bind_alpha(alpha);
 	get_background(bck2d,back);
 
     const gs_scalar tbx=bck2d->texbordx,tby=bck2d->texbordy;
@@ -329,7 +329,7 @@ void draw_background_tiled_area(int back, gs_scalar x, gs_scalar y, gs_scalar x1
 
 void draw_background_tiled_area_ext(int back, gs_scalar x, gs_scalar y, gs_scalar x1, gs_scalar y1, gs_scalar x2, gs_scalar y2, gs_scalar xscale, gs_scalar yscale, int color, gs_scalar alpha)
 {
-  alpha=bind_alpha(alpha)*draw_get_alpha();
+  alpha=bind_alpha(alpha);
 	get_background(bck2d,back);
 
     const gs_scalar tbx=bck2d->texbordx,tby=bck2d->texbordy;
