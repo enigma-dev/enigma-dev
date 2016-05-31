92d5199a83b1d9279e48b101518986c9aaca6a1c
faissaloo <faissaloo@gmail.com>
2016-05-31 21:17:12 +0100

    Made defaults use c_white rather than 16777215 for clarity

--- ENIGMAsystem/SHELL/Graphics_Systems/General/GSsprite.h
+++ ENIGMAsystem/SHELL/Graphics_Systems/General/GSsprite.h
@@ -20,22 +20,23 @@
 
 #include "Universal_System/scalar.h"
 #include "Universal_System/spritestruct.h"
+#include "GScolors.h"
 
 namespace enigma_user
 {
 
-void draw_sprite(int spr, int subimg, gs_scalar x, gs_scalar y, int color = 16777215, gs_scalar alpha = 1);
-void draw_sprite_stretched(int spr, int subimg, gs_scalar x, gs_scalar y, gs_scalar wid, gs_scalar hei, int color = 16777215, gs_scalar alpha = 1);
+void draw_sprite(int spr, int subimg, gs_scalar x, gs_scalar y, int color = c_white, gs_scalar alpha = 1);
+void draw_sprite_stretched(int spr, int subimg, gs_scalar x, gs_scalar y, gs_scalar wid, gs_scalar hei, int color = c_white, gs_scalar alpha = 1);
 void draw_sprite_pos(int spr, int subimg, gs_scalar x1, gs_scalar y1, gs_scalar x2, gs_scalar y2, gs_scalar x3, gs_scalar y3, gs_scalar x4, gs_scalar y4, gs_scalar alpha = 1);
-void draw_sprite_part(int spr, int subimg, gs_scalar left, gs_scalar top, gs_scalar wid, gs_scalar hei, gs_scalar x, gs_scalar y, int color = 16777215, gs_scalar alpha = 1);
-void draw_sprite_part_offset(int spr, int subimg, gs_scalar left, gs_scalar top, gs_scalar wid, gs_scalar hei, gs_scalar x, gs_scalar y, int color = 16777215, gs_scalar alpha = 1);
+void draw_sprite_part(int spr, int subimg, gs_scalar left, gs_scalar top, gs_scalar wid, gs_scalar hei, gs_scalar x, gs_scalar y, int color = c_white, gs_scalar alpha = 1);
+void draw_sprite_part_offset(int spr, int subimg, gs_scalar left, gs_scalar top, gs_scalar wid, gs_scalar hei, gs_scalar x, gs_scalar y, int color = c_white, gs_scalar alpha = 1);
 void draw_sprite_general(int spr, int subimg, gs_scalar left, gs_scalar top, gs_scalar wid, gs_scalar hei, gs_scalar x, gs_scalar y, gs_scalar xscale, gs_scalar yscale, double rot, int c1, int c2, int c3, int c4, gs_scalar alpha = 1);
-void draw_sprite_tiled(int spr, int subimg, gs_scalar x, gs_scalar y, int color = 16777215, gs_scalar alpha = 1);
-void draw_sprite_ext(int spr, int subimg, gs_scalar x, gs_scalar y, gs_scalar xscale = 1, gs_scalar yscale = 1, double rot = 0, int color = 16777215, gs_scalar alpha = 1);
-void draw_sprite_stretched_ext(int spr, int subimg, gs_scalar x, gs_scalar y, gs_scalar wid, gs_scalar hei, int blend = 16777215, gs_scalar alpha = 1);
-void draw_sprite_part_ext(int spr, int subimg, gs_scalar left, gs_scalar top, gs_scalar wid, gs_scalar hei, gs_scalar x, gs_scalar y, gs_scalar xscale = 1, gs_scalar yscale = 1, int color = 16777215, gs_scalar alpha = 1);
-void draw_sprite_tiled_ext(int spr, int subimg, gs_scalar x, gs_scalar y, gs_scalar xscale, gs_scalar yscale, int color = 16777215, gs_scalar alpha = 1);
-void draw_sprite_padded(int spr, int subimg, gs_scalar left, gs_scalar top, gs_scalar right, gs_scalar bottom, gs_scalar x1, gs_scalar y1, gs_scalar x2, gs_scalar y2, int color = 16777215, gs_scalar alpha = 1.0);
+void draw_sprite_tiled(int spr, int subimg, gs_scalar x, gs_scalar y, int color = c_white, gs_scalar alpha = 1);
+void draw_sprite_ext(int spr, int subimg, gs_scalar x, gs_scalar y, gs_scalar xscale = 1, gs_scalar yscale = 1, double rot = 0, int color = c_white, gs_scalar alpha = 1);
+void draw_sprite_stretched_ext(int spr, int subimg, gs_scalar x, gs_scalar y, gs_scalar wid, gs_scalar hei, int blend = c_white, gs_scalar alpha = 1);
+void draw_sprite_part_ext(int spr, int subimg, gs_scalar left, gs_scalar top, gs_scalar wid, gs_scalar hei, gs_scalar x, gs_scalar y, gs_scalar xscale = 1, gs_scalar yscale = 1, int color = c_white, gs_scalar alpha = 1);
+void draw_sprite_tiled_ext(int spr, int subimg, gs_scalar x, gs_scalar y, gs_scalar xscale, gs_scalar yscale, int color = c_white, gs_scalar alpha = 1);
+void draw_sprite_padded(int spr, int subimg, gs_scalar left, gs_scalar top, gs_scalar right, gs_scalar bottom, gs_scalar x1, gs_scalar y1, gs_scalar x2, gs_scalar y2, int color = c_white, gs_scalar alpha = 1.0);
 
 void d3d_draw_sprite(int spr, int subimg, gs_scalar x, gs_scalar y, gs_scalar z = 0);
 
