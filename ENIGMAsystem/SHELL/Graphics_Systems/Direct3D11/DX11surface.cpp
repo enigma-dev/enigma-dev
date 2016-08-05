a0dc9c629c82ee8cac7f282d77f8418c04b4defe
Robert B Colton <robertbcolton@gmail.com>
2016-08-05 09:48:52 -0400

    Update DX11surface.cpp

--- ENIGMAsystem/SHELL/Graphics_Systems/Direct3D11/DX11surface.cpp
+++ ENIGMAsystem/SHELL/Graphics_Systems/Direct3D11/DX11surface.cpp
@@ -45,7 +45,7 @@ extern int room_width, room_height/*, sprite_idmax*/;
 namespace enigma
 {
   vector<Surface*> Surfaces(0);
-  
+
 }
 
 
@@ -54,77 +54,77 @@ namespace enigma_user
 
 bool surface_is_supported()
 {
-	//TODO: Implement with IDirect3D9::CheckDeviceFormat
-	return true;
+  //TODO: Implement with IDirect3D9::CheckDeviceFormat
+  return true;
 }
 
-int surface_create(int width, int height)
+int surface_create(int width, int height, bool depthbuffer)
 {
   ID3D11Texture2D *renderTargetTexture;
   ID3D11RenderTargetView* renderTargetView;
   ID3D11ShaderResourceView* shaderResourceView;
-  
-	D3D11_TEXTURE2D_DESC textureDesc;
-	HRESULT result;
-	D3D11_RENDER_TARGET_VIEW_DESC renderTargetViewDesc;
-	D3D11_SHADER_RESOURCE_VIEW_DESC shaderResourceViewDesc;
+
+  D3D11_TEXTURE2D_DESC textureDesc;
+  HRESULT result;
+  D3D11_RENDER_TARGET_VIEW_DESC renderTargetViewDesc;
+  D3D11_SHADER_RESOURCE_VIEW_DESC shaderResourceViewDesc;
 
 
-	// Initialize the render target texture description.
-	ZeroMemory(&textureDesc, sizeof(textureDesc));
+  // Initialize the render target texture description.
+  ZeroMemory(&textureDesc, sizeof(textureDesc));
 
-	// Setup the render target texture description.
-	textureDesc.Width = width;
-	textureDesc.Height = height;
-	textureDesc.MipLevels = 1;
-	textureDesc.ArraySize = 1;
-	textureDesc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
-	textureDesc.SampleDesc.Count = 1;
-	textureDesc.Usage = D3D11_USAGE_DEFAULT;
-	textureDesc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
-	textureDesc.CPUAccessFlags = 0;
-	textureDesc.MiscFlags = 0;
+  // Setup the render target texture description.
+  textureDesc.Width = width;
+  textureDesc.Height = height;
+  textureDesc.MipLevels = 1;
+  textureDesc.ArraySize = 1;
+  textureDesc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+  textureDesc.SampleDesc.Count = 1;
+  textureDesc.Usage = D3D11_USAGE_DEFAULT;
+  textureDesc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
+  textureDesc.CPUAccessFlags = 0;
+  textureDesc.MiscFlags = 0;
 
-	// Create the render target texture.
-	result = m_device->CreateTexture2D(&textureDesc, NULL, &renderTargetTexture);
-	if (FAILED(result))
-	{
-		return false;
-	}
+  // Create the render target texture.
+  result = m_device->CreateTexture2D(&textureDesc, NULL, &renderTargetTexture);
+  if (FAILED(result))
+  {
+    return false;
+  }
 
-	// Setup the description of the render target view.
-	renderTargetViewDesc.Format = textureDesc.Format;
-	renderTargetViewDesc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
-	renderTargetViewDesc.Texture2D.MipSlice = 0;
+  // Setup the description of the render target view.
+  renderTargetViewDesc.Format = textureDesc.Format;
+  renderTargetViewDesc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
+  renderTargetViewDesc.Texture2D.MipSlice = 0;
 
-	// Create the render target view.
-	result = m_device->CreateRenderTargetView(renderTargetTexture, &renderTargetViewDesc, &renderTargetView);
-	if(FAILED(result))
-	{
-		return false;
-	}
+  // Create the render target view.
+  result = m_device->CreateRenderTargetView(renderTargetTexture, &renderTargetViewDesc, &renderTargetView);
+  if(FAILED(result))
+  {
+    return false;
+  }
 
-	// Setup the description of the shader resource view.
-	shaderResourceViewDesc.Format = textureDesc.Format;
-	shaderResourceViewDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
-	shaderResourceViewDesc.Texture2D.MostDetailedMip = 0;
-	shaderResourceViewDesc.Texture2D.MipLevels = 1;
+  // Setup the description of the shader resource view.
+  shaderResourceViewDesc.Format = textureDesc.Format;
+  shaderResourceViewDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+  shaderResourceViewDesc.Texture2D.MostDetailedMip = 0;
+  shaderResourceViewDesc.Texture2D.MipLevels = 1;
 
-	// Create the shader resource view.
-	result = m_device->CreateShaderResourceView(renderTargetTexture, &shaderResourceViewDesc, &shaderResourceView);
-	if(FAILED(result))
-	{
-		return false;
-	}
+  // Create the shader resource view.
+  result = m_device->CreateShaderResourceView(renderTargetTexture, &shaderResourceViewDesc, &shaderResourceView);
+  if(FAILED(result))
+  {
+    return false;
+  }
 
-	enigma::Surface* surface = new enigma::Surface();	 
-	TextureStruct* gmTexture = new TextureStruct(renderTargetTexture);
+  enigma::Surface* surface = new enigma::Surface();
+  TextureStruct* gmTexture = new TextureStruct(renderTargetTexture);
   textureStructs.push_back(gmTexture);
   surface->renderTargetView = renderTargetView;
   surface->tex = textureStructs.size() - 1;
-	surface->width = width; surface->height = height;
-	enigma::Surfaces.push_back(surface);
-	return enigma::Surfaces.size() - 1;
+  surface->width = width; surface->height = height;
+  enigma::Surfaces.push_back(surface);
+  return enigma::Surfaces.size() - 1;
 }
 
 int surface_create_msaa(int width, int height, int levels)
@@ -150,8 +150,8 @@ int surface_get_target()
 
 void surface_free(int id)
 {
-	get_surface(surf, id);
-	delete surf;
+  get_surface(surf, id);
+  delete surf;
 }
 
 bool surface_exists(int id)
@@ -161,8 +161,8 @@ bool surface_exists(int id)
 
 int surface_get_texture(int id)
 {
-	get_surfacev(surf,id,-1);
-	return (surf->tex);
+  get_surfacev(surf,id,-1);
+  return (surf->tex);
 }
 
 int surface_get_width(int id)
@@ -227,7 +227,7 @@ int sprite_create_from_surface(int id, int x, int y, int w, int h, bool removeba
 
 int sprite_create_from_surface(int id, int x, int y, int w, int h, bool removeback, bool smooth, int xorig, int yorig)
 {
-	return sprite_create_from_surface(id, x, y, w, h, removeback, smooth, true, xorig, yorig);
+  return sprite_create_from_surface(id, x, y, w, h, removeback, smooth, true, xorig, yorig);
 }
 
 void sprite_add_from_surface(int ind, int id, int x, int y, int w, int h, bool removeback, bool smooth)
