ebdac075e00f1a4c67424c2478d3273fb08eaeb4
Josh Ventura <joshv10@gmail.com>
2016-08-13 15:56:28 -0400

    Make iterator members private; clean up implementation a bit

--- ENIGMAsystem/SHELL/Universal_System/instance_iterator.h
+++ ENIGMAsystem/SHELL/Universal_System/instance_iterator.h
@@ -23,13 +23,19 @@
 #include "instance_system_base.h"
 
 namespace enigma {
-  struct iterator {
+  class iterator {
     inst_iter temp_iter;
     struct inst_iter* it;
-    
+
+    void addme();
+    void copy(const iterator& other);
+
+   public:
     operator bool();
-    object_basic* operator*();
-    object_basic* operator->();
+    object_basic* operator*() const;
+    object_basic* operator->() const;
+    
+    void handle_unlink(const inst_iter* dead);
     
     iterator &operator++();
     iterator operator++(int);
@@ -46,10 +52,6 @@ namespace enigma {
     iterator();
     
     ~iterator();
-    
-    private: 
-      void addme();
-      static inst_iter* copy(const iterator& other, inst_iter& temp_iter);
   };
   
   void update_iterators_for_destroy(const inst_iter*);
