843eec934dbcdffdb24400b59456f0161d4f3f68
RobertBColton <robertbcolton@gmail.com>
2016-09-08 16:37:30 -0400

    More whitespace

--- ENIGMAsystem/SHELL/Universal_System/estring.cpp
+++ ENIGMAsystem/SHELL/Universal_System/estring.cpp
@@ -49,7 +49,7 @@ bool is_base64(unsigned char c) {
 }
 
 string base64_encode(string const& str) {
-  unsigned in_len = str.size();
+  size_t in_len = str.size();
   string ret;
   int i = 0;
   int j = 0;
@@ -65,15 +65,15 @@ string base64_encode(string const& str) {
       char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
       char_array_4[3] = char_array_3[2] & 0x3f;
 
-      for(i = 0; (i <4) ; i++)
+      for (i = 0; (i <4); i++)
         ret += base64_chars[char_array_4[i]];
+
       i = 0;
     }
   }
 
-  if (i)
-  {
-    for(j = i; j < 3; j++)
+  if (i) {
+    for (j = i; j < 3; j++)
       char_array_3[j] = '\0';
 
     char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
@@ -84,9 +84,8 @@ string base64_encode(string const& str) {
     for (j = 0; (j < i + 1); j++)
       ret += base64_chars[char_array_4[j]];
 
-    while((i++ < 3))
+    while (i++ < 3)
       ret += '=';
-
   }
 
   return ret;
@@ -94,17 +93,17 @@ string base64_encode(string const& str) {
 }
 
 string base64_decode(string const& str) {
-  unsigned in_len = str.size();
+  size_t in_len = str.size();
   int i = 0;
   int j = 0;
   int in_ = 0;
   unsigned char char_array_4[4], char_array_3[3];
   string ret;
 
-  while (in_len-- && ( str[in_] != '=') && is_base64(str[in_])) {
+  while (in_len-- && (str[in_] != '=') && is_base64(str[in_])) {
     char_array_4[i++] = str[in_]; in_++;
     if (i == 4) {
-      for (i = 0; i <4; i++)
+      for (i = 0; i < 4; i++)
         char_array_4[i] = base64_chars.find(char_array_4[i]);
 
       char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
@@ -113,15 +112,16 @@ string base64_decode(string const& str) {
 
       for (i = 0; (i < 3); i++)
         ret += char_array_3[i];
+
       i = 0;
     }
   }
 
   if (i) {
-    for (j = i; j <4; j++)
+    for (j = i; j < 4; j++)
       char_array_4[j] = 0;
 
-    for (j = 0; j <4; j++)
+    for (j = 0; j < 4; j++)
       char_array_4[j] = base64_chars.find(char_array_4[j]);
 
     char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
