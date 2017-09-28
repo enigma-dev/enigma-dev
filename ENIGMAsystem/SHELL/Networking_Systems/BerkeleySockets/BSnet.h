c604ef97bf51c1f74be665766d06b06e3e38ab32
RobertBColton <robertbcolton@gmail.com>
2017-09-28 19:28:35 -0400

    fix recvfrom call in berkeley sockets

--- ENIGMAsystem/SHELL/Networking_Systems/BerkeleySockets/BSnet.h
+++ ENIGMAsystem/SHELL/Networking_Systems/BerkeleySockets/BSnet.h
@@ -28,7 +28,9 @@
  #define _WIN32_WINNT 0x0501
  #include <winsock2.h>
  #include <ws2tcpip.h>
+ typedef int socketlen_t;
 #else
+ #include <netdb.h>
  #include <sys/types.h>
  #include <sys/socket.h>
  #include <fcntl.h>
