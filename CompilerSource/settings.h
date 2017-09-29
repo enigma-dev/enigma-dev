b304d7a93a8f9c78dcc036639bc83929cea4c74b
RobertBColton <robertbcolton@gmail.com>
2017-09-29 01:52:57 -0400

    use something a little more descriptive than 'target' like 'compiler'

--- CompilerSource/settings.h
+++ CompilerSource/settings.h
@@ -43,7 +43,7 @@ namespace extensions
       windowLinks, graphicsLinks, audioLinks, collisionLinks, widgetLinks, networkLinks;
   };
   struct compiler_descriptor {
-    string target, identifier, resfile, buildext, buildname, runprog, runparam, builddir;
+    string compiler, identifier, resfile, buildext, buildname, runprog, runparam, builddir;
   };
 
   extern sdk_descriptor targetSDK;
