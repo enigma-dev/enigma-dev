b38e2410a686f57002103f94b1670dbc6294a2eb
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2017-12-03 13:25:01 -0500

    Extract path editor metadata

--- CompilerSource/ipc/codegen/Path_generated.h
+++ CompilerSource/ipc/codegen/Path_generated.h
@@ -8,6 +8,8 @@
 
 struct PathPoint;
 
+struct PathEditorMetadata;
+
 struct Path;
 
 MANUALLY_ALIGNED_STRUCT(4) PathPoint FLATBUFFERS_FINAL_CLASS {
@@ -37,22 +39,84 @@ MANUALLY_ALIGNED_STRUCT(4) PathPoint FLATBUFFERS_FINAL_CLASS {
 };
 STRUCT_END(PathPoint, 12);
 
+struct PathEditorMetadata FLATBUFFERS_FINAL_CLASS : private flatbuffers::Table {
+  enum {
+    VT_BACKGROUND_ROOM_ID = 4,
+    VT_SNAP_X = 6,
+    VT_SNAP_Y = 8
+  };
+  int32_t background_room_id() const {
+    return GetField<int32_t>(VT_BACKGROUND_ROOM_ID, 0);
+  }
+  int32_t snap_x() const {
+    return GetField<int32_t>(VT_SNAP_X, 0);
+  }
+  int32_t snap_y() const {
+    return GetField<int32_t>(VT_SNAP_Y, 0);
+  }
+  bool Verify(flatbuffers::Verifier &verifier) const {
+    return VerifyTableStart(verifier) &&
+           VerifyField<int32_t>(verifier, VT_BACKGROUND_ROOM_ID) &&
+           VerifyField<int32_t>(verifier, VT_SNAP_X) &&
+           VerifyField<int32_t>(verifier, VT_SNAP_Y) &&
+           verifier.EndTable();
+  }
+};
+
+struct PathEditorMetadataBuilder {
+  flatbuffers::FlatBufferBuilder &fbb_;
+  flatbuffers::uoffset_t start_;
+  void add_background_room_id(int32_t background_room_id) {
+    fbb_.AddElement<int32_t>(PathEditorMetadata::VT_BACKGROUND_ROOM_ID, background_room_id, 0);
+  }
+  void add_snap_x(int32_t snap_x) {
+    fbb_.AddElement<int32_t>(PathEditorMetadata::VT_SNAP_X, snap_x, 0);
+  }
+  void add_snap_y(int32_t snap_y) {
+    fbb_.AddElement<int32_t>(PathEditorMetadata::VT_SNAP_Y, snap_y, 0);
+  }
+  explicit PathEditorMetadataBuilder(flatbuffers::FlatBufferBuilder &_fbb)
+        : fbb_(_fbb) {
+    start_ = fbb_.StartTable();
+  }
+  PathEditorMetadataBuilder &operator=(const PathEditorMetadataBuilder &);
+  flatbuffers::Offset<PathEditorMetadata> Finish() {
+    const auto end = fbb_.EndTable(start_);
+    auto o = flatbuffers::Offset<PathEditorMetadata>(end);
+    return o;
+  }
+};
+
+inline flatbuffers::Offset<PathEditorMetadata> CreatePathEditorMetadata(
+    flatbuffers::FlatBufferBuilder &_fbb,
+    int32_t background_room_id = 0,
+    int32_t snap_x = 0,
+    int32_t snap_y = 0) {
+  PathEditorMetadataBuilder builder_(_fbb);
+  builder_.add_snap_y(snap_y);
+  builder_.add_snap_x(snap_x);
+  builder_.add_background_room_id(background_room_id);
+  return builder_.Finish();
+}
+
 struct Path FLATBUFFERS_FINAL_CLASS : private flatbuffers::Table {
   enum {
     VT_NAME = 4,
     VT_ID = 6,
-    VT_SMOOTH = 8,
-    VT_CLOSED = 10,
-    VT_PRECISION = 12,
-    VT_SNAP_X = 14,
-    VT_SNAP_Y = 16,
-    VT_POINTS = 18
+    VT_EDITOR_METADATA = 8,
+    VT_SMOOTH = 10,
+    VT_CLOSED = 12,
+    VT_PRECISION = 14,
+    VT_POINTS = 16
   };
   const flatbuffers::String *name() const {
     return GetPointer<const flatbuffers::String *>(VT_NAME);
   }
   int32_t id() const {
     return GetField<int32_t>(VT_ID, 0);
+  }
+  const PathEditorMetadata *editor_metadata() const {
+    return GetPointer<const PathEditorMetadata *>(VT_EDITOR_METADATA);
   }
   bool smooth() const {
     return GetField<uint8_t>(VT_SMOOTH, 0) != 0;
@@ -63,12 +127,6 @@ struct Path FLATBUFFERS_FINAL_CLASS : private flatbuffers::Table {
   int32_t precision() const {
     return GetField<int32_t>(VT_PRECISION, 0);
   }
-  int32_t snap_x() const {
-    return GetField<int32_t>(VT_SNAP_X, 0);
-  }
-  int32_t snap_y() const {
-    return GetField<int32_t>(VT_SNAP_Y, 0);
-  }
   const flatbuffers::Vector<const PathPoint *> *points() const {
     return GetPointer<const flatbuffers::Vector<const PathPoint *> *>(VT_POINTS);
   }
@@ -77,11 +135,11 @@ struct Path FLATBUFFERS_FINAL_CLASS : private flatbuffers::Table {
            VerifyOffset(verifier, VT_NAME) &&
            verifier.Verify(name()) &&
            VerifyField<int32_t>(verifier, VT_ID) &&
+           VerifyOffset(verifier, VT_EDITOR_METADATA) &&
+           verifier.VerifyTable(editor_metadata()) &&
            VerifyField<uint8_t>(verifier, VT_SMOOTH) &&
            VerifyField<uint8_t>(verifier, VT_CLOSED) &&
            VerifyField<int32_t>(verifier, VT_PRECISION) &&
-           VerifyField<int32_t>(verifier, VT_SNAP_X) &&
-           VerifyField<int32_t>(verifier, VT_SNAP_Y) &&
            VerifyOffset(verifier, VT_POINTS) &&
            verifier.Verify(points()) &&
            verifier.EndTable();
@@ -97,6 +155,9 @@ struct PathBuilder {
   void add_id(int32_t id) {
     fbb_.AddElement<int32_t>(Path::VT_ID, id, 0);
   }
+  void add_editor_metadata(flatbuffers::Offset<PathEditorMetadata> editor_metadata) {
+    fbb_.AddOffset(Path::VT_EDITOR_METADATA, editor_metadata);
+  }
   void add_smooth(bool smooth) {
     fbb_.AddElement<uint8_t>(Path::VT_SMOOTH, static_cast<uint8_t>(smooth), 0);
   }
@@ -105,12 +166,6 @@ struct PathBuilder {
   }
   void add_precision(int32_t precision) {
     fbb_.AddElement<int32_t>(Path::VT_PRECISION, precision, 0);
-  }
-  void add_snap_x(int32_t snap_x) {
-    fbb_.AddElement<int32_t>(Path::VT_SNAP_X, snap_x, 0);
-  }
-  void add_snap_y(int32_t snap_y) {
-    fbb_.AddElement<int32_t>(Path::VT_SNAP_Y, snap_y, 0);
   }
   void add_points(flatbuffers::Offset<flatbuffers::Vector<const PathPoint *>> points) {
     fbb_.AddOffset(Path::VT_POINTS, points);
@@ -131,17 +186,15 @@ inline flatbuffers::Offset<Path> CreatePath(
     flatbuffers::FlatBufferBuilder &_fbb,
     flatbuffers::Offset<flatbuffers::String> name = 0,
     int32_t id = 0,
+    flatbuffers::Offset<PathEditorMetadata> editor_metadata = 0,
     bool smooth = false,
     bool closed = false,
     int32_t precision = 0,
-    int32_t snap_x = 0,
-    int32_t snap_y = 0,
     flatbuffers::Offset<flatbuffers::Vector<const PathPoint *>> points = 0) {
   PathBuilder builder_(_fbb);
   builder_.add_points(points);
-  builder_.add_snap_y(snap_y);
-  builder_.add_snap_x(snap_x);
   builder_.add_precision(precision);
+  builder_.add_editor_metadata(editor_metadata);
   builder_.add_id(id);
   builder_.add_name(name);
   builder_.add_closed(closed);
@@ -153,21 +206,19 @@ inline flatbuffers::Offset<Path> CreatePathDirect(
     flatbuffers::FlatBufferBuilder &_fbb,
     const char *name = nullptr,
     int32_t id = 0,
+    flatbuffers::Offset<PathEditorMetadata> editor_metadata = 0,
     bool smooth = false,
     bool closed = false,
     int32_t precision = 0,
-    int32_t snap_x = 0,
-    int32_t snap_y = 0,
     const std::vector<const PathPoint *> *points = nullptr) {
   return CreatePath(
       _fbb,
       name ? _fbb.CreateString(name) : 0,
       id,
+      editor_metadata,
       smooth,
       closed,
       precision,
-      snap_x,
-      snap_y,
       points ? _fbb.CreateVector<const PathPoint *>(*points) : 0);
 }
 
