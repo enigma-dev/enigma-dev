0372e181d4514f2ae52eda9b51651dd6800fab5e
Robert B Colton <robertbcolton@gmail.com>
2017-12-24 19:53:02 -0500

    Change default Windows Make directory for games (#1121)
    Use LOCALAPPDATA instead of PROGRAMDATA.

--- CompilerSource/makedir.cpp
+++ CompilerSource/makedir.cpp
@@ -48,7 +48,9 @@ string escapeEnv(string str, string env) {
 }
 
 string escapeEnv(string str) {
-	string escaped = escapeEnv(str, "PROGRAMDATA");
+	string escaped = escapeEnv(str, "LOCALAPPDATA");
+	escaped = escapeEnv(escaped, "APPDATA");
+	escaped = escapeEnv(escaped, "PROGRAMDATA");
 	escaped = escapeEnv(escaped, "ALLUSERSPROFILE");
 	escaped = escapeEnv(escaped, "HOME");
 	return escaped;
