f89215e15f4ad74d6063278ec3632fa19003b952
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2017-12-28 00:27:00 -0500

    more gmx comments

--- CompilerSource/ipc/gmx.cpp
+++ CompilerSource/ipc/gmx.cpp
@@ -17,14 +17,19 @@ using namespace tinyxml2;
 void load_tree(XMLNode* root, TreeNodeBuilder *rootBuilder) {
   std::vector<flatbuffers::Offset<TreeNode>> children;
 
+  // loop the child elements of the current XML element
   for (const XMLElement* child = root.FirstChildElement();
        child != NULL;
        child = child.NextSiblingElement()) {
+    // grab the name attribute for this child element
     const char* nameAttribute = child->Attribute("name");
 
+    // create a builder for a tree node corresponding to this child
     flatbuffers::FlatBufferBuilder childFBB;
     TreeNodeBuilder childBuilder(childFBB);
 
+    // if this child has the name attribute, then GMX considers it a directory
+    // otherwise, it is a resource
     if (nameAttribute) {
       childuilder.add_file_name(childFBB.CreateString(nameAttribute));
       childuilder.add_is_directory(true);
@@ -34,11 +39,13 @@ void load_tree(XMLNode* root, TreeNodeBuilder *rootBuilder) {
       childuilder.add_is_directory(false);
     }
 
+    // finish the buffer for this child's tree node
     auto childBuffer = childBuilder.Finish();
     FinishTreeNodeBuffer(childFBB, childBuffer);
     children.push_back(childBuffer);
   }
 
+  // finish the children vector for this tree root
   rootBuilder->add_children(rootBuilder->CreateVector(children));
 }
 
