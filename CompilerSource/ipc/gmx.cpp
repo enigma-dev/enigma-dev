e86eaa9477a21998454480c68bacdcd20f14edb6
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2017-12-27 23:49:12 -0500

    gmx tree loading

--- CompilerSource/ipc/gmx.cpp
+++ CompilerSource/ipc/gmx.cpp
@@ -2,37 +2,74 @@
 #include <iostream>
 #include <iomanip>
 
+#include <vector>
+
 #include "flatbuffers/reflection.h"
 #include "flatbuffers/flatbuffers.h"
 #include "flatbuffers/util.h"
 
 #include "codegen/Project_generated.h"
 
-Project *load_gmx(const char *fileName) {
-	XMLDocument doc;
-	doc.LoadFile(filename);
+namespace gmx {
 
-	flatbuffers::FlatBufferBuilder builder;
-	ProjectBuilder projectBuilder(builder);
+using namespace tinyxml2;
 
-	std::string schemaFile;
-	flatbuffers::LoadFile("codegen/Project.bfbs", true, &schemaFile);
-	auto &schema = *reflection::GetSchema(schemaFile.c_str());
-	auto root_table = schema.root_table();
-	std::cout << root_table->name()->c_str() << std::endl;
+void load_tree(XMLNode* root, TreeNodeBuilder *rootBuilder) {
+	std::vector<flatbuffers::Offset<TreeNode>> children;
 
-	auto &fields = *root_table->fields();
-	for (auto field : fields) {
-		// loop fields of root Project table...
+	for (const XMLElement* child = root.FirstChildElement();
+		 child != NULL;
+		 child = child.NextSiblingElement()) {
+		const char* nameAttribute = child->Attribute("name");
+
+		flatbuffers::FlatBufferBuilder childFBB;
+		TreeNodeBuilder childBuilder(childFBB);
+
+		if (nameAttribute) {
+			childuilder.add_file_name(childFBB.CreateString(nameAttribute));
+			childuilder.add_is_directory(true);
+			load_tree(child, childBuilder);
+		} else {
+			childBuilder.add_file_name(childFBB.CreateString(nameAttribute));
+			childuilder.add_is_directory(false);
+		}
+
+		auto childBuffer = childBuilder.Finish();
+		FinishTreeNodeBuffer(childFBB, childBuffer);
+		children.push_back(childBuffer);
 	}
 
-	auto projectbuffer = projectBuilder.Finish();
-	FinishTestBuffer(builder, projectbuffer);
+	rootBuilder->add_children(rootBuilder->CreateVector(children));
+}
 
-	return builder.ReleaseBufferPointer().data();
+Project *load_project(const char *fileName) {
+	XMLDocument doc;
+	doc.LoadFile(fileName);
+
+	flatbuffers::FlatBufferBuilder projectFBB;
+	ProjectBuilder projectBuilder(projectFBB);
+
+	flatbuffers::FlatBufferBuilder rootFBB;
+	TreeNodeBuilder rootBuilder(rootFBB);
+	rootBuilder.add_file_name(rootFBB.CreateString(fileName));
+	rootBuilder.add_is_directory(false);
+
+	load_tree(&doc);
+
+	auto rootBuffer = rootBuilder.Finish();
+	FinishTreeNodeBuffer(rootFBB, rootBuffer);
+
+	projectBuilder.add_tree_root(rootBuffer);
+
+	auto projectBuffer = projectBuilder.Finish();
+	FinishProjectBuffer(projectFBB, projectBuffer);
+
+	return GetProject(projectBuffer);
 }
 
 int main() {
 
 	return 0;
 }
+
+} // end namespace gmx
