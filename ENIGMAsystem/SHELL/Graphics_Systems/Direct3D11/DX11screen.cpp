bcd44c5dff4ac81ed04f44bb0fc55fcbdebf7b33
Greg Williamson <cheeseboy16@gmail.com>
2017-12-31 22:18:02 +0000

    dx11 fix

--- ENIGMAsystem/SHELL/Graphics_Systems/Direct3D11/DX11screen.cpp
+++ ENIGMAsystem/SHELL/Graphics_Systems/Direct3D11/DX11screen.cpp
@@ -30,6 +30,7 @@ using namespace std;
 
 #include "Universal_System/var4.h"
 #include "Universal_System/estring.h"
+#include "Universal_System/background.h"
 #include "Universal_System/background_internal.h"
 
 #define __GETR(x) (((unsigned int)x & 0x0000FF))/255.0
