a8bc8869790286fd3dc7768a036f05762770be27
Josh Ventura <joshv10@gmail.com>
2018-01-08 00:59:23 -0500

    Add simple game file support to CLI. This should enable a test harness to quickly launch and verify simple test cases.

--- CommandLine/emake/Main.cpp
+++ CommandLine/emake/Main.cpp
@@ -2,6 +2,45 @@
 #include "EnigmaPlugin.hpp"
 #include "Game.hpp"
 
+#include <fstream>
+#include <iostream>
+#include <streambuf>
+
+static std::string tolower(const std::string &str) {
+  std::string res = str;
+  for (size_t i = 0; i < res.length(); ++i) {
+    if (res[i] >= 'A' && res[i] <= 'Z') res[i] += 'a' - 'A';
+  }
+  return res;
+}
+
+bool read_3fg(const std::string &input_file, Game *game) {
+  std::string create, step, draw;
+  if (std::ifstream f{input_file + "/create.edl"}) {
+    create = {
+      std::istreambuf_iterator<char>(f), std::istreambuf_iterator<char>()
+    };
+  }
+  if (std::ifstream f{input_file + "/step.edl"}) {
+    step = {
+      std::istreambuf_iterator<char>(f), std::istreambuf_iterator<char>()
+    };
+  }
+  if (std::ifstream f{input_file + "/draw.edl"}) {
+    draw = {
+      std::istreambuf_iterator<char>(f), std::istreambuf_iterator<char>()
+    };
+  }
+  if (create.empty() && step.empty() && draw.empty()) {
+    std::cerr << "Error: Failed to read input \"" << input_file << "\". "
+                 "Is the game empty?" << std::endl;
+    return false;
+  }
+  game->AddSimpleObject(create, step, draw);
+  game->AddDefaultRoom();
+  return true;
+}
+
 int main(int argc, char* argv[])
 {
   OptionsParser options;
@@ -14,8 +53,6 @@ int main(int argc, char* argv[])
     plugin.Init();
     plugin.LogMakeToConsole();
     plugin.SetDefinitions(options.APIyaml().c_str());
-
-    Game game;
 
     GameMode mode;
     std::string _mode = options.GetOption("mode").as<std::string>();
@@ -34,7 +71,37 @@ int main(int argc, char* argv[])
     bool _run = options.GetOption("run").as<bool>();
     if (!_run) plugin.HandleGameLaunch();
 
-    return plugin.BuildGame(game.ConstructGame(), mode, options.GetOption("output").as<std::string>().c_str());
+    Game game;
+    std::string input_file = options.GetOption("input").as<std::string>();
+    if (input_file.size()) {
+      if (input_file.size() < 4 || input_file[input_file.length() - 4] != '.') {
+        std::cerr << "Error: Unknown filetype: can't read input file \""
+                  << input_file << "\"." << std::endl;
+        return 1;
+      }
+      std::string ext = tolower(input_file.substr(input_file.length() - 3, 3));
+      if (ext != "3fg") {
+        if (ext == "egm") {
+          std::cerr << "EGM format not yet supported. "
+                       "Please use LateralGM for the time being." << std::endl;
+        } else if (ext == "gmx") {
+          std::cerr << "GMX format not supported. "
+                       "Please use LateralGM or find a converter." << std::endl;
+          return 1;
+        } else {
+          std::cerr << "Error: Unknown filetype: can't read input file \""
+                    << input_file << "\"." << std::endl;
+        }
+        return 1;
+      }
+      if (!read_3fg(input_file, &game)) return 1;
+    } else {
+      std::cerr << "Warning: No game file specified. "
+                   "Building an empty game." << std::endl;
+    }
+
+    std::string output_file = options.GetOption("output").as<std::string>();
+    return plugin.BuildGame(game.ConstructGame(), mode, output_file.c_str());
   }
 
   return result;
