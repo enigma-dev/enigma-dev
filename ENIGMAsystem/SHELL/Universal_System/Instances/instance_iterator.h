d7aef8b96a03767ff5e21ff7b40897e53d4470d8
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2018-01-17 03:50:55 -0500

    give instances a folder too

---
+++ ENIGMAsystem/SHELL/Universal_System/Instances/instance_iterator.h
@@ -0,0 +1,73 @@
+/** Copyright (C) 2008 Josh Ventura
+*** Copyright (C) 2014 Josh Ventura, Robert B. Colton
+*** Copyright (C) 2014 Seth N. Hetu
+***
+*** This file is a part of the ENIGMA Development Environment.
+***
+*** ENIGMA is free software: you can redistribute it and/or modify it under the
+*** terms of the GNU General Public License as published by the Free Software
+*** Foundation, version 3 of the license or any later version.
+***
+*** This application and its source code is distributed AS-IS, WITHOUT ANY
+*** WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+*** FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+*** details.
+***
+*** You should have received a copy of the GNU General Public License along
+*** with this code. If not, see <http://www.gnu.org/licenses/>
+**/
+
+#ifndef ENIGMA_INSTANCE_ITERATOR_H
+#define ENIGMA_INSTANCE_ITERATOR_H
+
+#include "instance_system_base.h"
+
+namespace enigma {
+
+class iterator {
+  enigma::inst_iter temp_iter;
+  enigma::inst_iter* it;
+
+  void addme();
+  void copy(const iterator& other);
+
+ public:
+  operator bool();
+  object_basic* operator*() const;
+  object_basic* operator->() const;
+
+  void handle_unlink(const inst_iter* dead);
+
+  iterator& operator++();
+  iterator operator++(int);
+  iterator& operator--();
+  iterator operator--(int);
+
+  iterator& operator=(const iterator&);
+  iterator& operator=(inst_iter*);
+  iterator& operator=(object_basic*);
+
+  iterator(const iterator&);
+  iterator(inst_iter*);
+  iterator(object_basic*);
+  iterator();
+
+  ~iterator();
+
+  class with;
+};
+
+class iterator::with : iterator, iterator_level {
+ public:
+  with(const iterator& push) : iterator(push), iterator_level(it) {}
+};
+
+void update_iterators_for_destroy(const inst_iter*);
+iterator instance_list_first();
+iterator fetch_inst_iter_by_id(int id);
+iterator fetch_inst_iter_by_int(int x);
+iterator fetch_roominst_iter_by_id(int x);
+
+}  //namespace enigma
+
+#endif  //ENIGMA_INSTANCE_ITERATOR_H
---
+++ ENIGMAsystem/SHELL/Universal_System/Instances/instance_system_frontend.h
@@ -0,0 +1,52 @@
+/********************************************************************************\
+**                                                                              **
+**  Copyright (C) 2008 Josh Ventura                                             **
+**                                                                              **
+**  This file is a part of the ENIGMA Development Environment.                  **
+**                                                                              **
+**                                                                              **
+**  ENIGMA is free software: you can redistribute it and/or modify it under the **
+**  terms of the GNU General Public License as published by the Free Software   **
+**  Foundation, version 3 of the license or any later version.                  **
+**                                                                              **
+**  This application and its source code is distributed AS-IS, WITHOUT ANY      **
+**  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS   **
+**  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more       **
+**  details.                                                                    **
+**                                                                              **
+**  You should have recieved a copy of the GNU General Public License along     **
+**  with this code. If not, see <http://www.gnu.org/licenses/>                  **
+**                                                                              **
+**  ENIGMA is an environment designed to create games and other programs with a **
+**  high-level, fully compilable language. Developers of ENIGMA or anything     **
+**  associated with ENIGMA are in no way responsible for its users or           **
+**  applications created by its users, or damages caused by the environment     **
+**  or programs made in the environment.                                        **
+**                                                                              **
+\********************************************************************************/
+
+#ifndef ENIGMA_INSTANCE_SYSTEM_FRONTEND_H
+#define ENIGMA_INSTANCE_SYSTEM_FRONTEND_H
+
+#include "instance_system_base.h"
+
+namespace enigma
+{
+  
+// This is an iterator typedef for use with the red-black backbone of the instance list.
+typedef struct winstance_list_iterator *pinstance_list_iterator;
+void winstance_list_iterator_delete(pinstance_list_iterator);
+
+// Linking
+pinstance_list_iterator link_instance(object_basic* who);
+inst_iter *link_obj_instance(object_basic* who, int oid);
+
+// Unlinking/Destroying
+void instance_iter_queue_for_destroy(object_basic*);
+void dispose_destroyed_instances();
+void unlink_main(pinstance_list_iterator);
+void unlink_object_id_iter(inst_iter*,int);
+
+} //namespace enigma
+
+#endif //ENIGMA_INSTANCE_SYSTEM_FRONTEND_H
