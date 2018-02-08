fd46e646d13c9edc518fa781305aad4ef2739f90
Josh Ventura <joshv10@gmail.com>
2018-02-08 01:11:15 -0500

    Fix mathnc; rewrite GTest extension.
    Rewrites the GTest extension to offer more type overloads. That extension could still use work, but it'll pass, for now.
    
    Major math refactor to get old games building and increase efficiency.
    
    - Moves simple mathnc functions into the header to make them easier to inline.
      - Templates force most of these to be inlined.
    - Adds template parameters to make them accept any arithmetic type.
      - Introduces new generic_args header to declare these templates and the ones for `abs()`.
      - Introduces new `ArithmeticTypes<>` template to allow anything that can be added.
    - Uses macros for return types that introduce template constraints to (A) improve readability and (B) function with the JDI implementation I wrote when I was 17.
    - ????
    - Profit

--- ENIGMAsystem/SHELL/Universal_System/Extensions/GTest/GTest.cpp
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/GTest/GTest.cpp
@@ -2,35 +2,56 @@
 #include <gtest/gtest.h>
 using std::string;
 
-#define IMPLEMENT_GTEST_BINARY_FUNC(FUNC, GTEST_CALL)                          \
-  static void FUNC##_impl(variant a, variant b, string msg, bool *survived) {  \
-    if (msg.empty()) ASSERT_EQ(a, b);                                          \
-    else GTEST_CALL(a, b) << msg;                                              \
-    *survived = true;                                                          \
-  }                                                                            \
-  void FUNC(variant a, variant b, string msg) {                                \
-    bool survived = false;                                                     \
-    FUNC##_impl(a, b, msg, &survived);                                         \
-    if (!survived) abort();                                                    \
+#define IMPLEMENT_GTEST_BINARY_FUNC_G(FUNC, GTEST_CALL, TYPE1, TYPE2)     \
+  static void FUNC##_impl(TYPE1 a, TYPE2 b, string msg, bool *survived) { \
+    if (msg.empty()) ASSERT_EQ(a, b);                                     \
+    else GTEST_CALL(a, b) << msg;                                         \
+    *survived = true;                                                     \
+  }                                                                       \
+  void FUNC(TYPE1 a, TYPE2 b, string msg) {                               \
+    bool survived = false;                                                \
+    FUNC##_impl(a, b, msg, &survived);                                    \
+    if (!survived) abort();                                               \
   }
-#define IMPLEMENT_GTEST_UNARY_FUNC(TYPE, FUNC, GTEST_CALL)         \
-  static void FUNC##_impl(TYPE exp, string msg, bool *survived) {  \
-    if (msg.empty()) GTEST_CALL(exp);                              \
-    else GTEST_CALL(exp) << msg;                                   \
-    *survived = true;                                              \
-  }                                                                \
-  void FUNC(TYPE exp, string message) {                            \
-    bool survived = false;                                         \
-    FUNC##_impl(exp, message, &survived);                          \
-    if (!survived) abort();                                        \
+#define IMPLEMENT_GTEST_BINARY_FUNC(F, C, T)                              \
+        IMPLEMENT_GTEST_BINARY_FUNC_G(F, C, T, T)
+#define IMPLEMENT_GTEST_UNARY_FUNC(TYPE, FUNC, GTEST_CALL)                \
+  static void FUNC##_impl(TYPE exp, string msg, bool *survived) {         \
+    if (msg.empty()) GTEST_CALL(exp);                                     \
+    else GTEST_CALL(exp) << msg;                                          \
+    *survived = true;                                                     \
+  }                                                                       \
+  void FUNC(TYPE exp, string message) {                                   \
+    bool survived = false;                                                \
+    FUNC##_impl(exp, message, &survived);                                 \
+    if (!survived) abort();                                               \
   }
+
+#define IMPLEMENT_GTEST_BINARY_FUNC_FOR_ALL_TYPES(FUNC, GTEST_CALL)       \
+  IMPLEMENT_GTEST_BINARY_FUNC(FUNC, GTEST_CALL, int8_t);                  \
+  IMPLEMENT_GTEST_BINARY_FUNC(FUNC, GTEST_CALL, int16_t);                 \
+  IMPLEMENT_GTEST_BINARY_FUNC(FUNC, GTEST_CALL, int32_t);                 \
+  IMPLEMENT_GTEST_BINARY_FUNC(FUNC, GTEST_CALL, int64_t);                 \
+  IMPLEMENT_GTEST_BINARY_FUNC(FUNC, GTEST_CALL, uint8_t);                 \
+  IMPLEMENT_GTEST_BINARY_FUNC(FUNC, GTEST_CALL, uint16_t);                \
+  IMPLEMENT_GTEST_BINARY_FUNC(FUNC, GTEST_CALL, uint32_t);                \
+  IMPLEMENT_GTEST_BINARY_FUNC(FUNC, GTEST_CALL, uint64_t);                \
+  IMPLEMENT_GTEST_BINARY_FUNC(FUNC, GTEST_CALL, float);                   \
+  IMPLEMENT_GTEST_BINARY_FUNC(FUNC, GTEST_CALL, double);                  \
+  IMPLEMENT_GTEST_BINARY_FUNC(FUNC, GTEST_CALL, long double);             \
+  IMPLEMENT_GTEST_BINARY_FUNC(FUNC, GTEST_CALL, std::string);             \
+  IMPLEMENT_GTEST_BINARY_FUNC(FUNC, GTEST_CALL, const var&);              \
+  IMPLEMENT_GTEST_BINARY_FUNC(FUNC, GTEST_CALL, const variant&);
 
 IMPLEMENT_GTEST_UNARY_FUNC(bool, gtest_assert_true, ASSERT_TRUE);
 IMPLEMENT_GTEST_UNARY_FUNC(bool, gtest_assert_false, ASSERT_FALSE);
 
-IMPLEMENT_GTEST_BINARY_FUNC(gtest_assert_eq, ASSERT_EQ);
-IMPLEMENT_GTEST_BINARY_FUNC(gtest_assert_ne, ASSERT_NE);
-IMPLEMENT_GTEST_BINARY_FUNC(gtest_assert_lt, ASSERT_LT);
-IMPLEMENT_GTEST_BINARY_FUNC(gtest_assert_le, ASSERT_LE);
-IMPLEMENT_GTEST_BINARY_FUNC(gtest_assert_gt, ASSERT_GT);
-IMPLEMENT_GTEST_BINARY_FUNC(gtest_assert_ge, ASSERT_GE);
+IMPLEMENT_GTEST_BINARY_FUNC_FOR_ALL_TYPES(gtest_assert_eq, ASSERT_EQ);
+IMPLEMENT_GTEST_BINARY_FUNC_FOR_ALL_TYPES(gtest_assert_ne, ASSERT_NE);
+IMPLEMENT_GTEST_BINARY_FUNC_FOR_ALL_TYPES(gtest_assert_lt, ASSERT_LT);
+IMPLEMENT_GTEST_BINARY_FUNC_FOR_ALL_TYPES(gtest_assert_le, ASSERT_LE);
+IMPLEMENT_GTEST_BINARY_FUNC_FOR_ALL_TYPES(gtest_assert_gt, ASSERT_GT);
+IMPLEMENT_GTEST_BINARY_FUNC_FOR_ALL_TYPES(gtest_assert_ge, ASSERT_GE);
+
+IMPLEMENT_GTEST_BINARY_FUNC_G(gtest_assert_eq, ASSERT_EQ, double, int);
+IMPLEMENT_GTEST_BINARY_FUNC_G(gtest_assert_eq, ASSERT_EQ, int, double);
