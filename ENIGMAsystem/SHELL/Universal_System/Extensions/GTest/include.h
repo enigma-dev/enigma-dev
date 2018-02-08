66d7b07eeac1d3df18749845e14a06ee7aa1ec7d
Josh Ventura <joshv10@gmail.com>
2018-02-08 01:11:15 -0500

    Fix mathnc; rewrite GTest extension.
    Rewrites the GTest extension to offer more type overloads. That extension could still use work, but it'll pass, for now.
    
    Major math refactor to get old games building and increase efficiency.
    
    - Moves simple mathnc functions into the header to make them easier to inline.
      - Templates force most of these to be inlined.
    - Adds template parameters to make them accept any arithmetic type.
      - Introduces new generic_args header to declare these templates and the ones for `abs()`.
      - Introduces new `ArithmeticTypes<>` template to allow anything that can be added.
    - Uses macros for return types that introduce template constraints to (A) improve readability and (B) function with the JDI implementation I wrote when I was 17.
    - ????
    - Profit

--- ENIGMAsystem/SHELL/Universal_System/Extensions/GTest/include.h
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/GTest/include.h
@@ -5,11 +5,38 @@
 void gtest_assert_true(bool exp,  std::string message = "");
 void gtest_assert_false(bool exp, std::string message = "");
 
-void gtest_assert_eq(variant a, variant b, std::string message = "");
-void gtest_assert_ne(variant a, variant b, std::string message = "");
-void gtest_assert_lt(variant a, variant b, std::string message = "");
-void gtest_assert_le(variant a, variant b, std::string message = "");
-void gtest_assert_gt(variant a, variant b, std::string message = "");
-void gtest_assert_ge(variant a, variant b, std::string message = "");
+#define DECLARE_BINARY_FOR_TYPE_G(FUNC, TYPE1, TYPE2)  \
+  void FUNC(TYPE1 a, TYPE2 b, std::string message = "");
+#define DECLARE_BINARY_FOR_TYPE(FUNC, TYPE)  \
+  DECLARE_BINARY_FOR_TYPE_G(FUNC, TYPE, TYPE);
+
+#define DECLARE_BINARY_FOR_ALL_TYPES(FUNC)     \
+  DECLARE_BINARY_FOR_TYPE(FUNC, int8_t);       \
+  DECLARE_BINARY_FOR_TYPE(FUNC, int16_t);      \
+  DECLARE_BINARY_FOR_TYPE(FUNC, int32_t);      \
+  DECLARE_BINARY_FOR_TYPE(FUNC, int64_t);      \
+  DECLARE_BINARY_FOR_TYPE(FUNC, uint8_t);      \
+  DECLARE_BINARY_FOR_TYPE(FUNC, uint16_t);     \
+  DECLARE_BINARY_FOR_TYPE(FUNC, uint32_t);     \
+  DECLARE_BINARY_FOR_TYPE(FUNC, uint64_t);     \
+  DECLARE_BINARY_FOR_TYPE(FUNC, float);        \
+  DECLARE_BINARY_FOR_TYPE(FUNC, double);       \
+  DECLARE_BINARY_FOR_TYPE(FUNC, long double);  \
+  DECLARE_BINARY_FOR_TYPE(FUNC, std::string);  \
+  DECLARE_BINARY_FOR_TYPE(FUNC, const var&);   \
+  DECLARE_BINARY_FOR_TYPE(FUNC, const variant&);
+
+DECLARE_BINARY_FOR_ALL_TYPES(gtest_assert_eq);
+DECLARE_BINARY_FOR_ALL_TYPES(gtest_assert_ne);
+DECLARE_BINARY_FOR_ALL_TYPES(gtest_assert_lt);
+DECLARE_BINARY_FOR_ALL_TYPES(gtest_assert_le);
+DECLARE_BINARY_FOR_ALL_TYPES(gtest_assert_gt);
+DECLARE_BINARY_FOR_ALL_TYPES(gtest_assert_ge);
+
+DECLARE_BINARY_FOR_TYPE_G(gtest_assert_eq, int, double);
+DECLARE_BINARY_FOR_TYPE_G(gtest_assert_eq, double, int);
+
+#undef DECLARE_BINARY_FOR_ALL_TYPES
+#undef DECLARE_BINARY_FOR_TYPE
 
 #endif  // ENIGMA_EXTENSION_GTEST_h
--- ENIGMAsystem/SHELL/Universal_System/mathnc.h
+++ ENIGMAsystem/SHELL/Universal_System/mathnc.h
@@ -34,6 +34,9 @@
   #undef y0
 #undef yn
 
+#include "dynamic_args.h"
+#include "generic_args.h"
+
 #ifndef INCLUDED_FROM_SHELLMAIN
 #error ln2math and stop including this damn header.
 #endif
@@ -45,110 +48,129 @@
 #include <cstdlib> // random()
 #include <string>
 
-namespace enigma {
-  template<typename T> struct SIntType {};
-  template<> struct SIntType<int8_t>  { typedef int16_t T; };
-  template<> struct SIntType<int16_t> { typedef int16_t T; };
-  template<> struct SIntType<int32_t> { typedef int32_t T; };
-  template<> struct SIntType<int64_t> { typedef int64_t T; };
-
-  template<typename T> struct UIntType {};
-  template<> struct UIntType<uint8_t>  { typedef uint16_t T; };
-  template<> struct UIntType<uint16_t> { typedef uint16_t T; };
-  template<> struct UIntType<uint32_t> { typedef uint32_t T; };
-  template<> struct UIntType<uint64_t> { typedef uint64_t T; };
-
-  template<typename T> struct FloatType {};
-  template<> struct FloatType<float>       { typedef float T; };
-  template<> struct FloatType<double>      { typedef double T; };
-  template<> struct FloatType<long double> { typedef long double T; };
-}
+#ifdef JUST_DEFINE_IT_RUN
+#  define WITH_ARITHMETIC_TYPES(types...)
+#  define WITH_RETURN_TYPE(type) type
+#  define ARITHMETIC_OPERATION(guess_type, expression...) guess_type
+#else
+#  define WITH_ARITHMETIC_TYPES(types...) typename enigma::ArithmeticTypes<types>
+#  define WITH_RETURN_TYPE(type) ::template returns<type>::T
+#  define ARITHMETIC_OPERATION(guess_type, expression...) decltype(expression)
+#endif
 
 namespace enigma_user
 {
   const ma_scalar pi = M_PI;
 
-  // Overloading
-  // TODO: Once the user space switch to namespace enigma_user has been made,
-  // remove these functions.
-  template<typename T> typename enigma::SIntType<T>::T abs(T x)  { return ::abs(x); }
-  template<typename T> typename enigma::UIntType<T>::T abs(T x)  { return x; }
-  template<typename T> typename enigma::FloatType<T>::T abs(T x) { return ::fabs(x); }
-  static inline double abs(const variant& x) { return ::fabs(ma_scalar(x)); }
-  static inline double abs(const var& x)     { return ::fabs(ma_scalar(x)); }
+  // STL functions.
+  using ::ceil;
+  using ::floor;
+  using ::round;
+  using ::exp;
+  using ::sqrt;
+  using ::log;
+  using ::log2;
+  using ::log10;
+  using ::sin;
+  using ::cos;
+  using ::tan;
 
-  // Functions
+  // Overloading - These functions have special implementations
+  // in C++ for each supported type.
 
-  // TODO: Once the user space switch to namespace enigma_user has been made,
-  // comment in all these functions.
-  //inline ma_scalar ceil(ma_scalar x)  { return ::ceil(x); }
-  //inline ma_scalar floor(ma_scalar x) { return ::floor(x); }
-  //inline ma_scalar round(ma_scalar x) { return ::round(x); }
-  //inline ma_scalar exp(ma_scalar x)   { return ::exp(x); }
-  //inline ma_scalar sqrt(ma_scalar x)  { return ::sqrt(x); }
-  inline ma_scalar ln(double x)    { return ::log(x); }
-  //inline ma_scalar log(ma_scalar x)   { return ::log(x); }
-  //inline ma_scalar log2(ma_scalar x)  { return ::log2(x); }
-  //inline ma_scalar log10(ma_scalar x) { return ::log10(x); }
-  //inline ma_scalar sin(ma_scalar x)   { return ::sin(x); }
-  //inline ma_scalar cos(ma_scalar x)   { return ::cos(x); }
-  //inline ma_scalar tan(ma_scalar x)   { return ::tan(x); }
-  inline ma_scalar power(ma_scalar x,ma_scalar p)   { return ::pow(x,p); }
-  inline ma_scalar arcsin(ma_scalar x)           { return ::asin(x); }
-  inline ma_scalar arccos(ma_scalar x)           { return ::acos(x); }
-  inline ma_scalar arctan(ma_scalar x)           { return ::atan(x); }
-  inline ma_scalar arctan2(ma_scalar y,ma_scalar x) { return ::atan2(y,x); }
+  // abs(x)
+  template<typename T> enigma::SIntType<T>  abs(T x) { return ::abs(x); }
+  template<typename T> enigma::UIntType<T>  abs(T x) { return x; }
+  template<typename T> enigma::FloatType<T> abs(T x) { return ::fabs(x); }
+  static inline double abs(const variant& x) { return ::fabs(double(x)); }
+  static inline double abs(const var& x)     { return ::fabs(double(x)); }
 
-  inline int64_t int64(ma_scalar x) { return (int64_t)x; }
+  // Aliases to STL functions.
+  inline ma_scalar ln(double x)     { return ::log(x); }
+  inline ma_scalar arcsin(double x) { return ::asin(x); }
+  inline ma_scalar arccos(double x) { return ::acos(x); }
+  inline ma_scalar arctan(double x) { return ::atan(x); }
+  inline ma_scalar arctan2(double y, double x) { return ::atan2(y,x); }
+  inline ma_scalar power(double x, double p)   { return ::pow(x,p); }
+
+  template<typename T>
+  inline int64_t int64(enigma::NumericType<T> x) { return (int64_t) x; }
   inline int64_t int64(std::string x) { return (int64_t) atol(x.c_str()); }
   inline int64_t int64(const variant& x) {
     return x.type == ty_string ? int64((std::string) x) : int64((double) x);
   }
   inline int64_t int64(const var& x) { return int64(*x); }
 
-  // TODO: Once the user space switch to namespace enigma_user has been made,
-  // comment in these functions.
-  /*
-  inline ma_scalar bessel_j0(ma_scalar x)        {return ::j0(x);};
-  inline ma_scalar bessel_j1(ma_scalar x)        {return ::j1(x);};
-  inline ma_scalar bessel_jn(int x, ma_scalar y) {return ::jn(x, y);};
-  inline ma_scalar bessel_y0(ma_scalar x)        {return ::y0(x);};
-  inline ma_scalar bessel_y1(ma_scalar x)        {return ::y1(x);};
-  inline ma_scalar bessel_yn(int x, ma_scalar y) {return ::yn(x, y);};
-  */
+  template<typename T>
+  inline ARITHMETIC_OPERATION(T, T() * T()) sqr(T x) { return x * x; }
+  inline double logn(double n, double x) { return ::log(x)/::log(n); }
 
-  ma_scalar sqr(ma_scalar x);
-  ma_scalar logn(ma_scalar n,ma_scalar x);
+  inline double sind(double x)    { return ::sin(x   * M_PI / 180.0); }
+  inline double cosd(double x)    { return ::cos(x   * M_PI / 180.0); }
+  inline double tand(double x)    { return ::tan(x   * M_PI / 180.0); }
+  inline double asind(double x)   { return ::asin(x) * 180.0 / M_PI; }
+  inline double acosd(double x)   { return ::acos(x) * 180.0 / M_PI; }
+  inline double atand(double x)   { return ::atan(x) * 180.0 / M_PI; }
+  inline double arcsind(double x) { return ::asin(x) * 180.0 / M_PI; }
+  inline double arccosd(double x) { return ::acos(x) * 180.0 / M_PI; }
+  inline double arctand(double x) { return ::atan(x) * 180.0 / M_PI; }
+  inline double atand2(double y, double x)   { return ::atan2(y,x) * 180.0 / M_PI; }
+  inline double arctand2(double y, double x) { return ::atan2(y,x) * 180.0 / M_PI; }
 
-  ma_scalar sind(ma_scalar x);
-  ma_scalar cosd(ma_scalar x);
-  ma_scalar tand(ma_scalar x);
-  ma_scalar asind(ma_scalar x);
-  ma_scalar acosd(ma_scalar x);
-  ma_scalar atand(ma_scalar x);
-  ma_scalar atand2(ma_scalar y,ma_scalar x);
-  ma_scalar arcsind(ma_scalar x);
-  ma_scalar arccosd(ma_scalar x);
-  ma_scalar arctand(ma_scalar x);
-  ma_scalar arctand2(ma_scalar y,ma_scalar x);
+  // Non-variadic (two-item) min/max
+  template<typename T, typename U> inline decltype(T() + U()) min(T x, U y) {
+    return y < x ? y : x;
+  }
+  template<typename T, typename U> inline decltype(T() + U()) max(T x, U y) {
+    return y > x ? y : x;
+  }
 
-  int sign(ma_scalar x);
-  int cmp(ma_scalar x,ma_scalar y);
-  bool equal(ma_scalar x, ma_scalar y);
-  ma_scalar frac(ma_scalar x);
+  // Comparison / epsilon-equality
+  /// C-like comparator; returns sign of x - y. Uses operator<().
+  template<typename T, typename U>
+  WITH_ARITHMETIC_TYPES(T, U) WITH_RETURN_TYPE(int) cmp(T x, U y) {
+    return (y < x) - (x < y);
+  }
+  /// Return the sign of the input; one of { -1, 0, 1 }.
+  template<typename T>
+  WITH_ARITHMETIC_TYPES(T) WITH_RETURN_TYPE(int) sign(T x) {
+    return cmp(x, 0);
+  }
+  /// Compare for equality using an epsilon.
+  template<typename T, typename U>
+  WITH_ARITHMETIC_TYPES(T, U) WITH_RETURN_TYPE(bool) equal(T x, U y) {
+    return ::fabs(x - y) <= (std::numeric_limits<T>::epsilon()
+                                * max(T(1), ::fmax(abs(x), abs(y))));
+  }
 
-  ma_scalar degtorad(ma_scalar x);
-  ma_scalar radtodeg(ma_scalar x);
+  // ENIGMA's own math helpers
+  inline ma_scalar frac(ma_scalar x)     { return x - (ma_scalar) floor(x); }
+  inline ma_scalar degtorad(ma_scalar x) { return x * (pi / (ma_scalar) 180); }
+  inline ma_scalar radtodeg(ma_scalar x) { return x * ((ma_scalar) 180 / pi); }
 
-  ma_scalar lengthdir_x(ma_scalar len,ma_scalar dir);
-  ma_scalar lengthdir_y(ma_scalar len,ma_scalar dir);
-  ma_scalar angle_difference(ma_scalar dir1,ma_scalar dir2);
-  ma_scalar point_direction(ma_scalar x1,ma_scalar y1,ma_scalar x2,ma_scalar y2);
-  ma_scalar point_distance(ma_scalar x1, ma_scalar y1, ma_scalar x2, ma_scalar y2);
-  ma_scalar point_distance_3d(ma_scalar x1, ma_scalar y1, ma_scalar z1, ma_scalar x2,
-  ma_scalar y2, ma_scalar z2);
+  inline double lengthdir_x(double len, double dir) { return len *  cosd(dir); }
+  inline double lengthdir_y(double len, double dir) { return len * -sind(dir); }
+  inline double angle_difference(double dir1, double dir2) {
+    return fmod(fmod(dir1 - dir2, 360.0) + 540.0, 360.0) - 180.0;
+  }
 
-  //NOTE: These are technically collision functions in Studio
+  template<typename X1, typename Y1, typename X2, typename Y2>
+  WITH_ARITHMETIC_TYPES(X1, Y1, X2, Y2) WITH_RETURN_TYPE(double)
+  point_direction(X1 x1, Y1 y1, X2 x2, Y2 y2) {
+    return fmod((::atan2(y1 - y2, x2 - x1) * (180 / M_PI)) + 360, 360);
+  }
+  template<typename X1, typename Y1, typename X2, typename Y2>
+  WITH_ARITHMETIC_TYPES(X1, Y1, X2, Y2) WITH_RETURN_TYPE(double)
+  point_distance(X1 x1, Y1 y1, X2 x2, Y2 y2) {
+    return ::hypot(x2 - x1, y2 - y1);
+  }
+  template<typename X1, typename Y1, typename Z1, typename X2, typename Y2, typename Z2>
+  WITH_ARITHMETIC_TYPES(X1, Y1, Z1, X2, Y2, Z2) WITH_RETURN_TYPE(double)
+  point_distance_3d(X1 x1, Y1 y1, Z1 z1, X2 x2, Y2 y2, Z2 z2) {
+    return ::sqrt(sqr(x1 - x2) + sqr(y1 - y2) + sqr(z1 - z2));
+  }
+
+  // NOTE: These are technically collision functions in Studio
   ma_scalar triangle_area(ma_scalar x1, ma_scalar y1, ma_scalar x2, ma_scalar y2, ma_scalar x3, ma_scalar y3);
   bool point_in_circle(ma_scalar px, ma_scalar py, ma_scalar x1, ma_scalar y1, ma_scalar rad);
   bool point_in_rectangle(ma_scalar px, ma_scalar py, ma_scalar x1, ma_scalar y1, ma_scalar x2, ma_scalar y2);
@@ -219,5 +241,9 @@ namespace enigma_user
   static inline int random32() { return rand(); }
   static inline long random64() { return ::rand(); }
 }
+
+#undef WITH_ARITHMETIC_TYPES
+#undef WITH_RETURN_TYPE
+#undef ARITHMETIC_OPERATION
 
 #endif // ENIGMA_MATHNC
