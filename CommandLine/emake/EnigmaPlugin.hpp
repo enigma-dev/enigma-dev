a41b8d025f097f528598686b7e54e7d92fd5e643
Greg Williamson <cheeseboy16@gmail.com>
2018-02-24 16:15:56 +0000

    cli print builtins

--- CommandLine/emake/EnigmaPlugin.hpp
+++ CommandLine/emake/EnigmaPlugin.hpp
@@ -32,7 +32,8 @@ public:
   void HandleGameLaunch();
   void LogMakeToConsole();
   int BuildGame(EnigmaStruct* data, GameMode mode, const char* fpath);
-
+  void PrintBuiltins(std::string& fName);
+  
 private:
   std::function<const char*(EnigmaCallbacks*)> plugin_Init = nullptr;
   std::function<void(const char*)> plugin_SetMakeDirectory = nullptr;
