27d6196cddbaea899c7c3c94008581aae79b7961
time-killer-games <samuelvenable@hotmail.com>
2018-03-24 21:17:18 -0400

    added short and widen to WINDOWSmain.h

--- ENIGMAsystem/SHELL/Platforms/Win32/WINDOWSmain.h
+++ ENIGMAsystem/SHELL/Platforms/Win32/WINDOWSmain.h
@@ -21,6 +21,14 @@
 
 #include "../General/PFmain.h"
 #include <windows.h>
+#include <wchar.h>
+#include <string>
+#include <vector>
+using std::string;
+
+typedef std::basic_string<WCHAR> tstring;
+tstring widen(const string &str);
+string shorten(tstring str);
 
 namespace enigma //TODO: Find where this belongs
 {
