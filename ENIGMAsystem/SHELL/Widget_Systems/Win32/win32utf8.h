b6bb64164d871290f36f4aaf13c4c38b77fc5dc3
time-killer-games <samuelvenable@hotmail.com>
2018-03-24 21:12:43 -0400

    Delete win32utf8.h

--- ENIGMAsystem/SHELL/Widget_Systems/Win32/win32utf8.h
+++
@@ -1,9 +0,0 @@
-#include <windows.h>
-#include <wchar.h>
-#include <string>
-#include <vector>
-using std::string;
-
-typedef std::basic_string<WCHAR> tstring;
-tstring widen(const string &str);
-string shorten(tstring str);
