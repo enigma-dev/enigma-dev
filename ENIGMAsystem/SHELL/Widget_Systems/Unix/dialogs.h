09c00c818032fd5a524e8acc7966c374e4eed054
time-killer-games <samuelvenable@hotmail.com>
2018-05-31 21:40:00 -0400

    Update dialogs.h

--- ENIGMAsystem/SHELL/Widget_Systems/Unix/dialogs.h
+++ ENIGMAsystem/SHELL/Widget_Systems/Unix/dialogs.h
@@ -1,24 +1,22 @@
 #include "tinyfiledialogs.h"
 #include <string>
 
-using std::string;
-
-//void show_error(string errortext, const bool fatal);
+//void show_error(std::string errortext, const bool fatal);
 
 namespace enigma_user
 {
-	extern string window_get_caption();
-	//int show_message(string str);
-	double show_question(string str);
-	string get_string(string str, string def);
-	string get_password(string str, string def);
-	double get_integer(string str, double def);
-	double get_passcode(string str, double def);
-	string get_open_filename(string filter, string fname);
-	string get_save_filename(string filter, string fname);
-	string get_open_filename_ext(string filter, string fname, string dir, string title);
-	string get_save_filename_ext(string filter, string fname, string dir, string title);
-	string get_directory(string dname);
-	string get_directory_alt(string capt, string root);
+	std::string window_get_caption();
+	//int show_message(std::string str);
+	double show_question(std::string str);
+	std::string get_string(std::string str, std::string def);
+	std::string get_password(std::string str, std::string def);
+	double get_integer(std::string str, double def);
+	double get_passcode(std::string str, double def);
+	std::string get_open_filename(std::string filter, std::string fname);
+	std::string get_save_filename(std::string filter, std::string fname);
+	std::string get_open_filename_ext(std::string filter, std::string fname, std::string dir, std::string title);
+	std::string get_save_filename_ext(std::string filter, std::string fname, std::string dir, std::string title);
+	std::string get_directory(std::string dname);
+	std::string get_directory_alt(std::string capt, std::string root);
 	double get_color(double defcol);
 }
