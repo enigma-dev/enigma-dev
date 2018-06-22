721e1195c80a75ea92f53fa44d8ac335aaddcf26
Greg Williamson <cheeseboy16@gmail.com>
2018-06-22 15:23:13 -0400

    fakutravis

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/POSIXproccess.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/POSIXproccess.cpp
@@ -1,4 +1,5 @@
-#include <sys/resource.h>  //setpriority
+#include <sys/resource.h> //setpriority
+#include <sys/types.h> //getpid
 
 void set_program_priority(int value) {
   setpriority(PRIO_PROCESS, getpid(), value);
