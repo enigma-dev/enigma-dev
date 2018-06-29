9f0edb763ba89291244bda267978da0ce405fcfe
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2018-06-29 13:44:05 -0400

    try to fix sleep now that image_compare is fixed

--- CommandLine/testing/Platform/TestHarness-X11.cpp
+++ CommandLine/testing/Platform/TestHarness-X11.cpp
@@ -101,7 +101,7 @@ class X11_TestHarness final: public TestHarness {
     system(("wmctrl -i -c " + to_string(window_id)).c_str());
   }
   void screen_save(std::string fPath) final {
-    usleep(62500); // give the game a chance to render a frame first
+    sleep(3); // give the game a chance to render a frame first
     system(("import -window " + to_string(window_id) + " " + fPath).c_str());
   }
   void file_delete(std::string fPath) final {
