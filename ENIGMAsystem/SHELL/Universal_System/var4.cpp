ca30fb5f3e45d077392f7cb1d5c505f2d52aac13
time-killer-games <samuelvenable@hotmail.com>
2018-07-05 00:08:03 -0400

    Update var4.cpp

--- ENIGMAsystem/SHELL/Universal_System/var4.cpp
+++ ENIGMAsystem/SHELL/Universal_System/var4.cpp
@@ -522,4 +522,5 @@ namespace enigma_user {
   bool is_real(variant val)   { return val.type == real; }
   bool is_string(variant val) { return val.type == tstr;  }
   bool is_ptr(variant val)   { return val.type == ty_pointer; }
+  variant pointer_null = nullptr, pointer_invalid = (val.type == ty_undefined);
 }
