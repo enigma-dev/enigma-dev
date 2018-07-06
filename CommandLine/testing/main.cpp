7631de05c98cdc6a0c132ce28f59d55118444e6b
Robert Colton <robertbcolton@gmail.com>
2018-07-06 12:48:45 -0400

    Update main.cpp

--- CommandLine/testing/main.cpp
+++ CommandLine/testing/main.cpp
@@ -7,6 +7,9 @@
 std::string gitMasterDir = "/tmp/enigma-master";
 
 int main(int argc, char **argv) {
+  std::cerr << "who da bitch: " << argc << std::endl;
+  std::cerr << std::flush;
+
   if (argc == 1) {
     int ret = system(("bash ./ci-regression.sh " + gitMasterDir).c_str());
     if (ret != 0) {
