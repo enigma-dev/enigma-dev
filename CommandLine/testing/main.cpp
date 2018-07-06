c9154903350f503d98bc7f0231fc8420a81ac964
Robert Colton <robertbcolton@gmail.com>
2018-07-06 13:12:26 -0400

    Regression Tests Fix (#1306)
    Fix the ordering of InitGoogleTests so that `ci-regression.sh` is executed only once.

--- CommandLine/testing/main.cpp
+++ CommandLine/testing/main.cpp
@@ -7,8 +7,6 @@
 std::string gitMasterDir = "/tmp/enigma-master";
 
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-
   if (argc == 1) {
     int ret = system(("bash ./ci-regression.sh " + gitMasterDir).c_str());
     if (ret != 0) {
@@ -17,5 +15,6 @@ int main(int argc, char **argv) {
     }
   }
 
+  ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
