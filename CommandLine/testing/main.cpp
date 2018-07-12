dcd9b82f573b5bdbe36380e5e4ac3ac0458d7c96
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2018-07-12 14:49:30 -0400

    simply return 1 since that's safe on non-POSIX [skip ci]

--- CommandLine/testing/main.cpp
+++ CommandLine/testing/main.cpp
@@ -9,14 +9,9 @@ std::string gitMasterDir = "/tmp/enigma-master";
 int main(int argc, char **argv) {
   if (argc == 1) {
     int ret = system(("bash ./ci-regression.sh " + gitMasterDir).c_str());
-
     if (ret != 0) {
-      if (WIFEXITED(ret)) {
-        std::cerr << "Error: ci-regression.sh returned non-zero " << WEXITSTATUS(ret) << std::endl;
-        return WEXITSTATUS(ret);
-      }
-      std::cerr << "Error: ci-regression.sh failed with " << ret << std::endl;
-      return -1;
+      std::cerr << "Error: ci-regression.sh returned non-zero " << ret << std::endl;
+      return 1;
     }
 
     // have the regular tests output to a different directory now to avoid
