ba845d63452783f045832bd5fe289b9585af4482
time-killer-games <samuelvenable@hotmail.com>
2018-07-13 19:28:24 -0400

    "resources.h" Uploaded
    This header defines the ID's of various resources used in the "resources.rc" and "dialogs.cpp" files.

---
+++ ENIGMAsystem/SHELL/Widget_Systems/Win32/resources.h
@@ -0,0 +1,5 @@
+#define IDC_EDIT                        204
+#define IDC_PROMPT                      205
+#define IDD_INPUTBOX                    206
+#define IDC_LABEL                       207
+#define IDC_DNAME                       208
