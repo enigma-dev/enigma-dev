adc006490a45c0fef9fed04d3b1d91096f221ab0
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2018-07-13 06:20:05 -0400

    make d3d_clear_depth function like draw_clear

--- ENIGMAsystem/SHELL/Graphics_Systems/OpenGL1/GLd3d.cpp
+++ ENIGMAsystem/SHELL/Graphics_Systems/OpenGL1/GLd3d.cpp
@@ -61,12 +61,9 @@ GLenum cullingstates[3] = {
 namespace enigma_user
 {
 
-void d3d_clear_depth() {
-  d3d_clear_depth_value(1.0f);
-}
-
-void d3d_clear_depth_value(float value) {
-  glClearDepthf(value);
+void d3d_clear_depth(double value) {
+  glClearDepth(value);
+  glClear(GL_DEPTH_BUFFER_BIT);
 }
 
 void d3d_set_software_vertex_processing(bool software) {
