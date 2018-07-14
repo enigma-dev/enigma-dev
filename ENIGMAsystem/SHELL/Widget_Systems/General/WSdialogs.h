2ac1b325797a538f2143e0cbd01e873131c62905
time-killer-games <samuelvenable@hotmail.com>
2018-07-13 20:17:48 -0400

    WSdialogs.h now reflects function and type changes
    - Return values and arguments now have the correct names and data types associated with them
    - Functions that were added in my previous commit are now applied to this header.

--- ENIGMAsystem/SHELL/Widget_Systems/General/WSdialogs.h
+++ ENIGMAsystem/SHELL/Widget_Systems/General/WSdialogs.h
@@ -1,5 +1,6 @@
 /** Copyright (C) 2008 Josh Ventura
 *** Copyright (C) 2014 Robert B. Colton
+*** Copyright (C) 2018 Samuel Venable
 ***
 *** This file is a part of the ENIGMA Development Environment.
 ***
@@ -22,71 +23,74 @@ using std::string;
 namespace enigma_user {
 
 enum {
-	text_type,
-	button_type,
-	input_type
+  text_type,
+  button_type,
+  input_type
 };
 
 // These are constants defined inside one of the win32 headers in mingw, no need to define them 2x
 /*
 enum {
-	ANSI_CHARSET,
-	DEFAULT_CHARSET,
-	EASTEUROPE_CHARSET,
-	RUSSIAN_CHARSET,
-	SYMBOL_CHARSET,
-	SHIFTJIS_CHARSET,
-	HANGEUL_CHARSET,
-	GB2312_CHARSET,
-	CHINESEBIG5_CHARSET,
-	JOHAB_CHARSET,
-	HEBREW_CHARSET,
-	ARABIC_CHARSET,
-	GREEK_CHARSET,
-	TURKISH_CHARSET,
-	VIETNAMESE_CHARSET,
-	THAI_CHARSET,
-	MAC_CHARSET,
-	BALTIC_CHARSET,
-	OEM_CHARSET
+  ANSI_CHARSET,
+  DEFAULT_CHARSET,
+  EASTEUROPE_CHARSET,
+  RUSSIAN_CHARSET,
+  SYMBOL_CHARSET,
+  SHIFTJIS_CHARSET,
+  HANGEUL_CHARSET,
+  GB2312_CHARSET,
+  CHINESEBIG5_CHARSET,
+  JOHAB_CHARSET,
+  HEBREW_CHARSET,
+  ARABIC_CHARSET,
+  GREEK_CHARSET,
+  TURKISH_CHARSET,
+  VIETNAMESE_CHARSET,
+  THAI_CHARSET,
+  MAC_CHARSET,
+  BALTIC_CHARSET,
+  OEM_CHARSET
 };
 */
 
-    void message_alpha(double alpha);
-    void message_background(int back);
-    void message_button(int spr);
-    void message_button_font(string name, int size, int color, int style);
-    void message_caption(bool show, string str);
-    void message_input_color(int col);
-    void message_input_font(string name, int size, int color, int style);
-    void message_mouse_color(int col);
-    void message_position(int x, int y);
-    void message_size(int w, int h);
-    void message_text_font(string name, int size, int color, int style); 
-	void message_text_charset(int type, int charset); 
-	
-	int show_message_ext(string str, string but1, string but2, string but3);
-  
-	bool show_question(string str);
-	inline bool action_if_question(string str)
-	{
-		return show_question(str);
-	}
-	
-	// IMPLEMENTS from widgets_mandatory:
-	// void show_error(string errortext, const bool fatal);
+  void message_alpha(double alpha);
+  void message_background(int back);
+  void message_button(int spr);
+  void message_button_font(string name, int size, int color, int style);
+  void message_caption(bool show, string str);
+  void message_input_color(int col);
+  void message_input_font(string name, int size, int color, int style);
+  void message_mouse_color(int col);
+  void message_position(int x, int y);
+  void message_size(int w, int h);
+  void message_text_font(string name, int size, int color, int style);
+  void message_text_charset(int type, int charset);
 
-	int get_color(int defcol, bool advanced = false);
-	
-	string get_open_filename(string filter, string fname, string caption="");
-	string get_save_filename(string filter, string fname, string caption="");
-	string get_directory(string dname, string caption="Select Folder");
-	string get_directory_alt(string message, string root, bool modern=false, string caption="Browse for Folder");
+  int show_message_ext(string str, string but1, string but2, string but3);
 
-	string get_login(string username, string password, string cap="");
-	string get_string(string message, string def, string cap="");
-	double get_number(string message, string def, string cap="");
-	int    get_integer(string message, string def, string cap="");
-	bool   get_string_canceled();
+  bool show_question(string str);
+  inline bool action_if_question(string str)
+  {
+    return show_question(str);
+  }
+
+  // IMPLEMENTS from widgets_mandatory:
+  // void show_error(string errortext, const bool fatal);
+
+  double get_color(double defcol, bool advanced = false, string title = "");
+
+  string get_open_filename(string filter, string fname, string title = "");
+  string get_save_filename(string filter, string fname, string title = "");
+  string get_open_filename_ext(string filter, string fname, string dir, string title);
+  string get_save_filename_ext(string filter, string fname, string dir, string title);
+  string get_directory(string dname, string title = "");
+  string get_directory_alt(string capt, string root, bool modern = false, string title = "");
+
+  string get_login(string username, string password, string title = "");
+  string get_string(string str, string def, string title = "");
+  string get_password(string str, string def, string title = "");
+  double get_integer(string str, double def, string title = "");
+  double get_passcode(string str, double def, string title = "");
+  bool   get_string_canceled();
 
 }
