1b0992a1ae3f5584da52216be855c0e90c082f1b
Josh Ventura <joshv10@gmail.com>
2018-07-21 16:21:38 -0400

    Add expect() methods to GTest extension

--- ENIGMAsystem/SHELL/Universal_System/Extensions/GTest/include.h
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/GTest/include.h
@@ -16,6 +16,20 @@ void gtest_assert_lt(var a, var b, std::string message = "");
 void gtest_assert_le(var a, var b, std::string message = "");
 void gtest_assert_gt(var a, var b, std::string message = "");
 void gtest_assert_ge(var a, var b, std::string message = "");
+
+
+void gtest_expect_true(bool exp,  std::string message = "");
+void gtest_expect_false(bool exp, std::string message = "");
+
+void gtest_expect_eq_eps(var a, var b, std::string message = "");
+
+void gtest_expect_eq(var a, var b, std::string message = "");
+void gtest_expect_eq(var a, var b, std::string message = "");
+void gtest_expect_ne(var a, var b, std::string message = "");
+void gtest_expect_lt(var a, var b, std::string message = "");
+void gtest_expect_le(var a, var b, std::string message = "");
+void gtest_expect_gt(var a, var b, std::string message = "");
+void gtest_expect_ge(var a, var b, std::string message = "");
 #else
 
 namespace enigma {
@@ -40,62 +54,72 @@ template<typename T, typename U> bool within_five_ulp(T a, U b) {
 namespace enigma_user {
 
 template<typename T, typename U>
-void gtest_assert_eq(T a, U b, std::string message, std::string aE, std::string bE) {
+void gtest_check_eq(T a, U b, std::string message, std::string aE, std::string bE, bool assert) {
   enigma::gtest_binary("(" + aE + ") == (" + bE + ")", toString(a), toString(b), aE, bE,
-                       "is equal to",                  a == b, true, message, "EDL", 1);
+                       "is equal to",                  a == b, assert, message, "EDL", 1);
 }
 template<typename T, typename U>
-void gtest_assert_ne(T a, U b, std::string message, std::string aE, std::string bE) {
+void gtest_check_ne(T a, U b, std::string message, std::string aE, std::string bE, bool assert) {
   enigma::gtest_binary("(" + aE + ") != (" + bE + ")", toString(a), toString(b), aE, bE,
-                       "is not equal to",              a != b, true, message, "EDL", 1);
+                       "is not equal to",              a != b, assert, message, "EDL", 1);
 }
 template<typename T, typename U>
-void gtest_assert_lt(T a, U b, std::string message, std::string aE, std::string bE) {
+void gtest_check_lt(T a, U b, std::string message, std::string aE, std::string bE, bool assert) {
   enigma::gtest_binary("(" + aE + ") < ("  + bE + ")", toString(a), toString(b), aE, bE,
-                       "is less than",                 a <  b, true, message, "EDL", 1);
+                       "is less than",                 a <  b, assert, message, "EDL", 1);
 }
 template<typename T, typename U>
-void gtest_assert_le(T a, U b, std::string message, std::string aE, std::string bE) {
+void gtest_check_le(T a, U b, std::string message, std::string aE, std::string bE, bool assert) {
   enigma::gtest_binary("(" + aE + ") <= (" + bE + ")", toString(a), toString(b), aE, bE,
-                       "is less than or equal to",     a <= b, true, message, "EDL", 1);
+                       "is less than or equal to",     a <= b, assert, message, "EDL", 1);
 }
 template<typename T, typename U>
-void gtest_assert_gt(T a, U b, std::string message, std::string aE, std::string bE) {
+void gtest_check_gt(T a, U b, std::string message, std::string aE, std::string bE, bool assert) {
   enigma::gtest_binary("(" + aE + ") > ("  + bE + ")", toString(a), toString(b), aE, bE,
-                       "is greater than",              a >  b, true, message, "EDL", 1);
+                       "is greater than",              a >  b, assert, message, "EDL", 1);
 }
 template<typename T, typename U>
-void gtest_assert_ge(T a, U b, std::string message, std::string aE, std::string bE) {
+void gtest_check_ge(T a, U b, std::string message, std::string aE, std::string bE, bool assert) {
   enigma::gtest_binary("(" + aE + ") >= (" + bE + ")", toString(a), toString(b), aE, bE,
-                       "is greater than or equal to",  a >= b, true, message, "EDL", 1);
+                       "is greater than or equal to",  a >= b, assert, message, "EDL", 1);
 }
 
 template<typename T, typename U>
-void gtest_assert_eq_eps(T a, U b, std::string message, std::string aE, std::string bE) {
+void gtest_check_eq_eps(T a, U b, std::string message, std::string aE, std::string bE, bool assert) {
   bool approx_eq = enigma::within_five_ulp(a, b);
   enigma::gtest_binary("(" + aE + ") == (" + bE + ")", toString(a), toString(b), aE, bE,
-                       "is approximately equal to", approx_eq, true, message, "EDL", 1);
+                       "is approximately equal to", approx_eq, assert, message, "EDL", 1);
 }
 
-static inline void gtest_assert_true(bool exp,  std::string message, std::string exp_str) {
-  enigma::gtest_unary(exp_str, (exp ? "true" : "false"), "true", exp, true, message, "EDL", 1);
+static inline void gtest_check_true(bool exp,  std::string message, std::string exp_str, bool assert) {
+  enigma::gtest_unary(exp_str, (exp ? "true" : "false"), "true", exp, assert, message, "EDL", 1);
 }
-static inline void gtest_assert_false(bool exp, std::string message, std::string exp_str) {
-  enigma::gtest_unary(exp_str, (exp ? "true" : "false"), "false", !exp, true, message, "EDL", 1);
+static inline void gtest_check_false(bool exp, std::string message, std::string exp_str, bool assert) {
+  enigma::gtest_unary(exp_str, (exp ? "true" : "false"), "false", !exp, assert, message, "EDL", 1);
 }
 
 }  // namespace enigma_user
 
 #ifdef INCLUDED_FROM_SHELLMAIN
-# define gtest_assert_eq_eps(a, b, m...) gtest_assert_eq_eps(a, b, std::string{m}, #a, #b)
-# define gtest_assert_eq(a, b, m...)     gtest_assert_eq    (a, b, std::string{m}, #a, #b)
-# define gtest_assert_ne(a, b, m...)     gtest_assert_ne    (a, b, std::string{m}, #a, #b)
-# define gtest_assert_lt(a, b, m...)     gtest_assert_lt    (a, b, std::string{m}, #a, #b)
-# define gtest_assert_le(a, b, m...)     gtest_assert_le    (a, b, std::string{m}, #a, #b)
-# define gtest_assert_gt(a, b, m...)     gtest_assert_gt    (a, b, std::string{m}, #a, #b)
-# define gtest_assert_ge(a, b, m...)     gtest_assert_ge    (a, b, std::string{m}, #a, #b)
-# define gtest_assert_true(exp, m...)    gtest_assert_true  (exp,  std::string{m}, #exp)
-# define gtest_assert_false(exp, m...)   gtest_assert_false (exp,  std::string{m}, #exp)
+# define gtest_assert_eq_eps(a, b, m...) gtest_check_eq_eps(a, b, std::string{m}, #a, #b, true)
+# define gtest_assert_eq(a, b, m...)     gtest_check_eq    (a, b, std::string{m}, #a, #b, true)
+# define gtest_assert_ne(a, b, m...)     gtest_check_ne    (a, b, std::string{m}, #a, #b, true)
+# define gtest_assert_lt(a, b, m...)     gtest_check_lt    (a, b, std::string{m}, #a, #b, true)
+# define gtest_assert_le(a, b, m...)     gtest_check_le    (a, b, std::string{m}, #a, #b, true)
+# define gtest_assert_gt(a, b, m...)     gtest_check_gt    (a, b, std::string{m}, #a, #b, true)
+# define gtest_assert_ge(a, b, m...)     gtest_check_ge    (a, b, std::string{m}, #a, #b, true)
+# define gtest_assert_true(exp, m...)    gtest_check_true  (exp,  std::string{m}, #exp,   true)
+# define gtest_assert_false(exp, m...)   gtest_check_false (exp,  std::string{m}, #exp,   true)
+
+# define gtest_expect_eq_eps(a, b, m...) gtest_check_eq_eps(a, b, std::string{m}, #a, #b, false)
+# define gtest_expect_eq(a, b, m...)     gtest_check_eq    (a, b, std::string{m}, #a, #b, false)
+# define gtest_expect_ne(a, b, m...)     gtest_check_ne    (a, b, std::string{m}, #a, #b, false)
+# define gtest_expect_lt(a, b, m...)     gtest_check_lt    (a, b, std::string{m}, #a, #b, false)
+# define gtest_expect_le(a, b, m...)     gtest_check_le    (a, b, std::string{m}, #a, #b, false)
+# define gtest_expect_gt(a, b, m...)     gtest_check_gt    (a, b, std::string{m}, #a, #b, false)
+# define gtest_expect_ge(a, b, m...)     gtest_check_ge    (a, b, std::string{m}, #a, #b, false)
+# define gtest_expect_true(exp, m...)    gtest_check_true  (exp,  std::string{m}, #exp,   false)
+# define gtest_expect_false(exp, m...)   gtest_check_false (exp,  std::string{m}, #exp,   false)
 #endif  // Included from SHELLmain
 #endif  // Not JDI
 
