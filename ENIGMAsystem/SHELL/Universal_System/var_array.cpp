ec519991d36435e01ed11a55c7367a9605b96451
Robert Colton <robertbcolton@gmail.com>
2018-07-26 00:15:28 -0400

    Array Tests & Improvements (#1335)
    * Add array_create, array_create_2d, array_equals, and array_copy
    * Remove some old comments I added, arrays already overloaded
    * Fix 2d var array constructor
    * Add simple arrays tests into math tests

--- ENIGMAsystem/SHELL/Universal_System/var_array.cpp
+++ ENIGMAsystem/SHELL/Universal_System/var_array.cpp
@@ -30,6 +30,20 @@
 
 namespace enigma_user {
 
+var array_create(size_t size, variant value) { return var(value, size); }
+var array_create_2d(size_t length, size_t height, variant value) { return var(value, length, height); }
+bool array_equals(const var& arr1, const var& arr2) {
+  if (arr1.array_len() != arr2.array_len()) return false;
+  for (size_t i = 0; i < arr1.array_len(); i++) {
+    if (arr1[i] != arr2[i]) return false;
+  }
+  return true;
+}
+void array_copy(var& dest, size_t dest_index, var& src, size_t src_index, size_t length) {
+  for (size_t i = 0; i < length; i++) {
+    dest[dest_index + i] = src[src_index + i];
+  }
+}
 int array_length_1d(const var& v) { return v.array_len(); }
 int array_length_2d(const var& v, int n) { return v.array_len(n); }
 int array_height_2d(const var& v) { return v.array_height(); }
