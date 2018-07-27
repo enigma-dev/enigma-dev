073eef048c6cdaacfd9d6bd639a3e51f41be0f72
Robert Colton <robertbcolton@gmail.com>
2018-07-27 09:49:09 -0400

    Remove Room Caption/Mouse Update From Bridges (#1338)
    Remove room caption and updating of mouse variables logic out of all of the bridges and into the platform code where it belongs. Also makes room caption optional now if it's set to an empty string. Refreshing the screen now only involves a simple swapping of the backbuffer.

--- ENIGMAsystem/SHELL/Bridges/Cocoa-OpenGL1/graphics_bridge.cpp
+++ ENIGMAsystem/SHELL/Bridges/Cocoa-OpenGL1/graphics_bridge.cpp
@@ -18,7 +18,6 @@
 #include <GL/glxew.h>
 #include "Platforms/Cocoa/CocoaMain.h"
 #include "Graphics_Systems/graphics_mandatory.h"
-#include "Platforms/General/PFwindow.h"
 #include "Graphics_Systems/General/GScolors.h"
 
 #include <iostream>
@@ -33,7 +32,7 @@ extern "C" {
 
 namespace enigma {
   GLuint msaa_fbo = 0;
-  
+
   extern void (*WindowResizedCallback)();
   void WindowResized() {
     // clear the window color, viewport does not need set because backbuffer was just recreated
@@ -47,9 +46,6 @@ namespace enigma {
   }
 }
 
-#include "Platforms/General/PFwindow.h" // window_set_caption
-#include "Universal_System/roomsystem.h" // room_caption, update_mouse_variables
-
 namespace enigma_user {
   // Don't know where to query this on Cocoa, just defaulting it to 2,4,and 8 samples all supported, Windows puts it in EnableDrawing
   int display_aa = 14;
@@ -57,17 +53,15 @@ namespace enigma_user {
   void set_synchronization(bool enable) {
 
   }
-    
+
   void display_reset(int samples, bool vsync) {
     set_synchronization(vsync);
     //TODO: Copy over from the Win32 bridge
   }
-    
+
   void screen_refresh() {
     cocoa_screen_refresh();
-    enigma::update_mouse_variables();
     cocoa_flush_opengl();
   }
 
 }
-
--- ENIGMAsystem/SHELL/Bridges/General/Win32-Direct3D11.cpp
+++ ENIGMAsystem/SHELL/Bridges/General/Win32-Direct3D11.cpp
@@ -17,9 +17,8 @@
 
 #include "libEGMstd.h"
 #include "Widget_Systems/widgets_mandatory.h"
-#include "Platforms/General/PFwindow.h"
 #include "Platforms/platforms_mandatory.h"
-#include "Universal_System/roomsystem.h"
+#include "Platforms/General/PFwindow.h"
 #include "Graphics_Systems/graphics_mandatory.h"
 #include "Bridges/General/DX11Context.h"
 #include "Graphics_Systems/General/GScolors.h"
@@ -203,8 +202,6 @@ namespace enigma
 
 }
 
-#include "Universal_System/roomsystem.h"
-
 namespace enigma_user
 {
   int display_aa = 0;
@@ -214,8 +211,6 @@ namespace enigma_user
   }
 
   void screen_refresh() {
-    window_set_caption(room_caption);
-    enigma::update_mouse_variables();
     m_swapChain->Present(0, 0);
   }
 
