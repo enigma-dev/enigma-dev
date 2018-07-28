d38cd0188fce3b49ecf9617e91593dcb69fa6276
Robert Colton <robertbcolton@gmail.com>
2018-07-28 15:46:39 -0400

    OpenGL Bridges Cleanup and Fixes (#1340)
    * Fix the SDL-OpenGL bridge's context version macros
    * Add glewInit to the SDL-OpenGL bridge
    * Use show_error for all bridges and fatal bridge initialization error messages
    * Remove unused includes

--- ENIGMAsystem/SHELL/Bridges/Win32-OpenGL1/graphics_bridge.cpp
+++ ENIGMAsystem/SHELL/Bridges/Win32-OpenGL1/graphics_bridge.cpp
@@ -70,12 +70,9 @@ void EnableDrawing(void*)
   hRC = wglCreateContext( enigma::window_hDC );
   wglMakeCurrent( enigma::window_hDC, hRC );
 
-  // -- Initialise GLEW
   GLenum err = glewInit();
   if (GLEW_OK != err)
-  {
-    return;
-  }
+    show_error(std::string("Failed to initialize glew for OpenGL. ") + glewGetErrorString(err), true);
 
   //TODO: This never reports higher than 8, but display_aa should be 14 if 2,4,and 8 are supported and 8 only when only 8 is supported
   glGetIntegerv(GL_MAX_SAMPLES_EXT, &enigma_user::display_aa);
