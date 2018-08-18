1ca547f8b4e490994e999b69aa3ccc70854cdece
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2018-08-18 01:28:42 -0400

    create draw_batch_* C namespace of functions

--- ENIGMAsystem/SHELL/Graphics_Systems/General/GSprimitives.cpp
+++ ENIGMAsystem/SHELL/Graphics_Systems/General/GSprimitives.cpp
@@ -23,8 +23,12 @@
 
 namespace {
 
-int draw_primitive_model = enigma_user::d3d_model_create(enigma_user::model_stream);
-int draw_primitive_texture = -1;
+// the batching mode is initialized to the default here
+int draw_batch_mode = enigma_user::batch_flush_immediate;
+// we create a stream model to use for combining the primitives in a batch
+int draw_batch_stream = enigma_user::d3d_model_create(enigma_user::model_stream);
+// the texture that was specified when the current primitive batch began
+int draw_batch_texture = -1;
 
 } // anonymous namespace
 
@@ -43,156 +47,179 @@ unsigned draw_primitive_count(int kind, unsigned vertex_count) {
   return 0;
 }
 
-void draw_primitive_flush() {
-  d3d_model_draw(draw_primitive_model, draw_primitive_texture);
-  d3d_model_clear(draw_primitive_model);
+void draw_batch_flush(int kind) {
+  // return if the kind of flush being requested
+  // is not the mode of flushing we have enabled
+  if (draw_batch_mode != kind) return;
+
+  // the never flush mode means the batch isn't drawn
+  // we must still clear it from memory to avoid leaks
+  if (draw_batch_mode != batch_flush_never) {
+    d3d_model_draw(draw_batch_stream, draw_batch_texture);
+  }
+  d3d_model_clear(draw_batch_stream);
+}
+
+void draw_set_batch_mode(int mode) {
+  if (draw_batch_mode != batch_flush_immediate &&
+      draw_batch_mode != mode) {
+    // if we were not doing immediate flushing before
+    // we need to flush now before we switch modes or
+    // the batch may never be drawn
+    draw_batch_flush(draw_batch_mode);
+  }
+  draw_batch_mode = mode;
+}
+
+int draw_get_batch_mode() {
+  return draw_batch_mode;
 }
 
 void draw_primitive_begin(int kind)
 {
-  d3d_model_primitive_begin(draw_primitive_model, kind);
-  draw_primitive_texture = -1;
+  d3d_model_primitive_begin(draw_batch_stream, kind);
+  draw_batch_texture = -1;
 }
 
 void draw_primitive_begin_texture(int kind, int texId)
 {
-  d3d_model_primitive_begin(draw_primitive_model, kind);
-  draw_primitive_texture = texId;
+  d3d_model_primitive_begin(draw_batch_stream, kind);
+  draw_batch_texture = texId;
 }
 
 void draw_primitive_end()
 {
-  d3d_model_primitive_end(draw_primitive_model);
-  draw_primitive_flush();
+  d3d_model_primitive_end(draw_batch_stream);
+  draw_batch_flush(batch_flush_immediate);
 }
 
 void draw_vertex(gs_scalar x, gs_scalar y)
 {
-  d3d_model_vertex_color(draw_primitive_model, x, y, draw_get_color(), draw_get_alpha());
+  d3d_model_vertex_color(draw_batch_stream, x, y, draw_get_color(), draw_get_alpha());
 }
 
 void draw_vertex_color(gs_scalar x, gs_scalar y, int col, float alpha)
 {
-  d3d_model_vertex_color(draw_primitive_model, x, y, col, alpha);
+  d3d_model_vertex_color(draw_batch_stream, x, y, col, alpha);
 }
 
 void draw_vertex_texture(gs_scalar x, gs_scalar y, gs_scalar tx, gs_scalar ty)
 {
-  d3d_model_vertex_texture_color(draw_primitive_model, x, y, tx, ty, draw_get_color(), draw_get_alpha());
+  d3d_model_vertex_texture_color(draw_batch_stream, x, y, tx, ty, draw_get_color(), draw_get_alpha());
 }
 
 void draw_vertex_texture_color(gs_scalar x, gs_scalar y, gs_scalar tx, gs_scalar ty, int col, float alpha)
 {
-  d3d_model_vertex_texture_color(draw_primitive_model, x, y, tx, ty, col, alpha);
+  d3d_model_vertex_texture_color(draw_batch_stream, x, y, tx, ty, col, alpha);
 }
 
 void d3d_primitive_begin(int kind)
 {
-  d3d_model_primitive_begin(draw_primitive_model, kind);
-  draw_primitive_texture = -1;
+  d3d_model_primitive_begin(draw_batch_stream, kind);
+  draw_batch_texture = -1;
 }
 
 void d3d_primitive_begin_texture(int kind, int texId)
 {
-  d3d_model_primitive_begin(draw_primitive_model, kind);
-  draw_primitive_texture = texId;
+  d3d_model_primitive_begin(draw_batch_stream, kind);
+  draw_batch_texture = texId;
 }
 
 void d3d_primitive_end()
 {
-  d3d_model_primitive_end(draw_primitive_model);
-  draw_primitive_flush();
+  d3d_model_primitive_end(draw_batch_stream);
+  draw_batch_flush(batch_flush_immediate);
 }
 
 void d3d_vertex(gs_scalar x, gs_scalar y, gs_scalar z)
 {
-  d3d_model_vertex_color(draw_primitive_model, x, y, z, draw_get_color(), draw_get_alpha());
+  d3d_model_vertex_color(draw_batch_stream, x, y, z, draw_get_color(), draw_get_alpha());
 }
 
 void d3d_vertex_color(gs_scalar x, gs_scalar y, gs_scalar z, int color, double alpha)
 {
-  d3d_model_vertex_color(draw_primitive_model, x, y, z, color, alpha);
+  d3d_model_vertex_color(draw_batch_stream, x, y, z, color, alpha);
 }
 
 void d3d_vertex_texture(gs_scalar x, gs_scalar y, gs_scalar z, gs_scalar tx, gs_scalar ty)
 {
-  d3d_model_vertex_texture_color(draw_primitive_model, x, y, z, tx, ty, draw_get_color(), draw_get_alpha());
+  d3d_model_vertex_texture_color(draw_batch_stream, x, y, z, tx, ty, draw_get_color(), draw_get_alpha());
 }
 
 void d3d_vertex_texture_color(gs_scalar x, gs_scalar y, gs_scalar z, gs_scalar tx, gs_scalar ty, int color, double alpha)
 {
-  d3d_model_vertex_texture_color(draw_primitive_model, x, y, z, tx, ty, color, alpha);
+  d3d_model_vertex_texture_color(draw_batch_stream, x, y, z, tx, ty, color, alpha);
 }
 
 void d3d_vertex_normal(gs_scalar x, gs_scalar y, gs_scalar z, gs_scalar nx, gs_scalar ny, gs_scalar nz)
 {
-  d3d_model_vertex_normal_color(draw_primitive_model, x, y, z, nx, ny, nz, draw_get_color(), draw_get_alpha());
+  d3d_model_vertex_normal_color(draw_batch_stream, x, y, z, nx, ny, nz, draw_get_color(), draw_get_alpha());
 }
 
 void d3d_vertex_normal_color(gs_scalar x, gs_scalar y, gs_scalar z, gs_scalar nx, gs_scalar ny, gs_scalar nz, int color, double alpha)
 {
-  d3d_model_vertex_normal_color(draw_primitive_model, x, y, z, nx, ny, nz, color, alpha);
+  d3d_model_vertex_normal_color(draw_batch_stream, x, y, z, nx, ny, nz, color, alpha);
 }
 
 void d3d_vertex_normal_texture(gs_scalar x, gs_scalar y, gs_scalar z, gs_scalar nx, gs_scalar ny, gs_scalar nz, gs_scalar tx, gs_scalar ty)
 {
-  d3d_model_vertex_normal_texture_color(draw_primitive_model, x, y, z, nx, ny, nz, tx, ty, draw_get_color(), draw_get_alpha());
+  d3d_model_vertex_normal_texture_color(draw_batch_stream, x, y, z, nx, ny, nz, tx, ty, draw_get_color(), draw_get_alpha());
 }
 
 void d3d_vertex_normal_texture_color(gs_scalar x, gs_scalar y, gs_scalar z, gs_scalar nx, gs_scalar ny, gs_scalar nz, gs_scalar tx, gs_scalar ty, int color, double alpha)
 {
-  d3d_model_vertex_normal_texture_color(draw_primitive_model, x, y, z, nx, ny, nz, tx, ty, color, alpha);
+  d3d_model_vertex_normal_texture_color(draw_batch_stream, x, y, z, nx, ny, nz, tx, ty, color, alpha);
 }
 
 void d3d_draw_floor(gs_scalar x1, gs_scalar y1, gs_scalar z1, gs_scalar x2, gs_scalar y2, gs_scalar z2, int texId, gs_scalar hrep, gs_scalar vrep)
 {
-  d3d_model_floor(draw_primitive_model, x1, y1, z1, x2, y2, z2, hrep, vrep);
-  draw_primitive_texture = texId;
-  draw_primitive_flush();
+  d3d_model_floor(draw_batch_stream, x1, y1, z1, x2, y2, z2, hrep, vrep);
+  draw_batch_texture = texId;
+  draw_batch_flush(batch_flush_immediate);
 }
 
 void d3d_draw_wall(gs_scalar x1, gs_scalar y1, gs_scalar z1, gs_scalar x2, gs_scalar y2, gs_scalar z2, int texId, gs_scalar hrep, gs_scalar vrep)
 {
-  d3d_model_wall(draw_primitive_model, x1, y1, z1, x2, y2, z2, hrep, vrep);
-  draw_primitive_texture = texId;
-  draw_primitive_flush();
+  d3d_model_wall(draw_batch_stream, x1, y1, z1, x2, y2, z2, hrep, vrep);
+  draw_batch_texture = texId;
+  draw_batch_flush(batch_flush_immediate);
 }
 
 void d3d_draw_block(gs_scalar x1, gs_scalar y1, gs_scalar z1, gs_scalar x2, gs_scalar y2, gs_scalar z2, int texId, gs_scalar hrep, gs_scalar vrep, bool closed)
 {
-  d3d_model_block(draw_primitive_model, x1, y1, z1, x2, y2, z2, hrep, vrep, closed);
-  draw_primitive_texture = texId;
-  draw_primitive_flush();
+  d3d_model_block(draw_batch_stream, x1, y1, z1, x2, y2, z2, hrep, vrep, closed);
+  draw_batch_texture = texId;
+  draw_batch_flush(batch_flush_immediate);
 }
 
 void d3d_draw_cylinder(gs_scalar x1, gs_scalar y1, gs_scalar z1, gs_scalar x2, gs_scalar y2, gs_scalar z2, int texId, gs_scalar hrep, gs_scalar vrep, bool closed, int steps)
 {
-  d3d_model_cylinder(draw_primitive_model, x1, y1, z1, x2, y2, z2, hrep, vrep, closed, steps);
-  draw_primitive_texture = texId;
-  draw_primitive_flush();
+  d3d_model_cylinder(draw_batch_stream, x1, y1, z1, x2, y2, z2, hrep, vrep, closed, steps);
+  draw_batch_texture = texId;
+  draw_batch_flush(batch_flush_immediate);
 }
 
 void d3d_draw_cone(gs_scalar x1, gs_scalar y1, gs_scalar z1, gs_scalar x2, gs_scalar y2, gs_scalar z2, int texId, gs_scalar hrep, gs_scalar vrep, bool closed, int steps)
 {
-  d3d_model_cone(draw_primitive_model, x1, y1, z1, x2, y2, z2, hrep, vrep, closed, steps);
-  draw_primitive_texture = texId;
-  draw_primitive_flush();
+  d3d_model_cone(draw_batch_stream, x1, y1, z1, x2, y2, z2, hrep, vrep, closed, steps);
+  draw_batch_texture = texId;
+  draw_batch_flush(batch_flush_immediate);
 }
 
 void d3d_draw_ellipsoid(gs_scalar x1, gs_scalar y1, gs_scalar z1, gs_scalar x2, gs_scalar y2, gs_scalar z2, int texId, gs_scalar hrep, gs_scalar vrep, int steps)
 {
-  d3d_model_ellipsoid(draw_primitive_model, x1, y1, z1, x2, y2, z2, hrep, vrep, steps);
-  draw_primitive_texture = texId;
-  draw_primitive_flush();
+  d3d_model_ellipsoid(draw_batch_stream, x1, y1, z1, x2, y2, z2, hrep, vrep, steps);
+  draw_batch_texture = texId;
+  draw_batch_flush(batch_flush_immediate);
 }
 
 void d3d_draw_icosahedron(gs_scalar x1, gs_scalar y1, gs_scalar z1, gs_scalar x2, gs_scalar y2, gs_scalar z2, int texId, gs_scalar hrep, gs_scalar vrep, int steps) {
 }
 
 void d3d_draw_torus(gs_scalar x1, gs_scalar y1, gs_scalar z1, int texId, gs_scalar hrep, gs_scalar vrep, int csteps, int tsteps, double radius, double tradius) {
-  d3d_model_torus(draw_primitive_model, x1, y1, z1, hrep, vrep, csteps, tsteps, radius, tradius);
-  draw_primitive_texture = texId;
-  draw_primitive_flush();
+  d3d_model_torus(draw_batch_stream, x1, y1, z1, hrep, vrep, csteps, tsteps, radius, tradius);
+  draw_batch_texture = texId;
+  draw_batch_flush(batch_flush_immediate);
 }
 
 }
