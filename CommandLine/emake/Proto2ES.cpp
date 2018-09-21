1f56b1394028d12746a884cc10809ee019dffa6d
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2018-09-21 15:13:12 -0400

    use libpng for decoding in Proto2ES

--- CommandLine/emake/Proto2ES.cpp
+++ CommandLine/emake/Proto2ES.cpp
@@ -250,38 +250,89 @@ unsigned char* zlib_compress(unsigned char* inbuffer,int &actualsize)
     return (unsigned char*)outbytef;
 }
 
-#include "lodepng.h"
+#include <png.h>
 
 Image AddImage(const std::string fname) {
   Image i = Image();
 
-  unsigned error;
-  unsigned char* image;
-  unsigned pngwidth, pngheight;
+  FILE *fp = fopen(fname.c_str(), "rb");
 
-  error = lodepng_decode32_file(&image, &pngwidth, &pngheight, fname.c_str());
-  if (error)
-  {
-    printf("error %u: %s\n", error, lodepng_error_text(error));
-    return i;
+  png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+  if (!png) printf("error: failed to create png read struct");
+  png_infop info = png_create_info_struct(png);
+  if (!info) printf("error: failed to create png info struct");
+
+  if (setjmp(png_jmpbuf(png))) printf("png error: failed to save current execution context");
+  png_init_io(png, fp);
+  png_read_info(png, info);
+
+  size_t pngwidth, pngheight;
+  png_byte color_type, bit_depth;
+  pngwidth      = png_get_image_width(png, info);
+  pngheight     = png_get_image_height(png, info);
+  color_type = png_get_color_type(png, info);
+  bit_depth  = png_get_bit_depth(png, info);
+
+  // Read any color_type into 8bit depth, RGBA format.
+  // See http://www.libpng.org/pub/png/libpng-manual.txt
+
+  if (bit_depth == 16)
+    png_set_strip_16(png);
+
+  if (color_type == PNG_COLOR_TYPE_PALETTE)
+    png_set_palette_to_rgb(png);
+
+  // PNG_COLOR_TYPE_GRAY_ALPHA is always 8 or 16bit depth.
+  if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
+    png_set_expand_gray_1_2_4_to_8(png);
+
+  if (png_get_valid(png, info, PNG_INFO_tRNS))
+    png_set_tRNS_to_alpha(png);
+
+  // These color_type don't have an alpha channel then fill it with 0xff.
+  if (color_type == PNG_COLOR_TYPE_RGB ||
+      color_type == PNG_COLOR_TYPE_GRAY ||
+      color_type == PNG_COLOR_TYPE_PALETTE)
+    png_set_filler(png, 0xFF, PNG_FILLER_AFTER);
+
+  if (color_type == PNG_COLOR_TYPE_GRAY ||
+      color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
+    png_set_gray_to_rgb(png);
+
+  png_read_update_info(png, info);
+
+  png_bytep *image;
+  image = (png_bytep*)malloc(sizeof(png_bytep) * pngheight);
+  for (size_t y = 0; y < pngheight; y++) {
+    image[y] = (png_byte*)malloc(png_get_rowbytes(png,info));
   }
 
-  unsigned ih,iw;
+  png_read_image(png, image);
+
+  fclose(fp);
+
+  size_t ih,iw;
   const int bitmap_size = pngwidth*pngheight*4;
   unsigned char* bitmap = new unsigned char[bitmap_size](); // Initialize to zero.
 
   for (ih = 0; ih < pngheight; ih++) {
-    unsigned tmp = ih*pngwidth*4;
+    png_byte* row = image[ih];
+    size_t tmp = ih*pngwidth*4;
     for (iw = 0; iw < pngwidth; iw++) {
-      bitmap[tmp+0] = image[4*pngwidth*ih+iw*4+2];
-      bitmap[tmp+1] = image[4*pngwidth*ih+iw*4+1];
-      bitmap[tmp+2] = image[4*pngwidth*ih+iw*4+0];
-      bitmap[tmp+3] = image[4*pngwidth*ih+iw*4+3];
+      png_byte* ptr = &(row[iw*4]);
+      bitmap[tmp+0] = ptr[2];
+      bitmap[tmp+1] = ptr[1];
+      bitmap[tmp+2] = ptr[0];
+      bitmap[tmp+3] = ptr[3];
       tmp+=4;
     }
   }
 
+  /* cleanup heap allocation */
+  for (size_t y=0; y<pngheight; y++)
+    free(image[y]);
   free(image);
+
   i.width  = pngwidth;
   i.height = pngheight;
   i.dataSize = bitmap_size;
