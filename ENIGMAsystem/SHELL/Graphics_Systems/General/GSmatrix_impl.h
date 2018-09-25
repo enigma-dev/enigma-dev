22344a4579b538c1f7e48d58a781d17fe5b84676
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2018-09-24 22:12:13 -0400

    add comment explaining GLM_FORCE_RADIANS

--- ENIGMAsystem/SHELL/Graphics_Systems/General/GSmatrix_impl.h
+++ ENIGMAsystem/SHELL/Graphics_Systems/General/GSmatrix_impl.h
@@ -24,6 +24,9 @@
 #ifndef ENIGMA_GSMATRIX_IMPL_H
 #define ENIGMA_GSMATRIX_IMPL_H
 
+// this is for GLM's transition to radians only
+// let's define it just in case some enigma
+// user's package manager happens to be old
 #define GLM_FORCE_RADIANS
 
 #include <glm/glm.hpp>
