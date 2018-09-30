d0f79e11b7eed7c80e2e2d5f40523cba3e3d3773
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2018-09-30 15:30:56 -0400

    reset surface changes hard

--- ENIGMAsystem/SHELL/Graphics_Systems/OpenGL1/GLsurface.cpp
+++ ENIGMAsystem/SHELL/Graphics_Systems/OpenGL1/GLsurface.cpp
@@ -180,14 +180,11 @@ void surface_set_target(int id)
   //This fixes several consecutive surface_set_target() calls without surface_reset_target.
   int prevFbo;
   glGetIntegerv(GL_FRAMEBUFFER_BINDING_EXT, &prevFbo);
-  if (prevFbo != 0) {
-    glPopAttrib(); glPopMatrix();
-    //d3d_projection_stack_pop();
-  }
+  if (prevFbo != 0) { glPopAttrib(); glPopMatrix(); d3d_projection_stack_pop(); }
   get_surface(surf,id);
   glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, surf.fbo); //bind it
   glPushMatrix(); //So you can pop it in the reset
-  //d3d_projection_stack_push();
+  d3d_projection_stack_push();
   glPushAttrib(GL_VIEWPORT_BIT); //same
   glViewport(0, 0, surf.width, surf.height);
   glScissor(0, 0, surf.width, surf.height);
@@ -201,7 +198,7 @@ void surface_reset_target(void)
   glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
   glPopAttrib();
   glPopMatrix();
-  //d3d_projection_stack_pop();
+  d3d_projection_stack_pop();
   glViewport(enigma::viewport_x, enigma::viewport_y, enigma::viewport_w, enigma::viewport_h);
   glScissor(enigma::viewport_x, enigma::viewport_y, enigma::viewport_w, enigma::viewport_h);
 }
