992d989f0d6e0ab6cfecb25116bd9f5c661eb31c
time-killer-games <samuelvenable@hotmail.com>
2018-10-11 21:08:09 -0400

    Update PFexternals.h

--- ENIGMAsystem/SHELL/Platforms/General/PFexternals.h
+++ ENIGMAsystem/SHELL/Platforms/General/PFexternals.h
@@ -27,7 +27,7 @@ enum {
 
 int external_define(std::string lib,std::string func,int calltype,bool returntype,int argcount,
                     bool t01=0,bool t02=0,bool t03=0,bool t04=0,bool t05=0,bool t06=0,bool t07=0,bool t08=0,
-                    bool t09=0,bool t10=0,bool t11=0,bool t12=0,bool t13=0,bool t14=0,bool t15=0,bool t16=0); 
+                    bool t09=0,bool t10=0,bool t11=0,bool t12=0,bool t13=0,bool t14=0,bool t15=0,bool t16=0);
 
 variant external_call(int id,variant a1=0,variant a2=0,variant a3=0,variant a4=0,variant a5=0,variant a6=0,variant a7=0,variant a8=0,
                              variant a9=0,variant a10=0,variant a11=0,variant a12=0,variant a13=0,variant a14=0,variant a15=0,variant a16=0);
