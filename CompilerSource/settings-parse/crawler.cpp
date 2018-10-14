7df1b07d5da31fb93b9870980ff8777b7caf8e49
Robert Colton <robertbcolton@gmail.com>
2018-10-14 19:43:12 -0400

    Fix Platform Inspection False Positives (#1404)
    * Changed the logic of `ff_matches` helper so that passing `fa_directory` to `file_find_*` returns only directories. This stops platform inspection from looking for `platforms_mandatory.h/Info/About.ey`.
    * Restructured the logic of platform inspection so that it no longer shows failed messages if one of the ey files it's looking for doesn't open. Reasoning behind this is because the platform inspection should not complain about not being able to open a file that doesn't exist. For example, this is so it doesn't complain about the "General" folders which do not need or have "Info" subfolders.
    * Changed `file_exists` in Win32 to use the `INVALID_FILE_ATTRIBUTES` define instead of a hardcoded hex.

--- CompilerSource/settings-parse/crawler.cpp
+++ CompilerSource/settings-parse/crawler.cpp
@@ -147,26 +147,25 @@ namespace extensions
     cout << "\n\n\n\nStarting platform inspection\n";
     for (string ef = file_find_first("ENIGMAsystem/SHELL/Platforms/*",fa_sysfile | fa_readonly | fa_directory | fa_nofiles); ef != ""; ef = file_find_next())
     {
-      cout << " - ENIGMAsystem/SHELL/Platforms/" + ef + "/Info/About.ey: ";
-      ifstream ext(("ENIGMAsystem/SHELL/Platforms/" + ef + "/Info/About.ey").c_str(), ios_base::in);
-      if (ext.is_open())
-      {
-        cout << "Opened.\n";
-        ey_data dat = parse_eyaml(ext,ef);
-        eyit hasname = dat.values.find("represents");
-        if (hasname == dat.values.end()) {
-          cout << "Skipping invalid platform API under `" << ef << "': File does not specify an OS it represents.";
-          continue;
-        }
-        
-        sdk_descriptor& sdk = all_platforms[toUpper(ef)];
-        sdk.name   = dat.get("name");
-        sdk.author = dat.get("author");
-        sdk.build_platforms = dat.get("build-platforms");
-        sdk.description = dat.get("description");
-        sdk.identifier  = dat.get("identifier");
-        sdk.represents  = dat.get("represents");
-      } else cout << "Failed!\n";
+      const string ef_path = "ENIGMAsystem/SHELL/Platforms/" + ef + "/Info/About.ey";
+      ifstream ext(ef_path.c_str(), ios_base::in);
+      if (!ext.is_open()) continue;
+
+      cout << " - " << ef_path << ": Opened.\n";
+      ey_data dat = parse_eyaml(ext,ef);
+      eyit hasname = dat.values.find("represents");
+      if (hasname == dat.values.end()) {
+        cout << "Skipping invalid platform API under `" << ef << "': File does not specify an OS it represents.";
+        continue;
+      }
+      
+      sdk_descriptor& sdk = all_platforms[toUpper(ef)];
+      sdk.name   = dat.get("name");
+      sdk.author = dat.get("author");
+      sdk.build_platforms = dat.get("build-platforms");
+      sdk.description = dat.get("description");
+      sdk.identifier  = dat.get("identifier");
+      sdk.represents  = dat.get("represents");
     }
     file_find_close();
     
