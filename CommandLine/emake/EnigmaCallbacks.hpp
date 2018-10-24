720106afbbdbc3de7fc9a9bbe5c18d1c22464635
Robert Colton <robertbcolton@gmail.com>
2018-10-24 14:43:25 -0400

    Async Compile & Remote Logging (#1421)
    * Added an accessor to `CallBack` to get the current progress status of the compile.
    * Added accessors to read the log message queue from `CallBack` which are synchronized using a semaphore. The design of the accessors is inherently thread-safe, as the user is expected to read the entire log queue and then clear the queue (which unlocks the queue) when they are done reading.
    * Added a public `ProcessOutput` method to `CallBack` that is expected to be called asynchronously as it attempts to read a single log message at a time from the IDE output redirect file.
    * Implemented the `AddFrame` IDE callback to `CallBack` that stores its text until the point a newline appears at the end of the current frame at which point it flushes a new log message to the queue.
    * Implemented the progress/output redirect mutator IDE callbacks in `CallBack` to accomplish the above.
    * Fixed warnings about incompatible function pointer cast in `EnigmaPlugin::Load` resulting from the fact that `GetProcAddress` returns `int*` by simply casting to the intermediate `void*` type. This looks like Windows is silly to me because `dlsym` on Unix/Linux/whatever returns `void*` so it doesn't have this warning.
    * Changed `EnigmaPlugin::Init` to accept a `CallBack` instance to initialize with.
    * Changed `RunServer` to accept a `CallBack` instance to read the log queue and such from.
    * Changed the server's implementation of the `CompileBuffer` rpc to launch the game compile in an async thread. It then enters a blocking (for now) loop to forward all logged messages and the current progress to the client. Later this can be made into a FIFO queue so the server can compile multiple games at once. That may later require that `CallBack` be changed to synchronize log messages coming from those new threads.
    * Fixed a warning about unused parameters in the `SetCurrentConfig` implementation by simply commenting the variable name. Also removed the reallocation which was wrong because GRPC actually allocates the reply for us.
    * Fixed a warning about unused local variable `syntax_error* err` in the `SetCurrentConfig` implementation by simply commenting it out. The reason the reply for this RPC is empty is because we're still considering making definitions a resource instead of tying it to settings.
    * Changed the protos makefile to always process the server proto. Building with the server mode disabled (the default) still works because we skip the grpc steps. Regardless, we want to use the same messages as the server in various places of the compiler now. In this particular case, I needed to use LogMessage over in `EnigmaCallbacks.hpp` which still needs to be built when the server is disabled.
    * Moved the `CompileMode` enum into the `CompileRequest` message.
    * Added the `LogMessage` and `ProgressMessage` proto messages Josh provided me to `server.proto` for structuring log messages.
    * Changed the `CompileReply` message to return a series of log messages and the current progress during game compile.

--- CommandLine/emake/EnigmaCallbacks.hpp
+++ CommandLine/emake/EnigmaCallbacks.hpp
@@ -6,12 +6,32 @@
 //Should be:
 //#include "backend/EnigmaCallbacks.h"
 
+#include "codegen/server.pb.h"
+
 #include <fstream>
+#include <string>
+#include <vector>
+#include <mutex>
+
+using ProgressMessage = buffers::ProgressMessage;
+using LogMessage = buffers::LogMessage;
 
 class CallBack : public EnigmaCallbacks
 {
 public:
   CallBack();
+  const ProgressMessage& GetProgress() const;
+  const LogMessage GetFirstLogMessage(bool &end) const;
+  const LogMessage GetNextLogMessage(bool &end) const;
+  void ClearLogMessages();
+  void ProcessOutput();
+
+private:
+  static ProgressMessage progressMessage;
+  static std::vector<LogMessage> logMessages;
+  static std::vector<LogMessage>::iterator logIt;
+  static std::mutex logMutex;
+  static std::ifstream outFile;
 
   static void FrameOpen();
   static void AppendFrame(const char*);
@@ -22,8 +42,6 @@ public:
   static void ResetRedirect();
   static int Execute(const char*, const char**, bool);
   static Image* CompressImage(char *, int);
-  static void* OutputThread(void*);
-  static std::ifstream _outFile;
 };
 
 #endif
