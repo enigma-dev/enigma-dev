b84c57407e713cdd9717d751395772dd235b9f2b
time-killer-games <samuelvenable@hotmail.com>
2018-12-11 18:11:51 -0500

    Update Main.cpp

--- ENIGMAsystem/SHELL/Platforms/SDL/Main.cpp
+++ ENIGMAsystem/SHELL/Platforms/SDL/Main.cpp
@@ -3,10 +3,13 @@
 #include <SDL2/SDL.h> //sdl does a #define main SDL_main...
 #include <string>
 
-namespace enigma_user {
-  working_directory = SDL_GetBasePath();
+namespace enigma {
+    void set_working_directory() {
+      enigma_user::working_directory = SDL_GetBasePath(); 
+    }
 }
 
 int main(int argc, char** argv) {
+  enigma::set_working_directory();
   return enigma::enigma_main(argc, argv);
 }
