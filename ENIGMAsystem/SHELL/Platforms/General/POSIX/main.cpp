a2ddd5d95f2cbc7f5abff71b560e35484ba1ca77
time-killer-games <samuelvenable@hotmail.com>
2018-12-12 16:17:58 -0500

    Update main.cpp

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/main.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/main.cpp
@@ -9,22 +9,6 @@
 
 using std::string;
 
-namespace enigma_user {
-  double set_working_directory(string dname) {
-    if (!dname.empty()) {
-      while (*dname.rbegin() == '/') {
-        dname.erase(dname.size() - 1);
-      }
-    }
-
-    struct stat sb;
-    if (stat((char *)dname.c_str(), &sb) == 0 && S_ISDIR(sb.st_mode))
-      return (chdir((char *)dname.c_str()) == 0);
-
-    return 0;
-  }
-}  // namespace enigma_user
-
 namespace enigma {
 void set_working_directory() {
   // Set the working_directory
