6d3a11707e513a57b2b597d1a921661658539414
time-killer-games <samuelvenable@hotmail.com>
2018-12-11 19:08:21 -0500

    thorax

--- ENIGMAsystem/SHELL/Platforms/xlib/XLIBmain.cpp
+++ ENIGMAsystem/SHELL/Platforms/xlib/XLIBmain.cpp
@@ -34,11 +34,6 @@
 #include <X11/Xutil.h>
 
 #include <sys/types.h>  //getpid
-#include <sys/stat.h>
-#include <limits.h>
-#include <unistd.h>
-#include <libgen.h>
-#include <cstdlib>
 #include <string>
 
 using std::string;
@@ -47,47 +42,12 @@ using namespace enigma::x11;
 
 namespace enigma_user {
   const int os_type = os_linux;
-
-  double set_working_directory(string dname) {
-    if (!dname.empty()) {
-      while (*dname.rbegin() == '/') {
-        dname.erase(dname.size() - 1);
-      }
-    }
-
-    struct stat sb;
-    if (stat((char *)dname.c_str(), &sb) == 0 && S_ISDIR(sb.st_mode))
-      return (chdir((char *)dname.c_str()) == 0);
-
-    return 0;
-  }
 }  // namespace enigma_user
 
 namespace enigma {
 
 void (*WindowResizedCallback)();
 void WindowResized();
-  
-void set_working_directory() {
-  // Set the working_directory
-  char buffer[PATH_MAX + 1];
-  if (getcwd(buffer, PATH_MAX + 1) != NULL)
-    enigma_user::working_directory = buffer;
-
-  // Set the program_directory
-  buffer[0] = 0;
-  ssize_t count = readlink("/proc/self/exe", buffer, PATH_MAX + 1);
-  if (count !=  -1)
-    enigma_user::program_directory = dirname(buffer) + string("/");
-
-  // Set the temp_directory
-  char const *env = getenv("TMPDIR");
-
-  if (env == 0)
-    env = "/tmp/";
-
-  enigma_user::temp_directory = env; 
-}
 
 int handleEvents() {
   while (XQLength(enigma::x11::disp) || XPending(enigma::x11::disp)) {
