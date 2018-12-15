bcc5b9cfc9c1bde7b1499a070cfcdf1568fdb48d
time-killer-games <samuelvenable@hotmail.com>
2018-12-15 04:29:21 -0500

    Update LINUXjoystick.cpp

--- ENIGMAsystem/SHELL/Platforms/xlib/LINUXjoystick.cpp
+++ ENIGMAsystem/SHELL/Platforms/xlib/LINUXjoystick.cpp
@@ -38,6 +38,14 @@
 using namespace std;
 
 namespace enigma {
+    
+  int joystick_decrease_id(int id) {
+    id -= 1;
+    if (id < 0)
+      id = 0;
+    return id;
+  }
+  
   struct e_joystick {
     int device;
     string name;
