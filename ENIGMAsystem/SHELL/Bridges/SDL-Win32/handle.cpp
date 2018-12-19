d17ef1661284adafcf0925990245b67797cd0692
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2018-12-19 02:49:19 -0500

    provide sdl hinstance

--- ENIGMAsystem/SHELL/Bridges/SDL-Win32/handle.cpp
+++ ENIGMAsystem/SHELL/Bridges/SDL-Win32/handle.cpp
@@ -7,6 +7,7 @@
 namespace enigma {
 
 HWND hWnd;
+HINSTANCE hInstance;
 
 HWND get_window_handle() {
   if (hWnd == NULL) { // dsound or d3d could init this first depending on systems used
@@ -14,9 +15,24 @@ HWND get_window_handle() {
     SDL_VERSION(&systemInfo.version);
     SDL_GetWindowWMInfo((SDL_Window*)windowHandle, &systemInfo);
     hWnd = systemInfo.info.win.window;
+    hInstance = systemInfo.info.win.hinstance;
   }
 
   return hWnd;
 }
 
 } //namespace enigma
+
+namespace enigma_user {
+
+#if GM_COMPATIBILITY_VERSION <= 81
+unsigned long long window_handle() {
+  return (unsigned long long)enigma::hWnd;
+}
+#else
+void* window_handle() {
+  return enigma::hWnd;
+}
+#endif
+
+} // namespace enigma_user
