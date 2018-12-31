8841882480f94d0e66ebba95eef407e69151c206
time-killer-games <samuelvenable@hotmail.com>
2018-12-30 22:47:30 -0500

    Update Joystick.cpp

--- ENIGMAsystem/SHELL/Platforms/SDL/Joystick.cpp
+++ ENIGMAsystem/SHELL/Platforms/SDL/Joystick.cpp
@@ -3,82 +3,55 @@
 #include "Platforms/General/PFjoystick.h"
 #include <string>
 
+namespace {
+
 SDL_Joystick *joystick1;
 SDL_Joystick *joystick2;
 
 bool update = true;
 
+static inline SDL_Joystick* joystick_get_handle(int id) {
+  return (id - 1 < 0.5) ? joystick1 : joystick2;
+}
+
+} // anonymous namespace
+
 namespace enigma_user {
 
 bool joystick_exists(int id) {
-  bool exists;
-
-  if (id - 1 < 0.5)
-    exists = (SDL_JoystickGetAttached(joystick1) == SDL_TRUE);
-  else
-    exists = (SDL_JoystickGetAttached(joystick2) == SDL_TRUE);
-
-  return exists;
+  return (SDL_JoystickGetAttached(joystick_get_handle(id)) == SDL_TRUE);
 }
 
 std::string joystick_name(int id) {
   const char *name;
-
-  if (id - 1 < 0.5)
-    name = SDL_JoystickName(joystick1);
-  else
-    name = SDL_JoystickName(joystick2);
-
-  return name;
+  name = SDL_JoystickName(joystick_get_handle(id));
+  return (name ? name : "");
 }
 
 int joystick_axes(int id) {
   int axes = 0;
-
-  if (id - 1 < 0.5) {
-    if (SDL_JoystickNumAxes(joystick1) >= 0)
-      axes = SDL_JoystickNumAxes(joystick1);
-  } else {
-    if (SDL_JoystickNumAxes(joystick2) >= 0)
-      axes = SDL_JoystickNumAxes(joystick2);
-  }
+  if (SDL_JoystickNumAxes(joystick_get_handle(id)) >= 0)
+    axes = SDL_JoystickNumAxes(joystick_get_handle(id));
 
   return axes;
 }
 
 int joystick_buttons(int id) {
   int buttons = 0;
-
-  if (id - 1 < 0.5) {
-    if (SDL_JoystickNumButtons(joystick1) >= 0)
-      buttons = SDL_JoystickNumButtons(joystick1);
-  } else {
-    if (SDL_JoystickNumButtons(joystick2) >= 0)
-      buttons = SDL_JoystickNumButtons(joystick2);
-  }
-
+  if (SDL_JoystickNumButtons(joystick_get_handle(id)) >= 0)
+    buttons = SDL_JoystickNumButtons(joystick_get_handle(id));
   return buttons;
 }
 
 bool joystick_button(int id, int numb) {
-  bool button;
-
-  if (id - 1 < 0.5)
-    button = SDL_JoystickGetButton(joystick1, numb - 1);
-  else
-    button = SDL_JoystickGetButton(joystick2, numb - 1);
-
-  return button;
+  return SDL_JoystickGetButton(joystick_get_handle(id), numb - 1);
 }
 
 double joystick_axis(int id, int numb) {
   double axis;
   double pos = 0;
 
-  if (id - 1 < 0.5)
-    axis = SDL_JoystickGetAxis(joystick1, axis - 1);
-  else
-    axis = SDL_JoystickGetAxis(joystick2, axis - 1);
+  axis = SDL_JoystickGetAxis(joystick_get_handle(id), axis - 1);
 
   if (axis != -256 && axis != 0) // don't ask why this shit is necessary; it just is...
     pos = (axis / 32768);
