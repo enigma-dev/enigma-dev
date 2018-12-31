63c4ca8f8f43c26b2ab939174cce345c380ff714
time-killer-games <samuelvenable@hotmail.com>
2018-12-30 23:58:35 -0500

    Update dialogs.h

--- ENIGMAsystem/SHELL/Widget_Systems/DlgMod/dialogs.h
+++ ENIGMAsystem/SHELL/Widget_Systems/DlgMod/dialogs.h
@@ -21,18 +21,18 @@
 
 namespace enigma_user {
   
-  //int show_message(std::string str);
-  double show_question(std::string str);
-  std::string get_string(std::string str, std::string def);
-  std::string get_password(std::string str, std::string def);
-  double get_integer(std::string str, double def);
-  double get_passcode(std::string str, double def);
-  std::string get_open_filename(std::string filter, std::string fname);
-  std::string get_save_filename(std::string filter, std::string fname);
-  std::string get_open_filename_ext(std::string filter, std::string fname, std::string dir, std::string title);
-  std::string get_save_filename_ext(std::string filter, std::string fname, std::string dir, std::string title);
-  std::string get_directory(std::string dname);
-  std::string get_directory_alt(std::string capt, std::string root);
-  double get_color(double defcol);
+//int show_message(std::string str);
+bool show_question(std::string str);
+std::string get_string(std::string str, std::string def);
+std::string get_password(std::string str, std::string def);
+double get_integer(std::string str, double def);
+double get_passcode(std::string str, double def);
+std::string get_open_filename(std::string filter, std::string fname);
+std::string get_save_filename(std::string filter, std::string fname);
+std::string get_open_filename_ext(std::string filter, std::string fname, std::string dir, std::string title);
+std::string get_save_filename_ext(std::string filter, std::string fname, std::string dir, std::string title);
+std::string get_directory(std::string dname);
+std::string get_directory_alt(std::string capt, std::string root);
+int get_color(int defcol);
   
 } // namespave enigma_user
