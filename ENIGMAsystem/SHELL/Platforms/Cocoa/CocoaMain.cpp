fcd6f00f07efbcfeb95d0ad0bd4468e615f2b7c3
time-killer-games <samuelvenable@hotmail.com>
2019-01-01 01:59:25 -0500

    Update CocoaMain.cpp

--- ENIGMAsystem/SHELL/Platforms/Cocoa/CocoaMain.cpp
+++ ENIGMAsystem/SHELL/Platforms/Cocoa/CocoaMain.cpp
@@ -37,33 +37,35 @@
 using std::string;
 
 namespace enigma {
-  void SetResizeFptr();
+
+void SetResizeFptr();
   
-  void initialize_directory_globals() {
-    // Set the working_directory
-    char buffer[PATH_MAX + 1];
-    if (getcwd(buffer, PATH_MAX + 1) != NULL)
-      enigma_user::working_directory = enigma::add_slash(buffer);
+void initialize_directory_globals() {
+  // Set the working_directory
+  char buffer[PATH_MAX + 1];
+  if (getcwd(buffer, PATH_MAX + 1) != NULL)
+    enigma_user::working_directory = add_slash(buffer);
 
-    // Set the program_directory
-    buffer[0] = 0;
+  // Set the program_directory
+  buffer[0] = 0;
 
-    uint32_t bufsize = sizeof(buffer);
-    if (_NSGetExecutablePath(buffer, &bufsize) == 0) {
-      enigma_user::program_directory = enigma::add_slash(dirname(buffer));
-    }
-
-    // Set the temp_directory
-    char *env = getenv("TMPDIR");
-
-    if (env == NULL)
-      enigma_user::temp_directory = "/tmp/";
-    else 
-      enigma_user::temp_directory = env;
-    
-    enigma_user::temp_directory = enigma::add_slash(enigma_user::temp_directory);
+  uint32_t bufsize = sizeof(buffer);
+  if (_NSGetExecutablePath(buffer, &bufsize) == 0) {
+    enigma_user::program_directory = add_slash(dirname(buffer));
   }
+
+  // Set the temp_directory
+  char *env = getenv("TMPDIR");
+
+  if (env == NULL)
+    enigma_user::temp_directory = "/tmp/";
+  else 
+    enigma_user::temp_directory = env;
+    
+  enigma_user::temp_directory = add_slash(enigma_user::temp_directory);
 }
+  
+} // namespace enigma
 
 int main(int argc,char** argv) {
   enigma::initialize_directory_globals();
