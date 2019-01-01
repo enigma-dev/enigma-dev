c86d2761a0d8bb1cea3412177ae00b20224d2a01
time-killer-games <samuelvenable@hotmail.com>
2019-01-01 01:42:24 -0500

    Update POSIXworkdir.cpp

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/POSIXworkdir.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/POSIXworkdir.cpp
@@ -6,22 +6,33 @@
 
 using std::string;
 
-namespace enigma_user {
-  bool set_working_directory(string dname) {
-    if (!dname.empty()) {
-      while (*dname.rbegin() == '/') {
-        dname.erase(dname.size() - 1);
-      }
-    }
+namespace enigma {
 
-    struct stat sb;
-    if (stat((char *)dname.c_str(), &sb) == 0 && S_ISDIR(sb.st_mode)) {
-      if (chdir((char *)dname.c_str()) == 0) {
-        working_directory = dname + string("/");
-        return true;
-      }
-    }
-
-    return false;
-  }
+static inline string add_slash(const string& dir) {
+  if (dir.empty() || *dir.rbegin() != '/') return dir + '/';
+  return dir;
 }
+  
+} // namespace enigma
+
+namespace enigma_user {
+
+bool set_working_directory(string dname) {
+  if (!dname.empty()) {
+    while (*dname.rbegin() == '/') {
+      dname.erase(dname.size() - 1);
+    }
+  }
+
+  struct stat sb;
+  if (stat((char *)dname.c_str(), &sb) == 0 && S_ISDIR(sb.st_mode)) {
+    if (chdir((char *)dname.c_str()) == 0) {
+      working_directory = dname + string("/");
+      return true;
+    }
+  }
+
+  return false;
+}
+
+} // namespace enigma_user
