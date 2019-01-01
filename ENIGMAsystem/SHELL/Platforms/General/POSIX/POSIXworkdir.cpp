e60a438a8f320f5ce4460d3cd34a06c0feef77d2
time-killer-games <samuelvenable@hotmail.com>
2019-01-01 10:05:18 -0500

    Update POSIXworkdir.cpp

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/POSIXworkdir.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/POSIXworkdir.cpp
@@ -21,7 +21,7 @@ bool set_working_directory(string dname) {
   if (chdir((char *)dname.c_str()) == 0) {
     char buffer[PATH_MAX + 1]; 
     if (getcwd(buffer, PATH_MAX + 1) != NULL) {
-      working_directory = enigma::add_slash();
+      working_directory = enigma::add_slash(buffer);
       return true;
     }
   }
