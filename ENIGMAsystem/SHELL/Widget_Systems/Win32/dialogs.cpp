719dc75e7c33d55164c90c62defe1ebcbfcf821d
time-killer-games <samuelvenable@hotmail.com>
2019-01-06 16:58:11 -0500

    Update dialogs.cpp

--- ENIGMAsystem/SHELL/Widget_Systems/Win32/dialogs.cpp
+++ ENIGMAsystem/SHELL/Widget_Systems/Win32/dialogs.cpp
@@ -491,12 +491,13 @@ string get_open_filename(string filter,string filename,string caption)
 
   char fn[MAX_PATH];
   strcpy(fn, filename.c_str());
+  strcpy(fn, remove_slash(filename).c_str());
 
   OPENFILENAME ofn;
   ofn.lStructSize = sizeof(ofn); ofn.hwndOwner = enigma::hWnd; ofn.hInstance = NULL;
   ofn.lpstrFilter = filter.c_str(); ofn.lpstrCustomFilter = NULL;
   ofn.nMaxCustFilter = 0; ofn.nFilterIndex = 0;
-  ofn.lpstrFile = (char *)enigma::remove_slash(fn).c_str(); ofn.nMaxFile = MAX_PATH;
+  ofn.lpstrFile = fn; ofn.nMaxFile = MAX_PATH;
   ofn.lpstrFileTitle = NULL; ofn.nMaxFileTitle = 0;
   ofn.lpstrInitialDir = NULL; ofn.lpstrTitle = caption.length() ? caption.c_str() : NULL;
   ofn.Flags=OFN_FILEMUSTEXIST | OFN_HIDEREADONLY | OFN_NOCHANGEDIR;
@@ -518,6 +519,7 @@ string get_save_filename(string filter, string filename, string caption)
 
   char fn[MAX_PATH];
   strcpy(fn, filename.c_str());
+  strcpy(fn, remove_slash(filename).c_str());
 
   OPENFILENAME ofn;
   ofn.lStructSize = sizeof(ofn); ofn.hwndOwner = enigma::hWnd; ofn.hInstance = NULL;
