0bc1f2b237aa624c921c5575ea5a4dad142c9949
time-killer-games <samuelvenable@hotmail.com>
2019-01-14 12:02:21 -0500

    Update POSIXdirectory.cpp

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/POSIXdirectory.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/POSIXdirectory.cpp
@@ -18,6 +18,7 @@ bool set_working_directory(string dname) {
     char buffer[PATH_MAX + 1]; 
     if (getcwd(buffer, PATH_MAX + 1) != NULL) {
       working_directory = add_slash(buffer);
+      game_save_id = working_directory;
       return true;
     }
   }
