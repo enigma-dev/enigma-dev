d5f47c07d106d6f4cd76b2e9f13d4e5d8947bdde
time-killer-games <samuelvenable@hotmail.com>
2019-01-14 19:15:59 -0500

    Update main.cpp

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/main.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/main.cpp
@@ -20,15 +20,23 @@ void initialize_directory_globals() {
   char buffer[PATH_MAX + 1];
   if (getcwd(buffer, PATH_MAX + 1) != NULL)
     enigma_user::working_directory = add_slash(buffer);
-  
-  // Set the game_save_id
-  enigma_user::game_save_id = enigma_user::working_directory;
 
   // Set the program_directory
   buffer[0] = 0;
   ssize_t count = readlink("/proc/self/exe", buffer, PATH_MAX + 1);
   if (count !=  -1)
     enigma_user::program_directory = add_slash(dirname(buffer));
+
+  // Set the game_save_id
+  buffer[0] = 0;
+  // we need to do this again because dirname modifies its argument
+  ssize_t count = readlink("/proc/self/exe", buffer, PATH_MAX + 1);
+  char *env = getenv("HOME");
+
+  if (count != -1 && env != NULL) {
+    enigma_user::game_save_id = add_slash(string(env)) + string(".config/");
+    enigma_user::game_save_id += add_slash(basename(buffer));
+  }
 
   // Set the temp_directory
   char *env = getenv("TMPDIR");
