632c68796cf8ada11115a62463e30be77c0d90aa
time-killer-games <samuelvenable@hotmail.com>
2019-02-03 17:07:06 -0500

    Update dialogs.cpp

--- ENIGMAsystem/SHELL/Widget_Systems/General/tinyfiledialogs/dialogs.cpp
+++ ENIGMAsystem/SHELL/Widget_Systems/General/tinyfiledialogs/dialogs.cpp
@@ -72,7 +72,7 @@ static inline void window_activate() {
   #endif
 }
 
-static inline string message_helper(const string &str) {
+static inline string message_helper(string str) {
   msg = tfd_add_escaping(msg);
   caption = tfd_add_escaping(caption);
   
@@ -279,7 +279,7 @@ void show_info(string text, int bgcolor, int left, int top, int width, int heigh
 
 }
 
-int show_message(const string &str) {
+int show_message(string str) {
   caption = window_get_caption();
   str = message_helper(str);
   tinyfd_messageBox(caption.c_str(), msg.c_str(), "ok", "info", 1, tfd_DialogEngine());
