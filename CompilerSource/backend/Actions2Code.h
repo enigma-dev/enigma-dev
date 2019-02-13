ff783971ec92ca908533fdf1c9c6d9820e60c055
Robert Colton <robertbcolton@gmail.com>
2019-02-12 22:28:35 -0500

    Remove Obsolete Proto2ES (#1533)
    Well, this is it, the dawn of a new ENIGMA and the final stage of the transition to Google Protocol Buffers. When Josh created the GameData structure (basically converts EnigmaStruct into a proto) in #1448, he made Proto2ES obsolete and no longer necessary. This pull request completes the transition by finally adding a new method to the `compileEGMf` DLL that accepts a protocol buffer and deleting Proto2ES from emake's sources. With this change, emake, RGM, and any new frontend tools should reach peak compiling performance as they are passing a proto directly to the compiler with no intermediate translation to other formats. Other tools, like LGM, will continue to use the outdated EnigmaStruct, subject to binary compatibility-breaking changes, for the foreseeable future until someone decides to migrate such tools to protos too.
    
    ### Summary of Changes
    * Changed the `EnigmaPlugin::BuildGame` overload that accepts a proto game to no longer call `Proto2ES` and instead call the newly loaded `plugin_CompileProto` function. It's worth mentioning that, for some reason, we had this set up to take `Game` instead of `Project` so I didn't have a need to change the signature yet. I also wasn't sure the distinction fundies was making between `Game` and `Project` when he made the latter contain the former. For now, I just made a temporary `Project` and set the passed `Game` on it.
    * Deleted the `Proto2ES` header and source file from emake's sources.
    * Changed the CompilerSource makefile to link to zlib and lodepng since it now needs both in order to compile a proto game.
    * Changed the `GameData` structure to define all of the proto-based copy constructors and flatten the tree. Some resources had specific requirements, such as objects and timelines which needed their drag and drop actions converted to code. The actions to code conversion will probably later be moved into libEGM because RGM is not going to support the old drag and drop actions format. Sounds, Sprites, and Backgrounds also needed the code from Proto2ES in order to load their binary data in for the compiler.
    * Changed `CompilerSource/compiler/compile.cpp` to export a new `compileProto` DLL function for frontend tools that would like to use ENIGMA's new Protocol Buffers interface. The Protocol Buffers interface, as should be obvious, has several advantages including the ability to serialize and communicate the buffers to a remote client or to make changes without breaking binary compatibility.
    * Changed the object parsing and linking logic to deal with events that have a subname instead of a subid (collision events in GMX have the other object as a string). From the comments, it appears that Josh was confused and thought that name was the composite name of the main id and subid, when in reality it's just the name of the "other" object for collision events at the moment. Later I'd like to see this changed around to support what I call "templated" events where you could hypothetically have an event for any kind of resource (perhaps an event for when a selected sprite loads). Regardless, this specifically addresses being able to build AI Tutorial project from emake.
    * Addressed the `"<undefined>"` resource name edge case in the GMX reader using the new resource reference option provided by #1547 to know which string fields to compare.
    * Tweaked the top-level makefile to have `liblodepng` as a dependency of the default target (which is the compiler). This has to be done because `GameData`, which is part of the compiler backend, now requires lodepng in order to load the binary data for Sprites and Backgrounds.
    * Changed the emake makefile to no longer link to zlib and lodepng since it no longer has a direct dependency on them. Also removed the dependency in the main makefile too.

---
+++ CompilerSource/backend/Actions2Code.h
@@ -0,0 +1,163 @@
+#include "Event.pb.h"
+
+static inline std::string string_replace_all(std::string str, std::string substr, std::string nstr)
+{
+  size_t pos = 0;
+  while ((pos = str.find(substr, pos)) != std::string::npos)
+  {
+    str.replace(pos, substr.length(), nstr);
+    pos += nstr.length();
+  }
+  return str;
+}
+
+static std::string Argument2Code(const buffers::resources::Argument& arg) {
+  using buffers::resources::ArgumentKind;
+  std::string val = arg.string();
+
+  switch (arg.kind()) {
+    case ArgumentKind::ARG_BOTH:
+      // treat as literal if starts with quote (")
+      if (val[0] == '"' || val[0] == '\'') return val;
+      // else fall through
+    case ArgumentKind::ARG_STRING:
+      return '\"' + string_replace_all(string_replace_all(val, "\\", "\\\\"), "\"", "\"+'\"'+\"") + '\"';
+    case ArgumentKind::ARG_BOOLEAN:
+      return std::to_string(val != "0");
+    case ArgumentKind::ARG_SPRITE:
+      return arg.has_sprite() ? arg.sprite() : "-1";
+    case ArgumentKind::ARG_SOUND:
+      return arg.has_sound() ? arg.sound() : "-1";
+    case ArgumentKind::ARG_BACKGROUND:
+      return arg.has_background() ? arg.background() : "-1";
+    case ArgumentKind::ARG_PATH:
+      return arg.has_path() ? arg.path() : "-1";
+    case ArgumentKind::ARG_SCRIPT:
+      return arg.has_script() ? arg.script() : "-1";
+    case ArgumentKind::ARG_OBJECT:
+      return arg.has_object() ? arg.object() : "-1";
+    case ArgumentKind::ARG_ROOM:
+      return arg.has_room() ? arg.room() : "-1";
+    case ArgumentKind::ARG_FONT:
+      return arg.has_font() ? arg.font() : "-1";
+    case ArgumentKind::ARG_TIMELINE:
+      return arg.has_timeline() ? arg.timeline() : "-1";
+    case ArgumentKind::ARG_MENU:
+    case ArgumentKind::ARG_COLOR:
+    default:
+      if (val.empty()) {
+        if (arg.kind() == ArgumentKind::ARG_STRING)
+          return "\"\"";
+        else
+          return "0";
+      }
+      // else fall all the way through
+  }
+
+  return val;
+}
+
+static std::string Actions2Code(const ::google::protobuf::RepeatedPtrField< buffers::resources::Action >& actions) {
+  using buffers::resources::ActionKind;
+  using buffers::resources::ActionExecution;
+  std::string code = "";
+
+  int numberOfBraces = 0; // gm ignores brace actions which are in the wrong place or missing
+  int numberOfIfs = 0; // gm allows multipe else actions after 1 if, so its important to track the number
+
+  for (const auto &action : actions) {
+    const auto &args = action.arguments();
+
+    bool in_with = action.use_apply_to() && action.who_name() != "self";
+    if (in_with)
+      code += "with (" + action.who_name() + ")\n";
+
+    switch (action.kind()) {
+      case ActionKind::ACT_BEGIN:
+        code += '{';
+        numberOfBraces++;
+        break;
+      case ActionKind::ACT_END:
+        if (numberOfBraces > 0) {
+          code += '}';
+          numberOfBraces--;
+        }
+        break;
+      case ActionKind::ACT_ELSE:
+        if (numberOfIfs > 0) {
+          code += "else ";
+          numberOfIfs--;
+        }
+        break;
+      case ActionKind::ACT_EXIT:
+        code += "exit;";
+        break;
+      case ActionKind::ACT_REPEAT:
+        code += "repeat (" + args.Get(0).string() + ")";
+        break;
+      case ActionKind::ACT_VARIABLE:
+        code += args.Get(0).string();
+        if (action.relative())
+          code += " += ";
+        else
+          code += " = ";
+        code += args.Get(1).string();
+        break;
+      case ActionKind::ACT_CODE:
+        code += "{\n" + args.Get(0).string() + "\n/**/\n}";
+        break;
+      case ActionKind::ACT_NORMAL:
+        if (action.exe_type() == ActionExecution::EXEC_NONE) break;
+
+        if (action.is_question()) {
+          code += "__if__ = ";
+          numberOfIfs++;
+        }
+
+        if (action.is_not())
+          code += "!";
+
+        if (action.relative()) {
+          if (action.is_question())
+            code += "(argument_relative = " + std::to_string(action.relative()) + ", ";
+          else
+            code += "{\nargument_relative = " + std::to_string(action.relative()) + ";\n";
+        }
+
+        if (action.is_question() && action.exe_type() == ActionExecution::EXEC_CODE)
+          code += action.code_string();
+        else
+          code += action.function_name();
+
+        if (action.exe_type() == ActionExecution::EXEC_FUNCTION) {
+          code += '(';
+          for (int i = 0; i < args.size(); i++) {
+            if (i != 0)
+              code += ',';
+            code += Argument2Code(args.Get(i));
+          }
+          code += ')';
+        }
+
+        if (action.relative())
+          code += action.is_question() ? ");" : "\n}";
+        if (action.is_question()) {
+          code += "\nif (__if__)";
+        }
+        break;
+      default:
+        break;
+    }
+    code += '\n';
+  }
+
+  // someone forgot the closing block action
+  if (numberOfBraces > 0)
+    for (int i = 0; i < numberOfBraces; i++)
+      code += "\n}";
+
+  if (numberOfIfs > 0)
+    code = "var __if__ = false;\n" + code;
+
+  return code;
+}
