89ddbaf10ba353e1ee73e45580e5ae29ceb69664
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2019-02-14 04:24:39 -0500

    add libpng-util to replace lodepng shared sources

---
+++ shared/libpng-util/libpng-util.h
@@ -0,0 +1,6 @@
+
+unsigned libpng_encode_memory(unsigned char** out, size_t* outsize,
+                               const unsigned char* image, unsigned w, unsigned h,
+                               LodePNGColorType colortype, unsigned bitdepth);
+unsigned libpng_encode32(unsigned char** out, size_t* outsize, const unsigned char* image, unsigned w, unsigned h);
+unsigned libpng_decode32(unsigned char** out, unsigned* w, unsigned* h, const unsigned char* in, size_t insize);
