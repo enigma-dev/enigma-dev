f43c7be3bea853c802a2bf31548b0091790239ad
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2019-03-08 14:25:36 -0500

    do not cache the window dimensions in fullscreen

--- ENIGMAsystem/SHELL/Platforms/General/PFwindow.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/PFwindow.cpp
@@ -71,7 +71,15 @@ void compute_window_size() {
     }
     enigma_user::window_set_rectangle(windowX, windowY, windowWidth, windowHeight);
   } else {
+    int oldX = windowX,
+        oldY = windowY,
+        oldWidth = windowWidth,
+        oldHeight = windowHeight;
     enigma_user::window_set_rectangle(0, 0, parWidth, parHeight);
+    windowX = oldX;
+    windowY = oldY;
+    windowWidth = oldWidth;
+    windowHeight = oldHeight;
   }
 }
 
