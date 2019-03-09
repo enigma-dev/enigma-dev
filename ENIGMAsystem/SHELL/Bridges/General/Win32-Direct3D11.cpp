e40b535cfb74cce6d061523d7d9a8aa63dcf40f1
Robert Colton <robertbcolton@gmail.com>
2019-03-09 15:31:06 -0500

    Add Direct3D11 Support for MSAA (#1579)
    Very simple, we just release the render target, its associated depth buffer, and the swap chain then set the desired samples and recreate the swap chain and render target. The query for the sample levels supported is basically the same as D3D9 but with an additional requirement to check the quality levels. Finally, I also changed the format to have 2-space indentation like the rest of the engine to make it easier to read for myself.

--- ENIGMAsystem/SHELL/Bridges/General/Win32-Direct3D11.cpp
+++ ENIGMAsystem/SHELL/Bridges/General/Win32-Direct3D11.cpp
@@ -53,7 +53,14 @@ using namespace enigma::dx11;
 
 int swap_interval = 0;
 
-void initialize_render_targets() {
+void release_render_targets() {
+  m_deviceContext->OMSetRenderTargets(0, 0, 0);
+  m_renderTargetView->Release();
+  m_depthStencilBuffer->Release();
+  m_depthStencilView->Release();
+}
+
+void initialize_render_targets(UINT samples=1) {
   HRESULT result;
 
   // Get the pointer to the back buffer.
@@ -83,8 +90,8 @@ void initialize_render_targets() {
   depthBufferDesc.MipLevels = 1;
   depthBufferDesc.ArraySize = 1;
   depthBufferDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
-  depthBufferDesc.SampleDesc.Count = 1;
-  depthBufferDesc.SampleDesc.Quality = 0;
+  depthBufferDesc.SampleDesc.Count = samples;
+  depthBufferDesc.SampleDesc.Quality = D3D11_STANDARD_MULTISAMPLE_PATTERN;
   depthBufferDesc.Usage = D3D11_USAGE_DEFAULT;
   depthBufferDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
   depthBufferDesc.CPUAccessFlags = 0;
@@ -98,7 +105,7 @@ void initialize_render_targets() {
   // Set up the depth stencil view description.
   D3D11_DEPTH_STENCIL_VIEW_DESC depthStencilViewDesc = { };
   depthStencilViewDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
-  depthStencilViewDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+  depthStencilViewDesc.ViewDimension = samples > 1 ? D3D11_DSV_DIMENSION_TEXTURE2DMS : D3D11_DSV_DIMENSION_TEXTURE2D;
   depthStencilViewDesc.Texture2D.MipSlice = 0;
 
   result = m_device->CreateDepthStencilView(m_depthStencilBuffer, &depthStencilViewDesc, &m_depthStencilView);
@@ -122,85 +129,59 @@ void initialize_render_targets() {
 
 } // namespace anonymous
 
-namespace enigma
-{
-  extern void (*WindowResizedCallback)();
-  void WindowResized() {
+namespace enigma {
 
-    // release all the old references first
-    m_deviceContext->OMSetRenderTargets(0, 0, 0);
-    m_renderTargetView->Release();
-    m_depthStencilBuffer->Release();
-    m_depthStencilView->Release();
+extern void (*WindowResizedCallback)();
+void WindowResized() {
+  // release all the old references first
+  release_render_targets();
 
-    m_swapChain->ResizeBuffers(0, 0, 0, DXGI_FORMAT_UNKNOWN, 0);
+  // resize the buffers to match the screen
+  m_swapChain->ResizeBuffers(0, 0, 0, DXGI_FORMAT_UNKNOWN, 0);
 
-    // recreate the render target and depth stencil buffers
-    // and their related views with the new window size
-    initialize_render_targets();
+  // recreate the render target and depth stencil buffers
+  // and their related views with the new window size
+  initialize_render_targets();
 
-    // clear the window color, viewport does not need set because backbuffer was just recreated
-    enigma_user::draw_clear(enigma_user::window_get_color());
-  }
+  // clear the window color, viewport does not need set because backbuffer was just recreated
+  enigma_user::draw_clear(enigma_user::window_get_color());
+}
 
-  void EnableDrawing(void* handle) {
-    WindowResizedCallback = &WindowResized;
+void EnableDrawing(void* handle) {
+  WindowResizedCallback = &WindowResized;
 
-    int screenWidth = enigma_user::window_get_width(),
-        screenHeight = enigma_user::window_get_height();
+  int screenWidth = enigma_user::window_get_width(),
+      screenHeight = enigma_user::window_get_height();
 
-    HRESULT result;
+  HRESULT result;
 
-    DXGI_SWAP_CHAIN_DESC swapChainDesc = { };
-    swapChainDesc.BufferCount = 1;
-    swapChainDesc.BufferDesc.Width = screenWidth;
-    swapChainDesc.BufferDesc.Height = screenHeight;
-    swapChainDesc.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
-    swapChainDesc.BufferDesc.RefreshRate.Numerator = 0;
-    swapChainDesc.BufferDesc.RefreshRate.Denominator = 1;
-    swapChainDesc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
-    swapChainDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
-    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-    swapChainDesc.SampleDesc.Count = 1;
-    swapChainDesc.SampleDesc.Quality = 0;
-    swapChainDesc.OutputWindow = enigma::hWnd;
-    swapChainDesc.Windowed = true; // initially windowed and not fullscreen
-    swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
-    swapChainDesc.Flags = 0;
+  DXGI_SWAP_CHAIN_DESC swapChainDesc = { };
+  swapChainDesc.BufferCount = 1;
+  swapChainDesc.BufferDesc.Width = screenWidth;
+  swapChainDesc.BufferDesc.Height = screenHeight;
+  swapChainDesc.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+  swapChainDesc.BufferDesc.RefreshRate.Numerator = 0;
+  swapChainDesc.BufferDesc.RefreshRate.Denominator = 1;
+  swapChainDesc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+  swapChainDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+  swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+  swapChainDesc.SampleDesc.Count = 1;
+  swapChainDesc.SampleDesc.Quality = D3D11_STANDARD_MULTISAMPLE_PATTERN;
+  swapChainDesc.OutputWindow = enigma::hWnd;
+  swapChainDesc.Windowed = true; // initially windowed and not fullscreen
+  swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+  swapChainDesc.Flags = 0;
 
-    D3D_FEATURE_LEVEL featureLevel = D3D_FEATURE_LEVEL_11_0;
+  D3D_FEATURE_LEVEL featureLevel = D3D_FEATURE_LEVEL_11_0;
 
-    UINT flags = 0;
-    #ifdef DEBUG_MODE
-    flags |= D3D11_CREATE_DEVICE_DEBUG;
-    #endif
-    result = D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, flags, &featureLevel, 1,
-                                           D3D11_SDK_VERSION, &swapChainDesc, &m_swapChain, &m_device, NULL, &m_deviceContext);
-    if (FAILED(result)) {
-      show_error("Failed to create Direct3D11 device and swap chain.", true);
-    }
-
-    initialize_render_targets();
-
-    // Setup the raster description which will determine how and what polygons will be drawn.
-    D3D11_RASTERIZER_DESC rasterDesc = { };
-    rasterDesc.AntialiasedLineEnable = false;
-    rasterDesc.CullMode = D3D11_CULL_NONE;
-    rasterDesc.DepthBias = 0;
-    rasterDesc.DepthBiasClamp = 0.0f;
-    rasterDesc.DepthClipEnable = false;
-    rasterDesc.FillMode = D3D11_FILL_SOLID;
-    rasterDesc.FrontCounterClockwise = false;
-    rasterDesc.MultisampleEnable = false;
-    rasterDesc.ScissorEnable = false;
-    rasterDesc.SlopeScaledDepthBias = 0.0f;
-
-    result = m_device->CreateRasterizerState(&rasterDesc, &m_rasterState);
-    if (FAILED(result)) {
-      show_error("Failed to create Direct3D11 rasterizer state.", true);
-    }
-
-    m_deviceContext->RSSetState(m_rasterState);
+  UINT flags = 0;
+  #ifdef DEBUG_MODE
+  flags |= D3D11_CREATE_DEVICE_DEBUG;
+  #endif
+  result = D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, flags, &featureLevel, 1,
+                                          D3D11_SDK_VERSION, &swapChainDesc, &m_swapChain, &m_device, NULL, &m_deviceContext);
+  if (FAILED(result)) {
+    show_error("Failed to create Direct3D11 device and swap chain.", true);
   }
 	
   void WindowResized() {
@@ -208,24 +189,95 @@ namespace enigma
     enigma_user::draw_clear(enigma_user::window_get_color());
   }
 
-  void DisableDrawing(void* handle) {}
+  initialize_render_targets();
 
-  void ScreenRefresh() {
-    m_swapChain->Present(swap_interval, 0);
+  // Setup the raster description which will determine how and what polygons will be drawn.
+  D3D11_RASTERIZER_DESC rasterDesc = { };
+  rasterDesc.AntialiasedLineEnable = false;
+  rasterDesc.CullMode = D3D11_CULL_NONE;
+  rasterDesc.DepthBias = 0;
+  rasterDesc.DepthBiasClamp = 0.0f;
+  rasterDesc.DepthClipEnable = false;
+  rasterDesc.FillMode = D3D11_FILL_SOLID;
+  rasterDesc.FrontCounterClockwise = false;
+  rasterDesc.MultisampleEnable = false;
+  rasterDesc.ScissorEnable = false;
+  rasterDesc.SlopeScaledDepthBias = 0.0f;
+
+  result = m_device->CreateRasterizerState(&rasterDesc, &m_rasterState);
+  if (FAILED(result)) {
+    show_error("Failed to create Direct3D11 rasterizer state.", true);
+  }
+
+  m_deviceContext->RSSetState(m_rasterState);
+
+  enigma_user::display_aa = 0;
+  UINT quality_levels = 0;
+  for (int i = 16; i > 1; i--) {
+    if (SUCCEEDED(m_device->CheckMultisampleQualityLevels(swapChainDesc.BufferDesc.Format, i, &quality_levels))) {
+      if (quality_levels > 0)
+        enigma_user::display_aa += i;
+    }
   }
 }
 
-namespace enigma_user
+void DisableDrawing(void* handle) {}
+
+void ScreenRefresh() {
+  m_swapChain->Present(swap_interval, 0);
+}
+
+} // namespace enigma
+
+namespace enigma_user {
+
+int display_aa = 0;
+
+void display_reset(int samples, bool vsync) {
+  swap_interval = vsync ? 1 : 0;
+
+  // when disabling msaa, we still need at least 1 samples
+  if (samples < 1) samples = 1;
+
+  // get the old swap chain description to reconfigure
+  DXGI_SWAP_CHAIN_DESC swapChainDesc;
+  m_swapChain->GetDesc(&swapChainDesc);
+
+  // short-circuit if we already have a swap chain with this many samples
+  if (swapChainDesc.SampleDesc.Count == UINT(samples)) return;
+
+  // release all the old references first
+  release_render_targets();
+  m_swapChain->Release();
+
+  // set the swap chain samples
+  swapChainDesc.SampleDesc.Count = samples;
+  swapChainDesc.SampleDesc.Quality = D3D11_STANDARD_MULTISAMPLE_PATTERN;
+
+  // obtain the dxgi factory we created the device with
+  IDXGIDevice* dxgiDevice = 0;
+  m_device->QueryInterface(__uuidof(IDXGIDevice), (void **)&dxgiDevice);
+  IDXGIAdapter* dxgiAdapter = 0;
+  dxgiDevice->GetParent(__uuidof(IDXGIAdapter), (void **)&dxgiAdapter);
+  IDXGIFactory* dxgiFactory = 0;
+  dxgiAdapter->GetParent(__uuidof(IDXGIFactory), (void **)&dxgiFactory);
+
+  // create the new swap chain
+  dxgiFactory->CreateSwapChain(m_device, &swapChainDesc, &m_swapChain);
+
+  // release the dxgi factory COM pointers
+  dxgiFactory->Release();
+  dxgiAdapter->Release();
+  dxgiDevice->Release();
+
+  // recreate the render target and depth stencil buffers
+  // and their related views with the new swap chain
+  initialize_render_targets(samples);
+}
+
+void set_synchronization(bool enable)
 {
-  int display_aa = 0;
-
-  void display_reset(int samples, bool vsync) {
-    swap_interval = vsync ? 1 : 0;
-  }
-
-  void set_synchronization(bool enable)
-  {
-    swap_interval = enable ? 1 : 0;
-  }
-
+  swap_interval = enable ? 1 : 0;
 }
+
+} // namespace enigma_user
