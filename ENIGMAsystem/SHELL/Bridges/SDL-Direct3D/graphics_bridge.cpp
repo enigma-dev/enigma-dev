df019fe241f3f6f432a40a73a8805e9386074c08
Robert Colton <robertbcolton@gmail.com>
2019-03-10 15:30:38 -0400

    Remove SDL Default Window Caption (#1584)
    I am changing the SDL system to use an empty window caption by default, like GM and all of our other platforms. If we later decide to have a default window caption, it should be handled in the UI/IDE so the user can easily remove it.

--- ENIGMAsystem/SHELL/Bridges/SDL-Direct3D/graphics_bridge.cpp
+++ ENIGMAsystem/SHELL/Bridges/SDL-Direct3D/graphics_bridge.cpp
@@ -6,7 +6,7 @@
 namespace enigma {
 bool initGameWindow() {
   SDL_Init(SDL_INIT_VIDEO);
-  windowHandle = SDL_CreateWindow("SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_HIDDEN | SDL_WINDOW_RESIZABLE);
+  windowHandle = SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_HIDDEN | SDL_WINDOW_RESIZABLE);
   get_window_handle();
   return (windowHandle != nullptr);
 }
