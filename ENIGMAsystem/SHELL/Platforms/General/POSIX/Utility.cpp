2aeece2b3a4abfdd99ed73c11035c18baeb95f1d
time-killer-games <samuelvenable@hotmail.com>
2019-03-11 09:29:45 -0400

    Update Utility.cpp

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/Utility.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/Utility.cpp
@@ -6,6 +6,60 @@
 #include <cstdio>          //popen,getline,pclose
 #include <cstdlib>         //free
 
+#ifdef _WIN32
+static inline size_t getline(char **lineptr, size_t *n, FILE *stream) {
+  char *bufptr = NULL;
+  char *p = bufptr;
+  size_t size;
+  int c;
+
+  if (lineptr == NULL && n == 0) {
+    return -1;
+  }
+  if (stream == NULL) {
+    return -1;
+  }
+  if (n == NULL) {
+    return -1;
+  }
+  bufptr = *lineptr;
+  size = *n;
+
+  c = fgetc(stream);
+  if (c == EOF) {
+    return -1;
+  }
+  if (bufptr == NULL) {
+    bufptr = malloc(128);
+    if (bufptr == NULL) {
+      return -1;
+    }
+    size = 128;
+  }
+  p = bufptr;
+  while(c != EOF) {
+    if ((p - bufptr) > (size - 1)) {
+      size = size + 128;
+      bufptr = realloc(bufptr, size);
+      if (bufptr == NULL) {
+        return -1;
+      }
+    }
+    *p++ = c;
+    if (c == '\n') {
+      break;
+    }
+    c = fgetc(stream);
+  }
+
+  *p++ = '\0';
+  *lineptr = bufptr;
+  *n = size;
+
+  return p - bufptr - 1;
+}
+#endif
+
 namespace enigma {
 
 static struct timespec time_offset;
