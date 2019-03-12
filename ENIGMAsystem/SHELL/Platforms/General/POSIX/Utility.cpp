cfb091034bb24502a64dd72b26ebe14457b081dc
time-killer-games <samuelvenable@hotmail.com>
2019-03-11 20:24:37 -0400

    Update Utility.cpp

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/Utility.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/Utility.cpp
@@ -7,56 +7,55 @@
 #include <cstdlib>         //free,malloc,realloc
 
 #ifdef _WIN32
-static inline ssize_t getline(char **lineptr, size_t *n, FILE *stream) {
-  char *bufptr = NULL;
-  char *p = bufptr;
-  ssize_t size;
+#include <errno.h>
+#include <stdint.h>
+
+ssize_t getline(char **lineptr, size_t *n, FILE *stream) {
+  size_t pos;
   int c;
 
-  if (lineptr == NULL && n == 0) {
+  if (lineptr == NULL || stream == NULL || n == NULL) {
+    errno = EINVAL;
     return -1;
   }
-  if (stream == NULL) {
-    return -1;
-  }
-  if (n == NULL) {
-    return -1;
-  }
-  bufptr = *lineptr;
-  size = *n;
 
   c = fgetc(stream);
   if (c == EOF) {
     return -1;
   }
-  if (bufptr == NULL) {
-    bufptr = malloc(128);
-    if (bufptr == NULL) {
+
+  if (*lineptr == NULL) {
+    *lineptr = malloc(128);
+    if (*lineptr == NULL) {
       return -1;
     }
-    size = 128;
+    *n = 128;
   }
-  p = bufptr;
+
+  pos = 0;
   while(c != EOF) {
-    if ((p - bufptr) > (size - 1)) {
-      size = size + 128;
-      bufptr = realloc((void *)bufptr, size);
-      if (bufptr == NULL) {
+    if (pos + 1 >= *n) {
+      size_t new_size = *n + (*n >> 2);
+      if (new_size < 128) {
+        new_size = 128;
+      }
+      char *new_ptr = realloc(*lineptr, new_size);
+      if (new_ptr == NULL) {
         return -1;
       }
+      *n = new_size;
+      *lineptr = new_ptr;
     }
-    *p++ = c;
+
+    ((unsigned char *)(*lineptr))[pos ++] = c;
     if (c == '\n') {
       break;
     }
     c = fgetc(stream);
   }
 
-  *p++ = '\0';
-  *lineptr = bufptr;
-  *n = size;
-
-  return p - bufptr - 1;
+  (*lineptr)[pos] = '\0';
+  return pos;
 }
 #endif
 
