2037d4f64cdd0effd85e8f203b1bb43272a4bde3
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2019-03-20 07:28:02 -0400

    centralize background sprite screen copy stuff

--- ENIGMAsystem/SHELL/Graphics_Systems/Direct3D11/DX11screen.cpp
+++ ENIGMAsystem/SHELL/Graphics_Systems/Direct3D11/DX11screen.cpp
@@ -17,9 +17,15 @@
 
 #include "Direct3D11Headers.h"
 #include "Graphics_Systems/General/GSscreen.h"
+#include "Graphics_Systems/General/GSbackground.h"
+#include "Graphics_Systems/General/GSsprite.h"
 #include "Graphics_Systems/General/GSprimitives.h"
 #include "Graphics_Systems/General/GScolors.h"
 
+#include "Universal_System/image_formats.h"
+#include "Universal_System/nlpo2.h"
+#include "Universal_System/background_internal.h"
+#include "Universal_System/sprites_internal.h"
 #include "Universal_System/roomsystem.h"
 #include "Platforms/General/PFwindow.h"
 
@@ -44,18 +50,6 @@ void scene_end() {
 
 namespace enigma_user {
 
-int screen_save(string filename)
-{
-  draw_batch_flush(batch_flush_deferred);
-  return -1; //TODO: implement
-}
-
-int screen_save_part(string filename,unsigned x,unsigned y,unsigned w,unsigned h)
-{
-  draw_batch_flush(batch_flush_deferred);
-  return -1; //TODO: implement
-}
-
 void screen_set_viewport(gs_scalar x, gs_scalar y, gs_scalar width, gs_scalar height) {
   draw_batch_flush(batch_flush_deferred);
 
@@ -76,6 +70,35 @@ void screen_set_viewport(gs_scalar x, gs_scalar y, gs_scalar width, gs_scalar he
   viewport.MaxDepth = 1.0f;
 
   m_deviceContext->RSSetViewports(1, &viewport);
+}
+
+int screen_save(string filename)
+{
+  draw_batch_flush(batch_flush_deferred);
+  return -1; //TODO: implement
+}
+
+int screen_save_part(string filename,unsigned x,unsigned y,unsigned w,unsigned h)
+{
+  draw_batch_flush(batch_flush_deferred);
+  return -1; //TODO: implement
+}
+
+int background_create_from_screen(int x, int y, int w, int h, bool removeback, bool smooth, bool preload)
+{
+  return -1; //TODO: implement
+}
+
+int sprite_create_from_screen(int x, int y, int w, int h, bool removeback, bool smooth, bool preload, int xorig, int yorig) {
+  return -1; //TODO: implement
+}
+
+int sprite_create_from_screen(int x, int y, int w, int h, bool removeback, bool smooth, int xorig, int yorig) {
+  return sprite_create_from_screen(x, y, w, h, removeback, smooth, true, xorig, yorig);
+}
+
+void sprite_add_from_screen(int id, int x, int y, int w, int h, bool removeback, bool smooth) {
+
 }
 
 } // namespace enigma_user
--- ENIGMAsystem/SHELL/Graphics_Systems/Direct3D9/DX9screen.cpp
+++ ENIGMAsystem/SHELL/Graphics_Systems/Direct3D9/DX9screen.cpp
@@ -17,10 +17,15 @@
 
 #include "Direct3D9Headers.h"
 #include "Graphics_Systems/General/GSscreen.h"
+#include "Graphics_Systems/General/GSbackground.h"
+#include "Graphics_Systems/General/GSsprite.h"
 #include "Graphics_Systems/General/GSprimitives.h"
 #include "Graphics_Systems/General/GScolors.h"
 
 #include "Universal_System/image_formats.h"
+#include "Universal_System/nlpo2.h"
+#include "Universal_System/background_internal.h"
+#include "Universal_System/sprites_internal.h"
 #include "Universal_System/roomsystem.h"
 
 #include "Platforms/General/PFwindow.h"
@@ -29,8 +34,7 @@ using namespace enigma;
 using namespace enigma::dx9;
 using namespace std;
 
-namespace enigma
-{
+namespace enigma {
 
 void scene_begin() {}
 
@@ -39,6 +43,20 @@ void scene_end() {}
 } // namespace enigma
 
 namespace enigma_user {
+
+void screen_set_viewport(gs_scalar x, gs_scalar y, gs_scalar width, gs_scalar height) {
+  draw_batch_flush(batch_flush_deferred);
+
+  x = (x / window_get_region_width()) * window_get_region_width_scaled();
+  y = (y / window_get_region_height()) * window_get_region_height_scaled();
+  width = (width / window_get_region_width()) * window_get_region_width_scaled();
+  height = (height / window_get_region_height()) * window_get_region_height_scaled();
+  gs_scalar sx, sy;
+  sx = (window_get_width() - window_get_region_width_scaled()) / 2;
+  sy = (window_get_height() - window_get_region_height_scaled()) / 2;
+	D3DVIEWPORT9 pViewport = { (DWORD)(sx + x), (DWORD)(sy + y), (DWORD)width, (DWORD)height, 0, 1.0f };
+	d3dmgr->device->SetViewport(&pViewport);
+}
 
 int screen_save(string filename) //Assumes native integers are little endian
 {
@@ -99,18 +117,21 @@ int screen_save_part(string filename,unsigned x,unsigned y,unsigned w,unsigned h
   return ret;
 }
 
-void screen_set_viewport(gs_scalar x, gs_scalar y, gs_scalar width, gs_scalar height) {
-  draw_batch_flush(batch_flush_deferred);
+int background_create_from_screen(int x, int y, int w, int h, bool removeback, bool smooth, bool preload)
+{
+  return -1; //TODO: implement
+}
 
-  x = (x / window_get_region_width()) * window_get_region_width_scaled();
-  y = (y / window_get_region_height()) * window_get_region_height_scaled();
-  width = (width / window_get_region_width()) * window_get_region_width_scaled();
-  height = (height / window_get_region_height()) * window_get_region_height_scaled();
-  gs_scalar sx, sy;
-  sx = (window_get_width() - window_get_region_width_scaled()) / 2;
-  sy = (window_get_height() - window_get_region_height_scaled()) / 2;
-	D3DVIEWPORT9 pViewport = { (DWORD)(sx + x), (DWORD)(sy + y), (DWORD)width, (DWORD)height, 0, 1.0f };
-	d3dmgr->device->SetViewport(&pViewport);
+int sprite_create_from_screen(int x, int y, int w, int h, bool removeback, bool smooth, bool preload, int xorig, int yorig) {
+  return -1; //TODO: implement
+}
+
+int sprite_create_from_screen(int x, int y, int w, int h, bool removeback, bool smooth, int xorig, int yorig) {
+  return sprite_create_from_screen(x, y, w, h, removeback, smooth, true, xorig, yorig);
+}
+
+void sprite_add_from_screen(int id, int x, int y, int w, int h, bool removeback, bool smooth) {
+
 }
 
 } // namespace enigma_user
