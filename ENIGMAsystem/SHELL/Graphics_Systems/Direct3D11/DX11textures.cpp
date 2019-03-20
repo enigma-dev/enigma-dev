e93850a0084f98ca049d48a7c2934e8bcfb21719
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2019-03-20 15:51:38 -0400

    start generifying textures

--- ENIGMAsystem/SHELL/Graphics_Systems/Direct3D11/DX11textures.cpp
+++ ENIGMAsystem/SHELL/Graphics_Systems/Direct3D11/DX11textures.cpp
@@ -164,29 +164,6 @@ void init_sampler_state() {
 
 namespace enigma_user {
 
-int texture_add(string filename, bool mipmap) {
-  unsigned int w, h, fullwidth, fullheight;
-  int img_num;
-
-  unsigned char *pxdata = enigma::image_load(filename,&w,&h,&fullwidth,&fullheight,&img_num,false);
-  if (pxdata == NULL) { printf("ERROR - Failed to append sprite to index!\n"); return -1; }
-  unsigned texture = enigma::graphics_create_texture(w, h, fullwidth, fullheight, pxdata, mipmap);
-  delete[] pxdata;
-
-  return texture;
-}
-
-void texture_save(int texid, string fname) {
-	unsigned w = 0, h = 0;
-	unsigned char* rgbdata = enigma::graphics_get_texture_pixeldata(texid, &w, &h);
-
-  string ext = enigma::image_get_format(fname);
-
-	enigma::image_save(fname, rgbdata, w, h, w, h, false);
-
-	delete[] rgbdata;
-}
-
 void texture_delete(int texid) {
   delete textureStructs[texid];
 }
--- ENIGMAsystem/SHELL/Graphics_Systems/OpenGL3/GL3textures.cpp
+++ ENIGMAsystem/SHELL/Graphics_Systems/OpenGL3/GL3textures.cpp
@@ -24,8 +24,6 @@
 #include "Graphics_Systems/General/GSprimitives.h"
 
 #include "Universal_System/image_formats.h"
-#include "Universal_System/background_internal.h"
-#include "Universal_System/sprites_internal.h"
 
 #include <vector>
 #include <stdio.h>
@@ -318,31 +316,7 @@ namespace enigma
   }
 }
 
-namespace enigma_user
-{
-
-int texture_add(string filename, bool mipmap) {
-  unsigned int w, h, fullwidth, fullheight;
-  int img_num;
-
-  unsigned char *pxdata = enigma::image_load(filename,&w,&h,&fullwidth,&fullheight,&img_num,false);
-  if (pxdata == NULL) { printf("ERROR - Failed to append sprite to index!\n"); return -1; }
-  unsigned texture = enigma::graphics_create_texture(w, h, fullwidth, fullheight, pxdata, mipmap);
-  delete[] pxdata;
-
-  return texture;
-}
-
-void texture_save(int texid, string fname) {
-	unsigned w, h;
-	unsigned char* rgbdata = enigma::graphics_get_texture_pixeldata(texid, &w, &h);
-
-  string ext = enigma::image_get_format(fname);
-
-	enigma::image_save(fname, rgbdata, w, h, w, h, false);
-
-	delete[] rgbdata;
-}
+namespace enigma_user {
 
 void texture_delete(int texid) {
   delete textureStructs[texid];
