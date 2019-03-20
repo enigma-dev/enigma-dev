7a16bc39261c441f41c421e2895b3f1e01752b33
Greg Williamson <fundies@users.noreply.github.com>
2019-03-20 19:19:15 -0400

    Quote CI args to allow spaces in args (#1616)

--- ci-build.sh
+++ ci-build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash +x
 
-./emake --compiler ${COMPILER} \
-        --mode ${MODE} \
-        --platform ${PLATFORM} \
-        --graphics ${GRAPHICS} \
-        --audio ${AUDIO} \
-        --collision ${COLLISION} \
-        --network ${NETWORK} \
-        --widgets ${WIDGETS} \
-        --extensions ${EXTENSIONS} \
-        -o ${OUTPUT}
+./emake --compiler "${COMPILER}" \
+        --mode "${MODE}" \
+        --platform "${PLATFORM}" \
+        --graphics "${GRAPHICS}" \
+        --audio "${AUDIO}" \
+        --collision "${COLLISION}" \
+        --network "${NETWORK}" \
+        --widgets "${WIDGETS}" \
+        --extensions "${EXTENSIONS}" \
+        -o "${OUTPUT}"
