0e2a6815054a390f63f93251804fa11749bec698
time-killer-games <samuelvenable@hotmail.com>
2019-03-21 04:38:46 -0400

    Update dialogs.cpp

--- ENIGMAsystem/SHELL/Widget_Systems/KDialog/dialogs.cpp
+++ ENIGMAsystem/SHELL/Widget_Systems/KDialog/dialogs.cpp
@@ -41,6 +41,16 @@ static string error_caption;
 static bool message_cancel  = false;
 static bool question_cancel = false;
 
+using std::string;
+
+namespace enigma {
+  
+bool widget_system_initialize() {
+  return true;
+}
+  
+} // namespace enigma
+
 static string shellscript_evaluate(string command) {
   char *buffer = NULL;
   size_t buffer_size = 0;
