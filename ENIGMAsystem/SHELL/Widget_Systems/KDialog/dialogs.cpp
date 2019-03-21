35b9d6bd3c63c7deb110c4b21f2b54e242ccbb17
time-killer-games <samuelvenable@hotmail.com>
2019-03-21 04:39:57 -0400

    Update dialogs.cpp

--- ENIGMAsystem/SHELL/Widget_Systems/KDialog/dialogs.cpp
+++ ENIGMAsystem/SHELL/Widget_Systems/KDialog/dialogs.cpp
@@ -190,6 +190,10 @@ void show_error(string str, const bool abort) {
 
 namespace enigma_user {
 
+void show_info(string info, int bgcolor, int left, int top, int width, int height, bool embedGameWindow, bool showBorder, bool allowResize, bool stayOnTop, bool pauseGame, string caption) {
+
+}
+
 int show_message(const string &str) {
   message_cancel = false;
   return show_message_helperfunc(str);
