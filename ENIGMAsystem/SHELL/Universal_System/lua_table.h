abbfaaecf3aa52ed19bfaf90ecce3ff20cde6ab0
Josh Ventura <JoshV10@gmail.com>
2019-04-09 23:50:19 -0400

    Improve `var` speed. (#1644)
    The `var` type is now faster than `variant`, which is logically impossible. Just the way I like it.

--- ENIGMAsystem/SHELL/Universal_System/lua_table.h
+++ ENIGMAsystem/SHELL/Universal_System/lua_table.h
@@ -25,16 +25,15 @@
 **                                                                              **
 \********************************************************************************/
 
-#ifdef INCLUDED_FROM_SHELLMAIN
-#error This file is high-impact and should not be included from SHELLmain.cpp.
-#endif
-
 #ifndef ENIGMA_H_LUA_TABLE
 #define ENIGMA_H_LUA_TABLE
 
-#include <map>      // Sparse part
-#include <string.h> // Memcpy
-#include <stdlib.h> // Malloc, Realloc, Free
+#ifndef JUST_DEFINE_IT_RUN
+#include <map>     // Sparse part
+#include <vector>  // Dense part
+#include <cstring> // Memcpy
+#include <cstdlib> // Malloc, Realloc, Free
+#endif
 
 /**
   This file implements a Lua-table-like structure. It borrows ideas not only from
@@ -61,59 +60,35 @@ T my_max(const T& lhs, const T& rhs) {
 }
 
 
-template <class T> struct lua_table
-{
-  // This is what kind of sparse container we'll be using
-  typedef std::map<size_t,T> lua_map_type;
+template <class T> struct lua_table {
+# ifndef JUST_DEFINE_IT_RUN
+  typedef std::vector<T> dense_type;
+  typedef std::map<size_t,T> sparse_type;
+# else
+  typedef T dense_type[1];
+  typedef T sparse_type[1];
+# endif
 
-private:
-  //Stuff relating to dense storage.
-  T* dense;
-  size_t dn_reserve; //Total available units.
+ private:
+  dense_type dense;
+  sparse_type sparse;
 
-  //Sparse storage is easier.
-  lua_map_type sparse;
-
-  //Our one item of book-keeping
-  size_t mx_size; //Actual max_element+1
+  // Our one item of book-keeping
+  size_t mx_size;  ///< highest accessed element + 1
 
 
-  void pick_up(const lua_table<T>& who)
-  {
-    dn_reserve = who.dn_reserve;
+  void pick_up(const lua_table<T>& who) {
     mx_size = who.mx_size;
-
-    //Free old memory?
-    if (dense) {
-      delete [] dense;
-      dense = NULL;
-    }
-
-    //Create a new dense chunk, copy over values.
-    dense = new T[dn_reserve];
-    for (size_t i=0; i<dn_reserve; i++) {
-      dense[i] = who.dense[i];
-    }
-
-    //Copy the sparse map
+    dense = who.dense;
     sparse = who.sparse;
   }
 
   void upsize(const size_t c)
   {
-    //Create a new dense section and copy over values; free old memory.
-    T* new_dense = new T[c];
-    if (dense) {
-      for (size_t i=0; i<dn_reserve; i++) {
-        new_dense[i] = dense[i];
-      }
-      delete [] dense;
-    }
-    dense = new_dense;
-    dn_reserve = c;
-
+    dense.resize(c);
+    mx_size = c;
     //Copy sparse array values that are now within this reserve space.
-    for (typename lua_map_type::iterator it=sparse.begin(); it!=sparse.end();) {
+    for (typename sparse_type::iterator it=sparse.begin(); it!=sparse.end();) {
       if (it->first >= c) {
         break;
       }
@@ -123,27 +98,43 @@ private:
   }
 
 
-public:
-  T& operator[] (size_t ind)
-  {
+ public:
+  const dense_type &dense_part() const {
+    return dense;
+  }
+
+  T& operator[] (size_t ind) {
     mx_size = my_max(ind+1, mx_size);
-    if (ind >= dn_reserve)
-    {
-      // Calculate tolerable size increase
-      const size_t c = (dn_reserve < 4) ? 4 : dn_reserve << 1;
-
-      // If we're far out of range of the currently allocated dense array
-      if (ind >= c) // Default to the map and return a sparse node
+    if (ind >= dense.size()) {
+      size_t nsize = my_max(dense.size() << 1, dense.capacity());
+      if (ind >= nsize) {
         return sparse[ind];
-
-      // Otherwise, resize and reconstruct
-      upsize(c);
+      }
+      upsize(ind + 1);
     }
-
     return dense[ind];
   }
 
+  const T& operator[] (size_t ind) const {
+    static const T sentinel;
+    if (ind >= dense.size()) {
+      auto f = sparse.find(ind);
+      return f == sparse.end()? sentinel : f->second;
+    }
+    return dense[ind];
+  }
+
+  void fill(const T &value, size_t length) {
+    upsize(length);
+    for (size_t i = 0; i < length; ++i)
+      dense[i] = value;
+  }
+
   T& operator*() {
+    return dense[0];
+  }
+
+  const T& operator*() const {
     return dense[0];
   }
 
@@ -151,20 +142,21 @@ public:
     return mx_size;
   }
 
-  lua_table<T>& operator= (const lua_table<T>& x)
-  {
+  size_t dense_length() const {
+    return dense.size();
+  }
+
+  lua_table<T>& operator= (const lua_table<T>& x) {
     pick_up(x);
     return *this;
   }
 
-  lua_table<T>() : dense(new T[1]), dn_reserve(1), mx_size(1) {
-  }
-  lua_table<T>(const lua_table<T> &x): dense(NULL), dn_reserve(0), mx_size(0) {
+  lua_table<T>(): mx_size(0) {}
+  lua_table<T>(const T &v, size_t len): dense(len, v), mx_size(len) {}
+  lua_table<T>(const lua_table<T> &x) {
     pick_up(x);
   }
-  ~lua_table<T>() {
-    if (dense) { delete [] dense; }
-  }
+  ~lua_table<T>() {}
 };
 
 #endif // ENIGMA_H_LUA_TABLE
