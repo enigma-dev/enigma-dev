233258130e073d9addcae9a5f88290f983c4428b
Greg Williamson <cheeseboy16@gmail.com>
2019-04-11 04:52:14 -0400

    mebe works now

--- CommandLine/emake/Main.cpp
+++ CommandLine/emake/Main.cpp
@@ -39,7 +39,12 @@ static std::string tolower(const std::string &str) {
 int main(int argc, char* argv[])
 {
   #if defined(_MSC_VER) && defined(_DEBUG)
-  //_CrtSetReportMode(_CRT_WARN | _CRT_ERROR | _CRT_ASSERT, _CRTDBG_MODE_DEBUG);
+  /*_CrtSetReportMode( _CRT_WARN, _CRTDBG_MODE_FILE );
+  _CrtSetReportMode( _CRT_ERROR, _CRTDBG_MODE_FILE );
+  _CrtSetReportMode( _CRT_ASSERT, _CRTDBG_MODE_FILE );
+  _CrtSetReportFile( _CRT_WARN, _CRTDBG_FILE_STDERR );
+  _CrtSetReportFile( _CRT_ERROR, _CRTDBG_FILE_STDERR );
+  _CrtSetReportFile( _CRT_ASSERT, _CRTDBG_FILE_STDERR );*/
   #endif
 
   OptionsParser options;
