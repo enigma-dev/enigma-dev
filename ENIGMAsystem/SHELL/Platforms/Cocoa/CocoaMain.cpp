a38e198dd987a923c5165b7cf2506c3b02c5d51e
time-killer-games <samuelvenable@hotmail.com>
2019-04-17 17:21:35 -0400

    Update CocoaMain.cpp

--- ENIGMAsystem/SHELL/Platforms/Cocoa/CocoaMain.cpp
+++ ENIGMAsystem/SHELL/Platforms/Cocoa/CocoaMain.cpp
@@ -19,7 +19,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <unistd.h> 
+#include <unistd.h>
 #include <stdio.h>
 #include <mach-o/dyld.h>
 #include <limits.h>
