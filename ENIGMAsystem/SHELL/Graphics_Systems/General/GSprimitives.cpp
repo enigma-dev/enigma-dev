1143f87f9e50125e9b102e7ad6fc3f7096fe11af
Robert Colton <robertbcolton@gmail.com>
2019-05-08 21:59:46 -0400

    Primitive Batch Format Specifying (#1707)
    Allow specifying primitive format ahead of time which is faster when known.

--- ENIGMAsystem/SHELL/Graphics_Systems/General/GSprimitives.cpp
+++ ENIGMAsystem/SHELL/Graphics_Systems/General/GSprimitives.cpp
@@ -122,16 +122,16 @@ int draw_get_batch_mode() {
   return draw_batch_mode;
 }
 
-void draw_primitive_begin(int kind)
+void draw_primitive_begin(int kind, int format)
 {
   draw_batch_begin_deferred(-1);
-  d3d_model_primitive_begin(draw_get_batch_stream(), kind);
+  d3d_model_primitive_begin(draw_get_batch_stream(), kind, format);
 }
 
-void draw_primitive_begin_texture(int kind, int texId)
+void draw_primitive_begin_texture(int kind, int texId, int format)
 {
   draw_batch_begin_deferred(texId);
-  d3d_model_primitive_begin(draw_get_batch_stream(), kind);
+  d3d_model_primitive_begin(draw_get_batch_stream(), kind, format);
 }
 
 void draw_primitive_end()
@@ -160,14 +160,14 @@ void draw_vertex_texture_color(gs_scalar x, gs_scalar y, gs_scalar tx, gs_scalar
   d3d_model_vertex_texture_color(draw_get_batch_stream(), x, y, tx, ty, col, alpha);
 }
 
-void d3d_primitive_begin(int kind)
+void d3d_primitive_begin(int kind, int format)
 {
-  draw_primitive_begin(kind);
+  draw_primitive_begin(kind, format);
 }
 
-void d3d_primitive_begin_texture(int kind, int texId)
+void d3d_primitive_begin_texture(int kind, int texId, int format)
 {
-  draw_primitive_begin_texture(kind, texId);
+  draw_primitive_begin_texture(kind, texId, format);
 }
 
 void d3d_primitive_end()
