7494b31e459c5b6a951a69bbf2541b5c01c50a57
time-killer-games <samuelvenable@hotmail.com>
2019-07-05 23:25:32 -0400

    Update PFmain.h

--- ENIGMAsystem/SHELL/Platforms/General/PFmain.h
+++ ENIGMAsystem/SHELL/Platforms/General/PFmain.h
@@ -81,6 +81,7 @@ void execute_program(std::string fname, std::string args, bool wait);
 void execute_program(std::string operation, std::string fname, std::string args, bool wait);
 
 std::string filename_absolute(std::string fname);
+std::string directory_contents(std::string dname);
 std::string environment_get_variable(std::string name);
 bool environment_set_variable(std::string name, std::string value);
 bool set_working_directory(std::string dname);
