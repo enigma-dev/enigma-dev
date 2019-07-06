f1e6a05a8aeede9870505b04fbf7eb242b5b8e8c
time-killer-games <samuelvenable@hotmail.com>
2019-07-06 13:19:11 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Platforms/Win32/WINDOWSmain.cpp
+++ ENIGMAsystem/SHELL/Platforms/Win32/WINDOWSmain.cpp
@@ -466,8 +466,9 @@ std::string filename_absolute(std::string fname) {
   if (file_exists(fname) || directory_exists(fname)) {
     wchar_t rpath[MAX_PATH];
     tstring tstr_fname = widen(fname);
-    GetFullPathNameW(tstr_fname.c_str(), MAX_PATH, rpath, NULL); 
-    return add_slash(shorten(rpath));
+    GetFullPathNameW(tstr_fname.c_str(), MAX_PATH, rpath, NULL);
+    if (directory_exists(ptr)) add_slash(shorten(rpath));
+    if (file_exists(ptr)) shorten(rpath);
   }
   return "";
 }
