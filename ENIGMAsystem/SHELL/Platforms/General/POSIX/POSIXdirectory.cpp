49c98fcba53625fd79f7d3fb07b921b2cc342c8b
time-killer-games <samuelvenable@hotmail.com>
2019-07-06 13:29:37 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/POSIXdirectory.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/POSIXdirectory.cpp
@@ -60,9 +60,8 @@ std::string filename_absolute(std::string fname) {
 }
 
 std::string filename_join(std::string prefix, std::string suffix) {
-  std::string res = filename_absolute(prefix) + suffix;
-  if (directory_exists(res)) return add_slash(res);
-  if (file_exists(res)) return res; 
+  if (directory_exists(filename_absolute(prefix)))
+    return filename_absolute(prefix) + suffix;
   return "";
 }
 
