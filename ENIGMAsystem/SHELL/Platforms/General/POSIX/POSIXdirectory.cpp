cb46ddba17d770ff025e85da82d0bec2c3fb825f
time-killer-games <samuelvenable@hotmail.com>
2019-07-05 23:19:47 -0400

    Update POSIXdirectory.cpp

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/POSIXdirectory.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/POSIXdirectory.cpp
@@ -1,5 +1,6 @@
 #include "Platforms/General/PFmain.h"
 #include "Platforms/General/PFfilemanip.h"
+#include "Universal_System/estring.h"
 
 #include <limits.h>
 #include <unistd.h>
@@ -9,6 +10,11 @@ using std::string;
 
 using enigma_user::file_exists;
 using enigma_user::directory_exists;
+using enigma_user::filename_absolute;
+using enigma_user::file_find_first;
+using enigma_user::file_find_next;
+using enigma_user::file_find_close;
+using enigma_user::string_replace_all;
 
 static inline string add_slash(const string& dir) {
   if (dir.empty() || *dir.rbegin() != '/') return dir + '/';
@@ -36,6 +42,25 @@ std::string filename_absolute(std::string fname) {
     char *ptr = realpath(fname.c_str(), rpath);
     if (ptr != NULL) { return rpath; }
   } return "";
+}
+
+string directory_contents(string dname) {
+  if (string_replace_all(dname, " ", "") == "") dname = ".";
+  if (directory_exists(dname)) {
+    string rpath = filename_absolute(dname);
+    if (rpath.back() != '/') rpath += "/";
+    string item = file_find_first(rpath + "*", fa_readonly + fa_hidden + fa_sysfile + fa_volumeid + fa_directory + fa_archive);
+    string res = rpath + item;
+    while ((file_exists(rpath + item) || directory_exists(rpath + item)) && item != "") {
+      item = file_find_next();
+      if (item != "." && item != ".." && item != "")
+        res += "\n" + rpath + item;
+    }
+    if (res == rpath + item) return "";
+    file_find_close();
+    return res;
+  }
+  return "";
 }
 
 string environment_get_variable(string name) {
