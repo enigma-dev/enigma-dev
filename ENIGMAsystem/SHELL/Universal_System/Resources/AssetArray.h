8ecff43ac0552fb4461840248dc21704b132c44d
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2019-07-07 13:57:20 -0400

    fix style around public/private sections & members

--- ENIGMAsystem/SHELL/Universal_System/Resources/AssetArray.h
+++ ENIGMAsystem/SHELL/Universal_System/Resources/AssetArray.h
@@ -30,7 +30,7 @@
   #include "Widget_Systems/widgets_mandatory.h" // for show_error
   #define CHECK_ID(id, ret) \
     if (!exists(id)) { \
-      enigma_user::show_error("Requested " + asset_type + " asset " + std::to_string(id) + " does not exist.", false); \
+      enigma_user::show_error("Requested " + asset_type_ + " asset " + std::to_string(id) + " does not exist.", false); \
       return ret; \
     }
   #define CHECK_ID_V(id) CHECK_ID(id,)
@@ -44,59 +44,59 @@ namespace enigma {
 // Asset storage container designed for dense cache-efficient resource processing.
 template<typename T>
 class AssetArray {
-public:
-  AssetArray(std::string asset_type): asset_type(asset_type) {}
+ public:
+  AssetArray(std::string asset_type): asset_type_(asset_type) {}
 
   int add(T asset) {
-    int id = assets.size();
-    assets.emplace_back(true, asset);
+    int id = assets_.size();
+    assets_.emplace_back(true, asset);
     return id;
   }
 
   int assign(int id, T asset) {
     #ifdef DEBUG_MODE
     if (id < 0) {
-      enigma_user::show_error("Attempting to assign " + asset_type + " asset " + std::to_string(id) + " to negative index.", false);
+      enigma_user::show_error("Attempting to assign " + asset_type_ + " asset " + std::to_string(id) + " to negative index.", false);
       return id;
     }
     #endif
     if (size_t(id) >= size()) {
-      assets.resize(size_t(id) + 1);
+      assets_.resize(size_t(id) + 1);
     }
-    assets[id] = {true,asset};
+    assets_[id] = {true,asset};
     return id;
   }
 
   T get(int id) {
     static T sentinel;
     CHECK_ID(id,sentinel);
-    return assets[id].second;
+    return assets_[id].second;
   }
 
   int replace(int id, T asset) {
     CHECK_ID(id, -1);
-    assets[id] = {true,asset};
+    assets_[id] = {true,asset};
     return id;
   }
 
   int duplicate(int id) {
     CHECK_ID(id, -1);
-    T asset = assets[id];
+    T asset = assets_[id];
     return add(asset);
   }
 
   void destroy(int id) {
     CHECK_ID_V(id);
     //TODO: call cleanup method
-    assets[id].first = false;
+    assets_[id].first = false;
   }
 
-  size_t size() const { return assets.size(); }
-  bool exists(int id) { return (id >= 0 && size_t(id) < size() && assets[id].first); }
+  size_t size() const { return assets_.size(); }
+  bool exists(int id) { return (id >= 0 && size_t(id) < size() && assets_[id].first); }
 
-private:
-  std::vector<std::pair<bool,T>> assets;
-  std::string asset_type;
+ private:
+  std::vector<std::pair<bool,T>> assets_;
+  std::string asset_type_;
 };
 
 } // namespace enigma
