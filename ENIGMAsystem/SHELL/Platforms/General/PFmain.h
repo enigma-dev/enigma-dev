dd6e1139358d906c72684878875d45e5eff653e4
time-killer-games <samuelvenable@hotmail.com>
2019-07-07 00:03:17 -0400

    File path and iteration improvements (#1779)
    Adds filename_join(path1, path2).
    Adds `fa_all` and `fa_files_all` to reduce verbosity of iterating all files/directories.
    Removes '.' and '..' from the file_find iteration results on Windows.
    Refactors `filename_absolute` to do less redundant I/O.

--- ENIGMAsystem/SHELL/Platforms/General/PFmain.h
+++ ENIGMAsystem/SHELL/Platforms/General/PFmain.h
@@ -81,6 +81,7 @@ void execute_program(std::string fname, std::string args, bool wait);
 void execute_program(std::string operation, std::string fname, std::string args, bool wait);
 
 std::string filename_absolute(std::string fname);
+std::string filename_join(std::string prefix, std::string suffix);
 std::string environment_get_variable(std::string name);
 bool environment_set_variable(std::string name, std::string value);
 bool set_working_directory(std::string dname);
