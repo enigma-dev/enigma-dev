502de90691e4c1ea72db379e62a264fbeadd0c05
time-killer-games <samuelvenable@hotmail.com>
2019-07-07 18:06:45 -0400

    Update ci-build.sh

--- ci-build.sh
+++ ci-build.sh
@@ -9,4 +9,6 @@
         --network "${NETWORK}" \
         --widgets "${WIDGETS}" \
         --extensions "${EXTENSIONS}" \
-        -o "${OUTPUT}"
+        -o "${OUTPUT}" \
+        "CommandLine/testing/SimpleTests/clean_exit.sog/"
+        
