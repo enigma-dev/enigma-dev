0fa187e125b00043a467d9c004c5fd68bf75cc80
Greg Williamson <cheeseboy16@gmail.com>
2019-07-08 21:59:43 -0400

    mebe

--- CI/install_engine_deps.sh
+++ CI/install_engine_deps.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-LINUX_DEPS="libc++-dev libprotobuf-dev protobuf-compiler libgtest-dev wmctrl xvfb xdotool xfwm4 lcov"
+LINUX_DEPS="libc++-dev libprotobuf-dev protobuf-compiler libgtest-dev wmctrl xvfb xdotool xfwm4 lcov\
+  libsdl2-dev libepoxy-dev libegl1-mesa-dev libgles2-mesa-dev libalure-dev libvorbisfile3 libvorbis-dev libdumb1-dev\
+  libsfml-dev libgtk2.0-dev zenity kdebase-bin libgme-dev libglew-dev libxrandr-dev libbox2d-dev libbullet-dev libfreetype6-dev"
+
 OSX_DEPS=""
 
-###### Compilers #######
+###### Cross Compilers #######
 if [ "$COMPILER" == "gcc32" ] || [ "$COMPILER" == "clang32" ]; then
   LINUX_DEPS="$LINUX_DEPS libc6:i386 libc++-dev:i386 libstdc++6:i386\
     libncurses5:i386 libx11-6:i386 libglew-dev:i386 libglu1-mesa-dev:i386\
@@ -11,51 +14,6 @@ if [ "$COMPILER" == "gcc32" ] || [ "$COMPILER" == "clang32" ]; then
     gcc-multilib g++-multilib libc++abi-dev:i386 libpng-dev:i386"
 elif [ "$COMPILER" == "MinGW64" ] || [ "$COMPILER" == "MinGW32" ]; then
   LINUX_DEPS="$LINUX_DEPS mingw-w64 wine-stable"
-fi
-
-###### Platforms #######
-if [ "$PLATFORM" == "SDL" ]; then
-  LINUX_DEPS="$LINUX_DEPS libsdl2-dev"
-fi
-
-if [ "$GRAPHICS" == "OpenGLES2" ]; then
-  LINUX_DEPS="$LINUX_DEPS libepoxy-dev libegl1-mesa-dev libgles2-mesa-dev"
-else
-  LINUX_DEPS="$LINUX_DEPS libglew-dev libxrandr-dev"
-  OSX_DEPS="$OSX_DEPS glew"
-fi
-
-###### Audio #######
-if [ "$AUDIO" == "OpenAL" ]; then
-  LINUX_DEPS="$LINUX_DEPS libalure-dev libvorbisfile3 libvorbis-dev libdumb1-dev"
-elif [ "$AUDIO" == "SFML" ]; then
-  LINUX_DEPS="$LINUX_DEPS libsfml-dev"
-fi
-
-###### Widgets #######
-if [ "$WIDGETS" == "GTK+" ]; then
-  LINUX_DEPS="$LINUX_DEPS libgtk2.0-dev"
-elif [ "$WIDGETS" == "Zenity" ]; then
-  LINUX_DEPS="$LINUX_DEPS zenity"
-elif [ "$WIDGETS" == "KDialog" ]; then
-  LINUX_DEPS="$LINUX_DEPS kdebase-bin"
-fi
-
-###### Extensions #######
-if [[ "$EXTENSIONS" =~ "GME" ]]; then
-  LINUX_DEPS="$LINUX_DEPS libgme-dev"
-fi
-
-if [[ "$EXTENSIONS" =~ "Box2DPhysics" ]] || [[ "$EXTENSIONS" =~ "StudioPhysics" ]]; then
-  LINUX_DEPS="$LINUX_DEPS libbox2d-dev"
-fi
-
-if [[ "$EXTENSIONS" =~ "BulletDynamics" ]]; then
-  LINUX_DEPS="$LINUX_DEPS libbullet-dev"
-fi
-
-if [[ "$EXTENSIONS" =~ "ttf" ]]; then
-  LINUX_DEPS="$LINUX_DEPS libfreetype6-dev"
 fi
 
 ###### Install Deps #######
---
+++ CI/linux_jobs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+LINUX_JOBS[0]='TEST_HARNESS=true AUDIO=OpenAL'
+LINUX_JOBS[1]='PLATFORM=None GRAPHICS=None AUDIO=None COLLISION=None NETWORK=None WIDGETS=None EXTENSIONS="None"'
+LINUX_JOBS[2]='PLATFORM=SDL GRAPHICS=OpenGL1'
+LINUX_JOBS[3]='PLATFORM=SDL GRAPHICS=OpenGL3'
+LINUX_JOBS[4]='GRAPHICS=OpenGL1 EXTENSIONS="ParticleSystems"'
+LINUX_JOBS[5]='GRAPHICS=OpenGL3 EXTENSIONS="ParticleSystems"'
+LINUX_JOBS[6]='GRAPHICS=OpenGLES2 PLATFORM=SDL EXTENSIONS="ParticleSystems"'
+LINUX_JOBS[7]='AUDIO=OpenAL EXTENSIONS="GME"'
+LINUX_JOBS[8]='AUDIO=SFML EXTENSIONS="GME"'
+LINUX_JOBS[9]='COLLISION=BBox NETWORK=None EXTENSIONS="Paths,MotionPlanning"'
+LINUX_JOBS[10]='COLLISION=Precise NETWORK=None EXTENSIONS="Paths,MotionPlanning"'
+LINUX_JOBS[11]='NETWORK=Asynchronous'
+LINUX_JOBS[12]='NETWORK=BerkeleySockets'
+LINUX_JOBS[13]='WIDGETS=GTK+'
+LINUX_JOBS[14]='WIDGETS=Zenity'
+LINUX_JOBS[15]='WIDGETS=KDialog'
+LINUX_JOBS[16]='COLLISION=BBox EXTENSIONS="Alarms,Timelines,DataStructures,Asynchronous,BasicGUI,DataStructures,DateTime,GM5Compat,IniFilesystem,DataStructures,Json,XRandR,Paths,MotionPlanning,GME,ttf,Box2DPhysics,StudioPhysics,BulletDynamics"'
+
+JOB_COUNT=16
+TRAVIS_WORKERS=4
+
+SPLIT=$(($JOB_COUNT / $TRAVIS_WORKERS))
+START=$(( $WORKER * $SPLIT ))
+if [[ $WORKER -lt $((TRAVIS_WORKERS-1)) ]]; then
+  END=$(($WORKER * $SPLIT + $SPLIT))
+else
+  END=$JOB_COUNT
+fi 
+
+for job in $(seq $START 1 $END);
+do
+  $job ./ci-build.sh
+done;
