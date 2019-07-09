4425d456860c90e79aa52138e819c31dfd799ddd
Greg Williamson <cheeseboy16@gmail.com>
2019-07-09 17:20:48 -0400

    quiet enigmas printing

---
+++ share_logs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for log in "/tmp/enigma_libegm.log" "/tmp/enigma_compiler.log" "/tmp/enigma_compile.log";
+do
+  echo -n "$log: "
+  cat $log | curl -F 'sprunge=<-' http://sprunge.us
+done
