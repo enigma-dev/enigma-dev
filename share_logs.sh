0653275af2d94ee17995376207778da422c6ece9
Greg Williamson <cheeseboy16@gmail.com>
2019-07-09 21:14:43 -0400

    bad idea

--- share_logs.sh
+++ share_logs.sh
@@ -3,4 +3,5 @@ for log in "/tmp/enigma_libegm.log" "/tmp/enigma_compiler.log" "/tmp/enigma_comp
 do
   echo -n "$log: "
   cat $log | curl -F 'sprunge=<-' http://sprunge.us
+  if [[ "$TRAVIS" == "true" ]] rm -f "$log"
 done
