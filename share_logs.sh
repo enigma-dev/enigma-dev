cb20fa6794d2ecf9ea21e2fb27977f49ae5684d1
Greg Williamson <fundies@users.noreply.github.com>
2019-07-10 19:46:43 -0400

    Fix big CI oopsie. (#1790)
    Bash pipe would only return error for last command, causing build to continue on failure and rerun old games.
    
    Other commits:
    * Quiet enigmas printing
    * Deal with Windows being too dumb to use /tmp
    * Fake CI folder add
    * Clear old compile log on new builds

--- share_logs.sh
+++ share_logs.sh
@@ -2,7 +2,7 @@
 for log in "logs/enigma_libegm.log" "logs/enigma_compiler.log" "logs/enigma_compile.log";
 do
   echo -n "$log: "
-  cat $log | curl -F 'sprunge=<-' http://sprunge.us
+  cat $log | sed 's/\x1b\[[0-9;]*m//g' | curl -F 'sprunge=<-' http://sprunge.us
   if [[ "$TRAVIS" == "true" ]]; then 
     rm -f "$log"
   fi
