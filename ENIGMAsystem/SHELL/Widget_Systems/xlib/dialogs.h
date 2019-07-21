1e79119f4cc1c99c019856841ebb84d189c644ff
time-killer-games <samuelvenable@hotmail.com>
2019-07-21 19:37:57 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Widget_Systems/xlib/dialogs.h
+++ ENIGMAsystem/SHELL/Widget_Systems/xlib/dialogs.h
@@ -20,60 +20,38 @@
 #include <string>
 using std::string;
 
-namespace zenity {
+namespace enigma {
 
-  void show_info(string info, int bgcolor, int left, int top, int width, int height, bool embedGameWindow, bool showBorder, bool allowResize, bool stayOnTop, bool pauseGame, string caption);
-  int show_message(const string &message);
-  int show_message_cancelable(string message);
-  int show_message_ext(string message, string but1, string but2, string but3);
-  bool show_question(string message);
-  int show_question_cancelable(string message);
-  int show_attempt(string errortext);
-  void show_debug_message(string errortext, MESSAGE_TYPE type);
-  string get_string(string message, string def);
-  string get_password(string message, string def);
-  double get_integer(string message, double def);
-  double get_passcode(string message, double def);
-  string get_open_filename(string filter, string fname);
-  string get_open_filenames(string filter, string fname);
-  string get_save_filename(string filter, string fname);
-  string get_open_filename_ext(string filter, string fname, string dir, string title);
-  string get_open_filenames_ext(string filter, string fname, string dir, string title);
-  string get_save_filename_ext(string filter, string fname, string dir, string title);
-  string get_directory(string dname);
-  string get_directory_alt(string capt, string root);
-  int get_color(int defcol);
-  int get_color_ext(int defcol, string title);
-  string message_get_caption();
-  void message_set_caption(string title);
+class CommandLineWidgetEngine {
+ public:
+  virtual ~CommandLineWidgetEngine() = default;
+  virtual void show_info(string info, int bgcolor, int left, int top, int width, int height, bool embedGameWindow, bool showBorder, bool allowResize, bool stayOnTop, bool pauseGame, string caption) = 0;
+  virtual int show_message(const string &message) = 0;
+  virtual int show_message_cancelable(string message) = 0;
+  virtual bool show_question(string message) = 0;
+  virtual int show_question_cancelable(string message) = 0;
+  virtual int show_attempt(string errortext) = 0;
+  virtual void show_debug_message(string errortext, MESSAGE_TYPE type) = 0;
+  virtual string get_string(string message, string def) = 0;
+  virtual string get_password(string message, string def) = 0;
+  virtual double get_integer(string message, double def) = 0;
+  virtual double get_passcode(string message, double def) = 0;
+  virtual string get_open_filename(string filter, string fname) = 0;
+  virtual string get_open_filenames(string filter, string fname) = 0;
+  virtual string get_save_filename(string filter, string fname) = 0;
+  virtual string get_open_filename_ext(string filter, string fname, string dir, string title) = 0;
+  virtual string get_open_filenames_ext(string filter, string fname, string dir, string title) = 0;
+  virtual string get_save_filename_ext(string filter, string fname, string dir, string title) = 0;
+  virtual string get_directory(string dname) = 0;
+  virtual string get_directory_alt(string capt, string root) = 0;
+  virtual int get_color(int defcol) = 0;
+  virtual int get_color_ext(int defcol, string title) = 0;
+  virtual string message_get_caption() = 0;
+  virtual void message_set_caption(string title) = 0;
 
-}
+}; // class CommandLineWidgetEngine
 
-namespace kdialog {
+extern CommandLineWidgetEngine *zenity_widgets;
+extern CommandLineWidgetEngine *kdialog_widgets;
 
-  void show_info(string info, int bgcolor, int left, int top, int width, int height, bool embedGameWindow, bool showBorder, bool allowResize, bool stayOnTop, bool pauseGame, string caption);
-  int show_message(const string &message);
-  int show_message_cancelable(string message);
-  int show_message_ext(string message, string but1, string but2, string but3);
-  bool show_question(string message);
-  int show_question_cancelable(string message);
-  int show_attempt(string errortext);
-  void show_debug_message(string errortext, MESSAGE_TYPE type);
-  string get_string(string message, string def);
-  string get_password(string message, string def);
-  double get_integer(string message, double def);
-  double get_passcode(string message, double def);
-  string get_open_filename(string filter, string fname);
-  string get_open_filenames(string filter, string fname);
-  string get_save_filename(string filter, string fname);
-  string get_open_filename_ext(string filter, string fname, string dir, string title);
-  string get_open_filenames_ext(string filter, string fname, string dir, string title);
-  string get_save_filename_ext(string filter, string fname, string dir, string title);
-  string get_directory(string dname);
-  string get_directory_alt(string capt, string root);
-  int get_color(int defcol);
-  int get_color_ext(int defcol, string title);
-  string message_get_caption();
-  void message_set_caption(string title);
-
-}
+} // namespace enigma
