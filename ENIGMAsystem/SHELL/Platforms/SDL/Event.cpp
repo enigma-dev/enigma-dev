c893560fff581cddbbc3a78f69c8ed2e881b6040
Robert Colton <robertbcolton@gmail.com>
2019-07-21 21:18:43 -0400

    SDL Mouse Capture (#1825)
    This allows the game to receive mouse release events from outside the game window when the mouse was initially pressed inside the game window.

--- ENIGMAsystem/SHELL/Platforms/SDL/Event.cpp
+++ ENIGMAsystem/SHELL/Platforms/SDL/Event.cpp
@@ -245,6 +245,7 @@ void SDL_Event_Handler::textInput(const SDL_Event *event) {
 void SDL_Event_Handler::mouseButtonDown(const SDL_Event *event) {
   int btn = SDL_map_button_enum(event->button.button);
   if (btn < 0) return;
+  SDL_CaptureMouse(SDL_TRUE);
   enigma::last_mousestatus[btn] = enigma::mousestatus[btn];
   enigma::mousestatus[btn] = true;
 }
@@ -252,6 +253,7 @@ void SDL_Event_Handler::mouseButtonDown(const SDL_Event *event) {
 void SDL_Event_Handler::mouseButtonUp(const SDL_Event *event) {
   int btn = SDL_map_button_enum(event->button.button);
   if (btn < 0) return;
+  SDL_CaptureMouse(SDL_FALSE);
   enigma::last_mousestatus[btn] = enigma::mousestatus[btn];
   enigma::mousestatus[btn] = false;
 }
