aece842e828973cad042b861b09ac70c961a3483
time-killer-games <samuelvenable@hotmail.com>
2019-07-26 09:08:26 -0400

    Update share_logs.sh

--- share_logs.sh
+++ share_logs.sh
@@ -3,7 +3,7 @@
 for log in "logs/enigma_libegm.log" "logs/enigma_compiler.log" "logs/enigma_compile.log" "logs/enigma_game.log";
 do
   echo -n "$log: "
-  cat $log | sed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g' | curl -F 'f:1=<-' ix.io
+  cat $log | sed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g' | curl -F 'sprunge=<-' http://sprunge.us
   if [[ "$TRAVIS" == "true" ]]; then 
     rm -f "$log"
   fi
