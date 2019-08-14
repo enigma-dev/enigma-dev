4fa4449cd85a9c27c1a5a5dfa6dc909686902c8f
time-killer-games <samuelvenable@hotmail.com>
2019-08-14 03:05:45 -0400

    Update PFmain.cpp

--- ENIGMAsystem/SHELL/Platforms/General/PFmain.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/PFmain.cpp
@@ -66,7 +66,6 @@ int enigma_main(int argc, char** argv) {
   // Call ENIGMA system initializers; sprites, audio, and what have you
   initialize_everything();
 
-  unsigned step = 0;
   while (!game_isending) {
 
     if (!((std::string)enigma_user::room_caption).empty())
