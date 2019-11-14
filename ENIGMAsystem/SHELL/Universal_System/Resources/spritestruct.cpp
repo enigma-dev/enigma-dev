5046dfdb2c40def7e5f9ddf76ab1977ab1209804
time-killer-games <samuelvenable@hotmail.com>
2019-11-13 21:28:58 -0500

    [skip ci] fucker

--- ENIGMAsystem/SHELL/Universal_System/Resources/spritestruct.cpp
+++ ENIGMAsystem/SHELL/Universal_System/Resources/spritestruct.cpp
@@ -372,9 +372,7 @@ namespace enigma
     unsigned char *pxdata = image_load(
         filename, &width, &height, &fullwidth, &fullheight, &imgnumb, false);
       
-      sprite_add_to_fucker(ns, pxdata, imgnumb,
-      precise, transparent, smooth, x_offset, y_offset,
-      mipmap)
+      sprite_add_to_fucker(ns, pxdata, imgnumb, precise, transparent, smooth, x_offset, y_offset, mipmap);
     }
 
   void sprite_add_copy(sprite *spr, sprite *spr_copy) {
