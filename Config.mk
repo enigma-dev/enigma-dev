282396c6d8df9e654d09ee00ef2d7172b6e5726b
Greg Williamson <cheeseboy16@gmail.com>
2020-01-22 11:22:19 +0000

    dammit travis

--- Config.mk
+++ Config.mk
@@ -16,6 +16,11 @@ endif
 CXXFLAGS := -Wall -Wextra -Wpedantic -g -I.
 LDFLAGS := -g
 
+# For older travis instances
+ifeq ($(shell expr $(GCCVER) \<= 6), 1)
+  CXXFLAGS += -std=c++11
+endif
+
 # These will be relative to the file that includes this Makefile
 SRC_DIR := .
 OBJ_DIR := .eobjs
