f0ac7ceb7b261f4bf5b5f0dc9f84aaeff7cab4c4
Josh Ventura <joshv10@gmail.com>
2020-01-29 22:32:37 -0500

    Build yaml-cpp anew for Android.

--- CI/install_emake_deps.sh
+++ CI/install_emake_deps.sh
@@ -4,11 +4,22 @@ set -e
 
 sudo add-apt-repository -y ppa:maarten-fonville/protobuf;
 
+install_yaml_cpp () {
+  git clone https://github.com/jbeder/yaml-cpp /tmp/yaml-cpp
+  mkdir /tmp/yaml-cpp/build
+  pushd /tmp/yaml-cpp/build
+  cmake -DYAML_CPP_BUILD_TESTS=OFF -DYAML_CPP_BUILD_TOLLS=OFF -DYAML_CPP_BUILD_SHARED_LIBS=ON ..
+  make
+  sudo make install
+  popd
+}
+
 if [ "$COMPILER" == "Android" ]; then
   sudo add-apt-repository -y ppa:mhier/libboost-latest;
   sudo apt-get update --option Acquire::Retries=100 --option Acquire::http::Timeout="60";
   sudo apt-get -y install build-essential zlib1g-dev libboost1.67-dev\
-        libprotobuf-dev protobuf-compiler libglm-dev libpng-dev libyaml-cpp-dev;
+        libprotobuf-dev protobuf-compiler libglm-dev libpng-dev;
+  install_yaml_cpp
 elif [ "$TRAVIS_OS_NAME" == "linux" ]; then
   sudo apt-get update --option Acquire::Retries=100 --option Acquire::http::Timeout="60";
   sudo apt-get -y install build-essential zlib1g-dev libboost-dev\
