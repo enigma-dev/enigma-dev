22c208ac8071ae4cc2fa4f6794e09e6945cc888f
time-killer-games <samuelvenable@hotmail.com>
2020-02-15 18:00:43 -0500

    Update include.h

--- ENIGMAsystem/SHELL/Platforms/General/include.h
+++ ENIGMAsystem/SHELL/Platforms/General/include.h
@@ -5,6 +5,7 @@
 // included before
 
 #include "Platforms/General/PFmain.h"
+#include "Platforms/General/nonSDL/PFcenter.h"
 #include "Platforms/General/PFwindow.h"
 #include "Platforms/General/PFthreads.h"
 #include "Platforms/General/PFfilemanip.h"
