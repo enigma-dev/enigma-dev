a77d39e6e0da858389ef247dd1cc8fe49ff0544f
time-killer-games <samuelvenable@hotmail.com>
2020-02-15 18:22:20 -0500

    skip ci

--- ENIGMAsystem/SHELL/Platforms/General/PFwindow.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/PFwindow.cpp
@@ -286,6 +286,19 @@ void window_default(bool center_size) {
   if (center) window_center();
 }
 
+void window_center() {
+  int i = 0;
+  int targetX = display_get_x() + ((display_get_width() - window_get_width()) / 2);
+  int targetY = display_get_y() + ((display_get_height() - window_get_height()) / 2);
+  // without this while loop, the window won't be guaranteed to center 
+  // at game init, in xlib, which is weird, but this makes it necessary.
+  while (enigma::windowX != targetX || enigma::windowY != targetY) {
+    window_set_position(targetX, targetY);
+    if (i >= 15) break; 
+    i++;
+  }
+}
+
 void window_set_freezeonlosefocus(bool freeze) { enigma::freezeOnLoseFocus = freeze; }
 
 bool window_get_freezeonlosefocus() { return enigma::freezeOnLoseFocus; }
