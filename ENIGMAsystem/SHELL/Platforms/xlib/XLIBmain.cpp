a48163670510af3749a894cd0433acd8df38eac9
time-killer-games <samuelvenable@hotmail.com>
2020-02-16 07:30:57 -0500

    Update XLIBmain.cpp

--- ENIGMAsystem/SHELL/Platforms/xlib/XLIBmain.cpp
+++ ENIGMAsystem/SHELL/Platforms/xlib/XLIBmain.cpp
@@ -207,6 +207,8 @@ may be relicensed under MIT for those who want to use
 it in their stuff permissively outside of enigma-dev;
 it's the least he can do since it wasn't easily done:
 
+Exmaple Usage: https://github.com/time-killer-games/XGetDisplay
+
 #include <X11/extensions/Xrandr.h>
 #include <X11/extensions/Xinerama.h>
 
