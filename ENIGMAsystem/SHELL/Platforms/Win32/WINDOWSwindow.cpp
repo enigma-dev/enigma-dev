66411f5afcd7baad783374340ad4a1456b7ce430
time-killer-games <samuelvenable@hotmail.com>
2020-02-21 14:38:23 -0500

    [skip ci]

--- ENIGMAsystem/SHELL/Platforms/Win32/WINDOWSwindow.cpp
+++ ENIGMAsystem/SHELL/Platforms/Win32/WINDOWSwindow.cpp
@@ -21,6 +21,7 @@
 #include "Platforms/General/PFmain.h" // For those damn vk_ constants.
 #include "Platforms/General/PFwindow.h"
 
+#include "WINDOWSicon.h"
 #include "Bridges/Win32/WINDOWShandle.h" // enigma::hWnd/hInstance
 
 #include "Widget_Systems/widgets_mandatory.h"
