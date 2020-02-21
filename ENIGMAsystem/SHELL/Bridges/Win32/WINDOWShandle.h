ea5eff26ce653e4ed0d49f6ce5bccea0cc4986bd
time-killer-games <samuelvenable@hotmail.com>
2020-02-20 20:00:11 -0500

    [skip ci]

--- ENIGMAsystem/SHELL/Bridges/Win32/WINDOWShandle.h
+++ ENIGMAsystem/SHELL/Bridges/Win32/WINDOWShandle.h
@@ -1,9 +1,10 @@
-// This function is called by d3d and dsound to ensure the window handle is set 
-
 #include <windows.h>
 
 namespace enigma {
-//extern HWND hWnd;
-HWND get_window_handle();
 
-}; //namespace enigma
+extern HINSTANCE hInstance;
+extern HWND hWnd;
+extern HDC window_hDC;
+extern HANDLE mainthread;
+
+} // namespace enigma
