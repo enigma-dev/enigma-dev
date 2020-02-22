121a425341c94e800b6dd05a0d20d10604ade780
time-killer-games <samuelvenable@hotmail.com>
2020-02-21 22:39:30 -0500

    [skip ci]

--- ENIGMAsystem/SHELL/Platforms/xlib/XLIBwindow.cpp
+++ ENIGMAsystem/SHELL/Platforms/xlib/XLIBwindow.cpp
@@ -156,11 +156,15 @@ static inline string remove_trailing_zeros(int numb) {
 
 namespace enigma_user {
 
-// returns gay window pointer for extensions
-// we cast to/from a void * for generic-ness
+#if GM_COMPATIBILITY_VERSION <= 81
+unsigned long long window_handle() {
+  return (unsigned long long)enigma::x11::win;
+}
+#else
 void *window_handle() {
   return (void *)enigma::x11::win;
 }
+#endif
 
 // returns an identifier for the gay window
 // this string can be used in shell scripts
