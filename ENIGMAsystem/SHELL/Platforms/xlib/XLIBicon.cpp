0e2c67fb750af1913481edf273cf3ae04d4c7abe
time-killer-games <samuelvenable@hotmail.com>
2020-02-21 21:29:17 -0500

    Update XLIBicon.cpp

--- ENIGMAsystem/SHELL/Platforms/xlib/XLIBicon.cpp
+++ ENIGMAsystem/SHELL/Platforms/xlib/XLIBicon.cpp
@@ -19,9 +19,9 @@
 #include "XLIBicon.h"
 
 #include "Graphics_Systems/graphics_mandatory.h"
+#include "Universal_System/image_formats.h"
 #include "Universal_System/Resources/sprites_internal.h"
 #include "Widget_Systems/widgets_mandatory.h"
-#include "Universal_System/nlpo2.h"
 
 #include <X11/Xatom.h>
 
@@ -29,40 +29,15 @@ namespace enigma {
 
 void XSetIconFromSprite(Display *display, Window window, int ind, unsigned subimg) {
   sprite *spr; if (!get_sprite_mtx(spr, ind)) return;  
-
   XSynchronize(display, True);
   Atom property = XInternAtom(display, "_NET_WM_ICON", False);
-
   unsigned char *data = nullptr; unsigned pngwidth, pngheight;
   data = graphics_copy_texture_pixels(spr->texturearray[subimg], &pngwidth, &pngheight);
-  
-  unsigned widfull = nlpo2dc(pngwidth) + 1, hgtfull = nlpo2dc(pngheight) + 1, ih,iw;
-  const int bitmap_size = widfull * hgtfull * 4;
-  unsigned char *bitmap = new unsigned char[bitmap_size]();
-
-  unsigned i = 0;
   unsigned elem_numb = 2 + pngwidth * pngheight;
-  unsigned long int *result = new unsigned long int[elem_numb]();
-
-  result[i++] = pngwidth;
-  result[i++] = pngheight;
-  for (ih = 0; ih < pngheight; ih++) {
-    unsigned tmp = ih * widfull * 4;
-    for (iw = 0; iw < pngwidth; iw++) {
-      bitmap[tmp + 0] = data[4 * pngwidth * ih + iw * 4 + 0];
-      bitmap[tmp + 1] = data[4 * pngwidth * ih + iw * 4 + 1];
-      bitmap[tmp + 2] = data[4 * pngwidth * ih + iw * 4 + 2];
-      bitmap[tmp + 3] = data[4 * pngwidth * ih + iw * 4 + 3];
-      result[i++] = bitmap[tmp + 0] | (bitmap[tmp + 1] << 8) | (bitmap[tmp + 2] << 16) | (bitmap[tmp + 3] << 24);
-      tmp += 4;
-    }
-  }
-  
+  unsigned long *result = bgra_to_argb(data, pngwidth, pngheight);
   XChangeProperty(display, window, property, XA_CARDINAL, 32, PropModeReplace, (unsigned char *)result, elem_numb);
-  
   XFlush(display);
   delete[] result;
-  delete[] bitmap;
   delete[] data;
 }
 
