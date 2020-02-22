1d35a229da663e9441cb9dace235fcfc047a1c6e
time-killer-games <samuelvenable@hotmail.com>
2020-02-21 22:42:31 -0500

    [skip ci]

--- ENIGMAsystem/SHELL/Platforms/SDL/Cocoa/Window.cpp
+++ ENIGMAsystem/SHELL/Platforms/SDL/Cocoa/Window.cpp
@@ -48,11 +48,15 @@ void window_id_init() {
 
 namespace enigma_user {
 
-// returns sdl window pointer for extensions
-// we cast to/from a void * for generic-ness
-void *window_handle() {
-  return enigma::NSWin;
+#if GM_COMPATIBILITY_VERSION <= 81
+unsigned long long window_handle() {
+  return (unsigned long long)enigma::NSWin;
 }
+#else
+void *window_handle() {
+  return (void *)enigma::NSWin;
+}
+#endif
 
 // returns an identifier for the sdl window
 // this string can be used in shell scripts
