659334d29049d970e17b20b1797b0167efadde38
time-killer-games <samuelvenable@hotmail.com>
2020-02-21 22:44:01 -0500

    [skip ci]

--- ENIGMAsystem/SHELL/Platforms/SDL/Android/Window.cpp
+++ ENIGMAsystem/SHELL/Platforms/SDL/Android/Window.cpp
@@ -39,11 +39,15 @@ void window_id_init() {
 namespace enigma_user {
 
 /*
-// returns sdl window pointer for extensions
-// we cast to/from a void * for generic-ness
+#if GM_COMPATIBILITY_VERSION <= 81
+unsigned long long window_handle() {
+  return (unsigned long long)enigma::ANWin;
+}
+#else
 void *window_handle() {
   return (void *)enigma::ANWin;
 }
+#endif
 */
   
 }
