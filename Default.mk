8ca7150780c16611ad43a6bea34b54f0924d6f6c
Robert Colton <robertbcolton@gmail.com>
2020-03-08 21:53:03 -0400

    Default Makefile Consistency (#1906)
    Add the underscore we forgot in #1905 to OBJ_DIR in the default makefile so all of the top-level makefiles are consistent with respect to OBJ_DIR usage.
    
    Note: SHELL makefiles still use the old OBJDIR without underscore because fundies did not touch those. However, they could be cleaned up at a later time by also using shared makefiles in a similar fashion.

--- Default.mk
+++ Default.mk
@@ -43,13 +43,13 @@ $(OBJ_DIR)/shared/%.o: $(SHARED_SRC_DIR)/%.cpp | obj_dirs
 # Proto rules
 # The order-only dependency on $(GENERATED) is an over-approximation to force all protos to be built
 # before any object files. This matters on the first run when Make can't yet know about #includes.
-$(OBJ_DIR)/%.pb.o $(OBJ_DIR)/%.pb.d: $(OBJDIR)/%.pb.cc | $(GENERATED)
+$(OBJ_DIR)/%.pb.o $(OBJ_DIR)/%.pb.d: $(OBJ_DIR)/%.pb.cc | $(GENERATED)
 	$(CXX) $(CXXFLAGS) -MMD -MP -c -o $(OBJ_DIR)/$*.pb.o $(OBJ_DIR)/$*.pb.cc
 
 $(OBJ_DIR)/%.pb.cc $(OBJ_DIR)/%.pb.h: %.proto | obj_dirs 
 	protoc -I. --cpp_out=$(OBJ_DIR) $<
 
-$(OBJ_DIR)/%.grpc.pb.cc $(OBJDIR)/%.grpc.pb.h: %.proto | obj_dirs 
+$(OBJ_DIR)/%.grpc.pb.cc $(OBJ_DIR)/%.grpc.pb.h: %.proto | obj_dirs 
 	protoc -I. --grpc_out=$(OBJ_DIR) --plugin=protoc-gen-grpc=$(GRPC_CPP_PLUGIN_PATH) $<
 
 .PRECIOUS: $(GENERATED)
