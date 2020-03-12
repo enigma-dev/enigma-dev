fccddb633c09bdfaf02f2b529b222e7a8d902bea
time-killer-games <samuelvenable@hotmail.com>
2020-03-12 04:28:11 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Platforms/xlib/BSD/progdir.cpp
+++ ENIGMAsystem/SHELL/Platforms/xlib/BSD/progdir.cpp
@@ -1,17 +1,22 @@
+#include "Platforms/General/PFmain.h"
+#include "Universal_System/estring.h"
 #include "progdir.h"
+#include <cstdlib>
 #include <unistd.h>
-#include <libproc.h>
+#include <sys/sysctl.h>
 
 namespace enigma {
 
 void initialize_program_directory() {
-  int result; pid_t pid = getpid();
-  char buffer[PROC_PIDPATHINFO_MAXSIZE];
-  result = proc_pidpath (pid, buffer, sizeof(buffer));
-  if ( result > 0 )
-    enigma_user::program_directory = enigma_user::filename_path(buffer);
+  int mib[4];
+  mib[0] = CTL_KERN;
+  mib[1] = KERN_PROC;
+  mib[2] = KERN_PROC_PATHNAME;
+  mib[3] = -1;
+  char buffer[1024];
+  size_t cb = sizeof(buffer);
+  sysctl(mib, 4, buffer, &cb, NULL, 0);
+  enigma_user::program_directory = enigma_user::filename_path(buffer);
 }
 
 } // namespace enigma
-
-
