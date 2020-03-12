d34176f4536b2ca334c3ab987d12bde760ed0be9
time-killer-games <samuelvenable@hotmail.com>
2020-03-11 20:50:19 -0400

    [skip ci]

---
+++ ENIGMAsystem/SHELL/Platforms/xlib/BSD/progdir.cpp
@@ -0,0 +1,16 @@
+#include "progdir.h"
+#include <libproc.h>
+
+namespace enigma {
+
+initialize_program_directory() {
+  int result; pid_t pid = getpid();
+  char buffer[PROC_PIDPATHINFO_MAXSIZE];
+  result = proc_pidpath (pid, buffer, sizeof(buffer));
+  if ( result > 0 )
+    enigma_user::program_directory = enigma_user::filename_path(buffer);
+}
+
+} // namespace enigma
+
+
