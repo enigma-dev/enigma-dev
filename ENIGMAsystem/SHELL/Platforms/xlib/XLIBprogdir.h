9c3216fb40b08b330fc1225f8dd842a08c1cc70d
time-killer-games <samuelvenable@hotmail.com>
2020-03-12 18:23:17 -0400

    Create XLIBprogdir.h

---
+++ ENIGMAsystem/SHELL/Platforms/xlib/XLIBprogdir.h
@@ -0,0 +1,11 @@
+#include "Platforms/General/PFmain.h"
+#include "Universal_System/estring.h"
+#include "progdir.h"
+#include <unistd.h>
+#include <climits>
+
+namespace enigma {
+
+void initialize_program_directory();
+
+} // namespace enigma
