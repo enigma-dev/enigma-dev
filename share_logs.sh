943410101dd9ed3df8cb717756218a343eeb022b
Greg Williamson <fundies@users.noreply.github.com>
2020-03-13 03:22:30 +0000

    fixes emake output for "--list" & "--server" and moves log files to /tmp (#1917)

--- share_logs.sh
+++ share_logs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for log in "logs/enigma_libegm.log" "logs/enigma_compiler.log" "logs/enigma_compile.log" "logs/enigma_game.log";
+for log in "/tmp/enigma_libegm.log" "/tmp/enigma_compiler.log" "/tmp/enigma_compile.log" "/tmp/enigma_game.log";
 do
   echo -n "$log: "
   cat $log | sed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g' | curl -F 'sprunge=<-' http://sprunge.us
