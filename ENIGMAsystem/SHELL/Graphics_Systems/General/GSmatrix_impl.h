f9053abe020fb7bfebb88228e6ab38438459d9eb
time-killer-games <samuelvenable@hotmail.com>
2020-03-12 20:28:48 -0400

    Update GSmatrix_impl.h

--- ENIGMAsystem/SHELL/Graphics_Systems/General/GSmatrix_impl.h
+++ ENIGMAsystem/SHELL/Graphics_Systems/General/GSmatrix_impl.h
@@ -28,9 +28,6 @@
 // let's define it just in case some enigma
 // user's package manager happens to be old
 #define GLM_FORCE_RADIANS
-// first one is deprecated but define it
-// anyway for the same reasons as above
-#define GLM_FORCE_LEFT_HANDED
 
 #include <glm/glm.hpp>
 
