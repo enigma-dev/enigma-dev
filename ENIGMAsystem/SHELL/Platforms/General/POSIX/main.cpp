4a4a15c91461bc85257cf1da1424b373034848a2
time-killer-games <samuelvenable@hotmail.com>
2020-03-13 01:19:34 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/main.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/main.cpp
@@ -12,12 +12,12 @@
 using std::string;
 
 namespace enigma {
-
+ 
 static inline string add_slash(const string& dir) {
   if (dir.empty() || *dir.rbegin() != '/') return dir + '/';
   return dir;
 }
-
+ 
 void initialize_directory_globals() {
   // Set the working_directory
   char buffer[PATH_MAX];
