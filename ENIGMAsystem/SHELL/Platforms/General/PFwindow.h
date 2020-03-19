c73de476729171c08fad1887f5e4fcb72c6d93db
time-killer-games <samuelvenable@hotmail.com>
2020-03-19 18:06:23 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Platforms/General/PFwindow.h
+++ ENIGMAsystem/SHELL/Platforms/General/PFwindow.h
@@ -213,13 +213,6 @@ bool display_set_all(int w, int h, int freq, int bitdepth);
 bool display_test_all(int w, int h, int freq, int bitdepth);
 void set_synchronization(bool enable);
 
-//NOTE: window_handle() should never be used by the engine, other systems, such as bridges, can make direct use of the HWND
-#if GM_COMPATIBILITY_VERSION <= 81
-unsigned long long window_handle();
-#else
-void *window_handle();
-#endif 
-
 std::string window_identifier(); // a string containing the number corresponding to the game's main window handle (shell script)
 std::string window_get_identifier(void *hwnd); // a string containing the number corresponding to the specified window pointer
   
