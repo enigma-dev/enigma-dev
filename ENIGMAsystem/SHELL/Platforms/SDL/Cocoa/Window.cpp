fc29880bac688ade84f73777d79d9e8ccbae1553
time-killer-games <samuelvenable@hotmail.com>
2020-03-19 23:11:11 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Platforms/SDL/Cocoa/Window.cpp
+++ ENIGMAsystem/SHELL/Platforms/SDL/Cocoa/Window.cpp
@@ -44,7 +44,8 @@ void window_id_init() {
 namespace enigma_user {
 
 window_t window_handle() {
-  return static_cast<window_t>(enigma::NSWin);
+  unsigned long long window_uint64 = static_cast<unsigned long long>(enigma::NSWin);
+  return static_cast<window_t>(window_uint64);
 }
 
 // returns an identifier for the SDL2 window
