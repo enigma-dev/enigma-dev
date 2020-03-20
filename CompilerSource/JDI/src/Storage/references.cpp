7ab38066630c68a26982317498804f4d456863ca
time-killer-games <samuelvenable@hotmail.com>
2020-03-20 02:06:11 -0400

    [skip ci]

--- CompilerSource/JDI/src/Storage/references.cpp
+++ CompilerSource/JDI/src/Storage/references.cpp
@@ -226,7 +226,7 @@ namespace jdi {
   void ref_stack::parameter::swap(ref_stack::parameter &param) {
     full_type::swap(param);
     
-    __REGISTER bool swb;
+    bool swb;
     swb = param.variadic;
     param.variadic = variadic;
     variadic = swb;
