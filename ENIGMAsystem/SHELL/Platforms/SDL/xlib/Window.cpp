184737e53fc9e662712435e412ba613f65caa21a
time-killer-games <samuelvenable@hotmail.com>
2020-03-20 18:48:12 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Platforms/SDL/xlib/Window.cpp
+++ ENIGMAsystem/SHELL/Platforms/SDL/xlib/Window.cpp
@@ -55,13 +55,13 @@ window_t window_handle() {
 // returns an identifier for the SDL2 window
 // this string can be used in shell scripts
 string window_identifier() {
-  return std::to_string(*(static_cast<unsigned long long *>(window_handle())));
+  return std::to_string(static_cast<unsigned long long>(window_handle()));
 }
 
 // returns an identifier for certain window
 // this string can be used in shell scripts
 string window_get_identifier(window_t hwnd) {
-  return std::to_string(*(static_cast<unsigned long long *>(hwnd)));
+  return std::to_string(static_cast<unsigned long long>(hwnd));
 }
 
 }
