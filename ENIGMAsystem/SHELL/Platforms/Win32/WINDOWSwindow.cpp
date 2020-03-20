ca5e0c68a89a0abc23747889f8ace9fe22e0e087
time-killer-games <samuelvenable@hotmail.com>
2020-03-20 19:26:26 -0400

    Update WINDOWSwindow.cpp

--- ENIGMAsystem/SHELL/Platforms/Win32/WINDOWSwindow.cpp
+++ ENIGMAsystem/SHELL/Platforms/Win32/WINDOWSwindow.cpp
@@ -78,7 +78,7 @@ void configure_devmode(DEVMODE &devMode, int w, int h, int freq, int bitdepth) {
 namespace enigma_user {
 
 window_t window_handle() {
-  unsigned long long window_uint64 = *(static_cast<unsigned long long *>(enigma::hWnd));
+  unsigned long long window_uint64 = static_cast<unsigned long long>(enigma::hWnd);
   return static_cast<window_t>(window_uint64);
 }
 
