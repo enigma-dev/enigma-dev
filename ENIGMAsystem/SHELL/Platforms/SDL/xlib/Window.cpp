fb069fda43aeccfbba5d6bf9fd181afdc9d30e6b
time-killer-games <samuelvenable@hotmail.com>
2020-03-20 18:19:40 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Platforms/SDL/xlib/Window.cpp
+++ ENIGMAsystem/SHELL/Platforms/SDL/xlib/Window.cpp
@@ -49,7 +49,7 @@ namespace enigma_user {
 
 window_t window_handle() {
   unsigned long long window_uint64 = *(static_cast<unsigned long long *>(enigma::x11::win));
-  return *(static_cast<window_t *>(window_uint64));
+  return static_cast<window_t>(window_uint64);
 }
 
 // returns an identifier for the SDL2 window
