ea94eb1b5abedbb733ef1a090a0ae4fd0e6d5cc9
time-killer-games <samuelvenable@hotmail.com>
2020-03-21 22:09:41 -0400

    Update Window.cpp

--- ENIGMAsystem/SHELL/Platforms/SDL/xlib/Window.cpp
+++ ENIGMAsystem/SHELL/Platforms/SDL/xlib/Window.cpp
@@ -48,7 +48,7 @@ void window_id_init() {
 namespace enigma_user {
 
 window_t window_handle() {
-  unsigned long long window_uint64 = enigma::x11:win;
+  unsigned long long window_uint64 = enigma::x11::win;
   return reinterpret_cast<window_t>(window_uint64);
 }
 
