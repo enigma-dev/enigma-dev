7dfa8f7c69f6ee828915d2d0fb905ac75ef3d654
time-killer-games <samuelvenable@hotmail.com>
2020-04-01 14:54:41 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Universal_System/Extensions/ProcManip/BSD/procmanip.cpp
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/ProcManip/BSD/procmanip.cpp
@@ -4,7 +4,7 @@
 
 using std::string;
 
-static inline void path_and_ppid_from_pid(char **path, pid_t *ppid) {
+static inline void path_and_ppid_from_pid(pid_t pid, char **path, pid_t *ppid) {
   struct kinfo_proc *proc_info = kinfo_getproc(pid);
   if (proc_info) {
     *(path) = proc_info->ki_comm;
@@ -17,13 +17,13 @@ namespace enigma_user {
 
 string path_from_pid(pid_t pid) {
   char *path, pid_t ppid;
-  path_and_ppid_from_pid(&path, &ppid);
+  path_and_ppid_from_pid(pid, &path, &ppid);
   return path;
 }
 
 pid_t ppid_from_pid(pid_t pid) {
   char *path, pid_t ppid;
-  path_and_ppid_from_pid(&path, &ppid);
+  path_and_ppid_from_pid(pid, &path, &ppid);
   return ppid;
 }
 
