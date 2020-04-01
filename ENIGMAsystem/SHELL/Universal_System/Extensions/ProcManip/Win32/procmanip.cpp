f5d3369e4abde6861662dcd391ec5f28595d7436
time-killer-games <samuelvenable@hotmail.com>
2020-04-01 14:32:50 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Universal_System/Extensions/ProcManip/Win32/procmanip.cpp
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/ProcManip/Win32/procmanip.cpp
@@ -6,37 +6,32 @@ using std::string;
 
 extern "C" unsigned long cocoa_pid_from_wid(unsigned long wid);
 
-namespace enigma_user {
-
-string path_from_pid(pid_t pid) {
-  string path;
+static inline void path_and_ppid_from_pid(pid_t *ppid, char **path) {
   HANDLE h = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
   PROCESSENTRY32 pe = { 0 };
   pe.dwSize = sizeof(PROCESSENTRY32);
   if(Process32First(h, &pe)) {
     do {
       if (pe.th32ProcessID == pid) {
-        path = pe.szExeFile;
+        *(ppid) = pe.th32ParentProcessID;
+        *(path) = pe.szExeFile;
       }
     } while(Process32Next(h, &pe));
   }
   CloseHandle(h);
+}
+
+namespace enigma_user {
+
+string path_from_pid(pid_t pid) {
+  pid_t ppid; char *path;
+  path_and_ppid_from_pid(&ppid, &path);
   return path;
 }
 
 pid_t ppid_from_pid(pid_t pid) {
-  pid_t ppid;
-  HANDLE h = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-  PROCESSENTRY32 pe = { 0 };
-  pe.dwSize = sizeof(PROCESSENTRY32);
-  if(Process32First(h, &pe)) {
-    do {
-      if (pe.th32ProcessID == pid) {
-        ppid = pe.th32ParentProcessID;
-      }
-    } while(Process32Next(h, &pe));
-  }
-  CloseHandle(h);
+  pid_t ppid; char *path;
+  path_and_ppid_from_pid(&ppid, &path);
   return ppid;
 }
 
