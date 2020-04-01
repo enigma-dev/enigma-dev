7bab7697db3ee96cc22ce0a2bd1fc0fba6c9bd29
time-killer-games <samuelvenable@hotmail.com>
2020-04-01 14:03:28 -0400

    [skip ci]

---
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/ProcManip/Win32/procmanip.cpp
@@ -0,0 +1,53 @@
+#include <windows.h>
+#include <tlhelp32.h>
+#include <string>
+
+using std::string;
+
+extern "C" unsigned long cocoa_pid_from_wid(unsigned long wid);
+
+namespace enigma_user {
+
+string path_from_pid(pid_t pid) {
+  string path;
+  HANDLE h = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+  PROCESSENTRY32 pe = { 0 };
+  pe.dwSize = sizeof(PROCESSENTRY32);
+  if(Process32First(h, &pe)) {
+    do {
+      if (pe.th32ProcessID == pid) {
+        path = pe.szExeFile;
+      }
+    } while(Process32Next(h, &pe));
+  }
+  CloseHandle(h);
+  return path;
+}
+
+pid_t ppid_from_pid(pid_t pid) {
+  pid_t ppid;
+  HANDLE h = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+  PROCESSENTRY32 pe = { 0 };
+  pe.dwSize = sizeof(PROCESSENTRY32);
+  if(Process32First(h, &pe)) {
+    do {
+      if (pe.th32ProcessID == pid) {
+        ppid = pe.th32ParentProcessID;
+      }
+    } while(Process32Next(h, &pe));
+  }
+  CloseHandle(h);
+  return ppid;
+}
+
+pid_t pid_from_wid(string wid) {
+  DWORD dwProcessId;
+  unsigned long long ull_wid = stoull(wid, nullptr, 10);
+  void *voidp_wid = reinterpret_cast<void *>(ull_wid);
+  HWND hwnd_wid = reinterpret_cast<HWND>(voidp_wid);
+  GetWindowThreadProcessId(hwnd_wid, &dwProcessId);
+  unsigned long ul_pid = reinterpret_cast<unsigned long>(dwProcessId);
+  return reinterpret_cast<pid_t>(ul_pid);
+}
+
+}
