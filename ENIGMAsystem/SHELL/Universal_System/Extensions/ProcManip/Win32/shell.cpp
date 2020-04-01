bc4969a6f1b5b7d3c8064d065d212747eeb3de25
time-killer-games <samuelvenable@hotmail.com>
2020-04-01 16:43:16 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Universal_System/Extensions/ProcManip/Win32/shell.cpp
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/ProcManip/Win32/shell.cpp
@@ -1,3 +1,22 @@
+/** Copyright (C) 2008-2017 Josh Ventura
+*** Copyright (C) 2013-2014 Robert B. Colton
+*** Copyright (C) 2020 Samuel Venable
+***
+*** This file is a part of the ENIGMA Development Environment.
+***
+*** ENIGMA is free software: you can redistribute it and/or modify it under the
+*** terms of the GNU General Public License as published by the Free Software
+*** Foundation, version 3 of the license or any later version.
+***
+*** This application and its source code is distributed AS-IS, WITHOUT ANY
+*** WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+*** FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+*** details.
+***
+*** You should have received a copy of the GNU General Public License along
+*** with this code. If not, see <http://www.gnu.org/licenses/>
+**/
+
 #include <windows.h>
 
 #include "Platforms/General/PFmain.h"
