451274d5d41337a27d50613131fb79a93047555d
time-killer-games <samuelvenable@hotmail.com>
2020-04-01 16:02:18 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Universal_System/Extensions/ProcManip/procmanip.h
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/ProcManip/procmanip.h
@@ -30,16 +30,16 @@ using std::string;
 
 namespace proc_manip {
 
-  // get executable path from process id
-  string path_from_pid(pid_t pid);
-  
-  // get parent process id of process id
-  pid_t ppid_from_pid(pid_t pid);
+// get executable path from process id
+string path_from_pid(pid_t pid);
 
-  // get process id from window id;
-  // e.g. wid = window_identifier()
-  // process_id = pid_from_wid(wid)
-  // for getting current process id
-  pid_t pid_from_wid(string wid);
+// get parent process id of process id
+pid_t ppid_from_pid(pid_t pid);
+
+// get process id from window id;
+// e.g. wid = window_identifier()
+// process_id = pid_from_wid(wid)
+// for getting current process id
+pid_t pid_from_wid(string wid);
 
 }
