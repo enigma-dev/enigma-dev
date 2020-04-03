2c71a5f4ff90fdaa2034ffdafe734f0286f33a1a
Greg Williamson <fundies@no-reply.github.com>
2020-04-03 13:19:09 -0400

    guess we build sdl now?

--- CI/solve_engine_deps.sh
+++ CI/solve_engine_deps.sh
@@ -21,7 +21,7 @@ if [ "$PLATFORM" == "SDL" ] || [ "$TEST_HARNESS" == true ]; then
   pushd /tmp
   curl -O https://www.libsdl.org/release/SDL2-2.0.12.tar.gz
   tar -xf SDL2-2.0.12.tar.gz
-  pusdhd SDL2-2.0.12/
+  pushd SDL2-2.0.12/
   ./configure --prefix=/usr
   make
   sudo make install
