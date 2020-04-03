69367b64e3e0c71575c5f25298ca8c8ac94324ba
Greg Williamson <fundies@no-reply.github.com>
2020-04-03 13:44:19 -0400

    wtf

--- CI/solve_engine_deps.sh
+++ CI/solve_engine_deps.sh
@@ -18,15 +18,7 @@ fi
 ###### Platforms #######
 if [ "$PLATFORM" == "SDL" ] || [ "$TEST_HARNESS" == true ]; then
   #LINUX_DEPS="$LINUX_DEPS libsdl2-dev"
-  pushd /tmp
-  curl -O https://www.libsdl.org/release/SDL2-2.0.12.tar.gz
-  tar -xf SDL2-2.0.12.tar.gz
-  pushd SDL2-2.0.12/
-  ./configure --prefix=/usr
-  make
-  sudo make install
-  popd
-  popd
+  ./CI/build-sdl.sh
 fi
 
 ###### Graphics #######
