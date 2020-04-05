fdf531b4b625a4bc9816025e2acccf5c5bc04823
time-killer-games <samuelvenable@hotmail.com>
2020-04-05 18:51:33 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Platforms/General/PFshell.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/PFshell.cpp
@@ -9,9 +9,9 @@ using enigma_insecure::execute_shell_for_output;
 static void request_permission() {
   if (!permission_requested) {
     if (enigma_user::show_question("This application is requesting permission to use the command line.\
-Be aware this can be security risk and we advise you to only allow this from application publishers\
-that you know you can trust.\n\nWould you like to grant this application permission to utilize\
-such functionailty?\n\nThis may or may not be required for the application to behave as expected.")) {
+Be aware this can be security risk and it is advised to only allow this from application publishers\
+that can trusted.\n\nAllow the application to utilize such functionailty?\n\nThis might be\
+required for the application to behave as expected.")) {
       secure_mode  = false; 
     }
       permission_requested = true;
