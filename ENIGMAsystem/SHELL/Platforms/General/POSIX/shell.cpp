656fc37db26126d5727e8ee4e03b41e206059159
time-killer-games <samuelvenable@hotmail.com>
2020-04-05 19:25:34 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/shell.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/shell.cpp
@@ -2,28 +2,30 @@
 #include "Widget_Systems/widgets_mandatory.h"
 
 namespace enigma_insecure {
-  void execute_program(string fname, string args, bool wait) {
-    if (system(NULL)) {
-      system(("\"" + fname + "\" " + args + (wait ? " &" : "")).c_str());
-    } else {
-      DEBUG_MESSAGE("shell execution cannot be used as there is no command processor!", MESSAGE_TYPE::M_ERROR);
-      return;
-    }
+
+void execute_program(string fname, string args, bool wait) {
+  if (system(NULL)) {
+    system(("\"" + fname + "\" " + args + (wait ? " &" : "")).c_str());
+  } else {
+    DEBUG_MESSAGE("shell execution cannot be used as there is no command processor!", MESSAGE_TYPE::M_ERROR);
+    return;
   }
+}
   
-  void execute_shell(string fname, string args) {
-    execute_program(fname, args, false);
-  }
+void execute_shell(string fname, string args) {
+  execute_program(fname, args, false);
+}
   
-  string execute_shell_for_output(const string &command) {
-    char buffer[BUFSIZ]; string res;
-    FILE *pf = popen(command.c_str(), "r");
-    while (!feof(pf)) {
-      res.append(buffer, fread(&buffer, sizeof(char), BUFSIZ, pf));
-    }
-    pclose(pf);
-    return res;
+string execute_shell_for_output(const string &command) {
+  char buffer[BUFSIZ]; string res;
+  FILE *pf = popen(command.c_str(), "r");
+  while (!feof(pf)) {
+    res.append(buffer, fread(&buffer, sizeof(char), BUFSIZ, pf));
   }
+  pclose(pf);
+  return res;
+}
+  
 } // namsepace enigma_insecure
 
 namespace enigma_user {
