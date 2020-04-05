19afb4475cd53179ce71b4e2def154a2091380a0
Greg Williamson <fundies@users.noreply.github.com>
2020-04-05 03:06:47 -0400

    Shell Execution Prompts (#1955)

--- ENIGMAsystem/SHELL/Platforms/General/PFmain.h
+++ ENIGMAsystem/SHELL/Platforms/General/PFmain.h
@@ -45,6 +45,11 @@ namespace enigma {
   void set_room_speed(int rs);
 }
 
+namespace enigma_insecure {
+  void execute_shell(std::string operation, std::string fname, std::string args, bool wait = false);
+  void execute_shell_for_output(const std::string &command, std::string& res);
+}  
+  
 namespace enigma_user {
 
 extern std::string working_directory;
--- ENIGMAsystem/SHELL/Platforms/General/POSIX/shell.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/shell.cpp
@@ -1,36 +1,57 @@
 #include "Platforms/General/PFmain.h"
 #include "Widget_Systems/widgets_mandatory.h"
 
+static bool asked_if_idiot = false;
+static bool confirmed_idiot = false;
+
+static void ask_if_idiot() {
+  if (!asked_if_idiot) {
+    if (enigma_user::show_question("The following game runs shell functions that are almost always unnecessary\
+      and can potentionally cause irreparable harm to your system such as deleting you home folder.\
+      We advise only enabling these functions after careful inspection of the game's source code\
+      Would you like to enable these dangerous funtions?")) { confirmed_idiot = true; }
+      asked_if_idiot = true;
+  }
+}
+
+namespace enigma_insecure {
+  void execute_shell(std::string operation, std::string fname, std::string args, bool wait) {
+    if (system(NULL)) {
+      system(("\"" + fname + "\" " + args + (wait ? " &" : "")).c_str());
+    } else {
+      DEBUG_MESSAGE("execute_program cannot be used as there is no command processor!", MESSAGE_TYPE::M_ERROR);
+      return;
+    }
+  }
+  
+  void execute_shell_for_output(const std::string &command, std::string& res) {
+    char buffer[BUFSIZ];
+    FILE *pf = popen(command.c_str(), "r");
+    while (!feof(pf)) {
+      res.append(buffer, fread(&buffer, sizeof(char), BUFSIZ, pf));
+    }
+    pclose(pf);
+  }
+}
+
 namespace enigma_user {
 
 void execute_shell(std::string operation, std::string fname, std::string args) {
-  if (system(NULL)) {
-    system(("\"" + fname + "\" " + args + " &").c_str());
-  } else {
-    DEBUG_MESSAGE("execute_shell cannot be used as there is no command processor!", MESSAGE_TYPE::M_ERROR);
-    return;
-  }
+  ask_if_idiot();
+  if (confirmed_idiot) enigma_insecure::execute_shell(operation, fname, args, false);
 }
 
 void execute_shell(std::string fname, std::string args) { execute_shell("", fname, args); }
 
 void execute_program(std::string operation, std::string fname, std::string args, bool wait) {
-  if (system(NULL)) {
-    system(("\"" + fname + "\" " + args + (wait ? " &" : "")).c_str());
-  } else {
-    DEBUG_MESSAGE("execute_program cannot be used as there is no command processor!", MESSAGE_TYPE::M_ERROR);
-    return;
-  }
+  ask_if_idiot();
+  if (confirmed_idiot) enigma_insecure::execute_shell(operation, fname, args, wait);
 }
 
 std::string execute_shell_for_output(const std::string &command) {
+  ask_if_idiot();
   std::string res;
-  char buffer[BUFSIZ];
-  FILE *pf = popen(command.c_str(), "r");
-  while (!feof(pf)) {
-    res.append(buffer, fread(&buffer, sizeof(char), BUFSIZ, pf));
-  }
-  pclose(pf);
+  if (confirmed_idiot) enigma_insecure::execute_shell_for_output(command, res);
   return res;
 }
 
