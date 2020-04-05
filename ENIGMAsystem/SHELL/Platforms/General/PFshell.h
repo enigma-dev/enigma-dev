20c55a268d57a266a984649e9ae3479daf48888f
time-killer-games <samuelvenable@hotmail.com>
2020-04-05 19:10:48 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Platforms/General/PFshell.h
+++ ENIGMAsystem/SHELL/Platforms/General/PFshell.h
@@ -13,5 +13,7 @@ namespace enigma_user {
 void execute_shell(std::string fname, std::string args);
 void execute_program(std::string fname, std::string args, bool wait);
 std::string execute_shell_for_output(const std::string &command);
+void url_open(std::string url);
+void action_webpage(const std::string &url);
 
 }
