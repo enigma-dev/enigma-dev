1f0d1f39e30ffdcbddb554c4cbff10787af11eec
Robert Colton <robertbcolton@gmail.com>
2020-04-04 23:51:10 -0400

    Update draw_3d_shapes_test.cpp

--- CommandLine/testing/Tests/draw_3d_shapes_test.cpp
+++ CommandLine/testing/Tests/draw_3d_shapes_test.cpp
@@ -3,5 +3,5 @@
 TEST(Regression, draw_3d_shapes_test) {
   if (!TestHarness::windowing_supported()) return;
   auto test_harness = LAUNCH_HARNESS_FOR_GAME(TestConfig(), "gmx");
-  test_common(test_harness, "draw_3d_shapes_test");
+  test_common(test_harness.get(), "draw_3d_shapes_test");
 }
