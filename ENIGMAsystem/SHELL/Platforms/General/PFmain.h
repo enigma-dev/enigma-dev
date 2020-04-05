63170548b6bed04a49acdb6673436efe5e48a5a8
time-killer-games <samuelvenable@hotmail.com>
2020-04-05 19:09:39 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Platforms/General/PFmain.h
+++ ENIGMAsystem/SHELL/Platforms/General/PFmain.h
@@ -65,10 +65,6 @@ void sleep(int ms);
 void game_end();
 void game_end(int ret);
 void action_end_game();
-void url_open(std::string url,std::string target="_self",std::string options="");
-void url_open_ext(std::string url, std::string target);
-void url_open_full(std::string url, std::string target,std::string options);
-void action_webpage(const std::string &url);
 
 // Data type could be unsigned for the paramter since the collection is size_t, however this would make the function not behave as GM.
 // show_message(parameter_string(-1)); in GM8.1 will show an empty string, if this function cast the parameter to unsigned that won't be the behavior.
@@ -81,12 +77,6 @@ unsigned long long disk_size(std::string drive);
 unsigned long long disk_free(std::string drive);
 
 void set_program_priority(int value);
-void execute_shell(std::string fname, std::string args);
-void execute_shell(std::string operation, std::string fname, std::string args);
-std::string execute_shell_for_output(const std::string &command);
-void execute_program(std::string fname, std::string args, bool wait);
-void execute_program(std::string operation, std::string fname, std::string args, bool wait);
-
 std::string filename_absolute(std::string fname);
 std::string filename_join(std::string prefix, std::string suffix);
 std::string environment_get_variable(std::string name);
