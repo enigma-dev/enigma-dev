d53642050a91d6a5ce2230a4a1982d48a31e9305
time-killer-games <samuelvenable@hotmail.com>
2020-04-05 19:35:01 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Platforms/General/PFshell.h
+++ ENIGMAsystem/SHELL/Platforms/General/PFshell.h
@@ -12,6 +12,8 @@ string execute_shell_for_output(const string &command);
 
 namespace enigma_user {
 
+bool securemode_get_enabled();
+void securemode_set_enabled(bool enable);
 void execute_shell(string fname, string args);
 void execute_program(string fname, string args, bool wait);
 string execute_shell_for_output(const string &command);
