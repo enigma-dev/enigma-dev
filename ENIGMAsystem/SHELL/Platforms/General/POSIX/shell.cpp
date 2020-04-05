e423eee9efaeb8867cc8426aa6d7ee78ff4e196d
time-killer-games <samuelvenable@hotmail.com>
2020-04-05 19:23:50 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/shell.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/shell.cpp
@@ -2,36 +2,49 @@
 #include "Widget_Systems/widgets_mandatory.h"
 
 namespace enigma_insecure {
-  void execute_shell(std::string operation, std::string fname, std::string args, bool wait) {
+  void execute_program(string fname, string args, bool wait) {
     if (system(NULL)) {
       system(("\"" + fname + "\" " + args + (wait ? " &" : "")).c_str());
     } else {
-      DEBUG_MESSAGE("execute_program cannot be used as there is no command processor!", MESSAGE_TYPE::M_ERROR);
+      DEBUG_MESSAGE("shell execution cannot be used as there is no command processor!", MESSAGE_TYPE::M_ERROR);
       return;
     }
   }
   
-  void execute_shell_for_output(const std::string &command, std::string& res) {
-    char buffer[BUFSIZ];
+  void execute_shell(string fname, string args) {
+    execute_program(fname, args, false);
+  }
+  
+  string execute_shell_for_output(const string &command) {
+    char buffer[BUFSIZ]; string res;
     FILE *pf = popen(command.c_str(), "r");
     while (!feof(pf)) {
       res.append(buffer, fread(&buffer, sizeof(char), BUFSIZ, pf));
     }
     pclose(pf);
+    return res;
   }
 }
 
 namespace enigma_user {
 
-void execute_program(std::string fname, std::string args, bool wait) {
-  execute_shell(fname, args, wait);
+void execute_program(string fname, string args, bool wait) {
+  execute_program(fname, args, wait);
 }
 
-void url_open(std::string url) {
+void execute_shell(string fname, string args) {
+  execute_shell(fname, args);
+}
+
+string execute_shell_for_output(const string &command) {
+  return execute_shell_for_output(command);
+}
+
+void url_open(string url) {
   execute_program("xdg-open", url, false);
 }
 
-void action_webpage(const std::string& url) {
+void action_webpage(const string& url) {
   url_open(url);
 }
 
