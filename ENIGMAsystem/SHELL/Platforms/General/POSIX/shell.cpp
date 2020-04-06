7375dca3599dd971d4eca730a6b9b653a9b8391a
time-killer-games <samuelvenable@hotmail.com>
2020-04-05 20:20:39 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/shell.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/shell.cpp
@@ -30,18 +30,6 @@ string execute_shell_for_output(const string &command) {
 
 namespace enigma_user {
 
-void execute_program(string fname, string args, bool wait) {
-  execute_program(fname, args, wait);
-}
-
-void execute_shell(string fname, string args) {
-  execute_shell(fname, args);
-}
-
-string execute_shell_for_output(const string &command) {
-  return execute_shell_for_output(command);
-}
-
 void url_open(string url) {
   execute_program("xdg-open", url, false);
 }
