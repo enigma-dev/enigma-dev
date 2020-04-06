212f10911054f0c180a7ed00828c2491727b67ae
Greg Williamson <fundies@no-reply.github.com>
2020-04-05 22:13:39 -0400

    iterate regression tests too

--- CommandLine/testing/Tests/draw_3d_shapes_test.cpp
+++ CommandLine/testing/Tests/draw_3d_shapes_test.cpp
@@ -2,6 +2,9 @@
 
 TEST(Regression, draw_3d_shapes_test) {
   if (!TestHarness::windowing_supported()) return;
-  auto test_harness = LAUNCH_HARNESS_FOR_GAME(TestConfig(), "gmx");
-  test_common(test_harness.get(), "draw_3d_shapes_test");
+  // Iterate only platforms and graphics systems
+  for (TestConfig tc : GetValidConfigs(true, true, false, false, false, false)) {
+    auto test_harness = LAUNCH_HARNESS_FOR_GAME(TestConfig(), "gmx");
+    test_common(test_harness.get(), "draw_3d_shapes_test" + tc.stringify());
+  }
 }
