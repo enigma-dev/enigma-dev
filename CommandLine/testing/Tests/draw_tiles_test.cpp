1c4c01978c40706c341b8750e8767ae7945c6c51
Greg Williamson <fundies@no-reply.github.com>
2020-04-10 16:25:03 -0400

    missed confict

--- CommandLine/testing/Tests/draw_tiles_test.cpp
+++ CommandLine/testing/Tests/draw_tiles_test.cpp
@@ -2,17 +2,10 @@
 
 TEST(Regression, draw_tiles_test) {
   if (!TestHarness::windowing_supported()) return;
-<<<<<<< HEAD
   // Iterate only platforms and graphics systems
   for (TestConfig tc : GetValidConfigs(true, true, false, false, false, false)) {
     tc.extensions = "Paths,libpng";
     auto test_harness = LAUNCH_HARNESS_FOR_SOG(tc);
     test_common(test_harness.get(), "draw_tiles_test" + tc.stringify());
   }
-=======
-  TestConfig tc;
-  tc.extensions = "Paths,libpng";
-  auto test_harness = LAUNCH_HARNESS_FOR_SOG(tc);
-  test_common(test_harness.get(), "draw_tiles_test");
->>>>>>> origin/master
 }
