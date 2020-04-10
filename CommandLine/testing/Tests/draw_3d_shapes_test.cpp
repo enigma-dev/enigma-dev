396a33d7bb8c135c26f5d7946a41d10b9a61f8f4
Robert Colton <robertbcolton@gmail.com>
2020-04-10 15:23:52 -0400

    Cleanup Tests (#1956)
    * Deduplicate test redundancy.
    * Fix common test includes

--- CommandLine/testing/Tests/draw_3d_shapes_test.cpp
+++ CommandLine/testing/Tests/draw_3d_shapes_test.cpp
@@ -1,24 +1,7 @@
-#include "TestHarness.hpp"
-
-#include <gtest/gtest.h>
+#include "CommonTest.hpp"
 
 TEST(Regression, draw_3d_shapes_test) {
   if (!TestHarness::windowing_supported()) return;
   auto test_harness = LAUNCH_HARNESS_FOR_GAME(TestConfig(), "gmx");
-  if (!test_harness) FAIL() << "Game could not be run.";
-
-  test_harness->wait();  // Let the game render a frame first.
-  ASSERT_TRUE(test_harness->game_is_running())
-      << "Game stopped running unexpectedly";
-
-  test_harness->screen_save("./test-harness-out/enigma_draw_3d_shapes_test.png");
-
-  test_harness->close_window();
-  bool game_running = test_harness->game_is_running();
-  for (int i = 0; game_running && i < 10; ++i) {
-    test_harness->wait();
-    game_running = test_harness->game_is_running();
-  }
-  ASSERT_FALSE(game_running) << "Game did not exit after window was closed!";
-  EXPECT_EQ(test_harness->get_return(), 0) << "Game returned non-zero exit code!";
+  test_common(test_harness.get(), "draw_3d_shapes_test");
 }
