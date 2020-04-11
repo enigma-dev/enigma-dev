2149e938ed95e052056b2cffb72d240e1af00ac0
time-killer-games <samuelvenable@hotmail.com>
2020-04-11 18:39:48 -0400

    Update GSmatrix_impl.h

--- ENIGMAsystem/SHELL/Graphics_Systems/General/GSmatrix_impl.h
+++ ENIGMAsystem/SHELL/Graphics_Systems/General/GSmatrix_impl.h
@@ -30,7 +30,7 @@
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_LEFT_HANDED
 #include <glm/glm.hpp>
-// FreeBSD won't compile otheriwse because this is deprecated
+// FreeBSD won't compile otheriwse
 #ifndef GLM_LEFT_HANDED
 #define GLM_LEFT_HANDED
 #endif
