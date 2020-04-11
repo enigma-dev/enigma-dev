9920c10bbb0f8ff30d5a69d4d0603acb9f0430b9
time-killer-games <samuelvenable@hotmail.com>
2020-04-11 14:54:08 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Platforms/platforms_mandatory.h
+++ ENIGMAsystem/SHELL/Platforms/platforms_mandatory.h
@@ -123,7 +123,7 @@ namespace enigma_user
 window_t window_handle();
 
 // This is used with roomsystem
-void window_default();
+void window_default(bool center_size = true);
 
 // These four are a surprisingly integral part of the system
 int window_mouse_get_x();
