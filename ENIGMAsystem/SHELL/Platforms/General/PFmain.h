387ab4ab6b685e253254be44627666eb36f72bc4
time-killer-games <samuelvenable@hotmail.com>
2020-04-11 00:56:05 -0400

    Add files via upload

--- ENIGMAsystem/SHELL/Platforms/General/PFmain.h
+++ ENIGMAsystem/SHELL/Platforms/General/PFmain.h
@@ -37,6 +37,7 @@ namespace enigma {
   void Sleep(int ms);
   void compute_window_scaling();
   void compute_window_size();
+  void initialize_program_directory();
   void initialize_directory_globals();
   void set_program_args(int argc, char** argv);
   void initTimer();
@@ -45,11 +46,6 @@ namespace enigma {
   void set_room_speed(int rs);
 }
 
-namespace enigma_insecure {
-  void execute_shell(std::string operation, std::string fname, std::string args, bool wait = false);
-  void execute_shell_for_output(const std::string &command, std::string& res);
-}  
-  
 namespace enigma_user {
 
 extern std::string working_directory;
--- ENIGMAsystem/SHELL/Platforms/General/PFwindow.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/PFwindow.cpp
@@ -77,7 +77,7 @@ void compute_window_size() {
       if (scaledWidth > windowWidth) windowWidth = scaledWidth;
       if (scaledHeight > windowHeight) windowHeight = scaledHeight;
     }
-    enigma_user::window_set_rectangle(windowX, windowY, windowWidth, windowHeight);
+    enigma_user::window_set_size(windowWidth, windowHeight);
   } else {
     enigma_user::window_set_rectangle(0, 0, parWidth, parHeight);
   }
@@ -252,7 +252,7 @@ void window_set_color(int color) { enigma::windowColor = color; }
 
 int window_get_color() { return enigma::windowColor; }
 
-void window_default(bool center_size) {
+void window_default() {
   int xm = room_width, ym = room_height;
   if (view_enabled) {
     int tx = 0, ty = 0;
@@ -273,16 +273,10 @@ void window_default(bool center_size) {
     if (xm > screen_width) xm = screen_width;
     if (ym > screen_height) ym = screen_height;
   }
-  bool center = true;
-  if (center_size) {
-    center = (xm != enigma::windowWidth || ym != enigma::windowHeight);
-  }
 
   enigma::windowWidth = enigma::regionWidth = xm;
   enigma::windowHeight = enigma::regionHeight = ym;
-
   enigma::compute_window_size();
-  if (center) window_center();
 }
 
 void window_center() {
