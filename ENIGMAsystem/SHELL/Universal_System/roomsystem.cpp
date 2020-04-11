2cd681de732b381958a87d34ec528d08d05f7c23
time-killer-games <samuelvenable@hotmail.com>
2020-04-11 14:55:02 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Universal_System/roomsystem.cpp
+++ ENIGMAsystem/SHELL/Universal_System/roomsystem.cpp
@@ -232,7 +232,7 @@ namespace enigma
     // In pull request 1831, it was decided to adopt GMS behavior instead of GM8.
     // The window is no longer moved, centered, or resized when switching rooms.
     // This is always true, even if the room sizes are different.
-    enigma_user::window_default();
+    enigma_user::window_default(false);
     // window sized by first room, can make visible now
     enigma_user::window_set_visible(true);
     enigma_user::io_clear();
