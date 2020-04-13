57bc377bc6609e5296c3d884c4221d4eb6773c27
time-killer-games <samuelvenable@hotmail.com>
2020-04-13 15:14:04 -0400

    [skip ci]

--- CommandLine/testing/Platform/TestHarness-Win32.cpp
+++ CommandLine/testing/Platform/TestHarness-Win32.cpp
@@ -1,4 +1,3 @@
-  
 #include "TestHarness.hpp"
 
 #include <gtest/gtest.h>
@@ -8,189 +7,227 @@
 #include <iostream>
 #include <memory>
 #include <string>
-#include <string_view>
-#include <sys/wait.h>
 #include <unistd.h>
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
-
-std::vector<TestConfig> GetValidConfigs(bool platforms, bool graphics, bool audio, bool collisions, bool widgets, bool network) {
-  std::vector<TestConfig> tcs;
-  
-  for (std::string_view p : {"xlib", "SDL"} ) {
-    // FIXME: glgetteximage is used in opengl-common and is unsupported by gles. This function currently is required to copy surfaces in some tests
-    for (std::string_view g : {"OpenGL1", "OpenGL3"/*, "OpenGLES2", "OpenGLES3"*/ }) {
-      // Invalid combos
-      if (g == "OpenGLES2" && p != "SDL") continue;
-      if (g == "OpenGLES3" && p != "SDL") continue;
-      for (std::string_view a : {"OpenAL", "SFML" }) {
-        for (std::string_view c : {"Precise", "BBox" }) {
-          for (std::string_view w : {"None", "GTK+", "xlib" }) {
-            for (std::string_view n : {"None", "BerkeleySockets", "Asynchronous" }) {
-              TestConfig tc;
-              tc.platform = p;
-              tc.graphics = g;
-              tc.audio = a;
-              tc.collision = c;
-              tc.widgets = w;
-              tc.network = n;
-              tcs.push_back(tc);
-              if (!network) break;
-            }
-            if (!widgets) break;
-          }
-          if (!collisions) break;
-        }
-        if (!audio) break;
-      }
-      if (!graphics) break;
-    }
-    if (!platforms) break;
-  }
-  
-  return tcs;
-}
+#include <windows.h>
+#include <direct.h>
+#include <process.h>
+#include <chrono>
+#include <thread>
+#include <cwchar>
 
 namespace {
 using std::string;
+using std::wstring;
+using std::size_t;
 using std::to_string;
 using std::unique_ptr;
 using std::getenv;
+
+wstring widen(string tstr) {
+  size_t wchar_count = tstr.size() + 1;
+  vector<wchar_t> buf(wchar_count);
+  return wstring{ buf.data(), (size_t)MultiByteToWideChar(CP_UTF8, 0, tstr.c_str(), -1, buf.data(), (int)wchar_count) };
+}
+
+string narrow(wstring wstr) {
+  int nbytes = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), (int)wstr.length(), NULL, 0, NULL, NULL);
+  vector<char> buf(nbytes);
+  return string{ buf.data(), (size_t)WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), (int)wstr.length(), buf.data(), nbytes, NULL, NULL) };
+}
+
+string get_temp_directory() {
+  wchar buffer[MAX_PATH + 1];
+  GetTempPathW(MAX_PATH + 1, buffer);
+  return (narrow(buffer).back() == '\\') ? narrow(buffer) : narrow(buffer) + "\\";
+}
+
+struct handle_data {
+  unsigned long process_id;
+  HWND window_handle;
+};
+
+BOOL CALLBACK enum_windows_callback(HWND handle, LPARAM lParam) {
+  handle_data& data = *(handle_data*)lParam;
+  unsigned long process_id = 0;
+  GetWindowThreadProcessId(handle, &process_id);
+  if (data.process_id != process_id || !is_main_window(handle))
+    return TRUE;
+  data.window_handle = handle;
+  return FALSE;
+}
+
+BOOL is_main_window(HWND handle) {
+  return GetWindow(handle, GW_OWNER) == (HWND)0 && IsWindowVisible(handle);
+}
+
+int pid_is_running(DWORD pid) {
+  HANDLE hProcess;
+  DWORD exitCode;
+  if (pid == 0) {
+    return 1;
+  }
+  if (pid < 0) {
+    return 0;
+  }
+  hProcess = handle_from_pid(pid);
+  if (NULL == hProcess) {
+    if (GetLastError() == ERROR_INVALID_PARAMETER) {
+      return 0;
+    }
+    return -1;
+  }
+  DWORD dwRetval = WaitForSingleObject(hProcess, 0);
+  CloseHandle(hProcess);
+
+  switch (dwRetval) {
+    case WAIT_OBJECT_0;
+      return 0;
+      case WAIT_TIMEOUT;
+        return 1;
+      default:
+        return -1;
+  }
+}
 
 void gather_coverage(const TestConfig&);
 bool config_supports_lcov(const TestConfig &tc) {
   return tc.compiler.empty() || tc.compiler == "TestHarness";
 }
 
-string get_window_caption(Display *disp, Window win) {
-  char *caption;
-  XFetchName(disp, win, &caption);
-  string res = caption ? caption : "";
-  XFree(caption);
+string get_window_caption(HWND win) {
+  wchar_t res[256];
+  GetWindowTextW(win, res, 256);
+  return narrow(res);
+}
+
+DWORD get_window_pid(HWND win) {
+  DWORD res;
+  GetWindowThreadProcessId(win, &res);
   return res;
 }
 
-pid_t get_window_pid(Display *disp, Window win) {
-  static const Atom kWMPID = XInternAtom(disp, "_NET_WM_PID", False);
-  static const Atom kCardinal = XInternAtom(disp, "CARDINAL", False);
-  constexpr long kExpectItems = sizeof(pid_t) / 4;
-  Atom type;
-  int format;
-  unsigned long items;
-  unsigned long remainder;
-  unsigned char *data = 0;
-  pid_t res = 0;
-  if (XGetWindowProperty(disp, win, kWMPID, 0, kExpectItems, False, kCardinal,
-                         &type, &format, &items, &remainder, &data)
-      == Success && data) {
-    if (items < kExpectItems) {
-      if (items == 1) {
-        res = *(uint32_t*) data;
-      }
-    } else {
-      res = *(pid_t*) data;
-    }
-    XFree(data);
-  }
-  return res;
+HWND find_window_by_pid(HWND win, DWORD pid) {
+  if (get_window_pid(win) == pid) return win;
+  handle_data data;
+  data.process_id = pid;
+  data.window_handle = 0;
+  EnumWindows(enum_windows_callback, (LPARAM)&data);
+  return data.window_handle;
 }
 
-Window find_window_by_pid(Display *disp, Window win, pid_t pid) {
-  if (get_window_pid(disp, win) == pid) return win;
-  Window root, parent, *child;
-  unsigned children;
-  if (XQueryTree(disp, win, &root, &parent, &child, &children)) {
-    for (unsigned i = 0; i < children; i++) {
-      Window recurse = find_window_by_pid(disp, child[i], pid);
-      if (recurse != None) return recurse;
-    }
-  }
-  return None;
-}
-
-class X11_TestHarness final: public TestHarness {
+class Win32_TestHarness final: public TestHarness {
  public:
- 
   string get_caption() final {
     return get_window_caption(display, window_id);
   }
   void minimize_window() final {
-    system(("xdotool windowminimize " + to_string(window_id)).c_str());
+    ShowWindow(window_id, SW_MINIMIZE);
   }
   void maximize_window() final {
-    system(("wmctrl -i -r " + to_string(window_id)
-                + " -b add,maximized_vert,maximized_horz").c_str());
+    ShowWindow(window_id, SW_MAXIMIZE);
   }
   void fullscreen_window() final {
-    system(("wmctrl -i -r " + to_string(window_id)
-                + " -b add,fullscreen").c_str());
+    DWORD style = GetWindowLongPtr(window_id, GWL_STYLE);
+    style &= ~(WS_CAPTION | WS_BORDER);
+    SetWindowLongPtr(window_id, GWL_STYLE, style);
+    ShowWindow(window_id, SW_MAXIMIZE);
   }
   void unminimize_window() final {
-    system(("xdotool windowactivate " + to_string(window_id)).c_str());
+    ShowWindow(window_id, SW_RESTORE);
   }
   void unmaximize_window() final {
-    system(("wmctrl -i -r " + to_string(window_id)
-                + " -b remove,maximized_vert,maximized_horz").c_str());
+    ShowWindow(window_id, SW_RESTORE);
   }
   void unfullscreen_window() final {
-    system(("wmctrl -i -r " + to_string(window_id)
-                + " -b remove,fullscreen").c_str());
+    DWORD style = GetWindowLongPtr(window_id, GWL_STYLE);
+    style |= WS_CAPTION | WS_BORDER;
+    SetWindowLongPtr(window_id, GWL_STYLE, style);
+    ShowWindow(window_id, SW_RESTORE);
   }
   void close_window() final {
-    system(("wmctrl -i -c " + to_string(window_id)).c_str());
+    DestroyWindow(window_id);
   }
   void screen_save(std::string fPath) final {
-    system(("import -window " + to_string(window_id) + " " + fPath).c_str());
+    RECT rc;
+    GetClientRect(window_id, &rc);
+
+    HDC hdcScreen = GetDC(NULL);
+    HDC hdc = CreateCompatibleDC(hdcScreen);
+    HBITMAP hbmp = CreateCompatibleBitmap(hdcScreen, 
+      rc.right - rc.left, rc.bottom - rc.top);
+
+    SelectObject(hdc, hbmp);
+    PrintWindow(window_id, hdc, PW_CLIENTONLY);
+
+    Gdiplus::Bitmap bmp(hbmp,(HPALETTE)0);
+    CLSID pngClsid;
+    GetEncoderClsid(L"image/png", &pngClsid);
+    bmp.Save(widen(fPath).c_str(), &pngClsid,NULL);    
+
+    DeleteDC(hdc);
+    DeleteObject(hbmp);
+    ReleaseDC(NULL, hdcScreen);
   }
 
   bool game_is_running() final {
     if (pid == -1) return false;
-    int status = 0, wr = waitpid(pid, &status, WNOHANG);
+    HANDLE hproc = OpenProcess(PROCESS_ALL_ACCESS, false, pid);
+    int status = 0, wr = WaitForSingleObject(hproc, INFINITE);
+    CloseHandle(hproc);
     if (!wr) return true;
     if (wr != -1) {
       if (WIFEXITED(status)) {
         return_code = WEXITSTATUS(status);
         return false;
       }
-      if (kill(pid, 0)) {
+      hproc = OpenProcess(PROCESS_ALL_ACCESS, false, pid);
+      bool running = pid_is_running(hproc);
+      CloseHandle(hproc);
+      if (kill_success) {
         return_code = ErrorCodes::GAME_CRASHED;
         return false;
       }
     }
-    return !kill(pid, 0);
+	hproc = OpenProcess(PROCESS_ALL_ACCESS, false, pid);
+	bool running = pid_is_running(hproc);
+	CloseHandle(hproc);
+    return !running;
   }
   void close_game() final {
     close_window();
     for (int i = 0; i < 16; ++i) { // Give the game a second to close
       if (!game_is_running()) return;
-      usleep(62500);
+      std::this_thread::sleep_for(std::chrono::microseconds((62500));
     }
     std::cerr << "Warning: game did not close; terminated" << std::endl;
-    kill(pid, SIGTERM);
+    HANDLE hproc = OpenProcess(PROCESS_ALL_ACCESS, false, pid);
+    TerminateProcess(hproc, 1);
+    CloseHandle(hproc);
     return_code = ErrorCodes::TIMED_OUT;
   }
 
   void wait() final {
-    usleep(1000000);
+    std::this_thread::sleep_for(std::chrono::microseconds((1000000));
   }
   int get_return() final {
     return return_code;
   }
-  X11_TestHarness(Display *disp, pid_t game_pid, Window game_window,
+  Win32_TestHarness(DWORD game_pid, HWND game_window,
                   const TestConfig &tc):
       pid(game_pid), window_id(game_window), display(disp), test_config(tc) {}
-  ~X11_TestHarness() {
+  ~Win32_TestHarness() {
     if (game_is_running()) {
-      kill(pid, SIGKILL);
+      HANDLE hproc = OpenProcess(PROCESS_ALL_ACCESS, false, pid);
+      TerminateProcess(hproc, 1);
+      CloseHandle(hproc);
       std::cerr << "Game still running; killed" << std::endl;
     }
     gather_coverage(test_config);
   }
 
  private:
-  pid_t pid;
-  Window window_id;
-  Display *display;
+  DWORD pid;
+  HWND window_id;
   bool run_lcov;
   TestConfig test_config;
   int return_code = 0x10000;
@@ -201,10 +238,13 @@ constexpr const char *kDefaultExtensions =
     "ParticleSystems";
 
 int build_game(const string &game, const TestConfig &tc, const string &out) {
-  if (pid_t emake = fork()) {
+  if (DWORD emake = fork()) {
     int status = 0;
     if (emake == -1) return -1;
-    int ret = (waitpid(emake, &status, 0) == -1);
+    HANDLE hproc = OpenProcess(PROCESS_ALL_ACCESS, false, pid);
+    int status = 0, wr = WaitForSingleObject(hproc, INFINITE);
+    CloseHandle(hproc);
+    int ret = (wr == -1);
     system("./share_logs.sh");
     if (ret) return -1;
     if (WIFEXITED(status)) return WEXITSTATUS(status);
@@ -216,11 +256,10 @@ int build_game(const string &game, const TestConfig &tc, const string &out) {
   string emake_cmd = "./emake";
   string compiler = "--compiler=" + tc.get_or(&TC::compiler, "TestHarness");
   string mode = "--mode=" + tc.get_or(&TC::mode, "Debug");
-  string platform = "--platform=" + tc.get_or(&TC::platform, "xlib");
   string graphics = "--graphics=" + tc.get_or(&TC::graphics, "OpenGL1");
   string audio = "--audio=" + tc.get_or(&TC::audio, "OpenAL");
-  string widgets = "--widgets=" + tc.get_or(&TC::widgets, "None");
-  string network = "--network=" + tc.get_or(&TC::network, "None");
+  string widgets = "--widgets=" + tc.get_or(&TC::widgets, "NULL");
+  string network = "--network=" + tc.get_or(&TC::network, "NULL");
   string collision = "--collision=" + tc.get_or(&TC::collision, "Precise");
   string extensions = "--extensions="
       + tc.get_or(&TC::extensions, kDefaultExtensions);
@@ -229,7 +268,6 @@ int build_game(const string &game, const TestConfig &tc, const string &out) {
     emake_cmd.c_str(),
     compiler.c_str(),
     mode.c_str(),
-    platform.c_str(),
     graphics.c_str(),
     audio.c_str(),
     widgets.c_str(),
@@ -242,7 +280,7 @@ int build_game(const string &game, const TestConfig &tc, const string &out) {
     nullptr
   };
 
-  execvp(emake_cmd.c_str(), (char**) args);
+  _execvp(emake_cmd.c_str(), (char**)args);
   abort();
 }
 
@@ -254,14 +292,17 @@ void gather_coverage(const TestConfig &config) {
     return;
   }
 
-  pid_t child = fork();
+  DWORD child = fork();
   if (child) {
     if (child == -1) {
       ADD_FAILURE() << "Coverage failed to execute for test " << test_num << '!';
       return;
     }
     int status = -1;
-    if (waitpid(child, &status, 0) == -1) {
+  HANDLE hproc = OpenProcess(PROCESS_ALL_ACCESS, false, pid);
+  int status = 0, wr = WaitForSingleObject(hproc, INFINITE);
+  CloseHandle(hproc);
+    if (wr == -1) {
       ADD_FAILURE() << "Waiting on coverage report for test " << test_num
                     << " somehow failed...";
       return;
@@ -279,7 +320,7 @@ void gather_coverage(const TestConfig &config) {
     return;
   }
 
-  string src_dir = "--directory=/tmp/ENIGMA/.eobjs/Linux/Linux/TestHarness/"
+  string src_dir = "--directory=" + get_temp_directory() + "ENIGMA/.eobjs/Win32/Win32/TestHarness/"
                  + config.get_or(&TestConfig::mode, "Debug") + "/";
   string out_file = "--output-file=coverage_" + to_string(test_num) + ".info";
 
@@ -294,7 +335,7 @@ void gather_coverage(const TestConfig &config) {
     nullptr
   };
 
-  execvp("lcov", (char**) lcovArgs);
+  _execvp("lcov", (char**) lcovArgs);
   abort();
 }
 
@@ -306,7 +347,7 @@ bool TestHarness::windowing_supported() {
 
 unique_ptr<TestHarness>
 TestHarness::launch_and_attach(const string &game, const TestConfig &tc) {
-  string out = "/tmp/test-game";
+  string out = get_temp_directory() + "test-game.exe";
   if (int retcode = build_game(game, tc, out)) {
     if (retcode != -1) {
       std::cerr << "Failed to run emake." << std::endl;
@@ -316,22 +357,20 @@ TestHarness::launch_and_attach(const string &game, const TestConfig &tc) {
     return nullptr;
   }
 
-  pid_t pid = fork();
+  DWORD pid = fork();
   if (!pid) {
-    execl(out.c_str(), out.c_str(), nullptr);
+    _execl(out.c_str(), out.c_str(), nullptr);
     abort();
   }
   if (pid == -1) return nullptr;
-  usleep(5000000); // Give the window a 5 seconds to load and display.
+  std::this_thread::sleep_for(std::chrono::microseconds((250000)); // Give the window a quarter second to load and display.
 
-  Display *display = XOpenDisplay(0);
-  Window root = XDefaultRootWindow(display);
   for (int i = 0; i < 50; ++i) {  // Try for over ten seconds to grab the window
-    Window win = find_window_by_pid(display, root, pid);
-    if (win != None)
-      return std::unique_ptr<X11_TestHarness>(
-          new X11_TestHarness(display, pid, win, tc));
-    usleep(250000);
+    HWND win = find_window_by_pid(display, pid);
+    if (win != NULL)
+      return std::unique_ptr<Win32_TestHarness>(
+          new Win32_TestHarness(display, pid, win, tc));
+    std::this_thread::sleep_for(std::chrono::microseconds((250000));
   }
 
   return nullptr;
@@ -342,7 +381,7 @@ constexpr int operator"" _million(unsigned long long x) {
 }
 
 int TestHarness::run_to_completion(const string &game, const TestConfig &tc) {
-  string out = "/tmp/test-game";
+  string out = get_temp_directory() + "test-game";
   if (int retcode = build_game(game, tc, out)) {
     if (retcode != -1) {
       std::cerr << "Failed to run emake." << std::endl;
@@ -352,17 +391,19 @@ int TestHarness::run_to_completion(const string &game, const TestConfig &tc) {
     return ErrorCodes::BUILD_FAILED;
   }
 
-  pid_t pid = fork();
+  DWORD pid = fork();
   if (!pid) {
-    chdir(game.substr(0, game.find_last_of("\\/")).c_str());
-    execl(out.c_str(), out.c_str(), nullptr);
+    _chdir(game.substr(0, game.find_last_of("\\/")).c_str());
+    _execl(out.c_str(), out.c_str(), nullptr);
     abort();
   }
   if (pid == -1) {
     return ErrorCodes::LAUNCH_FAILED;
   }
   for (int i = 0; i < 30000000; i += 12500) {
-    int status = 0, wr = waitpid(pid, &status, WNOHANG);
+    HANDLE hproc = OpenProcess(PROCESS_ALL_ACCESS, false, pid);
+    int status = 0, wr = WaitForSingleObject(hproc, INFINITE);
+    CloseHandle(hproc);
     if (wr) {
       if (wr != -1) {
         if (WIFEXITED(status)) {
@@ -374,9 +415,11 @@ int TestHarness::run_to_completion(const string &game, const TestConfig &tc) {
       // Ignore the error for now...
       std::cerr << "Warning: ignoring waitpid being dumb." << std::endl;
     }
-    usleep(12500);
+    std::this_thread::sleep_for(std::chrono::microseconds((12500));
   }
   std::cerr << "ERROR: game still running after 30 seconds; killed" << std::endl;
-  kill(pid, SIGKILL);  // We're not dicking around with this.
+  hproc = OpenProcess(PROCESS_ALL_ACCESS, false, pid);
+  TerminateProcess(hproc, 1); // We're not dicking around with this.
+  CloseHandle(hproc);
   return ErrorCodes::TIMED_OUT;
 }
