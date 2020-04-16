5fa05afe16fc80ac99d994e3defc69696b2b4e52
time-killer-games <samuelvenable@hotmail.com>
2020-04-15 20:39:15 -0400

    Update WINDOWSshell.cpp

--- ENIGMAsystem/SHELL/Platforms/Win32/WINDOWSshell.cpp
+++ ENIGMAsystem/SHELL/Platforms/Win32/WINDOWSshell.cpp
@@ -1,6 +1,5 @@
 /** Copyright (C) 2008-2017 Josh Ventura
 *** Copyright (C) 2013-2014 Robert B. Colton
-*** Copyright (C) 2019 Samuel Venable
 ***
 *** This file is a part of the ENIGMA Development Environment.
 ***
