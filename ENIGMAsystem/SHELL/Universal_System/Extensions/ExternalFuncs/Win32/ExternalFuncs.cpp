36e4d49f23b1083a1be9c1a39cdea28fdea04da4
time-killer-games <samuelvenable@hotmail.com>
2020-04-26 18:49:57 -0400

    Create ExternalFuncs.cpp

---
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/ExternalFuncs/Win32/ExternalFuncs.cpp
@@ -0,0 +1,17 @@
+#include "Universal_System/estring.h"
+
+void *ExternalLoad(const char *dll) {
+  string str_dll = dll;
+  tstring tstr_dll = widen(str_dll);
+  return (void *)LoadLibraryW(tstr_dll.c_str());
+}
+
+bool ExternalFree(void *handle) {
+  return (bool)FreeLibrary((HMODULE)handle);
+}
+
+void *ExternalFunc(void *handle, const char *func) {
+  string str_func = func;
+  tstring tstr_func = widen(str_func);
+  return (void *)GetProcAddress((HMODULE)handle, tstr_func.c_str());
+}
