0b060cc67713313ffe0da22febbc436c345c0572
time-killer-games <samuelvenable@hotmail.com>
2020-04-26 18:45:10 -0400

    [skip ci]

---
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/ExternalFuncs/POSIX/ExternalFunc.cpp
@@ -0,0 +1,11 @@
+void *ExternalLoad(const char *dll) {
+  return dlopen(dll, RTLD_LAZY);
+}
+
+bool ExternalFree(void *handle) {
+  dlclose(handle);
+}
+
+void *ExternalFunc(void *handle, const char *func) {
+  return (dlsym(handle, func) == 0);
+}
