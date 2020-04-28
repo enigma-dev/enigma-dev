1d4d13d1dd4c2056fc4847c7f03be3d8ddf1ea2e
time-killer-games <samuelvenable@hotmail.com>
2020-04-28 13:15:43 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Universal_System/Extensions/ExternalFuncs/Win32/ExternalFuncs.h
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/ExternalFuncs/Win32/ExternalFuncs.h
@@ -1,7 +1,3 @@
-#define byte __windows_byte_workaround
-#include <windows.h>
-#undef byte
-
 void *ExternalLoad(const char *dll);
 bool ExternalFree(void *handle);
 void *ExternalFunc(void *handle, const char *func);
