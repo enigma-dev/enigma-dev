45f633013d3200818fc4944af4ed6555f6779636
Greg Williamson <fundies@no-reply.github.com>
2020-05-10 07:23:06 -0400

    i hate travis

--- CI/install_emake_deps.sh
+++ CI/install_emake_deps.sh
@@ -38,18 +38,13 @@ if [ "$TRAVIS_OS_NAME" != "osx" ]; then
 
   g++ --version
   
+  sudo update-alternatives --remove-all clang;
   sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-10 \
               81 \
               --slave /usr/bin/clang++ clang++ /usr/bin/clang++-10 \
               --slave /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-10 \
               --slave /usr/bin/clang-format clang-format /usr/bin/clang-format-10;
   
-  sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-7.0 \
-              60 \
-              --slave /usr/bin/clang++ clang++ /usr/bin/clang++-7.0 \
-              --slave /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-7.0 \
-              --slave /usr/bin/clang-format clang-format /usr/bin/clang-format-7.0;
-
   clang++ --version
 fi
 
