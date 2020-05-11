2e8a53ab35484496b90dec5538aa6fba0100217c
time-killer-games <samuelvenable@hotmail.com>
2020-05-11 12:00:15 -0400

    Add files via upload

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/BSD/BSDprogdir.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/BSD/BSDprogdir.cpp
@@ -1,19 +1,51 @@
 #include "Platforms/General/POSIX/POSIXprogdir.h"
+#include <sys/types.h>
 #include <sys/sysctl.h>
+#include <cstddef>
 
-namespace enigma {
+using std::string;
+using std::size_t;
+using std::cout;
+using std::endl;
 
-void initialize_program_directory() {
+static inline string program_directory_helper(size_t length) {
+  string path;
+  length++;
   int mib[4];
   mib[0] = CTL_KERN;
   mib[1] = KERN_PROC;
   mib[2] = KERN_PROC_PATHNAME;
   mib[3] = -1;
-  char buffer[PATH_MAX];
-  size_t cb = sizeof(buffer);
-  if (sysctl(mib, 4, buffer, &cb, NULL, 0) == 0) {
-    enigma_user::program_directory = enigma_user::filename_path(buffer);
+  char *buffer = path.data();
+  size_t cb = length;
+  if (sysctl(mib, 4, buffer, &cb, NULL, 0) == -1) {
+    path = program_directory_helper(length);
+  } else if (sysctl(mib, 4, buffer, &cb, NULL, 0) == 0) {
+    path = enigma_user::filename_path(buffer);
   }
+  return path;
 }
+
+namespace enigma {
+
+  void initialize_program_directory() {
+    string path;
+    size_t length = 256;
+    int mib[4];
+    mib[0] = CTL_KERN;
+    mib[1] = KERN_PROC;
+    mib[2] = KERN_PROC_PATHNAME;
+    mib[3] = -1;
+    char *buffer = new char[length]();
+    size_t cb = sizeof(buffer);
+    if (sysctl(mib, 4, buffer, &cb, NULL, 0) == -1) {
+      path = program_directory_helper(length);
+    } else if (sysctl(mib, 4, buffer, &cb, NULL, 0) == 0) {
+      path = enigma_user::filename_path(buffer);
+    }
+    path.shrink_to_fit();
+    enigma_user::program_directory = path;
+    delete[] buffer;
+  }
 
 } // namespace enigma
