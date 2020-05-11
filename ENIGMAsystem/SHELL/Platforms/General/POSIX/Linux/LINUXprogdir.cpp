0d4340ad4c0a9194711ed43a3b89dd3aaf4edd7f
time-killer-games <samuelvenable@hotmail.com>
2020-05-11 12:31:21 -0400

    Update LINUXprogdir.cpp

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/Linux/LINUXprogdir.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/Linux/LINUXprogdir.cpp
@@ -5,7 +5,7 @@ namespace enigma {
 
   void initialize_program_directory() {
     char *buffer = realpath("/proc/self/exe", NULL);
-    path = buffer ? enigma_user::filename_path(buffer) : "";
+    enigma_user::program_directory = buffer ? enigma_user::filename_path(buffer) : "";
     free(buffer);
   }
 
