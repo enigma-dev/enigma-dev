75749cb1f71546824e79c013904ded62da7155f3
time-killer-games <samuelvenable@hotmail.com>
2020-05-11 11:54:01 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/Linux/LINUXprogdir.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/Linux/LINUXprogdir.cpp
@@ -1,13 +1,12 @@
 #include "Platforms/General/POSIX/POSIXprogdir.h"
+#include <cstdlib>
 
 namespace enigma {
 
 void initialize_program_directory() {
-  char buffer[PATH_MAX];
-  ssize_t count = readlink("/proc/self/exe", buffer, PATH_MAX);
-  if (count != -1) {
-    buffer[count] = 0;
-    enigma_user::program_directory = enigma_user::filename_path(buffer);
+    char *buffer = realpath("/proc/self/exe", NULL);
+    path = buffer ? enigma_user::filename_path(buffer) : "";
+    free(buffer);
   }
 }
 
