2aa30f7c75b0fc8810bb2aa5b1b35b7f61405b3e
time-killer-games <samuelvenable@hotmail.com>
2020-05-12 16:31:28 -0400

    Update BSDprogdir.cpp

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/BSD/BSDprogdir.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/BSD/BSDprogdir.cpp
@@ -21,6 +21,7 @@ static inline string program_directory_helper(size_t length) {
     path = program_directory_helper(length);
   } else if (sysctl(mib, 4, buffer, &cb, NULL, 0) == 0) {
     path = enigma_user::filename_path(buffer);
+    path.shrink_to_fit();
   }
   return path;
 }
@@ -28,22 +29,7 @@ static inline string program_directory_helper(size_t length) {
 namespace enigma {
 
   void initialize_program_directory() {
-    string path;
-    size_t length = 256;
-    int mib[4];
-    mib[0] = CTL_KERN;
-    mib[1] = KERN_PROC;
-    mib[2] = KERN_PROC_PATHNAME;
-    mib[3] = -1;
-    char *buffer = path.data();
-    size_t cb = length;
-    if (sysctl(mib, 4, buffer, &cb, NULL, 0) == -1) {
-      path = program_directory_helper(length);
-    } else if (sysctl(mib, 4, buffer, &cb, NULL, 0) == 0) {
-      path = enigma_user::filename_path(buffer);
-    }
-    path.shrink_to_fit();
-    enigma_user::program_directory = path;
+    enigma_user::program_directory = program_directory_helper(256);
   }
 
 } // namespace enigma
