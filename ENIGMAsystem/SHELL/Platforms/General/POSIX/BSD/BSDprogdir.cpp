f2a4ebe3ab2d203ef81064433fbbb5706ed2fd90
time-killer-games <samuelvenable@hotmail.com>
2020-05-12 19:54:42 -0400

    Update BSDprogdir.cpp

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/BSD/BSDprogdir.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/BSD/BSDprogdir.cpp
@@ -15,6 +15,7 @@ static inline string program_directory_helper(string path = "", size_t length = 
   mib[1] = KERN_PROC;
   mib[2] = KERN_PROC_PATHNAME;
   mib[3] = -1;
+  path.resize(length, '\0');
   char *buffer = path.data();
   size_t cb = length;
   if (sysctl(mib, 4, buffer, &cb, NULL, 0) == -1) {
