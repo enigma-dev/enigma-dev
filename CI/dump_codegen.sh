c525dbe7cbbb28478bf27e8209de8bf90dbad2bc
Greg Williamson <fundies@no-reply.github.com>
2020-05-13 14:20:08 -0400

    appease ci gods

---
+++ CI/dump_codegen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+GAMES=$(find CommandLine/testing/SimpleTests/* CommandLine/testing/Tests/* -maxdepth 0 -not -name "*.cpp")
+
+for game in ${GAMES}
+do
+ game_name=$(basename ${game%%.*})
+ ./emake -g "OpenGL1" \
+         -a "OpenAL" \
+         -c "Precise" \
+         -e 'Alarms,Timelines,Paths,MotionPlanning,IniFilesystem,ParticleSystems,DateTime,DataStructures,libpng,GTest' \
+         --codegen-only \
+         --codegen "/tmp/codegen/${game_name}/" \
+         -o /tmp/test.exe
+done
