bd0b6d8902e51be014276950524b09219630d3e0
Greg Williamson <fundies@users.noreply.github.com>
2020-05-14 09:21:47 -0400

    Update image_load_save_test.cpp

--- CommandLine/testing/Tests/image_load_save_test.cpp
+++ CommandLine/testing/Tests/image_load_save_test.cpp
@@ -1,6 +1,6 @@
 #include "CommonTest.hpp"
 
-TEST(Regression, draw_tiles_test) {
+TEST(Regression, image_load_save_test) {
   if (!TestHarness::windowing_supported()) return;
   // Iterate only platforms and graphics systems
   for (TestConfig tc : GetValidConfigs(true, true, false, false, false, false)) {
