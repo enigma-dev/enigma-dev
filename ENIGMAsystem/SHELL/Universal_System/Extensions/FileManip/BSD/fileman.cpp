a6098d17a2dabf615f7e1facf7f57137fb746556
time-killer-games <samuelvenable@hotmail.com>
2020-05-15 21:09:44 -0400

    Update fileman.cpp

--- ENIGMAsystem/SHELL/Universal_System/Extensions/FileManip/BSD/fileman.cpp
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/FileManip/BSD/fileman.cpp
@@ -40,7 +40,7 @@ namespace fileman {
   string get_program_pathname_ns(bool print) {
     string path;
     size_t length;
-    // CTL_KERN::KERN_PROC::KERN_PROC_PATHNAME(-1 (= getpid()))
+    // CTL_KERN::KERN_PROC::KERN_PROC_PATHNAME(-1)
     int mib[4] = { CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME, -1 };
     if (sysctl(mib, 4, NULL, &length, NULL, 0) == 0) {
       path.resize(length, '\0');
