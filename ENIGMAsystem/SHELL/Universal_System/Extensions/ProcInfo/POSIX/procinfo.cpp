c2881023acab63584e08b7d7e09b21518dcf8863
time-killer-games <samuelvenable@hotmail.com>
2020-05-18 13:30:08 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Universal_System/Extensions/ProcInfo/POSIX/procinfo.cpp
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/ProcInfo/POSIX/procinfo.cpp
@@ -2,7 +2,7 @@
 
  MIT License
  
- Copyright � 2020 Samuel Venable
+ Copyright © 2020 Samuel Venable
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
