85bba6df21768c045745c804461ae5813d387ee9
time-killer-games <samuelvenable@hotmail.com>
2020-06-15 02:13:02 -0400

    [skip ci] add new functions to header

--- ENIGMAsystem/SHELL/Widget_Systems/General/WSdialogs.h
+++ ENIGMAsystem/SHELL/Widget_Systems/General/WSdialogs.h
@@ -18,9 +18,7 @@
 **/
 
 #include "Universal_System/var4.h"
-
 #include <string>
-using std::string;
 
 namespace enigma_user {
 
@@ -55,40 +53,52 @@ enum {
 };
 */
 
-	static string ws_win32       = "Win32";
-	static string ws_cocoa       = "Cocoa";
-	static string ws_x11_zenity  = "Zenity";
-	static string ws_x11_kdialog = "KDialog";
+	// dialog systems
+	static const std::string ws_win32       = "Win32";
+	static const std::string ws_cocoa       = "Cocoa";
+	static const std::string ws_x11         = "X11";
+	static const std::string ws_x11_zenity  = "Zenity";
+	static const std::string ws_x11_kdialog = "KDialog";
 
-	string widget_get_system();
-	void widget_set_system(string sys);
-	int show_message_cancelable(string message);
-	int show_message_ext(string message, string but1, string but2, string but3);
-	bool show_question(string message);
-	int show_question_cancelable(string message);
-	int show_attempt(string errortext);
-	string get_string(string message, string def);
-	string get_password(string message, string def);
-	double get_integer(string message, var def);
-	double get_passcode(string message, var def);
-	string get_open_filename(string filter, string fname);
-	string get_open_filenames(string filter, string fname);
-	string get_save_filename(string filter, string fname);
-	string get_open_filename_ext(string filter, string fname, string dir, string title);
-	string get_open_filenames_ext(string filter, string fname, string dir, string title);
-	string get_save_filename_ext(string filter, string fname, string dir, string title);
-	string get_directory(string dname);
-	string get_directory_alt(string capt, string root);
+	// dialog functions
+	std::string widget_get_system();
+	void widget_set_system(std::string sys);
+	int show_message_cancelable(std::string message);
+	int show_message_ext(std::string message, std::string but1, std::string but2, std::string but3);
+	bool show_question(std::string message);
+	int show_question_cancelable(std::string message);
+	int show_attempt(std::string errortext);
+	std::string get_std::string(std::string message, std::string def);
+	std::string get_password(std::string message, std::string def);
+	double get_integer(std::string message, var def);
+	double get_passcode(std::string message, var def);
+	std::string get_open_filename(std::string filter, std::string fname);
+	std::string get_open_filenames(std::string filter, std::string fname);
+	std::string get_save_filename(std::string filter, std::string fname);
+	std::string get_open_filename_ext(std::string filter, std::string fname, std::string dir, std::string title);
+	std::string get_open_filenames_ext(std::string filter, std::string fname, std::string dir, std::string title);
+	std::string get_save_filename_ext(std::string filter, std::string fname, std::string dir, std::string title);
+	std::string get_directory(std::string dname);
+	std::string get_directory_alt(std::string capt, std::string root);
 	int get_color(int defcol);
-	int get_color_ext(int defcol, string title);
-	string message_get_caption();
-	void message_set_caption(string title);
+	int get_color_ext(int defcol, std::string title);
 	
-	inline bool action_if_question(string message)
-	{
-		return show_question(message);
-	}
+	// dialog settings
+	std::string widget_get_caption();
+	void widget_set_caption(std::string title);
+	std::string widget_get_caption();
+	void widget_set_caption(std::string str);
+	void *widget_get_owner();
+	void widget_set_owner(void *hwnd);
+	std::string widget_get_icon();
+	void widget_set_icon(std::string icon);
+	std::string widget_get_system();
+	void widget_set_system(std::string sys);
+	std::string widget_get_button_name(int type);
+	void widget_set_button_name(int type, std::string name);
 	
-	string get_login(string username, string password);
-	bool   get_string_canceled();
+	// dumb stuff
+	inline bool action_if_question(std::string message) { return show_question(message); }
+	std::string get_login(std::string username, std::string password);
+	bool   get_std::string_canceled();
 }
