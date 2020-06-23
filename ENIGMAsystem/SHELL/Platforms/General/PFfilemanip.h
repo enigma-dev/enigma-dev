e2fe5a03f12620e98319f88ab58ddf71407eb65d
Robert Colton <robertbcolton@gmail.com>
2020-06-23 11:14:41 -0400

    Rename directory_delete to directory_destroy.

--- ENIGMAsystem/SHELL/Platforms/General/PFfilemanip.h
+++ ENIGMAsystem/SHELL/Platforms/General/PFfilemanip.h
@@ -41,7 +41,7 @@ int file_rename(std::string oldname,std::string newname);
 int file_copy(std::string fname,std::string newname);
 int directory_exists(std::string dname);
 int directory_create(std::string dname);
-int directory_delete(std::string dname);
+int directory_destroy(std::string dname);
 
 std::string file_find_next();
 void file_find_close();
