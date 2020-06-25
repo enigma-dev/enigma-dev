c1af8483fc6b259bfa5fe842b386b8ccaa3bbbe8
Robert Colton <robertbcolton@gmail.com>
2020-06-24 21:54:08 -0400

    Fix Win32/SDL Mouse Capture Anomalies (#2039)
    Release mouse capture when all mouse buttons have been released and switch SDL right and middle mouse constants.

--- ENIGMAsystem/SHELL/Platforms/Win32/WINDOWScallback.cpp
+++ ENIGMAsystem/SHELL/Platforms/Win32/WINDOWScallback.cpp
@@ -236,11 +236,11 @@ namespace enigma
          hdeltadelta %= WHEEL_DELTA;
          return 0;
 
-      case WM_LBUTTONUP:   ReleaseCapture(); mousestatus[0]=0; return 0;
+      case WM_LBUTTONUP:   { if (!wParam) ReleaseCapture(); mousestatus[0]=0; return 0; }
       case WM_LBUTTONDOWN: SetCapture(hWnd); mousestatus[0]=1; return 0;
-      case WM_RBUTTONUP:   ReleaseCapture(); mousestatus[1]=0; return 0;
+      case WM_RBUTTONUP:   { if (!wParam) ReleaseCapture(); mousestatus[1]=0; return 0; }
       case WM_RBUTTONDOWN: SetCapture(hWnd); mousestatus[1]=1; return 0;
-      case WM_MBUTTONUP:   ReleaseCapture(); mousestatus[2]=0; return 0;
+      case WM_MBUTTONUP:   { if (!wParam) ReleaseCapture(); mousestatus[2]=0; return 0; }
       case WM_MBUTTONDOWN: SetCapture(hWnd); mousestatus[2]=1; return 0;
 
       case WM_ERASEBKGND:
