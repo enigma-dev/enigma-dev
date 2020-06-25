008a02462c81d2c94e14f388ff0dfa6770a7cb95
Robert Colton <robertbcolton@gmail.com>
2020-06-25 13:24:02 -0400

    Update zenity.cpp

--- ENIGMAsystem/SHELL/Widget_Systems/xlib/zenity.cpp
+++ ENIGMAsystem/SHELL/Widget_Systems/xlib/zenity.cpp
@@ -189,7 +189,7 @@ class ZenityWidgets : public enigma::CommandLineWidgetEngine {
  public:
 
 void show_debug_message(string message, MESSAGE_TYPE type) override {
-  show_debug_zenity_helperfunc(message);
+  show_debug_message_zenity_helperfunc(message);
 }
 
 void show_info(string info, int bgcolor, int left, int top, int width, int height, bool embedGameWindow, bool showBorder, bool allowResize, bool stayOnTop, bool pauseGame, string caption) override {
