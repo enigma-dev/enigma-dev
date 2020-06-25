08fbacf3472e8f6913407af5d7fdef980aac6421
Robert Colton <robertbcolton@gmail.com>
2020-06-25 14:17:01 -0400

    Update kdialog.cpp

--- ENIGMAsystem/SHELL/Widget_Systems/xlib/kdialog.cpp
+++ ENIGMAsystem/SHELL/Widget_Systems/xlib/kdialog.cpp
@@ -199,7 +199,7 @@ class KDialogWidgets : public enigma::CommandLineWidgetEngine {
  public:
 
 void show_debug_message(string message, MESSAGE_TYPE type) override {
-  show_debug_message_helperfunc(message);
+  show_debug_message_helperfunc(message, type);
 }
 
 void show_info(string info, int bgcolor, int left, int top, int width, int height, bool embedGameWindow, bool showBorder, bool allowResize, bool stayOnTop, bool pauseGame, string caption) override {
