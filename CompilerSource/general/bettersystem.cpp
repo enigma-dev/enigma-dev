36e9b301b2a67e24e46bc2d9dbc159f30bcad341
Robert Colton <robertbcolton@gmail.com>
2020-07-04 17:01:38 -0400

    Update bettersystem.cpp

--- CompilerSource/general/bettersystem.cpp
+++ CompilerSource/general/bettersystem.cpp
@@ -293,6 +293,8 @@ void myReplace(std::string& str, const std::string& oldStr, const std::string& n
       free(a);
     }
 
+    #include <mutex>
+    #include <condition_variable>
     std::mutex stop_mutex;
     std::condition_variable stop_condition;
     void e_exec_stop() {
@@ -414,7 +416,7 @@ void myReplace(std::string& str, const std::string& oldStr, const std::string& n
         exit(-1);
       }
 
-      while (!pthread_cond_wait(stop_condition.native_handle(), stop_mutex.native_handle()) && errno != ECHLD)
+      while (!pthread_cond_wait(stop_condition.native_handle(), stop_mutex.native_handle()) && errno != ECHILD)
         if (build_stopping) {
           kill(-fk,SIGINT); // send CTRL+C to process group
           // wait for entire process group to signal,
