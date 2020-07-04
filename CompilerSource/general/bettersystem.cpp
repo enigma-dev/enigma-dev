7b1f499bca12b52c8a5aa0100f2b7e68b2e23b47
Robert Colton <robertbcolton@gmail.com>
2020-07-03 23:57:55 -0400

    Update bettersystem.cpp

--- CompilerSource/general/bettersystem.cpp
+++ CompilerSource/general/bettersystem.cpp
@@ -405,7 +405,7 @@ void myReplace(std::string& str, const std::string& oldStr, const std::string& n
         exit(-1);
       }
 
-      while (!waitpid(-fk,&result,WNOHANG|__WALL)) {
+      while (!waitpid(fk,&result,WNOHANG|__WALL)) {
         if (build_stopping) {
           kill(-fk,SIGINT); // send CTRL+C to process group
           // wait for entire process group to signal,
