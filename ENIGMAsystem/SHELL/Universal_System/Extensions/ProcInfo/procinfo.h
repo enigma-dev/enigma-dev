f0db335046afd4125bd6cedb34c8d82cdd4b0d4a
time-killer-games <samuelvenable@hotmail.com>
2020-07-04 16:05:08 -0400

    Update procinfo.h

--- ENIGMAsystem/SHELL/Universal_System/Extensions/ProcInfo/procinfo.h
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/ProcInfo/procinfo.h
@@ -45,88 +45,33 @@ std::vector<std::string> string_split(std::string str, char delimiter);
 
 namespace procinfo {
 
-// execute process, returns process id
 process_t process_execute(std::string command);
-
-// execute process outside main thread
 void process_execute_async(std::string command);
-
-// return previous executed process id
 process_t process_previous();
-
-// evaluate last process output string
 std::string process_evaluate();
-
-// clears previous process id executed
 void process_clear_pid();
-
-// clears previous process output text
 void process_clear_out();
-
-// get process id from current process
 process_t pid_from_self();
-
-// parent process id from this process
 process_t ppid_from_self();
-
-// get executable path from process id
 std::string path_from_pid(process_t pid);
-
-// get exe parent path from process id
 std::string dir_from_pid(process_t pid);
-
-// get executable name from process id
 std::string name_from_pid(process_t pid);
-
-// return command line from process id
 std::string cmd_from_pid(process_t pid);
-
-// return environ vars from process id
 std::string env_from_pid(process_t pid);
-
-// check for existence from process id
 bool pid_exists(process_t pid);
-
-// check existence for given window id
 bool wid_exists(wid_t wid);
-
-// kill an application from process id
 bool pid_kill(process_t pid);
-
-// return window handle from window id
 window_t window_from_wid(wid_t wid);
-
-// return window id from window handle
 wid_t wid_from_window(window_t window);
-
-// get owner process id from window id
 process_t pid_from_wid(wid_t wid);
-
-// return strings for every process id
 std::string pids_enum(bool trim_dir, bool trim_empty);
-
-// get parent process id of process id
 process_t ppid_from_pid(process_t pid);
-
-// get list of process ids from parent
 std::string pids_from_ppid(process_t ppid);
-
-// get list of window ids from process
 std::string wids_from_pid(process_t pid);
-
-// get window id from frontmost window
 wid_t wid_from_top();
-
-// get process id from topmost process
 process_t pid_from_top();
-
-// bring window id to frontmost window
 void wid_to_top(wid_t wid);
-
-// add a parent window id to window id
 void wid_set_pwid(wid_t wid, wid_t pwid);
-
-// return output of a given expression
 std::string echo(std::string expression);
 
 } // namespace procinfo
