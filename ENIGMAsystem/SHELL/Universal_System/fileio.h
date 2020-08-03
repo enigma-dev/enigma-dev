c9e012d3ea9183df9d86c79b25a068a091802ea5
Greg Williamson <fundies@users.noreply.github.com>
2020-08-02 22:48:28 -0400

    Redo fileio with fstream (#2095)
    Rewrite file_io using fstream and add debuging output

--- ENIGMAsystem/SHELL/Universal_System/fileio.h
+++ ENIGMAsystem/SHELL/Universal_System/fileio.h
@@ -20,34 +20,31 @@
 #define ENIGMA_FILEIO_H
 
 #include <string>
-using std::string;
 
-namespace enigma_user
-{
-int     file_text_open_read(string fname);
-int     file_text_open_write(string fname);
-int     file_text_open_append(string fname);
-void    file_text_close(int fileid);
-void    file_text_write_string(int fileid, string str);
-void    file_text_write_real(int fileid, double x);
-void    file_text_writeln(int fileid);
-void    file_text_writeln(int fileid,string str);
-string  file_text_read_string(int fileid);
-string  file_text_read_all(int fileid);
-double  file_text_read_real(int fileid);
-string  file_text_readln(int fileid);
-bool    file_text_eof(int fileid);
-bool    file_text_eoln(int fileid);
-void    load_info(string fname); // game information function
-
-int     file_bin_open(string fname,int mode);
-bool    file_bin_rewrite(int fileid);
-void    file_bin_close(int fileid);
-size_t  file_bin_size(int fileid);
-size_t  file_bin_position(int fileid);
-void    file_bin_seek(int fileid,size_t pos);
-void    file_bin_write_byte(int fileid,unsigned char byte);
-int     file_bin_read_byte(int fileid);
+namespace enigma_user {
+int file_text_open_read(const std::string& fname);
+int file_text_open_write(const std::string& fname);
+int file_text_open_append(const std::string& fname);
+void file_text_close(int fileid);
+void file_text_write_string(int fileid, const std::string& str);
+void file_text_write_real(int fileid, double x);
+void file_text_writeln(int fileid);
+void file_text_writeln(int fileid, const std::string& str);
+std::string file_text_read_string(int fileid);
+std::string file_text_read_all(int fileid);
+double file_text_read_real(int fileid);
+std::string file_text_readln(int fileid);
+bool file_text_eof(int fileid);
+bool file_text_eoln(int fileid);
+void load_info(const std::string& fname); // game information function
+int file_bin_open(const std::string& fname, int mode);
+bool file_bin_rewrite(int fileid);
+void file_bin_close(int fileid);
+size_t file_bin_size(int fileid);
+size_t file_bin_position(int fileid);
+void file_bin_seek(int fileid, size_t pos);
+void file_bin_write_byte(int fileid, unsigned char byte);
+int file_bin_read_byte(int fileid);
 
 } //namespace enigma_user
 
