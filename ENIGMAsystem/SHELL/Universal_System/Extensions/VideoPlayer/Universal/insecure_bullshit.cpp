87ffe7636890db9ff777bcdc08891de06ae10251
time-killer-games <samuelvenable@hotmail.com>
2020-08-10 19:48:36 -0400

    [skip ci]

--- ENIGMAsystem/SHELL/Universal_System/Extensions/VideoPlayer/Universal/insecure_bullshit.cpp
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/VideoPlayer/Universal/insecure_bullshit.cpp
@@ -37,16 +37,6 @@ bool string_has_whitespace(string str) {
   return str.find_first_of("\t\r\n ") != string::npos;
 }
 
-vector<string> string_split_by_first_equalssign(string str) {
-  size_t pos = 0;
-  vector<string> vec;
-  if ((pos = str.find_first_of("=")) != string::npos) {
-    vec.push_back(str.substr(0, pos));
-    vec.push_back(str.substr(pos + 1));
-  }
-  return vec;
-}
-
 } // namespace enigma
 
 namespace enigma_insecure {
