34978e6186371e0c750bf2cb2095475d1cd5b3fe
time-killer-games <samuelvenable@hotmail.com>
2020-08-10 18:55:58 -0400

    [skip ci]

---
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/VideoPlayer/videoplayer.h
@@ -0,0 +1,41 @@
+/*
+
+ MIT License
+ 
+ Copyright © 2020 Samuel Venable
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ 
+*/
+
+#include <string>
+
+typedef unsigned long video_t;
+
+namespace enigma_user {
+
+video_t video_add(std::string fname);
+void video_play(video_t ind, wid_t wid);
+bool video_is_playing(video_t ind);
+wid_t video_get_winid(video_t ind);
+void video_stop(video_t ind);
+bool video_exists(video_t ind);
+void video_delete(video_t ind);
+
+} // namespace enigma_user
