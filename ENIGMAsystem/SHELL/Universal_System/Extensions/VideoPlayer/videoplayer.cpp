626ca0d780038e79be2ebdd47c8c55daa7984188
time-killer-games <samuelvenable@hotmail.com>
2020-09-28 21:12:51 -0400

    Update videoplayer.cpp

--- ENIGMAsystem/SHELL/Universal_System/Extensions/VideoPlayer/videoplayer.cpp
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/VideoPlayer/videoplayer.cpp
@@ -34,122 +34,111 @@
 
 using std::string;
 
-static std::map<string, string> vidmap;
-static std::map<string, string> widmap;
+struct VideoData {
+  mpv_handle *mpv;
+  std::string window_id;
+  std::string gpu_api;
+  int volume;
+  bool is_playing;
+  bool is_fullscreen;
+};
 
-static std::map<string, string> plymap;
-static std::map<string, string> gfxmap;
+static std::map<string, VideoData> videos;
 
-static std::map<string, bool>   fllmap;
-static std::map<string, int>    volmap;
-
-void video_loop(mpv_handle *mpv) {
+static void video_loop(string ind, mpv_handle *mpv) {
   while (true) {
     mpv_event *event = mpv_wait_event(mpv, -1);
     if (event->event_id == MPV_EVENT_END_FILE) break;
     if (event->event_id == MPV_EVENT_SHUTDOWN) break;
   }
-  plymap[std::to_string((unsigned long long)mpv)] = "no";
+  videos[ind].is_playing = false;
   mpv_terminate_destroy(mpv);
 }
 
 namespace enigma_user {
 
 string video_add(string fname) {
-  mpv_handle *mpv = mpv_create();
-  string ind = std::to_string((unsigned long long)mpv);
-  vidmap.insert(std::make_pair(ind, fname)); 
-  return ind;
+  VideoData data;
+  data.mpv            = mpv_create();
+  data.window_id      = "i";
+  data.gpu_api        = "opengl";
+  data.volume         = 100;
+  data.is_playing     = false;
+  data.is_fullscreen = false;
+  videos.insert(std::make_pair(fname, data)); 
+  return fname;
 }
 
 void video_set_option_string(string ind, string option, string value) {
-  mpv_handle *mpv = (mpv_handle *)stoull(ind, nullptr, 10);
-  mpv_set_option_string(mpv, option.c_str(), value.c_str());
+  mpv_set_option_string(videos[ind].mpv, option.c_str(), value.c_str());
 }
 
 void video_play(string ind) {
-  mpv_handle *mpv = (mpv_handle *)stoull(ind, nullptr, 10);
-  plymap.erase(ind);
-  plymap.insert(std::make_pair(ind, "yes"));
-  mpv_initialize(mpv);
-  const char *cmd[] = { "loadfile", vidmap.find(ind)->second.c_str(), NULL };
-  mpv_command(mpv, cmd);
-  std::thread stringhread(video_loop, mpv);
+  videos[ind].is_playing = true;
+  mpv_initialize(videos[ind].mpv);
+  const char *cmd[] = { "loadfile", ind.c_str(), NULL };
+  mpv_command(videos[ind].mpv, cmd);
+  std::thread stringhread(video_loop, ind, videos[ind].mpv);
   stringhread.detach();
 }
 
 bool video_is_playing(string ind) {
-  return (plymap[ind] == "yes");
+  return videos[ind].is_playing;
 }
 
 string video_get_graphics_system(string ind) {
-  return gfxmap.find(ind)->second;
+  return videos[ind].gpu_api;
 }
 
 void video_set_graphics_system(string ind, string system) {
-  mpv_handle *mpv = (mpv_handle *)stoull(ind, nullptr, 10);
-  mpv_set_option_string(mpv, "gpu-api", system.c_str());
-  gfxmap.erase(ind);
-  gfxmap.insert(std::make_pair(ind, system));
+  videos[ind].gpu_api = system;
+  mpv_set_option_string(videos[ind].mpv, "gpu-api", system.c_str());
 }
 
 bool video_get_fullscreen(string ind) {
-  return fllmap.find(ind)->second;
+  return videos[ind].is_fullscreen;
 }
 
 void video_set_fullscreen(string ind, bool fullscreen) {
-  mpv_handle *mpv = (mpv_handle *)stoull(ind, nullptr, 10);
-  mpv_set_option_string(mpv, "fs", fullscreen ? "yes" : "no");
-  fllmap.erase(ind);
-  fllmap.insert(std::make_pair(ind, fullscreen));
+  videos[ind].is_fullscreen = fullscreen;
+  mpv_set_option_string(videos[ind].mpv, "fs", std::to_string(fullscreen).c_str());
 }
 
 int video_get_volume_percent(string ind) {
-  return volmap.find(ind)->second;
+  return videos[ind].volume;
 }
 
 void video_set_volume_percent(string ind, int volume) {
-  mpv_handle *mpv = (mpv_handle *)stoull(ind, nullptr, 10);
-  mpv_set_option_string(mpv, "volume", std::to_string(volume).c_str());
-  volmap.erase(ind);
-  volmap.insert(std::make_pair(ind, volume));
+  videos[ind].volume = volume;
+  mpv_set_option_string(videos[ind].mpv, "volume", std::to_string(volume).c_str());
 }
 
 string video_get_window_identifier(string ind) {
-  return widmap.find(ind)->second;
+  return videos[ind].window_id;
 }
 
 void video_set_window_identifier(string ind, string wid) {
-  mpv_handle *mpv = (mpv_handle *)stoull(ind, nullptr, 10);
-  mpv_set_option_string(mpv, "wid", wid.c_str());
-  widmap.erase(ind);
-  widmap.insert(std::make_pair(ind, wid));
+  videos[ind].window_id = wid;
+  mpv_set_option_string(videos[ind].mpv, "wid", wid.c_str());
 }
 
 bool video_exists(string ind) {
-  return (!vidmap.find(ind)->second.empty());
+  return (videos.find(ind) != videos.end());
 }
 
 void video_pause(string ind) {
-  mpv_handle *mpv = (mpv_handle *)stoull(ind, nullptr, 10);
   const char *cmd[] = { "cycle", "pause", NULL };
-  mpv_command_async(mpv, 0, cmd);
+  mpv_command_async(videos[ind].mpv, 0, cmd);
 }
 
 void video_stop(string ind) {
-  mpv_handle *mpv = (mpv_handle *)stoull(ind, nullptr, 10);
   const char *cmd[] = { "quit", NULL, NULL };
-  mpv_command_async(mpv, 0, cmd);
-  plymap[ind] = "no";
+  mpv_command_async(videos[ind].mpv, 0, cmd);
+  videos[ind].is_playing = false;
 }
 
 void video_delete(string ind) {
-  vidmap.erase(ind);
-  widmap.erase(ind);
-  plymap.erase(ind);
-  gfxmap.erase(ind);
-  fllmap.erase(ind);
-  volmap.erase(ind);
+  videos.erase(ind);
 }
 
 } // namespace enigma_user
