7bb3ac160d446ecc3d80ba5e7dedb9912d90f1d1
time-killer-games <samuelvenable@hotmail.com>
2020-10-02 06:55:32 -0400

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,65 +1,14 @@
-include Config.mk
-
-PATH := $(eTCpath)$(PATH)
-SHELL=/bin/bash
-
-.PHONY: ENIGMA all clean Game clean-game emake emake-tests gm2egm libProtocols libEGM required-directories .FORCE
-
-ENIGMA: .FORCE libProtocols$(LIB_EXT) libENIGMAShared$(LIB_EXT)
-	$(MAKE) -C CompilerSource
-
-clean: .FORCE
-	$(MAKE) -C CompilerSource/ clean
-	$(MAKE) -C CommandLine/emake/ clean
-	$(MAKE) -C CommandLine/libEGM/ clean
-	$(MAKE) -C CommandLine/testing/ clean
-	$(MAKE) -C shared/ clean
-	$(MAKE) -C shared/protos/ clean
-	$(MAKE) -C CommandLine/gm2egm/ clean
-
-all: libENIGMAShared libProtocols libEGM ENIGMA emake emake-tests test-runner .FORCE
-
-Game: .FORCE
-	@$(RM) -f logs/enigma_compile.log
-	@$(MAKE) -C ENIGMAsystem/SHELL > >(tee -a /tmp/enigma_compile.log) 2> >(tee -a /tmp/enigma_compile.log >&2)
-
-clean-game: .FORCE
-	$(MAKE) -C ENIGMAsystem/SHELL clean
-
-libENIGMAShared$(LIB_EXT): libENIGMAShared
-libENIGMAShared: .FORCE
-	$(MAKE) -C shared/
-
-libProtocols$(LIB_EXT): libProtocols
-libProtocols: .FORCE
-	$(MAKE) -C shared/protos/
-
-libEGM$(LIB_EXT): libEGM
-libEGM: .FORCE libProtocols$(LIB_EXT) libENIGMAShared$(LIB_EXT)
-	$(MAKE) -C CommandLine/libEGM/
-
-EMAKE_TARGETS = .FORCE ENIGMA
-
-ifneq ($(CLI_ENABLE_EGM), FALSE)
-	EMAKE_TARGETS += libEGM$(LIB_EXT)
-else
-	EMAKE_TARGETS += libProtocols$(LIB_EXT)
+SOURCES += Universal_System/Extensions/VideoPlayer/videoplayer.cpp
+ifeq ($(TARGET-PLATFORM), Windows)
+	override LDLIBS += -Wl,-Bdynamic `pkg-config --libs --cflags mpv` -Wl,-Bstatic
+else ifeq ($(TARGET-PLATFORM), MacOSX)
+	SOURCES += Universal_System/Extensions/VideoPlayer/videoplayer.mm
+	override LDLIBS += -Wl,-Bdynamic `pkg-config --libs --cflags mpv` -Wl,-Bstatic
+	override CXXFLAGS += -ObjC
+	override CFLAGS += -ObjC
+	override LDLIBS += -framework Cocoa
+else ifeq ($(TARGET-PLATFORM), Android)
+	override LDLIBS += -Wl,-Bdynamic `pkg-config --libs --cflags mpv` -Wl,-Bstatic
+else # Linux, FreeBSD
+	override LDLIBS += `pkg-config --libs --cflags mpv` # mostly-dynamic platforms
 endif
-
-emake: $(EMAKE_TARGETS)
-	$(MAKE) -C CommandLine/emake/
-
-emake-tests: .FORCE libEGM$(LIB_EXT)
-	TESTS=TRUE $(MAKE) -C CommandLine/emake/
-
-gm2egm: libEGM$(LIB_EXT) .FORCE
-	$(MAKE) -C CommandLine/gm2egm/
-
-test-runner: emake .FORCE
-	$(MAKE) -C CommandLine/testing/
-
-required-directories: .FORCE
-	@mkdir -p "$(WORKDIR)"
-	@mkdir -p "$(CODEGEN)Preprocessor_Environment_Editable/"
-
-.FORCE:
---
+++ videoplayer.h
@@ -0,0 +1,71 @@
+/*
+
+ MIT License
+ 
+ Copyright © 2020 Samuel Venable
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ 
+*/
+
+#include <string>
+
+namespace enigma_user {
+
+// GameMaker 8.1 splash video functions
+void splash_set_main(bool main);
+void splash_set_caption(std::string cap);
+void splash_set_fullscreen(bool full);
+void splash_set_size(unsigned w, unsigned h);
+void splash_set_position(int x, int y);
+void splash_set_border(bool border);
+void splash_set_interupt(bool interupt);
+void splash_set_stop_mouse(bool stop);
+void splash_set_scale(unsigned scale);
+void splash_set_adapt(bool adapt);
+void splash_show_video(std::string fname, bool loop);
+
+// ENIGMA/GM extension video playback functions
+void splash_set_window(std::string wid);
+void splash_set_volume(int vol);
+std::string splash_get_video();
+std::string video_add(std::string fname);
+bool video_get_option_was_set(std::string ind, std::string option);
+std::string video_get_option_string(std::string ind, std::string option);
+void video_set_option_string(std::string ind, std::string option, std::string value);
+std::string video_get_property_string(std::string ind, std::string property);
+void video_play(std::string ind);
+bool video_is_paused(std::string ind);
+bool video_is_playing(std::string ind);
+bool video_get_fullscreen(std::string ind);
+void video_set_fullscreen(std::string ind, bool fullscreen);
+int video_get_volume_percent(std::string ind);
+void video_set_volume_percent(std::string ind, int volume);
+std::string video_get_window_identifier(std::string ind);
+void video_set_window_identifier(std::string ind, std::string wid);
+void video_pause(std::string ind);
+void video_stop(std::string ind);
+void video_seek(std::string ind, double seek);
+unsigned video_get_width(std::string ind);
+unsigned video_get_height(std::string ind);
+double video_get_duration(std::string ind);
+bool video_exists(std::string ind);
+void video_delete(std::string ind);
+
+} // namespace enigma_user
