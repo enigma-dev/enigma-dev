bfd3ddc168fbd38596aed7fac9a44b3a1e54f88d
Robert Colton <robertbcolton@gmail.com>
2020-11-15 19:37:15 -0500

    Update DX11textures.cpp

--- ENIGMAsystem/SHELL/Graphics_Systems/Direct3D11/DX11textures.cpp
+++ ENIGMAsystem/SHELL/Graphics_Systems/Direct3D11/DX11textures.cpp
@@ -38,8 +38,8 @@ int graphics_create_texture(const RawImage& img, bool mipmap, unsigned* fullwidt
   if (fullheight == nullptr) fullheight = &fh;
   
   if (img.pxdata != nullptr) {
-    *fullwidth  = nlpo2dc(img.w);
-    *fullheight = nlpo2dc(img.h);
+    *fullwidth  = nlpo2(img.w);
+    *fullheight = nlpo2(img.h);
   }
 
   tbsd.SysMemPitch = (*fullwidth) * 4;
