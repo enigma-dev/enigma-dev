0bf730f1956fa938133ecb59cbd2bf5043bd517c
time-killer-games <samuelvenable@hotmail.com>
2021-01-01 07:21:19 +0000

    Update OS_Switchboard.h

--- CompilerSource/OS_Switchboard.h
+++ CompilerSource/OS_Switchboard.h
@@ -3,7 +3,7 @@
 #define OS_WINDOWS 0
 #define OS_LINUX   1
 #define OS_MACOSX  2
-#define OS_BSD     3
+#define OS_FREEBSD 3
 
 #if defined(_WIN32) || defined(__WIN32__) || defined(_WIN64) || defined(__WIN64__)
   #define CURRENT_PLATFORM_NAME "Windows"
@@ -16,9 +16,9 @@
   #define CURRENT_PLATFORM_ID   OS_MACOSX
   #define IPHONE_DEVICE 0
   //#define WRITE_UNIMPLEMENTED_TXT 1
-#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__bsdi__) || defined(__DragonFly__)
-  #define CURRENT_PLATFORM_NAME "BSD"
-  #define CURRENT_PLATFORM_ID   OS_BSD
+#elif defined(__FreeBSD__)
+  #define CURRENT_PLATFORM_NAME "FreeBSD"
+  #define CURRENT_PLATFORM_ID   OS_FREEBSD
 #elif defined(unix) || defined(__unix) || defined(__unix__)
   #define CURRENT_PLATFORM_NAME "Linux"
   #define CURRENT_PLATFORM_ID   OS_LINUX
