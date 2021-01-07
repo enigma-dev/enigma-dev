2034f44ff8a69311e7efe52ac6db84a8383f2594
time-killer-games <samuelvenable@hotmail.com>
2021-01-06 21:57:44 -0500

    Update build_and_run_game.sh

--- CI/build_and_run_game.sh
+++ CI/build_and_run_game.sh
@@ -12,9 +12,9 @@ else
   do
     MODE="$mode" ./ci-build.sh
     if [ "$COMPILER" == "MinGW64" ]; then
-      WINEPREFIX=~/.wine64 WINEARCH=win64 xvfb-run wine64 $OUTPUT > >(tee -a tee /tmp/enigma_game.log) 2> >(tee -a tee /tmp/enigma_game.log >&2)
+      WINEPATH=/mingw64/bin WINEPREFIX=~/.wine64 WINEARCH=win64 xvfb-run wine64 $OUTPUT > >(tee -a tee /tmp/enigma_game.log) 2> >(tee -a tee /tmp/enigma_game.log >&2)
     elif [ "$COMPILER" == "MinGW32" ]; then
-      WINEPREFIX=~/.wine32 WINEARCH=win32 xvfb-run wine $OUTPUT > >(tee -a tee /tmp/enigma_game.log) 2> >(tee -a tee /tmp/enigma_game.log >&2)
+      WINEPATH=/mingw32/bin WINEPREFIX=~/.wine32 WINEARCH=win32 xvfb-run wine $OUTPUT > >(tee -a tee /tmp/enigma_game.log) 2> >(tee -a tee /tmp/enigma_game.log >&2)
     #FIXME: these should run but are bugged
     elif [[ ! "$COMPILER" =~ "clang" ]]; then
       xvfb-run $OUTPUT > >(tee -a tee /tmp/enigma_game.log) 2> >(tee -a tee /tmp/enigma_game.log >&2)
