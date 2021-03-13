479479decc996a3db8de36f6467ac91ac56914aa
Greg Williamson <gwill6@uis.edu>
2021-03-13 09:43:26 -0500

    hrmm

--- ci-regression.sh
+++ ci-regression.sh
@@ -3,7 +3,8 @@
 set -e  # exit if any command fails
 
 if [[ "$TRAVIS" -eq "true" ]]; then
-  startxfce4 & # We need a wm for these tests
+  export LIBGL_ALWAYS_SOFTWARE=1
+  openbox & # We need a wm for these tests
 fi
 
 if [ -z "$1" ]; then
