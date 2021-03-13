1c040f44bbf3c5f6ec0ab0c1e04b68ad64e4216b
Greg Williamson <gwill6@uis.edu>
2021-03-13 07:25:21 -0500

    hrmm

--- CI/solve_engine_deps.sh
+++ CI/solve_engine_deps.sh
@@ -16,9 +16,9 @@ elif [ "$COMPILER" == "MinGW64" ] || [ "$COMPILER" == "MinGW32" ]; then
 fi
 
 ###### Platforms #######
-#if [ "$PLATFORM" == "SDL" ] || [ "$TEST_HARNESS" == true ]; then
-  #LINUX_DEPS="$LINUX_DEPS libsdl2-dev"
-#fi
+if [ "$PLATFORM" == "SDL" ] || [ "$TEST_HARNESS" == true ]; then
+  LINUX_DEPS="$LINUX_DEPS libsdl2-dev libdrm-dev libgbm-dev"
+fi
 
 ###### Graphics #######
 LINUX_DEPS="$LINUX_DEPS libepoxy-dev libegl1-mesa-dev libgles2-mesa-dev libglew-dev libxrandr-dev libxinerama-dev"
