9674ac91af9918fa75d8b8cd2d9837518cfcddf8
time-killer-games <samuelvenable@hotmail.com>
2021-04-30 10:29:11 -0400

    [skip ci]

---
+++ ENIGMAsystem/SHELL/Platforms/xlib/Darwin/DARWINjoystick.cpp
@@ -0,0 +1,23 @@
+// FIXME: actually add support for joysticks. 
+
+#include "Platforms/General/PFjoystick.h"
+#include "Widget_Systems/widgets_mandatory.h"
+#include <string>
+
+using std::string;
+
+namespace enigma
+{
+  static bool init_jsmsg = false;
+  
+  void init_joysticks() {
+    if (init_jsmsg) {
+      DEBUG_MESSAGE("Joysticks are not yet implemented in X11 for Darwin!", MESSAGE_TYPE::M_WARNING);
+      init_jsmsg = true;
+    }
+  }
+
+  void handle_joysticks() {
+    
+  }
+}  // namespace enigma
