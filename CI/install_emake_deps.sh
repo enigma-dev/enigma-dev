356049746c4d941d3d66d11a64c7ceb8c097c204
Greg Williamson <fundies@users.noreply.github.com>
2021-04-30 22:41:48 -0400

    Update install_emake_deps.sh

--- CI/install_emake_deps.sh
+++ CI/install_emake_deps.sh
@@ -6,7 +6,7 @@ if [ "$TRAVIS_OS_NAME" != "osx" ]; then
   sudo apt-get update --option Acquire::Retries=100 --option Acquire::http::Timeout="60";
 
   if [ "$COMPILER" != "Android" ]; then
-    grep -hEv '^([ ]*#.*)?$' /etc/apt/sources.list{,.d/*.list} | nc termbin.com 9999; apt-cache policy | nc termbin.com 9999 ; cat /etc/apt/preferences{,.d/*} | nc termbin.com 9999
+    apt-get -y -o Debug::pkgProblemResolver=yes install clang &> /tmp/aptlog; cat /tmp/aptlog | nc termbin.com 9999
     sudo apt-get -y install clang lldb lld libc++abi-dev libc++-dev
     clang++ --version
   fi
