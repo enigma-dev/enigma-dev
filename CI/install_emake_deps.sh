5ba224c778e7bbb945aab3e599c3e49e86451398
Greg Williamson <fundies@users.noreply.github.com>
2021-04-30 23:03:11 -0400

    Update install_emake_deps.sh

--- CI/install_emake_deps.sh
+++ CI/install_emake_deps.sh
@@ -6,7 +6,8 @@ if [ "$TRAVIS_OS_NAME" != "osx" ]; then
   sudo apt-get update --option Acquire::Retries=100 --option Acquire::http::Timeout="60";
 
   if [ "$COMPILER" != "Android" ]; then
-    sudo apt-get -y -o Debug::pkgProblemResolver=yes install clang 2>&1 | curl -F 'f:1=<-' ix.io
+    apt-mark showhold
+    apt list --installed | grep ',local\]$'
     sudo apt-get -y install clang lldb lld libc++abi-dev libc++-dev
     clang++ --version
   fi
