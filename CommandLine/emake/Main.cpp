427e29c8bc9af2dc6073a10f015227537025e5eb
time-killer-games <samuelvenable@hotmail.com>
2021-05-29 15:53:05 -0400

    Add files via upload

--- CommandLine/emake/Main.cpp
+++ CommandLine/emake/Main.cpp
@@ -10,6 +10,7 @@
 #include "sog.h"
 
 #include "strings_util.h"
+#include "tempfix.h" // remove me when std::fs gets fixed on windows by mingw/msvc guys
 
 #include <filesystem>
 namespace fs = std::filesystem;
@@ -21,14 +22,6 @@ namespace fs = std::filesystem;
 
 std::ostream outputStream(nullptr);
 std::ostream errorStream(nullptr);
-
-#ifdef _WIN32
-/* Tempfix for fs::temp_directory_path() on Michaelsoft Binbows
-Broken in MinGW and Visual Studio since std::fs's conception */
-#define TEMP_DIRECTORY_PATH fs::path(getenv("TMP") ? getenv("TMP") : "C:/").string()
-#else
-#define TEMP_DIRECTORY_PATH fs::temp_directory_path().string()
-#endif
 
 int main(int argc, char* argv[])
 {
