75e182949d68bc85796ef931a5effc8ecb5bb2a5
time-killer-games <samuelvenable@hotmail.com>
2021-05-29 18:37:43 -0400

    Update tempfix.h

--- shared/tempfix.h
+++ shared/tempfix.h
@@ -1,5 +1,5 @@
 // assumes correct headers and fs namespace included/using before calling macro
-#if (defined(_WIN32) && !defined_WIN64)
+#if (defined(_WIN32) && !defined(_WIN64))
 /* Tempfix for fs::temp_directory_path() on Michaelsoft Binbows (MinGW 32-bit) */
 #define TEMP_DIRECTORY_PATH fs::path(getenv("TMP") ? getenv("TMP") : "C:/").string()
 #else
