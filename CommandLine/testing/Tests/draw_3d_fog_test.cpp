045d7fb68f551a0361673d3dcef4dc75093bdb7d
Robert Colton <robertbcolton@gmail.com>
2021-06-23 13:47:43 -0400

    oopsie

--- CommandLine/testing/Tests/draw_3d_fog_test.cpp
+++ CommandLine/testing/Tests/draw_3d_fog_test.cpp
@@ -4,7 +4,7 @@ TEST(Regression, draw_3d_fog_test) {
   if (!TestHarness::windowing_supported()) return;
   // Iterate only platforms and graphics systems
   for (TestConfig tc : GetValidConfigs(true, true, false, false, false, false)) {
-    auto test_harness = LAUNCH_HARNESS_FOR_GAME(TestConfig(), "gmx");
+    auto test_harness = LAUNCH_HARNESS_FOR_SOG(tc);
     test_common(test_harness.get(), "draw_3d_fog_test" + tc.stringify());
   }
 }
