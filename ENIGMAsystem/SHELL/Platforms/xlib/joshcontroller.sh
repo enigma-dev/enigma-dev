c105ced8408a74f1d74b77cba3403c847b3b7bd1
time-killer-games <samuelvenable@hotmail.com>
2021-07-12 05:45:07 -0400

    Add files via upload

--- ENIGMAsystem/SHELL/Platforms/xlib/joshcontroller.sh
+++ ENIGMAsystem/SHELL/Platforms/xlib/joshcontroller.sh
@@ -8,14 +8,14 @@ echo "  Usage: ./joshcontroller.sh joshcontroller.png > joshcontroller.h"
 echo ""
 echo "  #!/bin/sh"
 echo '  cd "${0%/*}"'
-echo "  echo 'unsigned long joshcontroller[] = {'"
-echo "  identify -format '%w, %h,' \$1"
+echo "  echo 'static unsigned long joshcontroller[] = {'"
+echo "  identify -format '%w, %h,\\\n' \$1"
 echo "  convert \$1 -color-matrix '0 0 1 0, 0 1 0 0, 1 0 0 0, 0 0 0 1' RGBA:- | hexdump -v -e '1/4 "0x%08x,\\\n"'"
 echo "  echo '};'"
 echo ""
 echo "*/"
 echo ""
-echo 'unsigned long joshcontroller[] = {'
-identify -format '%w, %h,' $1
+echo 'static unsigned long joshcontroller[] = {'
+identify -format '%w, %h,\n' $1
 convert $1 -color-matrix '0 0 1 0, 0 1 0 0, 1 0 0 0, 0 0 0 1' RGBA:- | hexdump -v -e '1/4 "0x%08x,\n"'
 echo '};' 
