c19ae040fb957baf1e2ef7f4c4d5660958ba91b1
time-killer-games <samuelvenable@hotmail.com>
2021-07-12 01:50:01 -0400

    Add files via upload

--- ENIGMAsystem/SHELL/Platforms/xlib/joshcontroller.h
+++ ENIGMAsystem/SHELL/Platforms/xlib/joshcontroller.h
@@ -2,11 +2,13 @@
 
   Made with script from (needs ImageMagick):
   https://unix.stackexchange.com/a/219526
+  Usage: ./joshcontroller.sh joshcontroller.png > joshcontroller.h
 
   #!/bin/sh
-  echo 'unsigned long rlogo_icon[] = {'
+  cd "${0%/*}"
+  echo 'unsigned long joshcontroller[] = {'
   identify -format '%w, %h,' $1
-  convert $1 -color-matrix '0 0 1 0, 0 1 0 0, 1 0 0 0, 0 0 0 1' RGBA:- | hexdump -v -e '1/4 "0x%08x,\n"'
+  convert $1 -color-matrix '0 0 1 0, 0 1 0 0, 1 0 0 0, 0 0 0 1' RGBA:- | hexdump -v -e '1/4 0x%08x,\n'
   echo '};'
 
 */
