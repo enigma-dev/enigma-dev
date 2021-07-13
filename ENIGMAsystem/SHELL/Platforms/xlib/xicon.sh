888c17c0f337fe670cc053a05c6a69104d7c5245
time-killer-games <samuelvenable@hotmail.com>
2021-07-12 21:51:38 -0400

    Delete xicon.sh

--- ENIGMAsystem/SHELL/Platforms/xlib/xicon.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd "${0%/*}"
-g++ xicon.cpp ../../../../shared/libpng-util/libpng-util.cpp -o xicon -I/usr/local/include -L/usr/local/lib -lpng -std=c++17 -DBUILD_XICON
-./xicon in.png > out.h
