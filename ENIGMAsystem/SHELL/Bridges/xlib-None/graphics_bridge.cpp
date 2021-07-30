3e0c6f9b4db903ddcadb6146efbaed46cdff7f51
time-killer-games <samuelvenable@hotmail.com>
2021-07-29 20:57:39 -0400

    [skip ci]

---
+++ ENIGMAsystem/SHELL/Bridges/xlib-None/graphics_bridge.cpp
@@ -0,0 +1,45 @@
+/** Copyright (C) 2013 forthevin
+***
+*** This file is a part of the ENIGMA Development Environment.
+***
+*** ENIGMA is free software: you can redistribute it and/or modify it under the
+*** terms of the GNU General Public License as published by the Free Software
+*** Foundation, version 3 of the license or any later version.
+***
+*** This application and its source code is distributed AS-IS, WITHOUT ANY
+*** WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+*** FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+*** details.
+***
+*** You should have received a copy of the GNU General Public License along
+*** with this code. If not, see <http://www.gnu.org/licenses/>
+**/
+
+#include "Graphics_Systems/General/GScolors.h"
+#include "Widget_Systems/widgets_mandatory.h"
+#include "Platforms/xlib/XLIBwindow.h"
+
+namespace enigma {
+
+  void WindowResized() {
+
+  }
+
+  void EnableDrawing(void* handle) {
+
+  }
+
+  void DisableDrawing(void* handle) {
+
+  }
+
+  void ScreenRefresh() {
+
+  }
+}
+
+namespace enigma_user {
+  void set_synchronization(bool enable) {
+
+  }
+}
