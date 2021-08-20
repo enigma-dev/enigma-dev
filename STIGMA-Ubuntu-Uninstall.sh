2cecff788c49685ffcb1cd68674347a99db30b11
time-killer-games <samuelvenable@hotmail.com>
2021-08-20 19:51:57 -0400

    Add files via upload

--- STIGMA-Ubuntu-Uninstall.sh
+++ STIGMA-Ubuntu-Uninstall.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 [ -d "/usr/bin/stigma-dev" ] && sudo rm -fr /usr/bin/stigma-dev && sudo rm -f /usr/share/applications/stigma-dev.desktop
+echo "Uninstallation Complete! Run \"sudo apt-get autoremove\" to remove unwanted dependencies."
