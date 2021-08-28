75069abccc966ecc66ed0554fbcf76c04716861f
Greg Williamson <fundies@users.noreply.github.com>
2021-08-28 12:31:23 -0400

    Update shared.cpp

--- ENIGMAsystem/SHELL/Graphics_Systems/OpenGL3/shared.cpp
+++ ENIGMAsystem/SHELL/Graphics_Systems/OpenGL3/shared.cpp
@@ -1,5 +1,3 @@
 #include "Graphics_Systems/OpenGL-Common/shared.cpp"
 #include "Graphics_Systems/OpenGL-Common/texture_copy.cpp"
 #include "Graphics_Systems/OpenGL-Desktop/shared.cpp"
-#include "Graphics_Systems/OpenGL-Desktop/d3d.cpp"
-
