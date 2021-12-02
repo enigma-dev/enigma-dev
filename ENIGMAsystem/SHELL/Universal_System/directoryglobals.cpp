ca2160406ac85f41d1ac47911e662fb5bbcab693
Samuel Venable <samuelvenable@hotmail.com>
2021-12-01 19:22:59 -0500

    Add files via upload

--- ENIGMAsystem/SHELL/Universal_System/directoryglobals.cpp
+++ ENIGMAsystem/SHELL/Universal_System/directoryglobals.cpp
@@ -6,38 +6,38 @@ namespace enigma_user {
 
 std::string get_working_directory() { 
   #if defined(_WIN32)
-  return CrossProcess::DirectoryGetCurrentWorking() + std::string("\\");
+  return ngs::proc::directory_get_current_working() + std::string("\\");
   #else
-  return CrossProcess::DirectoryGetCurrentWorking() + std::string("/");
+  return ngs::proc::directory_get_current_working() + std::string("/");
   #endif
 }
 
 std::string get_program_filename() { 
-  return enigma_user::filename_name(CrossProcess::ExecutableFromSelf()); 
+  return enigma_user::filename_name(ngs::proc::executable_from_self()); 
 }
 
 std::string get_program_directory() { 
-  return enigma_user::filename_path(CrossProcess::ExecutableFromSelf()); 
+  return enigma_user::filename_path(ngs::proc::executable_from_self()); 
 }
 
 std::string get_program_pathname() { 
-  return CrossProcess::ExecutableFromSelf(); 
+  return ngs::proc::executable_from_self(); 
 }
 
 std::string environment_get_variable(std::string name) {
-  return CrossProcess::EnvironmentGetVariable(name.c_str());
+  return ngs::proc::environment_get_variable(name.c_str());
 }
 
 bool environment_set_variable(const std::string &name, const std::string &value) {
-  return CrossProcess::EnvironmentSetVariable(name.c_str(), value.c_str());
+  return ngs::proc::environment_set_variable(name.c_str(), value.c_str());
 }
 
 bool environment_unset_variable(const std::string &name) {
-  return CrossProcess::EnvironmentUnsetVariable(name.c_str());
+  return ngs::proc::environment_unset_variable(name.c_str());
 }
 
 bool set_working_directory(std::string dname) {
-  return CrossProcess::DirectorySetCurrentWorking(dname.c_str());
+  return ngs::proc::directory_set_current_working(dname.c_str());
 }
 
 } // namespace enigma_user
