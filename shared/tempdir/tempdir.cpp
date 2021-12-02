f42b7a899ad66c2a339a31888a691f0311f5b818
Samuel Venable <samuelvenable@hotmail.com>
2021-12-01 22:39:09 -0500

    Add files via upload

--- shared/tempdir/tempdir.cpp
+++ shared/tempdir/tempdir.cpp
@@ -1,11 +1,11 @@
 #include <string>
 
-#include "../apiprocess/process.h"
+#include "apiprocess/process.h"
 
 namespace enigma_user {
 
 std::string get_temp_directory() { 
-  return ngs::proc::directory_get_temporary_path(); 
+  return ::ngs::proc::directory_get_temporary_path(); 
 }
 
 } // namespace enigma_user
