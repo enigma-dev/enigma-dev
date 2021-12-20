70166f4813e70b2dc87a7b1241df28dce75380bb
Samuel Venable <samuelvenable@hotmail.com>
2021-12-19 22:56:13 -0800

    Update tempdir.cpp

--- shared/tempdir/tempdir.cpp
+++ shared/tempdir/tempdir.cpp
@@ -5,7 +5,13 @@
 namespace enigma_user {
 
 std::string get_temp_directory() { 
-  return ::ngs::proc::directory_get_temporary_path(); 
+  std::string result = ::ngs::proc::directory_get_temporary_path();
+  #if defined(_WIN32)
+  if (!result.empty() && result.back() != '/' && result.back() != '\\') result.push_back('\\');
+  #else
+  if (!result.empty() && result.back() != '/') result.push_back('/');
+  #endif
+  return result;
 }
 
 } // namespace enigma_user
