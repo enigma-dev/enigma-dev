403e3f9015d1d81b1d814b1d9ce1d94b66ffaf16
Samuel Venable <samuelvenable@hotmail.com>
2022-01-15 12:32:24 -0500

    Update enigma.cpp

--- ENIGMAsystem/SHELL/Universal_System/Extensions/FileSystem/enigma.cpp
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/FileSystem/enigma.cpp
@@ -31,86 +31,86 @@ namespace filesystem = ngs::fs;
 
 namespace enigma_fs {
 
-  std::string fs_get_working_directory() {
-    return filesystem::get_working_directory();
-  }
-  
-  bool fs_set_working_directory(std::string dname) {
-    return filesystem::set_working_directory(dname);
-  }
-  
-  std::string fs_get_temp_directory() {
-    return filesystem::get_temp_directory();
-  }
-  
-  std::string fs_get_program_directory() {
-    return filesystem::get_program_directory();
-  }
-  
-  std::string fs_get_program_filename() {
-    return filesystem::get_program_filename();
-  }
-  
-  std::string fs_get_program_pathname() {
-    return filesystem::get_program_pathname();
-  }
+  std::string fs_directory_get_current_working() {
 
-  std::string fs_get_filedescriptor_pathname(int fd) {
-    return filesystem::get_filedescriptor_pathname(fd);
   }
-  
+ 
+  bool fs_directory_set_current_working(std::string dname) {
+
+  }
+ 
+  std::string fs_directory_get_temporary_path() {
+
+  }
+ 
+  std::string fs_executable_get_directory() {
+
+  }
+ 
+  std::string fs_executable_get_filename() {
+
+  }
+ 
+  std::string fs_executable_get_pathname() {
+
+  }
+ 
+  std::string fs_file_bin_pathname(int fd) {
+
+  }
+ 
   std::string fs_filename_absolute(std::string fname) {
-    return filesystem::filename_absolute(fname);
-  }
 
+  }
+ 
   std::string fs_filename_canonical(std::string fname) {
-    return filesystem::filename_canonical(fname);
+
   }
-  
+ 
   bool fs_file_exists(std::string fname) {
-    return filesystem::file_exists(fname);
+
   }
-  
+ 
   bool fs_file_delete(std::string fname) {
-    return filesystem::file_delete(fname);
+
   }
-  
+ 
   bool fs_file_rename(std::string oldname, std::string newname) {
-    return filesystem::file_rename(oldname, newname);
+
   }
-  
+ 
   bool fs_file_copy(std::string fname, std::string newname) {
-    return filesystem::file_copy(fname, newname);
+
   }
-  
+ 
   std::uintmax_t fs_file_size(std::string fname) {
-    return filesystem::file_size(fname);
+
   }
-  
+ 
   bool fs_directory_exists(std::string dname) {
-    return filesystem::directory_exists(dname);
+
   }
-  
+ 
   bool fs_directory_create(std::string dname) {
-    return filesystem::directory_create(dname);
+
   }
-  
+ 
   bool fs_directory_destroy(std::string dname) {
-    return filesystem::directory_destroy(dname);
+
   }
-  
+ 
   bool fs_directory_rename(std::string oldname, std::string newname) {
-    return filesystem::directory_rename(oldname, newname);
+
   }
-  
+ 
   bool fs_directory_copy(std::string dname, std::string newname) {
-    return filesystem::directory_copy(dname, newname);
+
   }
-  
+ 
   std::uintmax_t fs_directory_size(std::string dname) {
-    return filesystem::directory_size(dname);
+
   }
-  
+ 
   var fs_directory_contents(std::string dname, std::string pattern = "*.*", bool includedirs = true) {
     var resVar; std::vector<std::string> resVec = filesystem::directory_contents(dname, pattern, includedirs);
     for (unsigned i = 0; i < resVec.size(); i++) resVar[i] = resVec[i];
@@ -122,157 +122,253 @@ namespace enigma_fs {
     for (unsigned i = 0; i < resVec.size(); i++) resVar[i] = resVec[i];
     return resVar;
   }
-
+ 
   std::string fs_environment_get_variable(std::string name) {
-    return filesystem::environment_get_variable(name);
+
   }
-  
+ 
   bool fs_environment_set_variable(std::string name, std::string value) {
-    return filesystem::environment_set_variable(name, value);
-  }
 
+  }
+ 
   bool fs_environment_unset_variable(std::string name) {
-    return filesystem::environment_unset_variable(name);
-  }
-  
-  std::string fs_environment_expand_variables(std::string str) {
-    return filesystem::environment_expand_variables(str);
-  }
-  
-  int fs_file_get_date_accessed_year(std::string fname) {
-    return filesystem::file_get_date_accessed_year(fname);
-  }
-  
-  int fs_file_get_date_accessed_month(std::string fname) {
-    return filesystem::file_get_date_accessed_month(fname);
-  }
-  
-  int fs_file_get_date_accessed_day(std::string fname) {
-    return filesystem::file_get_date_accessed_day(fname);
-  }
-  
-  int fs_file_get_date_accessed_hour(std::string fname) {
-    return filesystem::file_get_date_accessed_hour(fname);
-  }
-  
-  int fs_file_get_date_accessed_minute(std::string fname) {
-    return filesystem::file_get_date_accessed_minute(fname);
-  }
-  
-  int fs_file_get_date_accessed_second(std::string fname) {
-    return filesystem::file_get_date_accessed_second(fname);
-  }
-  
-  int fs_file_get_date_modified_year(std::string fname) {
-    return filesystem::file_get_date_modified_year(fname);
-  }
-  
-  int fs_file_get_date_modified_month(std::string fname) {
-    return filesystem::file_get_date_modified_month(fname);
-  }
-  
-  int fs_file_get_date_modified_day(std::string fname) {
-    return filesystem::file_get_date_modified_day(fname);
-  }
-  
-  int fs_file_get_date_modified_hour(std::string fname) {
-    return filesystem::file_get_date_modified_hour(fname);
-  }
-  
-  int fs_file_get_date_modified_minute(std::string fname) {
-    return filesystem::file_get_date_modified_minute(fname);
-  }
-  
-  int fs_file_get_date_modified_second(std::string fname) {
-    return filesystem::file_get_date_modified_second(fname);
-  }
 
+  }
+ 
+  std::string environment_expand_variables(std::string str) {
+
+  }
+ 
+  int fs_file_datetime_accessed_year(std::string fname) {
+
+  }
+ 
+  int fs_file_datetime_accessed_month(std::string fname) {
+
+  }
+ 
+  int fs_file_datetime_accessed_day(std::string fname) {
+
+  }
+ 
+  int fs_file_datetime_accessed_hour(std::string fname) {
+
+  }
+ 
+  int fs_file_datetime_accessed_minute(std::string fname) {
+
+  }
+ 
+  int fs_file_datetime_accessed_second(std::string fname) {
+
+  }
+ 
+  int fs_file_datetime_modified_year(std::string fname) {
+
+  }
+ 
+  int fs_file_datetime_modified_month(std::string fname) {
+
+  }
+ 
+  int fs_file_datetime_modified_day(std::string fname) {
+
+  }
+ 
+  int fs_file_datetime_modified_hour(std::string fname) {
+
+  }
+ 
+  int fs_file_datetime_modified_minute(std::string fname) {
+
+  }
+ 
+  int fs_file_datetime_modified_second(std::string fname) {
+
+  }
+ 
+  int fs_file_datetime_created_year(std::string fname) {
+
+  }
+ 
+  int fs_file_datetime_created_month(std::string fname) {
+
+  }
+ 
+  int fs_file_datetime_created_day(std::string fname) {
+
+  }
+ 
+  int fs_file_datetime_created_hour(std::string fname) {
+
+  }
+ 
+  int fs_file_datetime_created_minute(std::string fname) {
+
+  }
+ 
+  int fs_file_datetime_created_second(std::string fname) {
+
+  }
+ 
+  int fs_file_bin_datetime_accessed_year(int fd) {
+
+  }
+ 
+  int fs_file_bin_datetime_accessed_month(int fd) {
+
+  }
+ 
+  int fs_file_bin_datetime_accessed_day(int fd) {
+
+  }
+ 
+  int fs_file_bin_datetime_accessed_hour(int fd) {
+
+  }
+ 
+  int fs_file_bin_datetime_accessed_minute(int fd) {
+
+  }
+ 
+  int fs_file_bin_datetime_accessed_second(int fd) {
+
+  }
+ 
+  int fs_file_bin_datetime_modified_year(int fd) {
+
+  }
+ 
+  int fs_file_bin_datetime_modified_month(int fd) {
+
+  }
+ 
+  int fs_file_bin_datetime_modified_day(int fd) {
+
+  }
+ 
+  int fs_file_bin_datetime_modified_hour(int fd) {
+
+  }
+ 
+  int fs_file_bin_datetime_modified_minute(int fd) {
+
+  }
+ 
+  int fs_file_bin_datetime_modified_second(int fd) {
+
+  }
+ 
+  int fs_file_bin_datetime_created_year(int fd) {
+
+  }
+ 
+  int fs_file_bin_datetime_created_month(int fd) {
+
+  }
+ 
+  int fs_file_bin_datetime_created_day(int fd) {
+
+  }
+ 
+  int fs_file_bin_datetime_created_hour(int fd) {
+
+  }
+ 
+  int fs_file_bin_datetime_created_minute(int fd) {
+
+  }
+ 
+  int fs_file_bin_datetime_created_second(int fd) {
+
+  }
+ 
   int fs_file_bin_open(std::string fname, int mode) {
-    return filesystem::file_bin_open(fname, mode);
+
   }
-  
+ 
   int fs_file_bin_rewrite(int fd) {
-    return filesystem::file_bin_rewrite(fd);
+
   }
-  
+ 
   int fs_file_bin_close(int fd) {
-    return filesystem::file_bin_close(fd);
+
   }
-  
+ 
   long fs_file_bin_size(int fd) {
-    return filesystem::file_bin_size(fd);
+
   }
-  
+ 
   long fs_file_bin_position(int fd) {
-    return filesystem::file_bin_position(fd);
+
   }
-  
+ 
   long fs_file_bin_seek(int fd, long pos) {
-    return filesystem::file_bin_seek(fd, pos);
+
   }
-  
+ 
   int fs_file_bin_read_byte(int fd) {
-    return filesystem::file_bin_read_byte(fd);
+
   }
-  
+ 
   int fs_file_bin_write_byte(int fd, int byte) {
-    return filesystem::file_bin_write_byte(fd, byte);
-  }
 
+  }
+ 
   int fs_file_text_open_read(std::string fname) {
-    return filesystem::file_text_open_read(fname);
-  }
 
+  }
+ 
   int fs_file_text_open_write(std::string fname) {
-    return filesystem::file_text_open_write(fname);
+
   }
-  
+ 
   int fs_file_text_open_append(std::string fname) {
-    return filesystem::file_text_open_append(fname);
+
   }
-  
+ 
   long fs_file_text_write_real(int fd, double val) {
-    return filesystem::file_text_write_real(fd, val);
+
   }
-  
+ 
   long fs_file_text_write_string(int fd, std::string str) {
-    return filesystem::file_text_write_string(fd, str);
+
   }
-  
+ 
   int fs_file_text_writeln(int fd) {
-    return filesystem::file_text_writeln(fd);
+
   }
-  
+ 
   bool fs_file_text_eoln(int fd) {
-    return filesystem::file_text_eoln(fd);
+
   }
-  
+ 
   bool fs_file_text_eof(int fd) {
-    return filesystem::file_text_eof(fd);
+
   }
 
   double fs_file_text_read_real(int fd) {
-    return filesystem::file_text_read_real(fd);
+
   }
-  
+
   std::string fs_file_text_read_string(int fd) {
-    return filesystem::file_text_read_string(fd);
+
   }
-  
+
   std::string fs_file_text_readln(int fd) {
-    return filesystem::file_text_readln(fd);
+
   }
-  
+ 
   std::string fs_file_text_read_all(int fd) {
-    return filesystem::file_text_read_all(fd);
+
   }
-  
+ 
   int fs_file_text_open_from_string(std::string str) {
-    return filesystem::file_text_open_from_string(str);
+
   }
-  
+ 
   int fs_file_text_close(int fd) {
-    return filesystem::file_text_close(fd);
+
   }
 
 } // namespace enigma_fs
