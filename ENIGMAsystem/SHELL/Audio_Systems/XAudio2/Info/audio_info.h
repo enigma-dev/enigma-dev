013618efb23626c6119f281aac2dbfda3dfa1cf5
time-killer-games <samuelvenable@hotmail.com>
2022-03-02 14:13:37 -0800

    Fix SDL2_mixer Audio System on Windows

--- ENIGMAsystem/SHELL/Audio_Systems/XAudio2/Info/audio_info.h
+++
@@ -1,5 +0,0 @@
-// Informative header designed to grant superior control over platform-
-// or API-dependent behavior. This file can define any number of macros
-// describing various compatibility and feature points.
-
-#define ENIGMA_AS_XAUDIO2 1
