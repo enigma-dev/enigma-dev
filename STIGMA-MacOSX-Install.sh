dac12b3092c1b2ed5c8a05394ee432a4a5365b5d
Samuel Venable <samuelvenable@hotmail.com>
2022-03-17 17:35:10 -0400

    Add files via upload

--- STIGMA-MacOSX-Install.sh
+++ STIGMA-MacOSX-Install.sh
@@ -3,7 +3,7 @@
 sudo xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install alure
-sudo port -N install openjdk17 gmake cmake pkgconfig grpc protobuf-c glew glm libpng openal-soft libsndfile libogg libvorbis vorbis-tools box2D libdumb libsdl2 freetype libffi rapidjson libyaml libvpx pugixml yaml-cpp boost
+sudo port -N install openjdk8 gmake cmake pkgconfig grpc protobuf-c glew glm libpng openal-soft libsndfile libogg libvorbis vorbis-tools box2D libdumb libsdl2 freetype libffi rapidjson libyaml libvpx pugixml yaml-cpp boost
 sudo mkdir -p /usr/local/bin/STIGMA/STIGMA
 sudo git clone --recurse-submodules -j8 https://github.com/time-killer-games/stigma-dev /usr/local/bin/STIGMA/STIGMA/stigma-dev
 cd /usr/local/bin/STIGMA/STIGMA/stigma-dev
@@ -14,3 +14,5 @@ sudo chmod +x ./install.sh
 sudo ./install.sh
 sudo chmod +x ./start.sh
 ln -s /usr/local/bin/STIGMA/STIGMA/stigma-dev /Applications/STIGMA
+echo "export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk8-temurin/Contents/Home" >> ~/.zshrc
+echo "export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk8-temurin/Contents/Home" >> ~/.bashrc
