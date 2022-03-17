8a79d46fbce8464b04732f10710e8dabca2f7908
Samuel Venable <samuelvenable@hotmail.com>
2022-03-16 22:46:55 -0400

    Add files via upload

--- STIGMA-MacOSX-Install.sh
+++ STIGMA-MacOSX-Install.sh
@@ -2,9 +2,8 @@
 # Requires MacPorts to be installed! (Don't use Homebrew!)
 # You also need to have Xcode installed from the AppStore!
 # Xcode select is NOT enough if you want to use any audio!
-/usr/bin/sudo xcode-select --install 
-/usr/bin/sudo port install boost
-/usr/bin/sudo port install openjdk17 +universal gmake +universal cmake +universal pkgconfig +universal grpc +universal protobuf-c +universal glew +universal glm +universal libpng +universal openal-soft +universal openal +universal libogg +universal libvorbis +universal vorbis-tools +universal box2D +universal libdumb +universal libsdl2 +universal freetype +universal libffi +universal rapidjson +universal libyaml +universal pugixml +universal yaml-cpp +universal
+/usr/bin/sudo xcode-select --install
+/usr/bin/sudo port install openjdk17 gmake cmake pkgconfig grpc protobuf-c glew glm libpng openal libogg libvorbis vorbis-tools box2D libdumb libsdl2 freetype libffi rapidjson libyaml pugixml yaml-cpp boost
 /usr/bin/sudo mkdir -p /usr/local/bin/STIGMA/STIGMA
 /usr/bin/sudo git clone --recurse-submodules -j8 https://github.com/time-killer-games/stigma-dev /usr/local/bin/STIGMA/STIGMA/stigma-dev
 cd /usr/local/bin/STIGMA/STIGMA/stigma-dev
