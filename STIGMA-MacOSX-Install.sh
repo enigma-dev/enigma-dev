d7f10c2e7278f2a649faf4edfa95896f3462bd11
Samuel Venable <samuelvenable@hotmail.com>
2022-03-20 19:46:49 -0400

    Add files via upload

--- STIGMA-MacOSX-Install.sh
+++ STIGMA-MacOSX-Install.sh
@@ -7,9 +7,9 @@ sudo port -N install openjdk8 gmake cmake pkgconfig grpc protobuf-c glew glm lib
 sudo git clone --recurse-submodules -j8 https://github.com/time-killer-games/stigma-dev /Applications/stigma-dev
 cd /Applications/stigma-dev
 sudo port -f activate libiconv
-gmake && sudo gmake emake
-chmod +x ./install.sh
-./install.sh
-chmod +x ./start.sh
+sudo gmake && sudo gmake emake
+sudo chmod +x ./install.sh
+sudo ./install.sh
+sudo chmod +x ./start.sh
 sudo port -f deactivate libiconv
 echo "Installation Complete! You may now run the \"/Applications/stigma-dev/start.sh\" script from the terminal app!"
