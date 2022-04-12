7c8cc22e86df307963ffda4cb3c0f21a118f4ae1
Samuel Venable <samuelvenable@hotmail.com>
2022-04-11 22:15:18 -0400

    Update STIGMA-MacOSX-Uninstall.sh

--- STIGMA-MacOSX-Uninstall.sh
+++ STIGMA-MacOSX-Uninstall.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo rm -fr /Applications/stigma-dev
-echo "Uninstallation Complete! Uninstallation of MacPorts and Homebrew files must be done separately!"
+echo "Uninstallation Complete! Uninstallation of MacPorts files must be done separately!"
