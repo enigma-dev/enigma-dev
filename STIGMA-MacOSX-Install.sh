170660c38bc92e26795843c41e84d7090214ce76
System Administrator <root@macintoshs-Mini.fios-router.home>
2022-04-13 11:41:52 -0400

    fix mac

--- STIGMA-MacOSX-Install.sh
+++ STIGMA-MacOSX-Install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install openjdk@8 make cmake pkg-config grpc protobuf-c glew glm libpng libsndfile libogg libvorbis vorbis-tools box2d dumb SDL2 SDL2_mixer freetype libffi rapidjson libyaml libvpx pugixml yaml-cpp boost flac mpg123 libmodplug opus opusfile fluidsynth
+brew install openjdk@8 make cmake pkg-config grpc protobuf-c glew glm libpng libsndfile libogg libvorbis vorbis-tools box2d dumb SDL2 SDL2_mixer freetype libffi rapidjson libyaml libvpx pugixml yaml-cpp boost flac mpg123 libmodplug opus opusfile fluidsynth zlib
 sudo git clone --recurse-submodules -j8 https://github.com/time-killer-games/stigma-dev /Applications/stigma-dev
 cd /Applications/stigma-dev
 sudo gmake && sudo gmake emake
