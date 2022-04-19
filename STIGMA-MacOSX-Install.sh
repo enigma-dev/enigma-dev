b63b772616845001a53993d13293587b708376a7
Samuel Venable <samuelvenable@hotmail.com>
2022-04-18 23:55:02 -0400

    Add files via upload

--- STIGMA-MacOSX-Install.sh
+++ STIGMA-MacOSX-Install.sh
@@ -4,7 +4,7 @@ sudo xcode-select --install
 brew install openjdk@8 make cmake pkg-config grpc protobuf-c glew glm libpng libsndfile libogg libvorbis vorbis-tools box2d dumb SDL2 SDL2_mixer freetype libffi rapidjson libyaml libvpx pugixml yaml-cpp boost flac mpg123 libmodplug opus opusfile fluidsynth zlib
 sudo git clone --recurse-submodules -j8 https://github.com/time-killer-games/stigma-dev /Applications/stigma-dev
 cd /Applications/stigma-dev
-sudo gmake && sudo gmake emake
+sudo env CC=clang CXX=clang++ gmake && sudo env CC=clang CXX=clang++ gmake emake
 sudo chmod +x ./install.sh
 sudo ./install.sh
 sudo chmod +x ./start.sh
