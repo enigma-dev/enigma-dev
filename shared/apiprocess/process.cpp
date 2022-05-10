71eb6f96c7d1349f3ffb38eaa3137946750d8cfd
Samuel Venable <samuelvenable@hotmail.com>
2022-05-10 06:41:18 +0000

    Add files via upload

--- shared/apiprocess/process.cpp
+++ shared/apiprocess/process.cpp
@@ -34,7 +34,7 @@
 #include <vector>
 #include <mutex>
 
-#if (defined(__APPLE__) && defined(__MACH__)) || (defined(__linux__) && !defined(__ANDROID__))
+#if (defined(__APPLE__) && defined(__MACH__)) || (defined(__linux__) && !defined(__ANDROID__)) || defined(__OpenBSD__)
 #include <set>
 #endif
 
@@ -676,6 +676,9 @@ namespace ngs::proc {
     kd = kvm_openfiles(nullptr, nullptr, nullptr, KVM_NO_FILES, errbuf); if (!kd) return;
     if ((proc_info = kvm_getprocs(kd, KERN_PROC_ALL, 0, sizeof(struct KINFO_PROC), &cntp))) {
       for (int j = cntp - 1; j >= 0; j--) {
+        if (proc_info[j].p_ppid == 0) {
+          vec.push_back(0); i++;
+        }
         if (proc_info[j].p_ppid == parent_proc_id) {
           vec.push_back(proc_info[j].p_pid); i++;
         }
@@ -683,6 +686,13 @@ namespace ngs::proc {
     #endif
     }
     kvm_close(kd);
+    #if defined(__OpenBSD__)
+    std::set<PROCID> s;
+    unsigned sz = vec.size();
+    for (unsigned j = 0; j < sz; j++) s.insert(vec[j]);
+    vec.assign(s.begin(), s.end());
+    i = vec.size();
+    #endif
     #endif
     *proc_id = (PROCID *)malloc(sizeof(PROCID) * vec.size());
     if (proc_id) {
