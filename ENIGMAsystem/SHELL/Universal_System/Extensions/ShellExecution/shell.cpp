8208b6e199684a02ebf108c9c2724bf9c48f37eb
Samuel Venable <samuelvenable@hotmail.com>
2022-05-12 23:11:29 -0400

    Update shell.cpp

--- ENIGMAsystem/SHELL/Universal_System/Extensions/ShellExecution/shell.cpp
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/ShellExecution/shell.cpp
@@ -40,8 +40,9 @@ void WindowIdSetParentWindowId(wid_t windowId, wid_t parentWindowId) {
   SetWindowLongPtr(parent, GWL_STYLE, GetWindowLongPtr(parent, GWL_STYLE) | WS_CLIPCHILDREN | WS_CLIPSIBLINGS);
   SetParent(child, parent); SetWindowPos(child, HWND_TOP, 0, 0, 0, 0, SWP_NOOWNERZORDER | SWP_SHOWWINDOW | SWP_NOMOVE | SWP_NOSIZE);
   RECT rect; GetClientRect(parent, &rect); MoveWindow(child, rect.left, rect.top, rect.right - rect.left, rect.bottom - rect.top, true);
-  ShowWindow(child, SW_MAXIMIZE); GetWindowRect(parent, &rect); MoveWindow(parent, rect.left, rect.top, rect.right - rect.left, rect.bottom - rect.top - 1, true);
-  GetWindowRect(parent, &rect); MoveWindow(parent, rect.left, rect.top, rect.right - rect.left, rect.bottom - rect.top + 1, true); ShowWindow(parent, SW_RESTORE);
+  ShowWindow(child, SW_MAXIMIZE); ShowWindow(parent, SW_RESTORE);
+  GetWindowRect(parent, &rect); MoveWindow(parent, rect.left, rect.top, rect.right - rect.left, rect.bottom - rect.top - 1, true);
+  GetWindowRect(parent, &rect); MoveWindow(parent, rect.left, rect.top, rect.right - rect.left, rect.bottom - rect.top + 1, true);
   #elif (defined(__linux__) && !defined(__ANDROID__)) || (defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__))
   Window child  = (Window)(uintptr_t)strtoull(windowId.c_str(), nullptr, 10);
   Window parent = (Window)(uintptr_t)strtoull(parentWindowId.c_str(), nullptr, 10);
