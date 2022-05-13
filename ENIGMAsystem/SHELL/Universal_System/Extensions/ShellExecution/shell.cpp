5e5162cdea1beaa39096ac6610ec4db52584336e
Samuel Venable <samuelvenable@hotmail.com>
2022-05-12 23:41:57 -0400

    Update shell.cpp

--- ENIGMAsystem/SHELL/Universal_System/Extensions/ShellExecution/shell.cpp
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/ShellExecution/shell.cpp
@@ -38,8 +38,9 @@ void WindowIdSetParentWindowId(wid_t windowId, wid_t parentWindowId) {
   SetWindowLongPtr(child, GWL_STYLE, (GetWindowLongPtr(child, GWL_STYLE) | WS_CHILD) & ~(WS_CAPTION | WS_BORDER | WS_SIZEBOX));
   SetWindowLongPtr(child, GWL_EXSTYLE, GetWindowLongPtr(child, GWL_EXSTYLE) | WS_EX_TOOLWINDOW);
   SetWindowLongPtr(parent, GWL_STYLE, GetWindowLongPtr(parent, GWL_STYLE) | WS_CLIPCHILDREN | WS_CLIPSIBLINGS);
-  SetParent(child, parent);RECT rect; GetClientRect(parent, &rect); ShowWindow(child, SW_MAXIMIZE);
+  SetParent(child, parent); RECT rect; GetClientRect(parent, &rect); ShowWindow(child, SW_MAXIMIZE);
   MoveWindow(child, rect.left, rect.top, rect.right - rect.left, rect.bottom - rect.top, true);
+  FillRect((HDC)GetDC(parent), &rect, CreateSolidBrush(0));
   #elif (defined(__linux__) && !defined(__ANDROID__)) || (defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__))
   Window child  = (Window)(uintptr_t)strtoull(windowId.c_str(), nullptr, 10);
   Window parent = (Window)(uintptr_t)strtoull(parentWindowId.c_str(), nullptr, 10);
