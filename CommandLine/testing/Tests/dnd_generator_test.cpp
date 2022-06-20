a1652ae4ef977dc7d36bea8df6c519502f9e7ce2
Dhruv Chawla <44582521+dc03@users.noreply.github.com>
2022-06-20 16:51:31 +0530

    Update test harness cpp file to copy polygon_collision

--- CommandLine/testing/Tests/dnd_generator_test.cpp
+++ CommandLine/testing/Tests/dnd_generator_test.cpp
@@ -1,10 +1,25 @@
-#include "CommonTest.hpp"
+#include "TestHarness.hpp"
+#include <gtest/gtest.h>
 
-TEST(Regression, dnd_generator_test) {
-  if (!TestHarness::windowing_supported()) return;
-  // Iterate only platforms and graphics systems
-  for (TestConfig tc : GetValidConfigs(true, true, false, false, false, false)) {
-    auto test_harness = LAUNCH_HARNESS_FOR_GAME(TestConfig(), "gmx");
-    test_common(test_harness.get(), "dnd_generator_test" + tc.stringify());
+TEST(Game, dnd_generator_test) {
+  TestConfig tc;
+  tc.extensions = "Alarms,GTest";
+  tc.audio = "None";
+  auto test_harness = LAUNCH_HARNESS_FOR_GAME(tc, "gmx");
+
+  if (!test_harness) FAIL() << "Game could not be run.";
+
+  ASSERT_TRUE(test_harness->game_is_running())
+      << "Game stopped running unexpectedly";
+
+  // Close the window and verify that the game ends normally.
+  test_harness->close_window();
+  bool game_running = test_harness->game_is_running();
+  for (int i = 0; game_running && i < 10; ++i) {
+    test_harness->wait();
+    game_running = test_harness->game_is_running();
   }
+  ASSERT_FALSE(game_running) << "Game did not exit after window was closed!";
+  EXPECT_EQ(test_harness->get_return(), 0) << "Game returned non-zero exit code!";
+
 }
