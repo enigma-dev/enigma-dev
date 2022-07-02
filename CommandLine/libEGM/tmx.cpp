2c579560bc2b223dafd5113fd9950d0158bdfeae
KartikShrivastava <shrivastavakartik19@gmail.com>
2022-07-02 16:14:53 +0530

    First iteration of TMX importer

--- CommandLine/libEGM/tmx.cpp
+++ CommandLine/libEGM/tmx.cpp
@@ -15,31 +15,86 @@ public:
   TMXMapLoader(buffers::TreeNode *root, const fs::path &fPath) : tmxPath(fPath) {
     root->set_name("/");
     nodes.push_back(root);
-    tiledEnigmaResourceMap["map"] = "room";
-//    tiledEnigmaResourceMap["tileset"] = "background";
+  }
+
+  bool Load(pugi::xml_document &xmlDoc) {
+    pugi::xml_node mapNode = xmlDoc.child("map");
+
+    // first load all tileset(s) in background(s)
+    bool backgroundsOk = LoadTilesets(mapNode, "background");
+    if(!backgroundsOk)
+      return false;
+
+    // now load map, backgrounds are available for setting room.tile and/or room.background
+    bool roomOk = LoadMap(mapNode, "room");
+    if(!roomOk)
+      return false;
+
+    return true;
   }
 
 private:
   std::vector<buffers::TreeNode *> nodes;
   fs::path tmxPath;
-  std::unordered_map<std::string, std::string> tiledEnigmaResourceMap;
   std::unordered_map<std::string, buffers::TreeNode *> resourceFolderRefs;
   // helps in generating ids for repetable messages present in resources like Room.proto
   std::unordered_map<std::string, int> resourceTypeIdCountMap;
-  std::unordered_map<std::string, buffers::resources::Background *> backgroundNameMsgPtrMap;
   std::map<int, std::string> tilesetIdSourcePathMap;
-  // TODO: Remove this hack with "resource name generator"
+  // TODO: Remove this hack and use "resource name generator"
   int idx=0;
 
-  virtual bool for_each(pugi::xml_node &xmlNode) {
-    if(tiledEnigmaResourceMap.find(xmlNode.name()) == tiledEnigmaResourceMap.end()) {
-      errStream << "Unsupported resource type: " << xmlNode.name() << std::endl;
-      return true;
+  // same as of saying LoadBackgrounds
+  bool LoadTilesets(pugi::xml_node& mapNode, std::string resType) {
+    pugi::xml_object_range<pugi::xml_named_node_iterator> tilesets = mapNode.children("tileset");
+    for(pugi::xml_node tileset : tilesets) {
+
+      buffers::TreeNode *folderNode;
+      if(resourceFolderRefs.find(resType) == resourceFolderRefs.end()) {
+        folderNode = nodes.back()->mutable_folder()->add_children();
+        std::string fixName = resType + 's'; // add an 's' at the end
+        fixName[0] -= 32; // make first character capital
+        folderNode->set_name(fixName);
+        resourceFolderRefs[resType] = folderNode;
+      }
+      else
+        folderNode = resourceFolderRefs[resType];
+
+      if(folderNode == NULL) {
+        std::cout << "Folder with name \""<< resType <<"\" not found" << std::endl;
+        return false;
+      }
+
+      std::string tilesetSrc = tileset.attribute("source").value();
+      int tilesetFirstGid = tileset.attribute("firstgid").as_int(-1);
+      if(!tilesetSrc.empty() && tilesetFirstGid != -1) {
+        std::string parentDirPath = tmxPath.parent_path().string()+"/";
+        std::string tsxPath = parentDirPath + tilesetSrc;
+
+        // load tsx xml
+        pugi::xml_document tilesetDoc;
+        if(!tilesetDoc.load_file(tsxPath.c_str())) {
+          std::cout << "Could not found tsx file with path: " << tsxPath << std::endl;
+          return false;
+        }
+
+        TSXTilesetLoader background_walker(tsxPath, nodes, folderNode);
+        tilesetDoc.traverse(background_walker);
+
+        tilesetIdSourcePathMap[tilesetFirstGid] = tilesetSrc;
+      }
+      else {
+        std::cout << "Fatal error: Tileset source or firstgid is invalid." << std::endl;
+        return false;
+      }
     }
 
-    std::string resType = tiledEnigmaResourceMap[xmlNode.name()];
-    std::string name = xmlNode.attribute("name").value();
-    // TODO: Remove this hack with "resource name generator"
+    return true;
+  }
+
+  // same as of saying LoadRoom
+  bool LoadMap(pugi::xml_node& mapNode, std::string resType) {
+    std::string name = mapNode.attribute("name").value();
+    // TODO: Remove this hack using "resource name generator"
     if(name.empty())
       name = resType+std::to_string(idx++);
 
@@ -58,44 +113,70 @@ private:
       return false;
     }
 
-    // only visit tileset/background in first iteration, when visited attribute is not set
-    if(resType == "background" && xmlNode.attribute("visited").as_bool() == false) {
-      // if image attribute exist then this tileset is contained inside a map and is referring to an external tileset
-      std::string tilesetSrc = xmlNode.attribute("source").value();
-      int tilesetFirstGid = xmlNode.attribute("firstgid").as_int(-1);
-      if(!tilesetSrc.empty() && tilesetFirstGid != -1) {
-        std::string parentDirPath = tmxPath.parent_path().string()+"/";
-        std::string tsxPath = parentDirPath + tilesetSrc;
+    buffers::TreeNode *resNode = folderNode->mutable_folder()->add_children();
+    resNode->set_name(name);
+    AddResource(resNode, resType, mapNode);
 
-        // load tsx xml
-        pugi::xml_document tilesetDoc;
-        if(!tilesetDoc.load_file(tsxPath.c_str())) {
-          std::cout << "Could not found tsx file with path: " << tsxPath << std::endl;
-          return false;
+    // correct width and height, convert from no. of tiles to no. of pixels
+    resNode->mutable_room()->set_width(resNode->room().width() * resNode->room().hsnap());
+    resNode->mutable_room()->set_height(resNode->room().height() * resNode->room().vsnap());
+
+    bool room_tilesOk = LoadObjects(mapNode, resNode);
+    if(!room_tilesOk){
+      std::cout << "Something went wrong while loading Room.Tiles" << std::endl;
+      return false;
+    }
+
+    return true;
+  }
+
+  // same as of saying LoadRoom.Tiles
+  bool LoadObjects(pugi::xml_node& mapNode, buffers::TreeNode *resNode){
+    pugi::xml_object_range<pugi::xml_named_node_iterator> objectGroups = mapNode.children("objectgroup");
+    // iterate over all objectGroups
+    for(pugi::xml_node objectGroupChild : objectGroups) {
+      // iterate over all children, which is most probably objects, create new tiles out of them
+      pugi::xml_object_range<pugi::xml_named_node_iterator> objects = objectGroupChild.children("object");
+      for(pugi::xml_node objectChild : objects) {
+
+        // let's update some not so directly corresponding fields
+        unsigned int gid = objectChild.attribute("gid").as_uint();
+        int localId = ConvertGlobalTileIdToLocal(gid);
+
+        // find the tileset which this tile belongs to
+        // using lower_bound( O(log(n)) ) to find first equal to or greater matching firstgid of a tileset
+        // then decrementing the iterator by one to get the first tileset with firstgid less than or equal to given
+        // local id of tile
+        std::map<int, std::string>::iterator itr = tilesetIdSourcePathMap.lower_bound(localId);
+        if(itr != tilesetIdSourcePathMap.begin()) {
+
+          buffers::resources::Room::Tile* tile = resNode->mutable_room()->add_tiles();
+          PackRes(objectChild, tile, "tiles/tile");
+
+          itr--;
+          localId = localId - itr->first;
+          std::cout<<"localId: "<<localId<<" and source "<<itr->second<<std::endl;
+          fs::path tilesetSourcePath(itr->second);
+          std::string backgroundName = tilesetSourcePath.stem().string() + "_" + std::to_string(localId);
+          tile->set_background_name(backgroundName);
+          tile->set_xoffset(0);
+          tile->set_yoffset(0);
+          tile->set_name(backgroundName+"_"+std::to_string(idx++));
+          tile->set_depth(0);
+          tile->set_xscale(1);
+          tile->set_yscale(1);
+          tile->set_color(2147483647);
         }
-
-        TSXTilesetLoader background_walker(tsxPath, nodes, folderNode, backgroundNameMsgPtrMap);
-        tilesetDoc.traverse(background_walker);
-
-        xmlNode.append_attribute("visited") = true;
-        tilesetIdSourcePathMap[tilesetFirstGid] = tilesetSrc;
-      }
-      else {
-        std::cout << "Fatal error: Tileset source or firstgid is invalid." << std::endl;
+        else {
+          std::cout<<"localId: "<<localId<<std::endl;
+        }
       }
     }
-    else if(resType == "room") {
-      buffers::TreeNode *resNode = folderNode->mutable_folder()->add_children();
-      resNode->set_name(name);
-      AddResource(resNode, resType, xmlNode);
 
-      // correct width and height, convert from no. of tiles to no. of pixels
-      resNode->mutable_room()->set_width(resNode->room().width() * resNode->room().hsnap());
-      resNode->mutable_room()->set_height(resNode->room().height() * resNode->room().vsnap());
+    return true;
+  }
 
-      xmlNode.append_attribute("visited") = true;
-    }
-
+  virtual bool for_each(pugi::xml_node &xmlNode){
     return true;
   }
 
@@ -129,7 +210,7 @@ private:
     errStream << "Unsupported resource type: " << resType << " " << xmlNode.value() << std::endl;
   }
 
-  void PackRes(pugi::xml_node &xmlNode, google::protobuf::Message *m, const std::string& resType, std::string fieldPrefix = "") {
+  void PackRes(pugi::xml_node &xmlNode, google::protobuf::Message *m, const std::string& resType) {
     const google::protobuf::Descriptor *desc = m->GetDescriptor();
     const google::protobuf::Reflection *refl = m->GetReflection();
 
@@ -142,72 +223,22 @@ private:
       const google::protobuf::FieldOptions opts = field->options();
 
       // tmx_option_string is used to fetch correct attribute from current xml node
-      std::string tsxPropertyName = fieldPrefix + opts.GetExtension(buffers::tmx);
+      std::string tsxPropertyName = opts.GetExtension(buffers::tmx);
 
       pugi::xml_attribute attr;
 
-      // TODO: needs improvement
-      // if forward slash '/' is found in tmx_option_string then correct attribute is present in child node
-      // for example in Background.proto fields with options [(tmx) = "image/source"] has a forward
-      // slash in it and it indicates that in .tsx file, source should be fetched from child image node
-      std::size_t splitPos = tsxPropertyName.find("/");
-      if(splitPos != std::string::npos) {
-        const std::string childNodeName = tsxPropertyName.substr(0, splitPos);
-        const std::string childNodeAttrName = tsxPropertyName.substr(splitPos + 1);
-        pugi::xml_node child = xmlNode.child(childNodeName.c_str());
-        if(!child.empty())
-          attr = child.attribute(childNodeAttrName.c_str());
-      }
-      // otherwise get the attribute directly
-      else {
-        attr = xmlNode.attribute(tsxPropertyName.c_str());
-      }
+      attr = xmlNode.attribute(tsxPropertyName.c_str());
 
-//      if(attr.empty())
-//        continue;
-
-      if(field->name() == "id"){
+      if(field->name() == "id") {
         // not sure what Getextension returns in cases when id_start extension is absent
         int id = opts.GetExtension(buffers::id_start) + resourceTypeIdCountMap[m->GetTypeName()]++;
         outStream << "Setting " << field->name() << " (" << field->type_name() << ") as " << id << std::endl;
         refl->SetInt32(m, field, id);
       }
-      else if(field->is_repeated()) {
-        switch(field->cpp_type()) {
-          case CppType::CPPTYPE_MESSAGE: {
-            // only visit repeated message "tiles/tile" when first traversal is done, as we need to create backgrounds before referencing them
-
-            if(resType == "room" && xmlNode.attribute("visited").as_bool() == true && field->name() == "tiles") {
-              std::cout<<"Repeated fields as a message: "<<field->name()<<std::endl;
-
-              pugi::xml_object_range<pugi::xml_named_node_iterator> objectGroups = xmlNode.children("objectgroup");
-              // iterate over all objectGroup
-              for(pugi::xml_node objectGroupChild : objectGroups) {
-                // iterate over all children, which is most probably objects, create new tiles out of them
-                pugi::xml_object_range<pugi::xml_named_node_iterator> objects = objectGroupChild.children("object");
-                for(pugi::xml_node objectChild : objects) {
-                  google::protobuf::Message *msg = refl->AddMessage(m, field);
-                  PackRes(objectChild, msg, "tiles/tile");
-
-                  // let's update some important values
-                  buffers::resources::Room::Tile *tile = static_cast<buffers::resources::Room::Tile *>(msg);
-                  unsigned int gid = objectChild.attribute("gid").as_uint();
-                  int localId = ConvertGlobalTileIdToLocal(gid);
-                  std::cout<<"localId: "<<localId<<std::endl;
-                  tile->set_background_name("");
-                  tile->set_xoffset(0);
-                  tile->set_yoffset(0);
-                }
-              }
-            }
-            break;
-          }
-        }
-      }
-      else if(xmlNode.attribute("visited").as_bool() == false && !attr.empty()) {
+      else if(!attr.empty()) {
         switch (field->cpp_type()) {
           case CppType::CPPTYPE_MESSAGE: {
-            google::protobuf::Message *msg = refl->MutableMessage(m, field);
+            // google::protobuf::Message *msg = refl->MutableMessage(m, field);
             // TODO: handle message case
             // PackRes(resMap, dir, ids, child, msg, depth + 1);
             break;
@@ -271,10 +302,11 @@ private:
     const unsigned int ROTATED_HEXAGONAL_120_FLAG = 0X10000000; // ignored in ortho or iso maps, indicates 120 deg clock rot in hexagonal maps
 
     unsigned int tileId = globalTileId;
-    bool flippedHorizontally = tileId & FLIPPED_HORIZONTALLY_FLAG;
+    // uncomment when required
+    /*bool flippedHorizontally = tileId & FLIPPED_HORIZONTALLY_FLAG;
     bool flippedVertically = tileId & FLIPPED_VERTICALLY_FLAG;
     bool flippedDiagonally = tileId & FLIPPED_DIAGONALLY_FLAG;
-    bool rotatedHex120 = tileId & ROTATED_HEXAGONAL_120_FLAG;
+    bool rotatedHex120 = tileId & ROTATED_HEXAGONAL_120_FLAG;*/
 
     tileId &= ~(FLIPPED_HORIZONTALLY_FLAG | FLIPPED_VERTICALLY_FLAG | FLIPPED_DIAGONALLY_FLAG | ROTATED_HEXAGONAL_120_FLAG);
 
@@ -291,17 +323,19 @@ std::unique_ptr<buffers::Project> TMXFileFormat::LoadProject(const fs::path& fPa
   std::unique_ptr<buffers::Project> proj = std::make_unique<buffers::Project>();
   buffers::Game* game = proj->mutable_game();
 
-  TMXMapLoader map_walker(game->mutable_root(), fPath);
-  // traverse the doc twice, first to fetch all individual tiled elements,
-  // second to use the loaded elements and filling up room messages making a use of them
-  doc.traverse(map_walker);
-  doc.traverse(map_walker);
+  TMXMapLoader mapWalker(game->mutable_root(), fPath);
 
-  std::string str;
+  bool success = mapWalker.Load(doc);
+  if(!success){
+    std::cout<<"Error occured while loading selected Tiled map"<<std::endl;
+    return NULL;
+  }
+
+  /*std::string str;
   google::protobuf::TextFormat::PrintToString(proj->game(), &str);
   std::ofstream out("/home/kash/github/radialgm-stuff/textProtos/test1tmx.txt");
   out<<str<<std::endl;
-  out.close();
+  out.close();*/
 
   std::cout<<"END LOADING TMX"<<std::endl;
   return proj;
