a85d177b17edd87836a9b2c998a21bd419b493f7
KartikShrivastava <shrivastavakartik19@gmail.com>
2022-07-07 23:18:06 +0530

    Fix naming of base64 encoder/decoder in shared

--- shared/libbase64_util/libbase64_util.h
+++ shared/libbase64_util/libbase64_util.h
@@ -3,8 +3,8 @@
 
 #include <string>
 
-bool libbase64_is_base64(unsigned char byte);
-std::string libbase64_encode(std::string const& str);
-std::string libbase64_decode(std::string const& str);
+bool is_base64(unsigned char byte);
+std::string base64_encode(std::string const& str);
+std::string base64_decode(std::string const& str);
 
 #endif // LIBBASE64_UTIL_H
