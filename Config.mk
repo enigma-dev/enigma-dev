590d73310e136bb2bdf60b5ced1f106376e586f9
Samuel Venable <samuelvenable@hotmail.com>
2022-07-20 13:31:57 -0400

    Update Config.mk

--- Config.mk
+++ Config.mk
@@ -57,10 +57,10 @@ LDFLAGS := -g
 
 # MacPorts include and lib folders
 ifeq ($(OS), Darwin)
-	CXXFLAGS += -I/opt/local/include
-	CFLAGS   += -I/opt/local/include
-	LDFLAGS  += -L/opt/local/lib
-	LDFLIBS  += -L/opt/local/lib
+	CXXFLAGS += -I/opt/local/include -I/usr/local/include
+	CFLAGS   += -I/opt/local/include -I/usr/local/include
+	LDFLAGS  += -L/opt/local/lib -L/usr/local/lib
+	LDFLIBS  += -L/opt/local/lib -L/usr/local/lib
 endif
 
 # FreeBSD include and lib folders
