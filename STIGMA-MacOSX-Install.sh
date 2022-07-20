30573ec2302515a51580f6862d29830989c41621
Samuel Venable <samuelvenable@hotmail.com>
2022-07-20 13:10:24 -0400

    Add files via upload

--- STIGMA-MacOSX-Install.sh
+++ STIGMA-MacOSX-Install.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 sudo xcode-select --install
 if [ -f "/opt/local/bin/port" ]; then
-  sudo port -N install curl wget gdb gmake cmake pkgconf flac opusfile mpg123 libmodplug libsndfile protobuf-c glew glm libpng libGLU libvpx libogg libsndfile libvorbis vorbis-tools box2D libsdl2 libsdl2_mixer freetype libffi openjdk8 jna rapidjson libyaml boost pugixml yaml-cpp grpc libdumb
+  sudo port -N install curl wget gdb gmake cmake pkgconf protobuf-c glew glm libpng libGLU libvpx flac opusfile mpg123 libmodplug box2D freetype libffi openjdk8 jna rapidjson libyaml boost pugixml yaml-cpp grpc
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew install libsndfile libogg libsndfile libvorbis vorbis-tools sdl2 sdl2_mixer dumb
   sudo git clone --recurse-submodules -j8 https://github.com/time-killer-games/stigma-dev /Applications/stigma-dev
   cd /Applications/stigma-dev
   sudo env CC=clang CXX=clang++ gmake && sudo env CC=clang CXX=clang++ gmake emake
   sudo chmod +x ./install.sh
   sudo ./install.sh
   sudo chmod +x ./start.sh
+  sudo port -f deactivate libiconv
   echo "Installation Complete! You may now run the \"/Applications/stigma-dev/start.sh\" script from the terminal app!"
 else
   echo "Macports installation not found! Please download and install Macports first from www.macports.org"
--- STIGMA-MacOSX-Uninstall.sh
+++ STIGMA-MacOSX-Uninstall.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo rm -fr /Applications/stigma-dev
-echo "Uninstallation Complete! Uninstallation of Macports must be done separately!"
+echo "Uninstallation Complete! Uninstallation of Macports and Homebrew must be done separately!"
