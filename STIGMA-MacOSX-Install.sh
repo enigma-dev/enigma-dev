7530a900a3c556d1c6a60c795e37f41fa1feea3a
Samuel Venable <samuelvenable@hotmail.com>
2022-07-28 22:39:10 +0000

    Add files via upload

--- STIGMA-MacOSX-Install.sh
+++ STIGMA-MacOSX-Install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 sudo xcode-select --install
 if [ -f "/opt/local/bin/port" ]; then
-  sudo port -N install protobuf-c glew glm libpng libvpx box2D freetype libffi rapidjson libyaml pugixml yaml-cpp grpc
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   brew install curl wget gdb make cmake pkgconf flac opusfile mpg123 libmodplug libsndfile libogg libsndfile libvorbis vorbis-tools sdl2 sdl2_mixer dumb boost
+  sudo port -N install protobuf-c glew glm libpng libvpx box2D freetype libffi rapidjson libyaml pugixml yaml-cpp grpc
+  sudo port -N uninstall libiconv
   sudo git clone --recurse-submodules -j8 https://github.com/time-killer-games/stigma-dev /Applications/stigma-dev
   cd /Applications/stigma-dev
   sudo env CC=clang CXX=clang++ gmake && sudo env CC=clang CXX=clang++ gmake emake
