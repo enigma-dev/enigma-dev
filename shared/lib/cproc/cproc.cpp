57fd2608e6cd15121cbbbbebff74374414710f2c
Samuel Venable <samuelvenable@hotmail.com>
2022-12-14 21:42:12 -0500

    Update cproc.cpp

--- shared/lib/cproc/cproc.cpp
+++ shared/lib/cproc/cproc.cpp
@@ -3,7 +3,6 @@
  MIT License
  
  Copyright © 2021-2022 Samuel Venable
- Copyright © 2021 Lars Nilsson
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
