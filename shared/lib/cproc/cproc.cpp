1b7972c345d6b6007c5216609086c32840b6bfd9
Samuel Venable <samuelvenable@hotmail.com>
2023-02-05 10:50:05 -0500

    Update cproc.cpp

--- shared/lib/cproc/cproc.cpp
+++ shared/lib/cproc/cproc.cpp
@@ -113,12 +113,16 @@ namespace {
   }
 
   std::wstring widen(std::string str) {
-    std::size_t wchar_count = str.size() + 1; std::vector<wchar_t> buf(wchar_count);
+    if (str.empty()) return L"";
+    std::size_t wchar_count = str.size() + 1; 
+    std::vector<wchar_t> buf(wchar_count);
     return std::wstring { buf.data(), (std::size_t)MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, buf.data(), (int)wchar_count) };
   }
 
   std::string narrow(std::wstring wstr) {
-    int nbytes = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), (int)wstr.length(), nullptr, 0, nullptr, nullptr); std::vector<char> buf(nbytes);
+    if (wstr.empty()) return "";
+    int nbytes = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), (int)wstr.length(), nullptr, 0, nullptr, nullptr); 
+    std::vector<char> buf(nbytes);
     return std::string { buf.data(), (std::size_t)WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), (int)wstr.length(), buf.data(), nbytes, nullptr, nullptr) };
   }
   #endif
