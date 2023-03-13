c7da52170338f3070f0ead7430c08e41853ab248
Samuel Venable <samuelvenable@hotmail.com>
2023-03-12 21:20:24 -0400

    Update imguial_msgbox.h

--- ENIGMAsystem/SHELL/Widget_Systems/SDL/msgbox/imguial_msgbox.h
+++ ENIGMAsystem/SHELL/Widget_Systems/SDL/msgbox/imguial_msgbox.h
@@ -1,7 +1,8 @@
 /*
-The MIT License (MIT)
 
-Copyright (c) 2016 Andre Leiradella
+MIT License
+
+Copyright © 2016 Andre Leiradella
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -20,6 +21,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
 */
 
 #pragma once
