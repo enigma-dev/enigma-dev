51ee9a0b569c85d5c70c5906da1cc1af7552d887
Samuel Venable <samuelvenable@hotmail.com>
2023-03-12 22:24:17 -0400

    Update imguial_msgbox.cpp

--- ENIGMAsystem/SHELL/Widget_Systems/SDL/msgbox/imguial_msgbox.cpp
+++ ENIGMAsystem/SHELL/Widget_Systems/SDL/msgbox/imguial_msgbox.cpp
@@ -39,7 +39,7 @@ void AlignForWidth(float width, float alignment = 0.5f)
   float cursor_pos = ImGui::GetCursorPosY();
   int text_height = ImGui::GetFontSize() * 1;
   cursor_pos += text_height + style.FramePadding.y * 2;
-  ImGui::SetCursorPosY(ImGui::GetContentRegionAvail().y - cursor_pos);
+  ImGui::SetCursorPosY(ImGui::GetContentRegionAvail().y + cursor_pos);
 }
 
 bool ImGuiAl::MsgBox::Init( const char* title, const char* icon, const char* text, std::vector<std::string> captions, bool show_checkbox )
