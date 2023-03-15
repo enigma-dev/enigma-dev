5501e7e547983f96405fd904c6dc52399b5f492a
Samuel Venable <samuelvenable@hotmail.com>
2023-03-14 21:29:37 -0400

    Update WSdialogs.cpp

--- ENIGMAsystem/SHELL/Widget_Systems/General/WSdialogs.cpp
+++ ENIGMAsystem/SHELL/Widget_Systems/General/WSdialogs.cpp
@@ -1,5 +1,4 @@
 #include "Widget_Systems/widgets_mandatory.h"
-#include "Widget_Systems/General/WSdialogs.h"
 #include "Platforms/General/PFexternals.h"
 #include "Platforms/General/PFwindow.h"
 #include "Universal_System/estring.h"
