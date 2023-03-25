3e75d2dbd329d4279f810101aba6fae828e4dc16
Samuel Venable <samuelvenable@hotmail.com>
2023-03-25 12:49:26 -0400

    Update tempdir.cpp

--- shared/tempdir/tempdir.cpp
+++ shared/tempdir/tempdir.cpp
@@ -1,17 +1,11 @@
 #include <string>
 
-#include "lib/cproc/cproc.hpp"
+#include "apifilesystem/filesystem.hpp"
 
 namespace enigma_user {
 
 std::string get_temp_directory() { 
-  std::string result = ::ngs::cproc::directory_get_temporary_path();
-  #if defined(_WIN32)
-  if (!result.empty() && result.back() != '/' && result.back() != '\\') result.push_back('\\');
-  #else
-  if (!result.empty() && result.back() != '/') result.push_back('/');
-  #endif
-  return result;
+  return ngs::fs::directory_get_temporary_path();
 }
 
 } // namespace enigma_user
