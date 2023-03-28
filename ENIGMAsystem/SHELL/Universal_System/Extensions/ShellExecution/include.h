5856346a5dfe4d1bb5f24be4325f47900f32d027
Samuel Venable <samuelvenable@hotmail.com>
2023-03-27 22:23:54 -0400

    Update include.h

--- ENIGMAsystem/SHELL/Universal_System/Extensions/ShellExecution/include.h
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/ShellExecution/include.h
@@ -1,3 +1,4 @@
+#include "apiprocess/process.hpp"
 #include "Universal_System/Extensions/ShellExecution/shell.h"
 #include "Universal_System/Extensions/ShellExecution/xprocess.hpp"
 #include "Platforms/platforms_mandatory.h"
