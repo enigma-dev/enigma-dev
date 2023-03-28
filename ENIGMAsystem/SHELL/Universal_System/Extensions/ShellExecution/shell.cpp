0b56eba79c91c1653e329d67a5ea7d9525b1582d
Samuel Venable <samuelvenable@hotmail.com>
2023-03-27 22:02:35 -0400

    Update shell.cpp

--- ENIGMAsystem/SHELL/Universal_System/Extensions/ShellExecution/shell.cpp
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/ShellExecution/shell.cpp
@@ -32,10 +32,8 @@
 #include <cstdlib>
 
 #include "shell.h"
+#include "xprocess.h"
 #include "strings_util.h"
-
-#include "Platforms/General/PFmain.h"
-#include "Widget_Systems/widgets_mandatory.h"
 
 #if (defined(__linux__) && !defined(__ANDROID__)) || (defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__))
 #include <X11/Xlib.h>
