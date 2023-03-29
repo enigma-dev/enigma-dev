6601beeea779208c99ade977df4bc685628059cb
Samuel Venable <samuelvenable@hotmail.com>
2023-03-28 21:22:11 -0400

    Copyright devKathy's Suspend/Resume Code

--- shared/apiprocess/process.cpp
+++ shared/apiprocess/process.cpp
@@ -3,6 +3,7 @@
  MIT License
  
  Copyright © 2021-2023 Samuel Venable
+ Copyright © 2021-2023 devKathy
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
