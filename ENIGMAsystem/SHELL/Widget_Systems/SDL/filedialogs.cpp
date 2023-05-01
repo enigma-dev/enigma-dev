1740223ddddaf80c8a2a9538278893cebc20b35f
Samuel Venable <samuelvenable@hotmail.com>
2023-05-01 16:40:21 -0400

    Update filedialogs.cpp

--- ENIGMAsystem/SHELL/Widget_Systems/SDL/filedialogs.cpp
+++ ENIGMAsystem/SHELL/Widget_Systems/SDL/filedialogs.cpp
@@ -113,7 +113,7 @@ namespace {
     if (icon == nullptr)
       icon = LoadIconW((HINSTANCE)GetWindowLongPtrW(hWnd, GWLP_HINSTANCE), MAKEINTRESOURCEW(0));
     if (icon == nullptr)
-      icon = LoadIconW(nullptr, IDI_APPLICATION);
+      icon = LoadIconW(nullptr, MAKEINTRESOURCEW(32512));
     return icon;
   }
   #endif
