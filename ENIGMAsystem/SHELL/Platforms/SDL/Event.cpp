a6f9abf20c427573ba10da5188c668060f1a4aef
Samuel Venable <samuelvenable@hotmail.com>
2023-05-30 16:22:30 -0400

    Update Event.cpp

--- ENIGMAsystem/SHELL/Platforms/SDL/Event.cpp
+++ ENIGMAsystem/SHELL/Platforms/SDL/Event.cpp
@@ -19,7 +19,6 @@ int SDL_Event_Handler::processEvents() {
   SDL_Event e;
 
   while (SDL_PollEvent(&e)) {
-    enigma::compute_window_size();
     switch (e.type) {
       case SDL_WINDOWEVENT:
         windowEvent(&e);
@@ -130,6 +129,7 @@ int SDL_Event_Handler::processEvents() {
         //dropEnd(&e);
         break;
 #endif
+      enigma::compute_window_size();
     }
   }
 
