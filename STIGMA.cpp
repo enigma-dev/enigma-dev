0977f6a32f31f8c6a2f15d580446101913f66f47
Samuel Venable <samuelvenable@hotmail.com>
2023-06-03 02:50:17 -0400

    Update STIGMA.cpp

--- STIGMA.cpp
+++ STIGMA.cpp
@@ -24,18 +24,17 @@ int main() {
   ngs::fs::directory_set_current_working(ngs::fs::executable_get_directory());
   std::string dir = ngs::fs::executable_get_directory();
   std::replace(dir.begin(), dir.end(), '\\', '/');
-  ngs::fs::environment_set_variable("PATH", (InstDir() + "\\msys64;" + InstDir() + "\\msys64\\mingw64\\bin;" + InstDir() + "\\msys64\\usr\\local\\bin;" + InstDir() + "\\msys64\\usr\\bin;%PATH%").c_str());
   if (ngs::fs::file_exists(ngs::fs::executable_get_directory() + "LateralGM-Windows-i686.exe")) {
+    ngs::fs::environment_set_variable("PATH", (InstDir() + "\\msys64;" + InstDir() + "\\msys64\\mingw32\\bin;" + InstDir() + "\\msys64\\usr\\local\\bin;" + InstDir() + "\\msys64\\usr\\bin;%PATH%").c_str());
     pid = ngs::ps::spawn_child_proc_id("\"" + dir + "LateralGM-Windows-i686.exe\"", false);
-    ngs::ps::proc_id_kill(pid);
   } else if (ngs::fs::file_exists(ngs::fs::executable_get_directory() + "LateralGM-Windows-x86_64.exe")) {
+    ngs::fs::environment_set_variable("PATH", (InstDir() + "\\msys64;" + InstDir() + "\\msys64\\mingw64\\bin;" + InstDir() + "\\msys64\\usr\\local\\bin;" + InstDir() + "\\msys64\\usr\\bin;%PATH%").c_str());
     pid = ngs::ps::spawn_child_proc_id("\"" + dir + "LateralGM-Windows-x86_64.exe\"", false);
   }
   if (pid) {
     while (!ngs::ps::child_proc_id_is_complete(pid)) {
       printf("%s", ngs::ps::read_from_stdout_for_child_proc_id(pid));
       std::this_thread::sleep_for(std::chrono::seconds(1));
-      system("cls");
     }
     ngs::ps::free_stdout_for_child_proc_id(pid);
     ngs::ps::free_stdin_for_child_proc_id(pid);
