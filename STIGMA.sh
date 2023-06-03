6f20331c932f89ad66c192a768935a395a6966b3
Samuel Venable <samuelvenable@hotmail.com>
2023-06-03 05:15:50 -0400

    Update STIGMA.sh

--- STIGMA.sh
+++ STIGMA.sh
@@ -2,4 +2,3 @@ cd "${0%/*}"
 windres "${0%/*}/Resources.rc" "${0%/*}/Resources.o"
 g++ -c "${0%/*}/STIGMA.cpp" "${0%/*}/shared/apifilesystem/filesystem.cpp" "${0%/*}/shared/apiprocess/process.cpp" -std=c++17 -m64
 g++ "${0%/*}/STIGMA.o" "${0%/*}/filesystem.o" "${0%/*}/process.o" "${0%/*}/Resources.o" -o "${0%/*}/STIGMA.exe" -std=c++17 -m64 -static -lshell32 -lole32 -luuid -lntdll -Wl,--subsystem,windows
-rm "${0%/*}/*.o"
