fcbf45a31842d9366a196f4cc7892f19f5195b3e
Samuel Venable <samuelvenable@hotmail.com>
2023-06-03 02:00:06 -0400

    Update STIGMA.cpp

--- STIGMA.cpp
+++ STIGMA.cpp
@@ -1,4 +1,6 @@
 #include <algorithm>
+#include <thread>
+#include <chrono>
 
 #include "shared/apifilesystem/filesystem.hpp"
 #include "shared/apiprocess/process.hpp"
@@ -9,9 +11,11 @@ int main() {
   std::replace(dir.begin(), dir.end(), '\\', '/');
   if (ngs::fs::file_exists(ngs::fs::executable_get_directory() +  + "LateralGM-Windows-i686.exe")) {
     unsigned long pid = ngs::ps::spawn_child_proc_id("cmd /c start \"\" ..\\msys64\\msys2_shell.cmd -defterm -mingw32 -no-start -here -lc \"" + dir +  + "LateralGM-Windows-i686.exe\"", false);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
     ngs::ps::proc_id_kill(pid);
   } else if (ngs::fs::file_exists(ngs::fs::executable_get_directory() +  + "LateralGM-Windows-x86_64.exe")) {
     unsigned long pid = ngs::ps::spawn_child_proc_id("cmd /c start \"\"  ..\\msys64\\msys2_shell.cmd -defterm -mingw64 -no-start -here -lc \"" + dir +  + "LateralGM-Windows-x86_64.exe\"", false);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
     ngs::ps::proc_id_kill(pid);
   }
   return 0;
