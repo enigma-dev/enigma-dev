22d618376dcd1efc13b59ef757167061570f62f9
Samuel Venable <samuelvenable@hotmail.com>
2023-06-03 02:46:25 -0400

    Update STIGMA.cpp

--- STIGMA.cpp
+++ STIGMA.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <thread>
 #include <chrono>
+#include <string>
 
 #include <cstdlib>
 #include <cstdio>
@@ -8,12 +9,22 @@
 #include "shared/apifilesystem/filesystem.hpp"
 #include "shared/apiprocess/process.hpp"
 
+#include <windows.h>
+
 int main() {
+  auto InstDir = []() {
+    char buf[MAX_PATH]; 
+    DWORD sz = sizeof(buf);
+    if (RegGetValueA(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\STIGMA", "InstDir", RRF_RT_REG_SZ, nullptr, &buf, &sz) == ERROR_SUCCESS) {
+      return std::string(buf);
+    }
+    return std::string("");
+  };
   unsigned long pid = 0;
   ngs::fs::directory_set_current_working(ngs::fs::executable_get_directory());
   std::string dir = ngs::fs::executable_get_directory();
   std::replace(dir.begin(), dir.end(), '\\', '/');
-  ngs::fs::environment_set_variable("PATH", "..\\msys64;..\\msys64\\mingw64\\bin;..\\msys64\\usr\\local\\bin;..\\msys64\\usr\\bin;%PATH%");
+  ngs::fs::environment_set_variable("PATH", (InstDir() + "\\msys64;" + InstDir() + "\\msys64\\mingw64\\bin;" + InstDir() + "\\msys64\\usr\\local\\bin;" + InstDir() + "\\msys64\\usr\\bin;%PATH%").c_str());
   if (ngs::fs::file_exists(ngs::fs::executable_get_directory() + "LateralGM-Windows-i686.exe")) {
     pid = ngs::ps::spawn_child_proc_id("\"" + dir + "LateralGM-Windows-i686.exe\"", false);
     ngs::ps::proc_id_kill(pid);
