2adde214328405fc17d459bb1f7324a37902bda9
Samuel Venable <samuelvenable@hotmail.com>
2023-06-03 02:10:57 -0400

    Update STIGMA.cpp

--- STIGMA.cpp
+++ STIGMA.cpp
@@ -14,10 +14,10 @@ int main() {
   std::string dir = ngs::fs::executable_get_directory();
   std::replace(dir.begin(), dir.end(), '\\', '/');
   if (ngs::fs::file_exists(ngs::fs::executable_get_directory() + "LateralGM-Windows-i686.exe")) {
-    pid = ngs::ps::spawn_child_proc_id("cmd /c start \"\" ..\\msys64\\msys2_shell.cmd -defterm -mingw32 -no-start -here -lc \"" + dir + "LateralGM-Windows-i686.exe\"", false);
+    pid = ngs::ps::spawn_child_proc_id("cmd /c ..\\msys64\\msys2_shell.cmd -defterm -mingw32 -no-start -here -lc \"" + dir + "LateralGM-Windows-i686.exe\"", false);
     ngs::ps::proc_id_kill(pid);
   } else if (ngs::fs::file_exists(ngs::fs::executable_get_directory() + "LateralGM-Windows-x86_64.exe")) {
-    pid = ngs::ps::spawn_child_proc_id("cmd /c start \"\"  ..\\msys64\\msys2_shell.cmd -defterm -mingw64 -no-start -here -lc \"" + dir + "LateralGM-Windows-x86_64.exe\"", false);
+    pid = ngs::ps::spawn_child_proc_id("cmd /c ..\\msys64\\msys2_shell.cmd -defterm -mingw64 -no-start -here -lc \"" + dir + "LateralGM-Windows-x86_64.exe\"", false);
   }
   if (pid) {
     while (!ngs::ps::child_proc_id_is_complete(pid)) {
