6ee7920b3b9ddd45e15585174f33aa3c7bdc7696
Samuel Venable <samuelvenable@hotmail.com>
2023-06-03 02:07:13 -0400

    Update STIGMA.cpp

--- STIGMA.cpp
+++ STIGMA.cpp
@@ -2,21 +2,31 @@
 #include <thread>
 #include <chrono>
 
+#inlcude <cstdlib>
+#include <cstdio>
+
 #include "shared/apifilesystem/filesystem.hpp"
 #include "shared/apiprocess/process.hpp"
 
-int main() {  
+int main() {
+  unsigned long pid = 0;
   ngs::fs::directory_set_current_working(ngs::fs::executable_get_directory());
   std::string dir = ngs::fs::executable_get_directory();
   std::replace(dir.begin(), dir.end(), '\\', '/');
-  if (ngs::fs::file_exists(ngs::fs::executable_get_directory() +  + "LateralGM-Windows-i686.exe")) {
-    unsigned long pid = ngs::ps::spawn_child_proc_id("cmd /c start \"\" ..\\msys64\\msys2_shell.cmd -defterm -mingw32 -no-start -here -lc \"" + dir +  + "LateralGM-Windows-i686.exe\"", false);
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  if (ngs::fs::file_exists(ngs::fs::executable_get_directory() + "LateralGM-Windows-i686.exe")) {
+    pid = ngs::ps::spawn_child_proc_id("cmd /c start \"\" ..\\msys64\\msys2_shell.cmd -defterm -mingw32 -no-start -here -lc \"" + dir + "LateralGM-Windows-i686.exe\"", false);
     ngs::ps::proc_id_kill(pid);
-  } else if (ngs::fs::file_exists(ngs::fs::executable_get_directory() +  + "LateralGM-Windows-x86_64.exe")) {
-    unsigned long pid = ngs::ps::spawn_child_proc_id("cmd /c start \"\"  ..\\msys64\\msys2_shell.cmd -defterm -mingw64 -no-start -here -lc \"" + dir +  + "LateralGM-Windows-x86_64.exe\"", false);
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    ngs::ps::proc_id_kill(pid);
+  } else if (ngs::fs::file_exists(ngs::fs::executable_get_directory() + "LateralGM-Windows-x86_64.exe")) {
+    pid = ngs::ps::spawn_child_proc_id("cmd /c start \"\"  ..\\msys64\\msys2_shell.cmd -defterm -mingw64 -no-start -here -lc \"" + dir + "LateralGM-Windows-x86_64.exe\"", false);
+  }
+  if (pid) {
+    while (!ngs::ps::child_proc_id_is_complete(pid)) {
+      printf("%s", read_from_stdout_for_child_proc_id(pid));
+      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+      system("cls");
+    }
+    ngs::ps::free_stdout_for_child_proc_id(pid);
+    ngs::ps::free_stdin_for_child_proc_id(pid);
   }
   return 0;
 }
