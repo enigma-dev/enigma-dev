ecb78987f176e176921b2b8e12595eb6b02b4d07
Samuel Venable <samuelvenable@hotmail.com>
2023-06-03 02:07:41 -0400

    Update STIGMA.cpp

--- STIGMA.cpp
+++ STIGMA.cpp
@@ -21,7 +21,7 @@ int main() {
   }
   if (pid) {
     while (!ngs::ps::child_proc_id_is_complete(pid)) {
-      printf("%s", read_from_stdout_for_child_proc_id(pid));
+      printf("%s", ngs::ps::read_from_stdout_for_child_proc_id(pid));
       std::this_thread::sleep_for(std::chrono::milliseconds(1000));
       system("cls");
     }
