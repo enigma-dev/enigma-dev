3350b138dc8c59593f684a89cd8c5b0cf1529f8d
= <=>
2023-06-05 15:36:21 +0300

    fixed exceptions wall

--- ENIGMAsystem/SHELL/Universal_System/Extensions/Steamworks/general.cpp
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/Steamworks/general.cpp
@@ -3,13 +3,7 @@
 namespace enigma_user {
 
 bool steam_initialised() {
-  try {
-    Steamworks::SteamClient::init( k_uAppIdInvalid ); // Replace "k_uAppIdInvalid" with your app id
-    return true;
-  } catch (const std::exception& e) {
-    DEBUG_MESSAGE(e.what(), M_ERROR);
-  }
-  return false;
+  return steamworks::steam_client::init(k_uAppIdInvalid);  // Replace "k_uAppIdInvalid" with your app id
 }
 
 }  // namespace enigma_user
--- ENIGMAsystem/SHELL/Universal_System/Extensions/Steamworks/wrapper/steam_client.cpp
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/Steamworks/wrapper/steam_client.cpp
@@ -1,22 +1,51 @@
 #include "steam_client.h"
 
-namespace Steamworks {
+namespace steamworks {
 
-void SteamClient::init(unsigned int appid) {
-  if (SteamClient::initialized_) throw std::exception("Calling SteamClient::init but is already initialized");
+bool steam_client::initialised_{false};  // TODO: Why this line is nessesary?
 
-  if (!SteamMain::init()) {
-    throw std::exception(
-        "SteamApi_Init returned false. Steam isn't running, couldn't find Steam, App ID is ureleased, Don't own App "
-        "ID.");
+bool steam_client::init(unsigned int appid) {
+  if (steam_client::initialised_) {
+    DEBUG_MESSAGE("Calling steam_client::init but is already initialized", M_ERROR);
+    return false;
   }
 
-  SteamClient::initialized_ = true;
+  if (!steam_main::init()) {
+    DEBUG_MESSAGE(
+        "SteamApi_Init returned false. Steam isn't running, couldn't find Steam, App ID is ureleased, Don't own App "
+        "ID.",
+        M_ERROR);
+    return false;
+  }
+
+  steam_client::initialised_ = true;
+  return true;
 }
 
-/// <summary>
-/// Check if Steam is loaded and accessible.
-/// </summary>
-bool SteamClient::is_valid() { return SteamClient::initialized_; }
+bool steam_client::shutdown() {
+  if (!is_valid) {
+    DEBUG_MESSAGE("Calling steam_client::shutdown but not initialized, consider calling steam_client::init first",
+                  M_ERROR);
+    return false;
+  }
 
-}  // namespace Steamworks
+  steam_client::initialised_ = false;
+
+  steam_main::shutdown();
+
+  return true;
+}
+
+bool steam_client::is_valid() { return steam_client::initialised_; }
+
+bool steam_client::is_logged_on() { return SteamUser()->BLoggedOn(); }
+
+CSteamID steam_client::steam_id() { return SteamUser()->GetSteamID(); }
+
+const char* steam_client::name() { return SteamFriends()->GetPersonaName(); }
+
+EPersonaState steam_client::state() { return SteamFriends()->GetPersonaState(); }
+
+bool steam_client::restart_app_if_necessary(unsigned int appid) { return steam_main::restart_app_if_necessary(appid); }
+
+}  // namespace steamworks
