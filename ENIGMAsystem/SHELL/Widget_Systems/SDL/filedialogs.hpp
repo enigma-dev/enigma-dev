076c96ddbb450510f98acb838a5b139692edeed1
Samuel Venable <samuelvenable@hotmail.com>
2023-06-11 17:13:21 -0400

    Update filedialogs.hpp

--- ENIGMAsystem/SHELL/Widget_Systems/SDL/filedialogs.hpp
+++ ENIGMAsystem/SHELL/Widget_Systems/SDL/filedialogs.hpp
@@ -2,7 +2,7 @@
 
  MIT License
 
- Copyright © 2021-2022 Samuel Venable
+ Copyright © 2021-2023 Samuel Venable
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
