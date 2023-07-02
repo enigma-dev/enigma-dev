349c42e90f2c4d6e9aff08b506717bf96c18c716
Saif Kandil <74428638+k0T0z@users.noreply.github.com>
2023-07-02 15:19:14 +0300

    bug fixes

--- ENIGMAsystem/SHELL/Universal_System/Extensions/Steamworks/overlay.h
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/Steamworks/overlay.h
@@ -5,6 +5,18 @@
 
 namespace enigma_user {
 
+const unsigned ov_friends = 0;
+const unsigned ov_community = 1;
+const unsigned ov_players = 2;
+const unsigned ov_settings = 3;
+const unsigned ov_gamegroup = 4;
+const unsigned ov_achievements = 5;
+
+const unsigned steam_overlay_notification_position_top_left = 6;
+const unsigned steam_overlay_notification_position_top_right = 7;
+const unsigned steam_overlay_notification_position_bottom_left = 8;
+const unsigned steam_overlay_notification_position_bottom_right = 9;
+
 bool steam_is_overlay_enabled();
 
 bool steam_is_overlay_activated();
