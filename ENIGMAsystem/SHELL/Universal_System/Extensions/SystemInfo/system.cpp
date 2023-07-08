3cd99869d8fa96edab20ece1d056ada48102b53e
Samuel Venable <samuelvenable@hotmail.com>
2023-07-07 20:48:04 -0400

    Add files via upload

--- ENIGMAsystem/SHELL/Universal_System/Extensions/SystemInfo/system.cpp
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/SystemInfo/system.cpp
@@ -447,9 +447,27 @@ std::string utsname_codename() {
   }
   static std::string str = result;
   return str;
-  #else
+  #elif !defined(__sun)
   return utsname_sysname() + " " + utsname_release();
+  #else
+  char *buf = nullptr;
+  std::size_t len = 0;
+  std::string buffer;
+  FILE *file = popen("cat /etc/release | awk '{$1=$1};1'", "r");
+  if (file) {
+    if (getline(&buf, &len, file) != -1) {
+      buffer = buf;
+      free(buf);
+    }
+    pclose(file);
+  }
+  if (!buffer.empty() && buffer.back() == '\n')
+    buffer.pop_back();
+  static std::string result;
+  result = buffer;
+  return result;
   #endif
+  return "";
 }
 
 std::string utsname_machine() {
@@ -524,6 +542,22 @@ long long memory_totalram() {
     return info.totalram;
   }
   return -1;
+  #elif defined(__sun)
+  char buf[1024];
+  long long total = -1;
+  const char *result = nullptr;
+  FILE *fp = popen("vmstat 1 1 | tail -1 | awk '{print $4}'", "r");
+  if (fp) {
+    if (fgets(buf, sizeof(buf), fp)) {
+      buf[strlen(buf) - 1] = '\0';
+      result = buf;
+    }
+    pclose(fp);
+    static std::string str;
+    str = (result && strlen(result)) ? result : "-1";
+    total = strtoll(str.c_str(), nullptr, 10) * 1024;
+  }
+  return total;
   #else
   return -1;
   #endif
@@ -557,6 +591,22 @@ long long memory_availram() {
     return info.freeram;
   }
   return -1;
+  #elif defined(__sun)
+  char buf[1024];
+  long long avail = -1;
+  const char *result = nullptr;
+  FILE *fp = popen("vmstat 1 1 | tail -1 | awk '{print $5}'", "r");
+  if (fp) {
+    if (fgets(buf, sizeof(buf), fp)) {
+      buf[strlen(buf) - 1] = '\0';
+      result = buf;
+    }
+    pclose(fp);
+    static std::string str;
+    str = (result && strlen(result)) ? result : "-1";
+    avail = strtoll(str.c_str(), nullptr, 10) * 1024;
+  }
+  return avail;
   #else
   return -1;  
   #endif
@@ -570,7 +620,7 @@ long long memory_usedram() {
     return (long long)(statex.ullTotalPhys - statex.ullAvailPhys);
   }
   return -1;
-  #elif ((defined(__APPLE__) && defined(__MACH__)) ||defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__))
+  #elif ((defined(__APPLE__) && defined(__MACH__)) ||defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__sun))
   long long total = memory_totalram();
   long long avail = memory_availram();
   if (total != -1 && avail != -1) {
