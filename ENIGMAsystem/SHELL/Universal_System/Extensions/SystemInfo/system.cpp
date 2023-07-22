9f49bb244fc74ff10621117f8c91bd72c7ae2d64
Samuel Venable <samuelvenable@hotmail.com>
2023-07-22 02:22:42 +0000

    Update system.cpp

--- ENIGMAsystem/SHELL/Universal_System/Extensions/SystemInfo/system.cpp
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/SystemInfo/system.cpp
@@ -446,7 +446,7 @@ std::string utsname_codename() {
   return product_name;
   #elif (defined(__APPLE__) && defined(__MACH__))
   std::string result;
-  FILE *fp = popen("echo $(sw_vers | grep 'ProductName:' | uniq | awk '{$1=$1};1' && sw_vers | grep 'ProductVersion:' | uniq | awk '{$1=$1};1')", "r");
+  FILE *fp = popen("echo $(sw_vers | grep 'ProductName:' | uniq | awk 'NR==1{$1=$1;print}' && sw_vers | grep 'ProductVersion:' | uniq | awk 'NR==1{$1=$1;print}')", "r");
   if (fp) {
     char buf[1024];
     if (fgets(buf, sizeof(buf), fp)) {
@@ -506,7 +506,7 @@ std::string utsname_codename() {
   char *buf = nullptr;
   std::size_t len = 0;
   std::string buffer;
-  FILE *file = popen("cat /etc/release | awk '{$1=$1};1'", "r");
+  FILE *file = popen("cat /etc/release | awk 'NR==1{$1=$1;print}'", "r");
   if (file) {
     if (getline(&buf, &len, file) != -1) {
       buffer = buf;
@@ -1010,7 +1010,7 @@ std::string gpu_vendor() {
   #endif
   #if defined(__sun)
   char buf[1024];
-  FILE *fp = popen("prtconf | awk '/display/{p++;if(p==1){next}}p' | awk -F'pci' 'FNR==2{print $0}' | sed 's/.*pci//g' | awk -F' ' '{print $1}' | awk -F',' '{print $1}'", "r");
+  FILE *fp = popen("prtconf |  awk '/display/{p=3} p > 0 {print $0; p--}'| awk -F'pci' 'NR==3{print $0}' | sed 's/.*pci//g' | awk -F' ' '{print $1}' | awk -F',' '{print $1}'", "r");
   if (fp) {
     if (fgets(buf, sizeof(buf), fp)) {
       buf[strlen(buf) - 1] = '\0';
@@ -1039,7 +1039,7 @@ std::string gpu_vendor() {
   }
   #else
   char buf[1024];
-  FILE *fp = popen("ioreg -bls | grep -n2 '    | |   | |   \"model\" = <\"' | awk -F',\"pci' 'FNR==5{print $2}' | rev | cut -c 2- | rev | awk -F',' '{print $1}'", "r");
+  FILE *fp = popen("ioreg -bls | grep -n2 '    | |   | |   \"model\" = <\"' | awk -F',\"pci' 'NR==5{print $2}' | rev | cut -c 2- | rev | awk -F',' '{print $1}'", "r");
   if (fp) {
     if (fgets(buf, sizeof(buf), fp)) {
       buf[strlen(buf) - 1] = '\0';
@@ -1054,7 +1054,7 @@ std::string gpu_vendor() {
   }
   if (result.empty()) {
     char buf[1024];
-    FILE *fp = popen("system_profiler SPDisplaysDataType | grep -i 'Vendor: ' | uniq | awk -F 'Vendor: ' '{$1=$1};1' | awk '{$1=$1};1'", "r");
+    FILE *fp = popen("system_profiler SPDisplaysDataType | grep -i 'Vendor: ' | uniq | awk -F 'Vendor: ' 'NR==1{$1=$1;print}' | awk 'NR==1{$1=$1;print}'", "r");
     if (fp) {
       if (fgets(buf, sizeof(buf), fp)) {
         buf[strlen(buf) - 1] = '\0';
@@ -1100,7 +1100,7 @@ std::string gpu_renderer() {
   #endif
   #if defined(__sun)
   char buf[1024];
-  FILE *fp = popen("prtconf | awk '/display/{p++;if(p==1){next}}p' | awk -F'pci' 'FNR==2{print $0}' | sed 's/.*pci//g' | awk -F' ' '{print $1}' | awk -F',' '{print $2}'", "r");
+  FILE *fp = popen("prtconf | awk '/display/{p=3} p > 0 {print $0; p--}' | awk -F'pci' 'NR==3{print $0}' | sed 's/.*pci//g' | awk -F' ' '{print $1}' | awk -F',' '{print $2}'", "r");
   if (fp) {
     if (fgets(buf, sizeof(buf), fp)) {
       buf[strlen(buf) - 1] = '\0';
@@ -1129,7 +1129,7 @@ std::string gpu_renderer() {
   }
   #else
   char buf[1024];
-  FILE *fp = popen("ioreg -bls | grep -n2 '    | |   | |   \"model\" = <\"' | awk -F',\"pci' 'FNR==5{print $2}' | rev | cut -c 2- | rev | awk -F',' '{print $2}'", "r");
+  FILE *fp = popen("ioreg -bls | grep -n2 '    | |   | |   \"model\" = <\"' | awk -F',\"pci' 'NR==5{print $2}' | rev | cut -c 2- | rev | awk -F',' '{print $2}'", "r");
   if (fp) {
     if (fgets(buf, sizeof(buf), fp)) {
       buf[strlen(buf) - 1] = '\0';
@@ -1144,7 +1144,7 @@ std::string gpu_renderer() {
   }
   if (result.empty()) {
     char buf[1024];
-    FILE *fp = popen("system_profiler SPDisplaysDataType | grep -i 'Chipset Model: ' | uniq | awk -F 'Chipset Model: ' '{$1=$1};1' | awk '{$1=$1};1'", "r");
+    FILE *fp = popen("system_profiler SPDisplaysDataType | grep -i 'Chipset Model: ' | uniq | awk -F 'Chipset Model: ' 'NR==1{$1=$1;print}' | awk 'NR==1{$1=$1;print}'", "r");
     if (fp) {
       if (fgets(buf, sizeof(buf), fp)) {
         buf[strlen(buf) - 1] = '\0';
@@ -1223,7 +1223,7 @@ std::string cpu_vendor() {
   #elif defined(__linux__)
   char buf[1024];
   const char *result = nullptr;
-  FILE *fp = popen("lscpu | grep 'Vendor ID:' | uniq | cut -d' ' -f3- | awk '{$1=$1};1'", "r");
+  FILE *fp = popen("lscpu | grep 'Vendor ID:' | uniq | cut -d' ' -f3- | awk 'NR==1{$1=$1;print}'", "r");
   if (fp) {
     if (fgets(buf, sizeof(buf), fp)) {
       buf[strlen(buf) - 1] = '\0';
@@ -1238,7 +1238,7 @@ std::string cpu_vendor() {
   #elif defined(__FreeBSD__)
   char buf[1024];
   const char *result = nullptr;
-  FILE *fp = popen("dmesg | awk '/CPU: /{p++;if(p==0){next}}p' | awk -F'Origin=\"' 'FNR==2{print $0}' | sed 's/.*Origin=\"//g' | awk -F' ' '{print $1}' | awk -F',' '{print $1}' | awk '{print substr($0, 1, length($0) - 1)}'", "r");
+  FILE *fp = popen("dmesg | awk '/CPU: /{p++;if(p==0){next}}p' | awk -F'Origin=\"' 'NR==2{print $0}' | sed 's/.*Origin=\"//g' | awk -F' ' '{print $1}' | awk -F',' '{print $1}' | awk '{print substr($0, 1, length($0) - 1)}'", "r");
   if (fp) {
     if (fgets(buf, sizeof(buf), fp)) {
       buf[strlen(buf) - 1] = '\0';
@@ -1253,7 +1253,7 @@ std::string cpu_vendor() {
   #elif defined(__DragonFly__)
   char buf[1024];
   const char *result = nullptr;
-  FILE *fp = popen("dmesg | awk '/CPU: /{p++;if(p==0){next}}p' | awk -F'Origin = \"' 'FNR==2{print $0}' | sed 's/.*Origin = \"//g' | awk -F' ' '{print $1}' | awk -F',' '{print $1}' | awk '{print substr($0, 1, length($0) - 1)}'", "r");
+  FILE *fp = popen("dmesg | awk '/CPU: /{p++;if(p==0){next}}p' | awk -F'Origin = \"' 'NR==2{print $0}' | sed 's/.*Origin = \"//g' | awk -F' ' '{print $1}' | awk -F',' '{print $1}' | awk '{print substr($0, 1, length($0) - 1)}'", "r");
   if (fp) {
     if (fgets(buf, sizeof(buf), fp)) {
       buf[strlen(buf) - 1] = '\0';
@@ -1268,7 +1268,7 @@ std::string cpu_vendor() {
   #elif defined(__NetBSD__)
   char buf[1024];
   const char *result = nullptr;
-  FILE *fp = popen("cat /proc/cpuinfo | grep 'vendor_id' | awk 'FNR==1{print $3}'", "r");
+  FILE *fp = popen("cat /proc/cpuinfo | grep 'vendor_id' | awk 'NR==1{print $3}'", "r");
   if (fp) {
     if (fgets(buf, sizeof(buf), fp)) {
       buf[strlen(buf) - 1] = '\0';
@@ -1298,7 +1298,7 @@ std::string cpu_vendor() {
   #elif defined(__sun)
   char buf[1024];
   const char *result = nullptr;
-  FILE *fp = popen("psrinfo -v -p | awk 'FNR==2{print substr($2, 2)}'", "r");
+  FILE *fp = popen("psrinfo -v -p | awk 'NR==2{print substr($2, 2)}'", "r");
   if (fp) {
     if (fgets(buf, sizeof(buf), fp)) {
       buf[strlen(buf) - 1] = '\0';
@@ -1335,7 +1335,7 @@ std::string cpu_brand() {
   #elif defined(__linux__)
   char buf[1024];
   const char *result = nullptr;
-  FILE *fp = popen("lscpu | grep 'Model name:' | uniq | cut -d' ' -f3- | awk '{$1=$1};1'", "r");
+  FILE *fp = popen("lscpu | grep 'Model name:' | uniq | cut -d' ' -f3- | awk 'NR==1{$1=$1;print}'", "r");
   if (fp) {
     if (fgets(buf, sizeof(buf), fp)) {
       buf[strlen(buf) - 1] = '\0';
@@ -1360,7 +1360,7 @@ std::string cpu_brand() {
   #elif defined(__sun)
   char buf[1024];
   const char *result = nullptr;
-  FILE *fp = popen("psrinfo -v -p | awk 'FNR==3{print}' | awk '{$1=$1};1''", "r");
+  FILE *fp = popen("psrinfo -v -p | awk 'NR==3{print}' | awk 'NR==1{$1=$1;print}''", "r");
   if (fp) {
     if (fgets(buf, sizeof(buf), fp)) {
       buf[strlen(buf) - 1] = '\0';
@@ -1403,7 +1403,7 @@ int cpu_numcores() {
   #elif defined(__linux__)
   char buf[1024];
   const char *result = nullptr;
-  FILE *fp = popen("lscpu | grep 'Core(s) per socket:' | uniq | cut -d' ' -f4- | awk '{$1=$1};1'", "r");
+  FILE *fp = popen("lscpu | grep 'Core(s) per socket:' | uniq | cut -d' ' -f4- | awk 'NR==1{$1=$1;print}'", "r");
   if (fp) {
     if (fgets(buf, sizeof(buf), fp)) {
       buf[strlen(buf) - 1] = '\0';
@@ -1413,7 +1413,7 @@ int cpu_numcores() {
     static std::string str;
     str = (result && strlen(result)) ? result : "-1";
     if (str == "-1") {
-      FILE *fp = popen("lscpu | grep 'Core(s) per cluster:' | uniq | cut -d' ' -f4- | awk '{$1=$1};1'", "r");
+      FILE *fp = popen("lscpu | grep 'Core(s) per cluster:' | uniq | cut -d' ' -f4- | awk 'NR==1{$1=$1;print}'", "r");
       if (fp) {
         if (fgets(buf, sizeof(buf), fp)) {
           buf[strlen(buf) - 1] = '\0';
@@ -1430,7 +1430,7 @@ int cpu_numcores() {
   #elif defined(__FreeBSD__)
   char buf[1024];
   const char *result = nullptr;
-  FILE *fp = popen("sysctl -a | grep -i -o '[^ ]* core(s)' | awk 'FNR==1{print $1}'", "r");
+  FILE *fp = popen("sysctl -a | grep -i -o '[^ ]* core(s)' | awk 'NR==1{print $1}'", "r");
   if (fp) {
     if (fgets(buf, sizeof(buf), fp)) {
       buf[strlen(buf) - 1] = '\0';
@@ -1519,7 +1519,7 @@ int cpu_numcpus() {
   #elif defined(__linux__)
   char buf[1024];
   const char *result = nullptr;
-  FILE *fp = popen("lscpu | grep 'CPU(s):' | uniq | cut -d' ' -f4- | awk '{$1=$1};1'", "r");
+  FILE *fp = popen("lscpu | grep 'CPU(s):' | uniq | cut -d' ' -f4- | awk 'NR==1{$1=$1;print}'", "r");
   if (fp) {
     if (fgets(buf, sizeof(buf), fp)) {
       buf[strlen(buf) - 1] = '\0';
