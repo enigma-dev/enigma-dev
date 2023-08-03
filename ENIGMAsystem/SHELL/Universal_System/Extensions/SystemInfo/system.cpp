2d6407ea3d98086d3741bd6c673b621a9b68b18d
Samuel Venable <samuelvenable@hotmail.com>
2023-08-03 05:40:48 +0000

    Update system.cpp

--- ENIGMAsystem/SHELL/Universal_System/Extensions/SystemInfo/system.cpp
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/SystemInfo/system.cpp
@@ -43,15 +43,12 @@
 #include <cstdio>
 #include <cmath>
 #if (!defined(_WIN32) && (!defined(__APPLE__) && !defined(__MACH__)))
-#if defined(CREATE_CONTEXT)
 #include <SDL.h>
 #include <SDL_opengl.h>
-#endif
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <GL/gl.h>
 #include <GL/glx.h>
-#include <GL/glext.h>
 #endif
 #if defined(_WIN32)
 #include <winsock2.h>
