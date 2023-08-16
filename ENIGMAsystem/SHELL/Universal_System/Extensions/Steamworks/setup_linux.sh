a57b4fb76b5626e2632b1fd53bfba8e7ad36ed7d
Saif Kandil <74428638+k0T0z@users.noreply.github.com>
2023-08-16 07:46:02 +0300

    ignore
    Signed-off-by: Saif Kandil <74428638+k0T0z@users.noreply.github.com>

--- ENIGMAsystem/SHELL/Universal_System/Extensions/Steamworks/setup_linux.sh
+++ ENIGMAsystem/SHELL/Universal_System/Extensions/Steamworks/setup_linux.sh
@@ -16,6 +16,17 @@
 # with this code. If not, see <http://www.gnu.org/licenses/>
 ############################################################################
 
+#!/bin/bash
+
+search_directory="/path/to/search"  # Replace with the directory you want to search in
+
+if find "$search_directory" -type d -name "your_directory_name" -print -quit 2>/dev/null; then
+    echo "Directory exists"
+else
+    echo "Directory does not exist"
+fi
+
+
 # Append the lines to .bashrc
 echo "export LD_LIBRARY_PATH=~/Desktop/enigma-dev/ENIGMAsystem/SHELL/Universal_System/Extensions/Steamworks/game_client/Steamv157/sdk/redistributable_bin/linux64:${LD_LIBRARY_PATH}" >> ~/.bashrc
 echo "export LD_PRELOAD=~/.local/share/Steam/ubuntu12_64/gameoverlayrenderer.so:${LD_PRELOAD}" >> ~/.bashrc
