1e915be9f409d47c4233cd044bc7373e6b0b7cb5
Samuel Venable <samuelvenable@hotmail.com>
2024-01-04 22:15:13 -0800

    Delete askpass.sh

--- askpass.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-pw="$(osascript -e 'Tell application "System Events" to display dialog "Password:" default answer "" with hidden answer' -e 'text returned of result' 2>/dev/null)" && echo "$pw"
