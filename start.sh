f892c5d4b8da1fea50e6962c6320e197b7f93bf9
Samuel Venable <samuelvenable@hotmail.com>
2024-02-21 21:46:48 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd "${0%/*}";
 if [ $(uname) = "Darwin" ]; then
-  sudo "java" -Djna.nosys=true -jar "lateralgm.jar" "$1";
+  sudo env PATH=$PATH:/opt/homebrew/bin "java" -Djna.nosys=true -jar "lateralgm.jar" "$1";
 else
   "java" -Djna.nosys=true -jar "lateralgm.jar" "$1";
 fi
