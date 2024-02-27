0bce615e44e062a88cab3cedc3e732ade1309d2d
Samuel Venable <samuelvenable@hotmail.com>
2024-02-26 22:48:13 -0500

    Update dialogs.cpp

--- ENIGMAsystem/SHELL/Widget_Systems/SDL/dialogs.cpp
+++ ENIGMAsystem/SHELL/Widget_Systems/SDL/dialogs.cpp
@@ -44,7 +44,7 @@ namespace enigma {
 
   bool widget_system_initialize() {
     std::string dname = enigma_user::directory_set_current_working();
-    enigma_user::directory_set_current_working(enigma_user::filename_change_ext(enigma_user::executable_get_pathname(), "") + "_files/fonts");
+    enigma_user::directory_set_current_working(enigma_user::filename_change_ext(enigma_user::executable_get_pathname(), "") + "_files");
     ngs::imgui::ifd_load_fonts();
     enigma_user::directory_set_current_working(dname);
     return true;
