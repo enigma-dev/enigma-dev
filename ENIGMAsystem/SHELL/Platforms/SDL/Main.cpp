7d6c2bc7be9219a90fc366688fbc5f2dd59e3a51
Samuel Venable <samuelvenable@hotmail.com>
2024-03-02 18:57:22 -0500

    Update Main.cpp

--- ENIGMAsystem/SHELL/Platforms/SDL/Main.cpp
+++ ENIGMAsystem/SHELL/Platforms/SDL/Main.cpp
@@ -3,6 +3,7 @@
 #include <SDL2/SDL.h> //sdl does a #define main SDL_main...
 
 int main(int argc, char** argv) {
+  SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengl");
   SDL_SetHint(SDL_HINT_VIDEO_HIGHDPI_DISABLED, "1");
   SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0");
   return enigma::enigma_main(argc, argv);
