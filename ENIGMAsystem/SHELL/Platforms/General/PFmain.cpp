6d4dbf8facfba60c05537bb363800a5ee042f04a
Samuel Venable <samuelvenable@hotmail.com>
2024-03-02 05:59:24 -0800

    Update PFmain.cpp

--- ENIGMAsystem/SHELL/Platforms/General/PFmain.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/PFmain.cpp
@@ -168,6 +168,10 @@ int updateTimer() {
 }
 
 int enigma_main(int argc, char** argv) {
+  #if defined(COMPILE_MODE)
+  enigma_user::directory_set_current_working(enigma_user::executable_get_directory());
+  #endif
+
   // Copy our parameters
   set_program_args(argc, argv);
   
