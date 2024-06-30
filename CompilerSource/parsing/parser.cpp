5cdecd5a95e2fc504c6bddab5d90c6fcd137be48
FaresAtef1 <faresatef553@gmail.com>
2024-07-01 01:26:02 +0300

    `TryParseDeclOrTypeExpression` instead of `MaybeDeclaration`

--- CompilerSource/parsing/parser.cpp
+++ CompilerSource/parsing/parser.cpp
@@ -1950,7 +1950,7 @@ std::unique_ptr<AST::Node> TryParseControlExpression(SyntaxMode mode_) {
   }
 }
 
-std::unique_ptr<AST::Node> MaybeDeclaration() {
+std::unique_ptr<AST::Node> TryParseDeclOrTypeExpression() {
   if (next_is_decl_specifier() || next_maybe_functional_cast()) {
     if (token.type == TT_SCOPEACCESS) {
       token = lexer->ReadToken();
@@ -1969,7 +1969,7 @@ std::unique_ptr<AST::Node> MaybeDeclaration() {
 // since we can place declarations in the same places we can place statements,
 // so this function will check first if this is a declaration, to be able to handle both
 std::unique_ptr<AST::Node> TryParseStatement() {
-  auto decl_node = MaybeDeclaration();
+  auto decl_node = TryParseDeclOrTypeExpression();
   if (decl_node != nullptr) {
     return decl_node;
   }
