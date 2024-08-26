c7266f96213afab94ba1374d6fe38a6997eac341
Samuel Venable <samuelvenable@hotmail.com>
2024-08-26 08:05:01 -0400

    Update process.cpp

--- shared/apiprocess/process.cpp
+++ shared/apiprocess/process.cpp
@@ -71,7 +71,6 @@
 #include <kvm.h>
 #elif defined(__sun)
 #include <kvm.h>
-#include <tcl.h>
 #include <dirent.h>
 #include <libproc.h>
 #include <sys/time.h>
@@ -993,36 +992,19 @@ namespace ngs::ps {
       errno = 0;
     }
     #elif defined(__sun)
+    int err = 0;
     char exe[PATH_MAX];
-    if (proc_id == proc_id_from_self()) {
-      std::vector<std::string> args = cmdline_from_proc_id(proc_id);
-      if (!args[0].empty()) {
-        char *argv0 = strdup(args[0].c_str());
-        if (argv0) { 
-          Tcl_FindExecutable(argv0);
-          const char *nameOfExecutable = Tcl_GetNameOfExecutable();
-          if (nameOfExecutable) {
-            if (realpath(nameOfExecutable, exe)) {
-              path = exe;
-            }
-          }
-          free(argv0);
-        }
-      }
-    } else {
-      int err = 0;
-      struct ps_prochandle *P = Pgrab(proc_id, PGRAB_RDONLY, &err);
-      if (P) {
-        if (!err && !errno) {
-          char buffer[PATH_MAX];
-          if (Pexecname(P, buffer, sizeof(buffer))) {
-            if (realpath(buffer, exe)) {
-              path = exe;
-            }
+    struct ps_prochandle *P = Pgrab(proc_id, PGRAB_RDONLY, &err);
+    if (P) {
+      if (!err && !errno) {
+        char buffer[PATH_MAX];
+        if (Pexecname(P, buffer, sizeof(buffer))) {
+          if (realpath(buffer, exe)) {
+            path = exe;
           }
         }
-        Pfree(P);
       }
+      Pfree(P);
     }
     if (!path.empty()) {
       goto finish;
