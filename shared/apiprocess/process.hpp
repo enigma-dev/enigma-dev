120c861d856a4c80a6bbb4ad9ce21417c7192dca
Samuel Venable <samuelvenable@hotmail.com>
2024-08-26 03:52:53 -0400

    Update process.hpp

--- shared/apiprocess/process.hpp
+++ shared/apiprocess/process.hpp
@@ -2,8 +2,8 @@
 
  MIT License
 
- Copyright © 2021-2023 Samuel Venable
- Copyright © 2021-2023 devKathy
+ Copyright © 2021-2024 Samuel Venable
+ Copyright © 2021-2024 devKathy
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
