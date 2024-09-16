ed6315597d9eb3629e15383755e782c9ed9fa46a
Samuel Venable <samuelvenable@hotmail.com>
2024-09-16 14:25:37 -0400

    Update include.h

--- ENIGMAsystem/SHELL/Widget_Systems/Native/include.h
+++ ENIGMAsystem/SHELL/Widget_Systems/Native/include.h
@@ -1,4 +1,5 @@
 #include "Widget_Systems/Native/dialogs.h"
 #include "Widget_Systems/widgets_mandatory.h"
+#include "Widget_Systems/Native/libdlgmod/libdlgmod.h"
 #include "Platforms/General/PFwindow.h"
 #include "Platforms/General/PFmain.h"
