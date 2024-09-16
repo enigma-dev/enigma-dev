e1c3720fda1cbde7363c52f52b22a59be12ed616
Samuel Venable <samuelvenable@hotmail.com>
2024-09-16 15:06:12 -0400

    Update dialogs.cpp

--- ENIGMAsystem/SHELL/Widget_Systems/Native/dialogs.cpp
+++ ENIGMAsystem/SHELL/Widget_Systems/Native/dialogs.cpp
@@ -26,9 +26,10 @@
 
 #include "Widget_Systems/Native/dialogs.h"
 #include "Widget_Systems/widgets_mandatory.h"
-#include "Widget_Systems/Native/libdlgmod/libdlgmod.h"
 #include "Platforms/General/PFwindow.h"
 #include "Platforms/General/PFmain.h"
+
+#include <libdlgmod/libdlgmod.h>
 
 using std::string;
 
