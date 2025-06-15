d6b942f91914093b6cd3f587edefa149690ddafa
high on tantor <josephvenable@verizon.net>
2025-06-14 21:12:03 -0400

    Update emake.sh

--- emake.sh
+++ emake.sh
@@ -24,7 +24,8 @@ elif [ $(uname) = "SunOS" ]; then
   g++ -c "../../emake.cpp" "../../shared/apiprocess/process.cpp" -std=c++17 `pkg-config --cflags sdl2` `pkg-config --cflags x11` `pkg-config --cflags gtk+-3.0` `pkg-config --cflags gio-2.0` `pkg-config --cflags glib-2.0`
   export PKG_CONFIG_PATH=/usr/lib/64/pkgconfig && g++ "/tmp/stigma.a" "../../shared/lunasvg/liblunasvg.a" "emake.o" "process.o" -o "../../emake" -std=c++17 `pkg-config --libs sdl2` `pkg-config --libs x11` `pkg-config --libs gtk+-3.0` `pkg-config --libs gio-2.0` `pkg-config --libs glib-2.0` -static-libgcc -lkvm -lc -lproc -lpthread -fPIC;
 else
+  cd "${0%/*}"
   windres "Resources.rc" "Resources.o"
-  g++ -c "../../emake.cpp" "../../shared/apiprocess/process.cpp" -std=c++17 `pkg-config --cflags sdl2 --static`
-  g++ "/tmp/stigma.a" "emake.o" "process.o" "../../Resources.o" -o "../../emake.exe" -std=c++17 `pkg-config --libs sdl2 --static` -static -lshell32 -lole32 -luuid -lntdll -Wl,--subsystem,windows
+  g++ -c "emake.cpp" "shared/apiprocess/process.cpp" -std=c++17 `pkg-config --cflags sdl2 --static`
+  g++ "/tmp/stigma.a" "emake.o" "process.o" "Resources.o" -o "emake.exe" -std=c++17 `pkg-config --libs sdl2 --static` -static -lshell32 -lole32 -luuid -lntdll -Wl,--subsystem,windows
 fi
