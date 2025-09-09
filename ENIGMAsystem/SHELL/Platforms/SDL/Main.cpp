dac321dde0ed1c089ad911df8f00ead47178070b
high on tantor <josephvenable@verizon.net>
2025-09-08 22:35:15 -0400

    Update Main.cpp

--- ENIGMAsystem/SHELL/Platforms/SDL/Main.cpp
+++ ENIGMAsystem/SHELL/Platforms/SDL/Main.cpp
@@ -11,28 +11,6 @@ namespace enigma {
     }
 
     void initialize_imgui_widgets_settings() {
-        // File dialogs default window width and height:
-        ngs::fs::environment_set_variable("IMGUI_DIALOG_WIDTH", std::to_string(720));
-        ngs::fs::environment_set_variable("IMGUI_DIALOG_HEIGHT", std::to_string(382));
-
-        // Widgets grayscale color scheme for imgui dialogs:
-        ngs::fs::environment_set_variable("IMGUI_DIALOG_THEME", std::to_string(2));
-        ngs::fs::environment_set_variable("IMGUI_TEXT_COLOR_0", std::to_string(1));
-        ngs::fs::environment_set_variable("IMGUI_TEXT_COLOR_1", std::to_string(1));
-        ngs::fs::environment_set_variable("IMGUI_TEXT_COLOR_2", std::to_string(1));
-        ngs::fs::environment_set_variable("IMGUI_HEAD_COLOR_0", std::to_string(0.35));
-        ngs::fs::environment_set_variable("IMGUI_HEAD_COLOR_1", std::to_string(0.35));
-        ngs::fs::environment_set_variable("IMGUI_HEAD_COLOR_2", std::to_string(0.35));
-        ngs::fs::environment_set_variable("IMGUI_AREA_COLOR_0", std::to_string(0.05));
-        ngs::fs::environment_set_variable("IMGUI_AREA_COLOR_1", std::to_string(0.05));
-        ngs::fs::environment_set_variable("IMGUI_AREA_COLOR_2", std::to_string(0.05));
-        ngs::fs::environment_set_variable("IMGUI_BODY_COLOR_0", std::to_string(1));
-        ngs::fs::environment_set_variable("IMGUI_BODY_COLOR_1", std::to_string(1));
-        ngs::fs::environment_set_variable("IMGUI_BODY_COLOR_2", std::to_string(1));
-        ngs::fs::environment_set_variable("IMGUI_POPS_COLOR_0", std::to_string(0.07));
-        ngs::fs::environment_set_variable("IMGUI_POPS_COLOR_1", std::to_string(0.07));
-        ngs::fs::environment_set_variable("IMGUI_POPS_COLOR_2", std::to_string(0.07));
-
         #ifdef SDL_VIDEO_DRIVER_X11
         // Prefer Xlib/X11 over Wayland to be compatible with widgets:
         ngs::fs::environment_set_variable("SDL_VIDEODRIVER", "x11");
