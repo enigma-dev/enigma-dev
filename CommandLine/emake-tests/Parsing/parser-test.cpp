ab0481e4118387beab00523173ee2997c3210e7e
Greg Williamson <cheeseboy16@gmail.com>
2025-12-15 13:10:34 -0500

    add test for problem code

--- CommandLine/emake-tests/Parsing/parser-test.cpp
+++ CommandLine/emake-tests/Parsing/parser-test.cpp
@@ -2684,3 +2684,109 @@ TEST(ParserTest, TestSetUp) {
   ASSERT_EQ(block->statements.size(), 1);
   ASSERT_EQ(block->statements[0]->type, AST::NodeType::BINARY_EXPRESSION);
 }
+
+TEST(ParserTest, ModOperatorInParenthesesWithComparison) {
+  // Test the exact code block from the user's issue
+  // The key expression is: (game_line_visible mod 2) == 1
+  // where 'mod' is a macro that expands to '%'
+  std::string code = R"({
+draw_self();
+
+
+
+//      The draw event will handle the drawing of the field and the current piece.
+
+
+
+var a, b, str, xx, yy;
+
+
+
+if (game_current_piece > -1)
+
+{
+
+    xx = 0;
+
+    yy = 0;
+
+    str = game_piece[game_current_piece, game_current_piece_rotation];
+
+    for (a = 1; a < string_length(str) + 1; a += 1)                    //Loop through the string of the current piece.
+
+    {
+
+        if (string_char_at(str, a) == '1')                          //Draw a block if we encounter a '1'.
+
+        {
+
+            draw_sprite(spr_game, game_current_piece, (game_current_piece_x + xx) * 16, (game_current_piece_y + yy) * 16);
+
+        }
+
+        xx += 1;
+
+        if (string_char_at(str, a) == '-')                          //Jump down if we encounter a '-'.
+
+        {
+
+            xx = 0;
+
+            yy += 1;
+
+        }
+
+    }
+
+}
+
+
+
+for (a = 0; a < 22; a += 1)            //Loop through the field.
+
+{
+
+    for (b = 0; b < 12; b += 1)
+
+    {
+
+        if (b > 0 && b < 11 && a > 0 && a < 21)
+
+        {
+
+            if (game_field[a, b] > -1 && (game_line[a] == 0 || (game_line_visible mod 2) == 1))       //Draw a block if the row is not "completed".
+
+            {
+
+                draw_sprite(spr_game, game_field[a, b], b * 16, a * 16);
+
+            }
+
+        }
+
+        else
+
+        {
+
+            draw_sprite(spr_game, 7, b * 16, a * 16);           //Draw the border.
+
+        }
+
+    }
+
+}
+/**/
+})";
+  
+  ParserTester test = ParserTester::CreateWithCpp(code);
+  
+  // This should parse without errors - use ParseCode for blocks
+  auto node = test->ParseCode();
+  ASSERT_NE(node, nullptr);
+  
+  // Verify we consumed all tokens
+  ASSERT_EQ(test->current_token().type, TT_ENDOFCODE);
+  
+  // Verify it's a block
+  ASSERT_EQ(node->type, AST::NodeType::BLOCK);
+}
