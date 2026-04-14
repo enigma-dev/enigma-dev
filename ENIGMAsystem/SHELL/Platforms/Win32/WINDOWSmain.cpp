0fb433ecd08ca905db38e94f8881410dba0445c4
high on tantor <josephvenable@verizon.net>
2026-04-14 10:49:05 -0400

    Update WINDOWSmain.cpp

--- ENIGMAsystem/SHELL/Platforms/Win32/WINDOWSmain.cpp
+++ ENIGMAsystem/SHELL/Platforms/Win32/WINDOWSmain.cpp
@@ -184,7 +184,7 @@ void initialize_directory_globals() {
   enigma_user::program_directory = enigma_user::filename_path(get_executable_path());
   enigma_user::temp_directory = add_slash(std::filesystem::temp_directory_path(ec).u8string());
   enigma_user::game_save_id = add_slash(enigma_user::environment_get_variable("LOCALAPPDATA")) + 
-    add_slash(std::to_string(enigma_user::game_id));
+    add_slash(enigma_user::filename_name(get_executable_path()));
   std::filesystem::create_directories(enigma_user::game_save_id, ec);
 }
 
