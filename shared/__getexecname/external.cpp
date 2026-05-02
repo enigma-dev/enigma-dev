8f120322d1716070b8c1613389675a8b959d1e14
high on tantor <josephvenable@verizon.net>
2026-05-02 16:03:45 -0400

    Add files via upload

--- shared/__getexecname/external.cpp
+++ shared/__getexecname/external.cpp
@@ -28,9 +28,11 @@ SOFTWARE.
 #include <string>
 #if defined(_WIN32)
 #include <vector>
+#include <cwchar>
 #include <cstddef>
 #include <cstdlib>
 #include <windef.h>
+#include <fileapi.h>
 #include <stringapiset.h>
 #include <processthreadsapi.h>
 #include <securitybaseapi.h>
@@ -95,6 +97,25 @@ const char *__getexecname(int pid) {
     return nullptr;
   }
   #if defined(_WIN32)
+  auto resolve_symbolic_links = [](std::wstring wstr) {
+    std::wstring result;
+    wchar_t path[MAX_PATH];
+    HANDLE hFile = CreateFileW(wstr.c_str(), GENERIC_READ, 
+    FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, 
+    nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+    if (hFile && hFile != INVALID_HANDLE_VALUE) {
+      DWORD len = GetFinalPathNameByHandleW(hFile, path, MAX_PATH, 0);
+      if (len) {
+        if (wcslen(path) >= 4 && path[0] == '\\' && path[1] == '\\' && path[2] == '?' && path[3] == '\\') {
+          result = path + 4;
+        } else {
+          result = path;
+        }
+      }
+      CloseHandle(hFile);
+    }
+    return result;
+  };
   auto narrow = [](std::wstring wstr) {
     if (wstr.empty()) return std::string("");
     int nbytes = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), (int)wstr.length(), nullptr, 0, nullptr, nullptr);
@@ -127,7 +148,8 @@ const char *__getexecname(int pid) {
     if (GetModuleFileNameW(nullptr, buffer, sizeof(buffer))) {
       wchar_t exe[MAX_PATH];
       if (_wfullpath(exe, buffer, MAX_PATH)) {
-        path = narrow(exe);
+        std::wstring resolved = resolve_symbolic_links(exe);
+        path = narrow(resolved);
       }
     }
   } else {
@@ -140,7 +162,8 @@ const char *__getexecname(int pid) {
     if (QueryFullProcessImageNameW(process, 0, buffer, &size)) {
       wchar_t exe[MAX_PATH];
       if (_wfullpath(exe, buffer, MAX_PATH)) {
-        path = narrow(exe);
+        std::wstring resolved = resolve_symbolic_links(exe);
+        path = narrow(resolved);
       }
     }
     CloseHandle(process);
