a37a4423a918db96e5d23fe5004c1fbc03af9aaa
high on tantor <josephvenable@verizon.net>
2026-05-02 13:05:49 -0400

    Update main.cpp

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/main.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/main.cpp
@@ -36,8 +36,9 @@ namespace enigma {
     enigma_user::program_directory = enigma_user::filename_path(execname ? execname : "");
     enigma_user::working_directory = add_slash(std::filesystem::current_path(ec).u8string());
     enigma_user::temp_directory = add_slash(std::filesystem::temp_directory_path(ec).u8string());
+    std::filesystem::create_directories(add_slash(enigma_user::environment_get_variable("HOME")) + std::string(".config"), ec);
     enigma_user::game_save_id = add_slash(enigma_user::environment_get_variable("HOME")) + 
-      string(".config/") + add_slash(std::to_string(enigma_user::game_id));
+    std::string(".config/") + add_slash(std::to_string(enigma_user::game_id));
   }
 } // namespace enigma
 
