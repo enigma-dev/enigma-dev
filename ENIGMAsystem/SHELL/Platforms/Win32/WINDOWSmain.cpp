9f22a124ed63dda8a9320defd42f066f4b413299
high on tantor <josephvenable@verizon.net>
2026-05-19 03:20:11 -0400

    Update WINDOWSmain.cpp

--- ENIGMAsystem/SHELL/Platforms/Win32/WINDOWSmain.cpp
+++ ENIGMAsystem/SHELL/Platforms/Win32/WINDOWSmain.cpp
@@ -174,7 +174,11 @@ void destroyWindow() { DestroyWindow(enigma::hWnd); }
 void initialize_directory_globals() {
   std::error_code ec;
   const char *basepath = __getbasepath();
-  enigma_user::program_directory = basepath ? basepath : "";
+  const char *execname = __getexecname();
+  const char *progname = __getprogname();
+  enigma_user::program_directory = basepath ? basepath : ""; // C:\Path\To\Executable.exe
+  enigma_user::program_pathname  = execname ? execname : ""; // C:\Path\To\
+  enigma_user::program_filename  = progname ? progname : ""; // Executable.exe
   enigma_user::working_directory = add_slash(std::filesystem::current_path(ec).u8string());
   enigma_user::temp_directory = add_slash(std::filesystem::temp_directory_path(ec).u8string());
   std::string localappdata = enigma_user::environment_get_variable("LOCALAPPDATA");
