73e20c0416de2a806901a040b8537b8e546b377c
high on tantor <josephvenable@verizon.net>
2026-05-19 05:24:17 -0400

    Update PFmain.h

--- ENIGMAsystem/SHELL/Platforms/General/PFmain.h
+++ ENIGMAsystem/SHELL/Platforms/General/PFmain.h
@@ -79,8 +79,6 @@ namespace enigma_user {
 
 extern std::string working_directory;
 extern std::string program_directory;
-extern std::string program_pathname;
-extern std::string program_filename;
 extern std::string temp_directory;
 extern std::string keyboard_string;
 extern double fps;
