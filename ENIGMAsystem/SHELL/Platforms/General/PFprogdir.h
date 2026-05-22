ba41fd5f461e61c106654ff77eca0e9204ca59f4
high on tantor <josephvenable@verizon.net>
2026-05-22 06:02:14 -0400

    Update PFprogdir.h

--- ENIGMAsystem/SHELL/Platforms/General/PFprogdir.h
+++ ENIGMAsystem/SHELL/Platforms/General/PFprogdir.h
@@ -3,7 +3,3 @@
 #include "__getprogname/internal.h"
 #include "Platforms/General/PFmain.h"
 #include "Universal_System/estring.h"
-namespace enigma_user {
-  std::string program_pathname;
-  std::string program_filename;
-} // namespace enigma_user
