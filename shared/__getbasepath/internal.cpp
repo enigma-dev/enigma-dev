397916ed57ab1fe78c852d67b1549f4ee819c5fe
high on tantor <josephvenable@verizon.net>
2026-05-22 02:20:31 -0400

    Add files via upload

--- shared/__getbasepath/internal.cpp
+++ shared/__getbasepath/internal.cpp
@@ -92,7 +92,7 @@ const char *__getbasepath(void) {
     wchar_t path[MAX_PATH];
     HANDLE hFile = CreateFileW(wstr.c_str(), GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
     if (hFile != INVALID_HANDLE_VALUE) {
-      DWORD len = GetFinalPathNameByHandleW(hFile, path, MAX_PATH, FILE_NAME_OPENED);
+      DWORD len = GetFinalPathNameByHandleW(hFile, path, MAX_PATH, FILE_NAME_NORMALIZED | VOLUME_NAME_DOS);
       if (len) {
         result = path;
         if (!result.substr(0, 8).compare(L"\\\\?\\UNC\\")) {
