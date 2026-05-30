105d0ea123677e5bf87f2c701a2b37024b168156
high on tantor <josephvenable@verizon.net>
2026-05-29 22:57:09 -0400

    Update main.cpp

--- ENIGMAsystem/SHELL/Platforms/General/POSIX/main.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/POSIX/main.cpp
@@ -35,12 +35,10 @@ namespace enigma_user {
 namespace enigma {
   void initialize_directory_globals() {
     std::error_code ec;
-    const char *basepath = __getbasepath();
     const char *execname = __getexecname();
-    const char *progname = __getprogname();
-    enigma_user::program_directory = basepath ? basepath : ""; // /path/to/executable
-    enigma_user::program_pathname  = execname ? execname : ""; // /path/to/
-    enigma_user::program_filename  = progname ? progname : ""; // executable
+    enigma_user::program_pathname  = ((execname) ? execname : "");
+    enigma_user::program_directory = ((!enigma_user::program_pathname.empty()) ? enigma_user::filename_path(enigma_user::program_pathname) : "");
+    enigma_user::program_filename  = ((!enigma_user::program_pathname.empty()) ? enigma_user::filename_name(enigma_user::program_pathname) : "");
     enigma_user::working_directory = add_slash(std::filesystem::current_path(ec).u8string());
     enigma_user::temp_directory = add_slash(std::filesystem::temp_directory_path(ec).u8string());
     std::filesystem::create_directories(add_slash(enigma_user::environment_get_variable("HOME")) + std::string(".config"), ec);
