5ce2f1859a6cd80ad179bf4a10984c8647211f94
high on tantor <josephvenable@verizon.net>
2026-05-30 17:48:20 -0400

    fuck

--- ENIGMAsystem/SHELL/Platforms/General/PFmain.cpp
+++ ENIGMAsystem/SHELL/Platforms/General/PFmain.cpp
@@ -319,7 +319,7 @@ void initialize_directory_globals() {
   // if "/Path/To/YourAppBundle.app/Contents/MacOS/YourExe" and "/Path/To/YourAppBundle.app/Contents/Resources/" exists
   if (macos_bname == "MacOS" && contents_bname == "Contents" && app_ename == ".app" && directory_exists(resources_pname)) {
     // set working directory to "/Path/To/YourAppBundle.app/Contents/Resources/" and allow loading normal included files
-    enigma_user::set_working_directory(enigma_user::filename_absolute(resources_pname));
+    enigma_user::set_working_directory(enigma_user::filename_absolute(enigma_user::filename_dir(resources_pname)));
   }
 
   #endif
